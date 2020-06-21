8aaa984acecc7731e65b478b3e8e383fa54bc2f7
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-06-21 22:36:39 +0900

    codesignal-22(o)

--- codesignal.py
+++ codesignal.py
@@ -1,1 +1,21 @@
 def avoidObstacles(inputArray):
+    i = 3
+    while True:
+        count = 0
+        for j in inputArray:
+            if j % i != 0:
+                count += 1
+        if count == len(inputArray):
+            return i
+        i += 1
+
+#================================================#
+#     ^ my answer      ||  most voted answer v   #
+#================================================#
+
+def avoidObstacles(inputArray):
+    c = 2
+    while True:
+        if sorted([i%c for i in inputArray])[0]>0:
+            return c
+        c += 1
