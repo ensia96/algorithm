6e687eeb5836365bcf0911ba38ee608c1c7d06fe
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-06-21 23:07:53 +0900

    codesignal-23

--- codesignal.py
+++ codesignal.py
@@ -1,21 +1,73 @@
-def avoidObstacles(inputArray):
-    i = 3
-    while True:
-        count = 0
-        for j in inputArray:
-            if j % i != 0:
-                count += 1
-        if count == len(inputArray):
-            return i
-        i += 1
+def boxBlur(image):
+    j = 1
+    answer = []
+    while j < len(image)-1:
+        i = 1
+        a = []
+        while i < len(image[0])-1:
+            count = 0
+            for s in range(j-1, j+2):
+                count += sum(image[s][i-1:i+2])
+            a.append(count//9)
+            i += 1
+        j += 1
+        answer.append(a)
+    return answer
 
 #================================================#
 #     ^ my answer      ||  most voted answer v   #
 #================================================#
 
-def avoidObstacles(inputArray):
-    c = 2
-    while True:
-        if sorted([i%c for i in inputArray])[0]>0:
-            return c
-        c += 1
+
+
+
+
+#================================================#
+#                 question v                     #
+#================================================#
+
+# Last night you partied a little too hard. Now there's a black and white photo of you that's about to go viral! You can't let this ruin your reputation, so you want to apply the box blur algorithm to the photo to hide its content.
+
+# The pixels in the input image are represented as integers. The algorithm distorts the input image in the following way: Every pixel x in the output image has a value equal to the average value of the pixel values from the 3 × 3 square that has its center at x, including x itself. All the pixels on the border of x are then removed.
+
+# Return the blurred image as an integer, with the fractions rounded down.
+
+# Example
+
+# For
+
+# image = [[1, 1, 1], 
+#          [1, 7, 1], 
+#          [1, 1, 1]]
+# the output should be boxBlur(image) = [[1]].
+
+# To get the value of the middle pixel in the input 3 × 3 square: (1 + 1 + 1 + 1 + 7 + 1 + 1 + 1 + 1) = 15 / 9 = 1.66666 = 1. The border pixels are cropped from the final result.
+
+# For
+
+# image = [[7, 4, 0, 1], 
+#          [5, 6, 2, 2], 
+#          [6, 10, 7, 8], 
+#          [1, 4, 2, 0]]
+# the output should be
+
+# boxBlur(image) = [[5, 4], 
+#                   [4, 4]]
+# There are four 3 × 3 squares in the input image, so there should be four integers in the blurred output. To get the first value: (7 + 4 + 0 + 5 + 6 + 2 + 6 + 10 + 7) = 47 / 9 = 5.2222 = 5. The other three integers are obtained the same way, then the surrounding integers are cropped from the final result.
+
+# Input/Output
+
+# [execution time limit] 4 seconds (py3)
+
+# [input] array.array.integer image
+
+# An image, stored as a rectangular matrix of non-negative integers.
+
+# Guaranteed constraints:
+# 3 ≤ image.length ≤ 100,
+# 3 ≤ image[0].length ≤ 100,
+# 0 ≤ image[i][j] ≤ 255.
+
+# [output] array.array.integer
+
+# A blurred image represented as integers, obtained through the process in the description.
