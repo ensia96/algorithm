5ea984d4df19c4e203360d61b64f8c952f11f960
최호준 <ensia96@gmail.com>
2020-07-02 21:44:07 +0900

    leetcode-136

--- leetcode.py
+++ leetcode.py
@@ -1,32 +1,40 @@
 class Solution:
-    def maxPathSum(self, root: TreeNode) -> int:
-        return False
+    def singleNumber(self, nums: List[int]) -> int:
+        return [_ for _ in set(nums) if nums.count(_) == 1][0]
 
 
+# Success
+# Details
+# Runtime: 5408 ms, faster than 6.85% of Python3 online submissions for Single Number.
+# Memory Usage: 16.2 MB, less than 73.89% of Python3 online submissions for Single Number.
 # ================================================#
 #     ^ my answer      ||  most voted answer v   #
 # ================================================#
 
 
 class Solution:
-    def maxPathSum(self, root: TreeNode) -> int:
-        max_path = float("-inf")
+    def singleNumber1(self, nums):
+        dic = {}
+        for num in nums:
+            dic[num] = dic.get(num, 0) + 1
+        for key, val in dic.items():
+            if val == 1:
+                return key
 
-        def get_max_gain(node):
-            nonlocal max_path
-            if node is None:
-                return 0
+    # def singleNumber2(self, nums):
+    #     res = 0
+    #     for num in nums:
+    #         res ^= num
+    #     return res
 
-            gain_on_left = max(get_max_gain(node.left), 0)
-            gain_on_right = max(get_max_gain(node.right), 0)
+    # def singleNumber3(self, nums):
+    #     return 2*sum(set(nums))-sum(nums)
 
-            current_max_path = node.val + gain_on_left + gain_on_right
-            max_path = max(max_path, current_max_path)
+    # def singleNumber4(self, nums):
+    #     return reduce(lambda x, y: x ^ y, nums)
 
-            return node.val + max(gain_on_left, gain_on_right)
-
-        get_max_gain(root)
-        return max_path
+    # def singleNumber(self, nums):
+    #     return reduce(operator.xor, nums)
 
 
 # ================================================#
