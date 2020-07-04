395dd7ef73f460ade3b5decb26a665dcc8c20ab7
최호준 <ensia96@gmail.com>
2020-07-04 17:23:02 +0900

    codesignal-39

--- codesignal.py
+++ codesignal.py
@@ -1,11 +1,19 @@
-def growingPlant(upSpeed, downSpeed, desiredHeight):
-    height = upSpeed
-    answer = 1
-    while True:
-        if height >= desiredHeight:
-            return answer
-        height += upSpeed - downSpeed
-        answer += 1
+def knapsackLight(value1, weight1, value2, weight2, maxW):
+    bag = {"value": 0, "weight": 0}
+
+    def logic(value, weight):
+        if bag["weight"] + weight <= maxW:
+            bag["value"] += value
+            bag["weight"] += weight
+        elif bag["value"] < value:
+            bag["value"] = value
+
+    if weight1 <= maxW:
+        logic(value1, weight1)
+    if weight2 <= maxW:
+        logic(value2, weight2)
+
+    return bag["value"]
 
 
 # ================================================#
@@ -13,65 +21,64 @@ def growingPlant(upSpeed, downSpeed, desiredHeight):
 # ================================================#
 
 
-def growingPlant(upSpeed, downSpeed, desiredHeight):
-    if desiredHeight <= upSpeed:
-        return 1
-    return math.ceil((desiredHeight - upSpeed) / (upSpeed - downSpeed) + 1)
+def knapsackLight(v1, w1, v2, w2, W):
+    return max(int(w1 <= W) * v1, int(w2 <= W) * v2, int(w1 + w2 <= W) * (v1 + v2))
 
-    # return 1 if desiredHeight<=upSpeed else (desiredHeight-upSpeed-1)//(upSpeed-downSpeed)+2
+    # return max(v for v, w in ((0, 0), (v1, w1), (v2, w2), (v1+v2, w1+w2)) if w <= maxW)
 
 
 # ================================================#
 #                 question v                     #
 # ================================================#
 
-# Caring for a plant can be hard work, but since you tend to it regularly, you have a plant that grows consistently. Each day, its height increases by a fixed amount represented by the integer upSpeed. But due to lack of sunlight, the plant decreases in height every night, by an amount represented by downSpeed.
+# You found two items in a treasure chest! The first item weighs weight1 and is worth value1, and the second item weighs weight2 and is worth value2. What is the total maximum value of the items you can take with you, assuming that your max weight capacity is maxW and you can't come back for the items later?
 
-# Since you grew the plant from a seed, it started at height 0 initially. Given an integer desiredHeight, your task is to find how many days it'll take for the plant to reach this height.
+# Note that there are only two items and you can't bring more than one item of each type, i.e. you can't take two first items or two second items.
 
 # Example
 
-# For upSpeed = 100, downSpeed = 10, and desiredHeight = 910, the output should be
-# growingPlant(upSpeed, downSpeed, desiredHeight) = 10.
+# For value1 = 10, weight1 = 5, value2 = 6, weight2 = 4, and maxW = 8, the output should be
+# knapsackLight(value1, weight1, value2, weight2, maxW) = 10.
 
-# #	Day	Night
-# 1	100	90
-# 2	190	180
-# 3	280	270
-# 4	370	360
-# 5	460	450
-# 6	550	540
-# 7	640	630
-# 8	730	720
-# 9	820	810
-# 10	910	900
-# The plant first reaches a height of 910 on day 10.
+# You can only carry the first item.
+
+# For value1 = 10, weight1 = 5, value2 = 6, weight2 = 4, and maxW = 9, the output should be
+# knapsackLight(value1, weight1, value2, weight2, maxW) = 16.
+
+# You're strong enough to take both of the items with you.
+
+# For value1 = 5, weight1 = 3, value2 = 7, weight2 = 4, and maxW = 6, the output should be
+# knapsackLight(value1, weight1, value2, weight2, maxW) = 7.
+
+# You can't take both items, but you can take any of them.
 
 # Input/Output
 
 # [execution time limit] 4 seconds (py3)
 
-# [input] integer upSpeed
-
-# A positive integer representing the daily growth of the plant.
+# [input] integer value1
 
 # Guaranteed constraints:
-# 3 ≤ upSpeed ≤ 100.
+# 2 ≤ value1 ≤ 20.
 
-# [input] integer downSpeed
-
-# A positive integer representing the nightly decline of the plant.
+# [input] integer weight1
 
 # Guaranteed constraints:
-# 2 ≤ downSpeed < upSpeed.
+# 2 ≤ weight1 ≤ 10.
 
-# [input] integer desiredHeight
-
-# A positive integer representing the goal height.
+# [input] integer value2
 
 # Guaranteed constraints:
-# 4 ≤ desiredHeight ≤ 1000.
+# 2 ≤ value2 ≤ 20.
+
+# [input] integer weight2
+
+# Guaranteed constraints:
+# 2 ≤ weight2 ≤ 10.
+
+# [input] integer maxW
+
+# Guaranteed constraints:
+# 1 ≤ maxW ≤ 20.
 
 # [output] integer
-
-# The number of days that it will take for the plant to reach / pass desiredHeight.
