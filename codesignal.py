969dddf46b4daec3e39d2da139428e7cd269fdfe
최호준 <ensia96@gmail.com>
2020-07-05 15:35:30 +0900

    codesignal-41

--- codesignal.py
+++ codesignal.py
@@ -1,5 +1,15 @@
-def longestDigitsPrefix(inputString):
-    return "".join([_ if _.isdigit() else "_" for _ in inputString]).split("_")[0]
+def digitDegree(n):
+    i = 0
+    if len(str(n)) == 1:
+        return 0
+    while True:
+        a = 0
+        for _ in str(n):
+            a += int(_)
+        n = a
+        i += 1
+        if len(str(a)) == 1:
+            return i
 
 
 # ================================================#
@@ -7,28 +17,45 @@ def longestDigitsPrefix(inputString):
 # ================================================#
 
 
-def longestDigitsPrefix(inputString):
-    return re.findall("^\d*", inputString)[0]
+def digitDegree(n):
+    if n < 10:
+        return 0
+    sumOfDigits = sum([int(i) for i in str(n)])
+    return digitDegree(sumOfDigits) + 1
 
+
+# def digitDegree(n):
+#     d=0
+#     while n>=10:
+#         n=sum([int(i) for i in str(n)])
+#         d+=1
+#     return d
 
 # ================================================#
 #                 question v                     #
 # ================================================#
 
-# Given a string, output its longest prefix which contains only digits.
+# Let's define digit degree of some positive integer as the number of times we need to replace this number with the sum of its digits until we get to a one digit number.
+
+# Given an integer, find its digit degree.
 
 # Example
 
-# For inputString = "123aa1", the output should be
-# longestDigitsPrefix(inputString) = "123".
-
+# For n = 5, the output should be
+# digitDegree(n) = 0;
+# For n = 100, the output should be
+# digitDegree(n) = 1.
+# 1 + 0 + 0 = 1.
+# For n = 91, the output should be
+# digitDegree(n) = 2.
+# 9 + 1 = 10 -> 1 + 0 = 1.
 # Input/Output
 
 # [execution time limit] 4 seconds (py3)
 
-# [input] string inputString
+# [input] integer n
 
 # Guaranteed constraints:
-# 3 ≤ inputString.length ≤ 100.
+# 5 ≤ n ≤ 109.
 
-# [output] string
+# [output] integer
