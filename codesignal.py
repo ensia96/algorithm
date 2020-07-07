82f533bc62dd1bb5c8745503b3dd23d05c8874d8
최호준 <ensia96@gmail.com>
2020-07-07 14:51:01 +0900

    codesignal-43

--- codesignal.py
+++ codesignal.py
@@ -1,14 +1,15 @@
-def bishopAndPawn(bishop, pawn):
+def isBeautifulString(inputString):
     from string import ascii_lowercase
 
-    x = list(ascii_lowercase)
-    a = x.index(bishop[0])
-    aa = int(bishop[1])
-    b = x.index(pawn[0])
-    bb = int(pawn[1])
-    if abs(a - b) == abs(aa - bb):
-        return True
-    return False
+    a = float("inf")
+    for i in [
+        inputString.count(_) if _ in inputString else 0 for _ in list(ascii_lowercase)
+    ]:
+        if i <= a:
+            a = i
+        else:
+            return False
+    return True
 
 
 # ================================================#
@@ -16,51 +17,44 @@ def bishopAndPawn(bishop, pawn):
 # ================================================#
 
 
-def bishopAndPawn(bishop, pawn):
-    return abs(ord(bishop[0]) - ord(pawn[0])) == abs(int(pawn[1]) - int(bishop[1]))
+def isBeautifulString(inputString):
+    r = [inputString.count(i) for i in string.ascii_lowercase]
+    return r[::-1] == sorted(r)
 
 
 # ================================================#
 #                 question v                     #
 # ================================================#
 
-# Given the positions of a white bishop and a black pawn on the standard chess board, determine whether the bishop can capture the pawn in one move.
+# A string is said to be beautiful if each letter in the string appears at most as many times as the previous letter in the alphabet within the string; ie: b occurs no more times than a; c occurs no more times than b; etc.
 
-# The bishop has no restrictions in distance for each move, but is limited to diagonal movement. Check out the example below to see how it can move:
-
+# Given a string, check whether it is beautiful.
 
 # Example
 
-# For bishop = "a1" and pawn = "c3", the output should be
-# bishopAndPawn(bishop, pawn) = true.
+# For inputString = "bbbaacdafe", the output should be isBeautifulString(inputString) = true.
 
+# This string contains 3 as, 3 bs, 1 c, 1 d, 1 e, and 1 f (and 0 of every other letter), so since there aren't any letters that appear more frequently than the previous letter, this string qualifies as beautiful.
 
-# For bishop = "h1" and pawn = "h3", the output should be
-# bishopAndPawn(bishop, pawn) = false.
+# For inputString = "aabbb", the output should be isBeautifulString(inputString) = false.
 
+# Since there are more bs than as, this string is not beautiful.
+
+# For inputString = "bbc", the output should be isBeautifulString(inputString) = false.
+
+# Although there are more bs than cs, this string is not beautiful because there are no as, so therefore there are more bs than as.
 
 # Input/Output
 
 # [execution time limit] 4 seconds (py3)
 
-# [input] string bishop
+# [input] string inputString
 
-# Coordinates of the white bishop in the chess notation.
-
-# Guaranteed constraints:
-# bishop.length = 2,
-# 'a' ≤ bishop[0] ≤ 'h',
-# 1 ≤ bishop[1] ≤ 8.
-
-# [input] string pawn
-
-# Coordinates of the black pawn in the same notation.
+# A string of lowercase English letters.
 
 # Guaranteed constraints:
-# pawn.length = 2,
-# 'a' ≤ pawn[0] ≤ 'h',
-# 1 ≤ pawn[1] ≤ 8.
+# 3 ≤ inputString.length ≤ 50.
 
 # [output] boolean
 
-# true if the bishop can capture the pawn, false otherwise.
+# Return true if the string is beautiful, false otherwise.
