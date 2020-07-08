b43e2600f2d1cc3b80d778d23c0f87abe7aad1b7
최호준 <ensia96@gmail.com>
2020-07-08 12:56:39 +0900

    leetcode-202

--- leetcode.py
+++ leetcode.py
@@ -1,45 +1,51 @@
 class Solution:
-    def rangeBitwiseAnd(self, m: int, n: int) -> int:
+    def isHappy(self, n: int) -> bool:
         i = 0
-        while m != n:
-            m >>= 1
-            n >>= 1
+        while i < 10:
+            n = sum(int(i) ** 2 for i in str(n))
+            if n == 1:
+                return True
             i += 1
-        return n << i
+        return False
 
 
 # Success
 # Details
-# Runtime: 96 ms, faster than 14.18% of Python3 online submissions for Bitwise AND of Numbers Range.
-# Memory Usage: 13.7 MB, less than 90.52% of Python3 online submissions for Bitwise AND of Numbers Range.
+# Runtime: 44 ms, faster than 26.81% of Python3 online submissions for Happy Number.
+# Memory Usage: 13.9 MB, less than 44.48% of Python3 online submissions for Happy Number.
 # ================================================#
 #     ^ my answer      ||  most voted answer v   #
 # ================================================#
 
 
 class Solution:
-    def rangeBitwiseAnd(self, m: int, n: int) -> int:
-        if len(bin(m)) != len(bin(n)):
-            return 0
-        for i in range(m + 1, n + 1):
-            m &= i
-        return m
+    def isHappy(self, n):
+        seen = set()
+        while n not in seen:
+            seen.add(n)
+            n = sum([int(x) ** 2 for x in str(n)])
+        return n == 1
 
 
 # ================================================#
 #                  question v                     #
 # ================================================#
 
-# 201. Bitwise AND of Numbers Range
-# Medium
+# 202. Happy Number
+# Easy
 
-# Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+# Write an algorithm to determine if a number n is "happy".
 
-# Example 1:
+# A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
 
-# Input: [5,7]
-# Output: 4
-# Example 2:
+# Return True if n is a happy number, and False if not.
 
-# Input: [0,1]
-# Output: 0
+# Example:
+
+# Input: 19
+# Output: true
+# Explanation:
+# 12 + 92 = 82
+# 82 + 22 = 68
+# 62 + 82 = 100
+# 12 + 02 + 02 = 1
