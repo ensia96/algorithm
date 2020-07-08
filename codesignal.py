08f7333d4fa4655e99eef191959ba3f1ee746dd2
최호준 <ensia96@gmail.com>
2020-07-08 12:36:47 +0900

    codesignal-44

--- codesignal.py
+++ codesignal.py
@@ -1,15 +1,5 @@
-def isBeautifulString(inputString):
-    from string import ascii_lowercase
-
-    a = float("inf")
-    for i in [
-        inputString.count(_) if _ in inputString else 0 for _ in list(ascii_lowercase)
-    ]:
-        if i <= a:
-            a = i
-        else:
-            return False
-    return True
+def findEmailDomain(address):
+    return address.split("@")[-1]
 
 
 # ================================================#
@@ -17,44 +7,33 @@ def isBeautifulString(inputString):
 # ================================================#
 
 
-def isBeautifulString(inputString):
-    r = [inputString.count(i) for i in string.ascii_lowercase]
-    return r[::-1] == sorted(r)
+def findEmailDomain(a):
+    return a[a.rfind("@") + 1 :]
 
 
 # ================================================#
 #                 question v                     #
 # ================================================#
 
-# A string is said to be beautiful if each letter in the string appears at most as many times as the previous letter in the alphabet within the string; ie: b occurs no more times than a; c occurs no more times than b; etc.
+# An email address such as "John.Smith@example.com" is made up of a local part ("John.Smith"), an "@" symbol, then a domain part ("example.com").
 
-# Given a string, check whether it is beautiful.
+# The domain name part of an email address may only consist of letters, digits, hyphens and dots. The local part, however, also allows a lot of different special characters. Here you can look at several examples of correct and incorrect email addresses.
+
+# Given a valid email address, find its domain part.
 
 # Example
 
-# For inputString = "bbbaacdafe", the output should be isBeautifulString(inputString) = true.
-
-# This string contains 3 as, 3 bs, 1 c, 1 d, 1 e, and 1 f (and 0 of every other letter), so since there aren't any letters that appear more frequently than the previous letter, this string qualifies as beautiful.
-
-# For inputString = "aabbb", the output should be isBeautifulString(inputString) = false.
-
-# Since there are more bs than as, this string is not beautiful.
-
-# For inputString = "bbc", the output should be isBeautifulString(inputString) = false.
-
-# Although there are more bs than cs, this string is not beautiful because there are no as, so therefore there are more bs than as.
-
+# For address = "prettyandsimple@example.com", the output should be
+# findEmailDomain(address) = "example.com";
+# For address = "fully-qualified-domain@codesignal.com", the output should be
+# findEmailDomain(address) = "codesignal.com".
 # Input/Output
 
 # [execution time limit] 4 seconds (py3)
 
-# [input] string inputString
-
-# A string of lowercase English letters.
+# [input] string address
 
 # Guaranteed constraints:
-# 3 ≤ inputString.length ≤ 50.
+# 10 ≤ address.length ≤ 50.
 
-# [output] boolean
-
-# Return true if the string is beautiful, false otherwise.
+# [output] string
