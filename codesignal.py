50649a9cf503744a61036e096b1b2271d8d0ad10
최호준 <ensia96@gmail.com>
2020-07-12 15:57:20 +0900

    codesignal-48

--- codesignal.py
+++ codesignal.py
@@ -1,18 +1,7 @@
-def isMAC48Address(inputString):
-    from string import hexdigits
+def isDigit(symbol):
+    from string import digits
 
-    if inputString.count("-") == 5:
-        return (
-            len(
-                [
-                    True
-                    for _ in inputString.split("-")
-                    if (len(_) == 2) and set(_).issubset(hexdigits)
-                ]
-            )
-            == 6
-        )
-    return False
+    return symbol in digits
 
 
 # ================================================#
@@ -20,37 +9,33 @@ def isMAC48Address(inputString):
 # ================================================#
 
 
-def isMAC48Address(s):
-    return bool(re.match(("^" + "[\dA-F]{2}-" * 6)[:-1] + "$", s))
+def isDigit(symbol):
+    return symbol.isdigit()
 
 
 # ================================================#
 #                 question v                     #
 # ================================================#
 
-# A media access control address (MAC address) is a unique identifier assigned to network interfaces for communications on the physical network segment.
-
-# The standard (IEEE 802) format for printing MAC-48 addresses in human-friendly form is six groups of two hexadecimal digits (0 to 9 or A to F), separated by hyphens (e.g. 01-23-45-67-89-AB).
-
-# Your task is to check by given string inputString whether it corresponds to MAC-48 address or not.
+# Determine if the given character is a digit or not.
 
 # Example
 
-# For inputString = "00-1B-63-84-45-E6", the output should be
-# isMAC48Address(inputString) = true;
-# For inputString = "Z1-1B-63-84-45-E6", the output should be
-# isMAC48Address(inputString) = false;
-# For inputString = "not a MAC-48 address", the output should be
-# isMAC48Address(inputString) = false.
+# For symbol = '0', the output should be
+# isDigit(symbol) = true;
+# For symbol = '-', the output should be
+# isDigit(symbol) = false.
 # Input/Output
 
 # [execution time limit] 4 seconds (py3)
 
-# [input] string inputString
+# [input] char symbol
+
+# A character which is either a digit or not.
 
 # Guaranteed constraints:
-# 15 ≤ inputString.length ≤ 20.
+# Given symbol is from ASCII table.
 
 # [output] boolean
 
-# true if inputString corresponds to MAC-48 address naming rules, false otherwise.
+# true if symbol is a digit, false otherwise.
