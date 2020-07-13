af13abac4df86e08ab11fb968c2418d4095a9891
최호준 <ensia96@gmail.com>
2020-07-13 16:35:31 +0900

    codesignal-49

--- codesignal.py
+++ codesignal.py
@@ -1,7 +1,14 @@
-def isDigit(symbol):
-    from string import digits
-
-    return symbol in digits
+def lineEncoding(s):
+    s_list = [""]
+    for _ in s:
+        if _ not in s_list[-1]:
+            s_list.append(_)
+        else:
+            s_list[-1] += _
+    s_list.pop(0)
+    return "".join(
+        [str(_.count(_[0])) + _[0] if _.count(_[0]) != 1 else _ for _ in s_list]
+    )
 
 
 # ================================================#
@@ -9,33 +16,50 @@ def isDigit(symbol):
 # ================================================#
 
 
-def isDigit(symbol):
-    return symbol.isdigit()
+def lineEncoding(s):
+    return re.sub(r"(.)\1+", lambda m: str(len(m.group(0))) + m.group(1), s)
+
+
+# def lineEncoding(s):
+#     from itertools import groupby
+
+#     x = ""
+#     for k, g in groupby(s):
+#         y = len((list(g)))
+#         if y == 1:
+#             x += k
+#         else:
+#             x += str(y) + k
+#     return x
 
 
 # ================================================#
 #                 question v                     #
 # ================================================#
 
-# Determine if the given character is a digit or not.
+# Given a string, return its encoding defined as follows:
 
+# First, the string is divided into the least possible number of disjoint substrings consisting of identical characters
+# for example, "aabbbc" is divided into ["aa", "bbb", "c"]
+# Next, each substring with length greater than one is replaced with a concatenation of its length and the repeating character
+# for example, substring "bbb" is replaced by "3b"
+# Finally, all the new strings are concatenated together in the same order and a new string is returned.
 # Example
 
-# For symbol = '0', the output should be
-# isDigit(symbol) = true;
-# For symbol = '-', the output should be
-# isDigit(symbol) = false.
+# For s = "aabbbc", the output should be
+# lineEncoding(s) = "2a3bc".
+
 # Input/Output
 
 # [execution time limit] 4 seconds (py3)
 
-# [input] char symbol
+# [input] string s
 
-# A character which is either a digit or not.
+# String consisting of lowercase English letters.
 
 # Guaranteed constraints:
-# Given symbol is from ASCII table.
+# 4 ≤ s.length ≤ 15.
 
-# [output] boolean
+# [output] string
 
-# true if symbol is a digit, false otherwise.
+# Encoded version of s.
