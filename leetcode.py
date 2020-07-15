87ff79f181a62ddd44826031f185c34a70f4425a
춤추는망고 <ensia96@naver.com>
2020-07-15 10:28:29 +0900

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -36,18 +36,18 @@ class Solution:
 #                  question v                     #
 # ================================================#
 
-# 543. Diameter of Binary Tree
-# Easy
+# 560. Subarray Sum Equals K
+# Medium
 
-# Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+# Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
 
-# Example:
-# Given a binary tree
-#           1
-#          / \
-#         2   3
-#        / \
-#       4   5
-# Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+# Example 1:
 
-# Note: The length of path between two nodes is represented by the number of edges between them.
+# Input:nums = [1,1,1], k = 2
+# Output: 2
+ 
+
+# Constraints:
+
+# The length of the array is in range [1, 20,000].
+# The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
