1314032738c1dd3ed4ece7b946c4113d13b2c704
최호준 <ensia96@gmail.com>
2020-07-17 14:21:22 +0900

    codesignal-53

--- codesignal.py
+++ codesignal.py
@@ -1,8 +1,8 @@
-def longestWord(text):
-    from string import ascii_letters
-
-    words = "".join([_ if _ in list(ascii_letters) else " " for _ in text]).split(" ")
-    return max(words, key=len)
+def validTime(time):
+    if time.count(":"):
+        hour, minute = time.split(":")
+        return int(hour) < 24 and int(minute) < 60
+    return False
 
 
 # ================================================#
@@ -10,36 +10,33 @@ def longestWord(text):
 # ================================================#
 
 
-def longestWord(text):
-    return max(re.split("[^a-zA-Z]", text), key=len)
-
-
-# def longestWord(t):
-#     return max(
-#         "".join([i if i in string.ascii_letters else " " for i in t]).split(), key=len
-#     )
+def validTime(time):
+    h, m = map(int, time.split(":"))
+    return 0 <= h < 24 and 0 <= m < 60
 
 
 # ================================================#
 #                 question v                     #
 # ================================================#
 
-# Define a word as a sequence of consecutive English letters. Find the longest word from the given string.
+# Check if the given string is a correct time representation of the 24-hour clock.
 
 # Example
 
-# For text = "Ready, steady, go!", the output should be
-# longestWord(text) = "steady".
-
+# For time = "13:58", the output should be
+# validTime(time) = true;
+# For time = "25:51", the output should be
+# validTime(time) = false;
+# For time = "02:76", the output should be
+# validTime(time) = false.
 # Input/Output
 
 # [execution time limit] 4 seconds (py3)
 
-# [input] string text
+# [input] string time
 
-# Guaranteed constraints:
-# 4 ≤ text.length ≤ 50.
+# A string representing time in HH:MM format. It is guaranteed that the first two characters, as well as the last two characters, are digits.
 
-# [output] string
+# [output] boolean
 
-# The longest word from text. It's guaranteed that there is a unique output.
+# true if the given representation is correct, false otherwise.
