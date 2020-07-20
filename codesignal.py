fe93a311f9ba395c0fe70127b8b85e5358d47ac0
최호준 <ensia96@gmail.com>
2020-07-20 20:07:25 +0900

    codesignal-57

--- codesignal.py
+++ codesignal.py
@@ -1,123 +1,51 @@
-def ExistPlus_Non1(input_d, k):
-    if k in input_d.keys():
-        input_d[k] += 1
-    else:
-        input_d[k] = 1
+def fileNaming(names):
+    answer = []
+    for name in names:
+        if name in answer:
+            num = 1
+            while f"{name}({num})" in answer:
+                num += 1
+            name = f"{name}({num})"
+        answer.append(name)
+    return answer
 
-
-def DestructNum(n):
-    if n >= 2:
-        i = 2
-        r = {}
-        while n >= 2:
-            if n % i == 0:
-                ExistPlus_Non1(r, i)
-                n = n // i
-            else:
-                i += 1
-        return r
-    else:
-        return {1: 1}
-
-
-def DictToList(input_d):
-    r = []
-    for k in input_d.keys():
-        r += [k for i in range(0, input_d[k])]
-    return r
-
-
-def IsPrimeAndOver10(n):
-    c = 0
-    if n < 10:
-        return False
-    for i in range(2, n):
-        if n % i == 0:
-            c += 1
-    return False if c >= 1 else True
-
-
-def digitsProduct(product):
-    if product == 0:
-        return 10
-    n_dict = DestructNum(product)
-    if any([IsPrimeAndOver10(k) for k in n_dict.keys()]):
-        return -1
-    else:
-        while True:
-            if 3 in n_dict.keys() and n_dict[3] >= 2:
-                n_dict[3] -= 2
-                ExistPlus_Non1(n_dict, 9)
-            else:
-                if 2 in n_dict.keys() and n_dict[2] >= 3:
-                    n_dict[2] -= 3
-                    ExistPlus_Non1(n_dict, 8)
-                else:
-                    if (
-                        2 in n_dict.keys()
-                        and n_dict[2] >= 1
-                        and 3 in n_dict.keys()
-                        and n_dict[3] >= 1
-                    ):
-                        n_dict[2] -= 1
-                        n_dict[3] -= 1
-                        ExistPlus_Non1(n_dict, 6)
-                    else:
-                        if 2 in n_dict.keys() and n_dict[2] >= 2:
-                            n_dict[2] -= 2
-                            ExistPlus_Non1(n_dict, 4)
-                        else:
-                            break
-    return int("".join([str(d) for d in sorted(DictToList(n_dict))]))
-
-
-# https://frhyme.github.io/algorithm/Algorithm-digitsProduct/
-# 1 ~ 9 사이의 수로 반복적으로 나누게 하는 반복문에 대한 종결값 설정, 나누기를 1로 시작하지 않는 반복문의 조건설정을 못해서 구글링함.
 
 # ================================================#
 #     ^ my answer      ||  most voted answer v   #
 # ================================================#
 
 
-def digitsProduct(p):
-    if p == 0:
-        return 10
-    elif p == 1:
-        return 1
-
-    n = []
-
-    while 1 < p:
-        for d in range(9, 1, -1):
-            if p % d == 0:
-                p /= d
-                n.append(d)
-                break
-        else:
-            return -1
-
-    return int("".join(map(str, sorted(n))))
+def fileNaming(names):
+    for i in range(len(names)):
+        if names[i] in names[:i]:
+            j = 1
+            while names[i] + "(" + str(j) + ")" in names[:i]:
+                j += 1
+            names[i] += "(" + str(j) + ")"
+    return names
 
 
 # ================================================#
 #                 question v                     #
 # ================================================#
 
-# Given an integer product, find the smallest positive (i.e. greater than 0) integer the product of whose digits is equal to product. If there is no such integer, return -1 instead.
+# You are given an array of desired filenames in the order of their creation. Since two files cannot have equal names, the one which comes later will have an addition to its name in a form of (k), where k is the smallest positive integer such that the obtained name is not used yet.
+
+# Return an array of names that will be given to the files.
 
 # Example
 
-# For product = 12, the output should be
-# digitsProduct(product) = 26;
-# For product = 19, the output should be
-# digitsProduct(product) = -1.
+# For names = ["doc", "doc", "image", "doc(1)", "doc"], the output should be
+# fileNaming(names) = ["doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"].
+
 # Input/Output
 
 # [execution time limit] 4 seconds (py3)
 
-# [input] integer product
+# [input] array.string names
 
 # Guaranteed constraints:
-# 0 ≤ product ≤ 600.
+# 5 ≤ names.length ≤ 1000,
+# 1 ≤ names[i].length ≤ 15.
 
-# [output] integer
+# [output] array.string
