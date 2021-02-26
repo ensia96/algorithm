caaf18fa32eadbc28292055b1735b5c2ce14a5f0
춤추는망고 <ensia96@gmail.com>
2021-02-26 14:38:13 +0900

    [add] code base & examples

---
+++ programmers/practice/hash/완주하지 못한 선수(lv.1).py
@@ -0,0 +1,18 @@
+def solution(participant, completion):
+    answer = ''
+    return answer
+
+print(solution(
+    ['leo', 'kiki', 'eden'],
+    ['eden', 'kiki']
+)) # => 'leo'
+
+print(solution(
+    ['marina', 'josipa', 'nikola', 'vinko', 'filipa'],
+    ['josipa', 'filipa', 'marina', 'nikola']
+)) # => 'vinko'
+
+print(solution(
+    ['mislav', 'stanko', 'mislav', 'ana'],
+    ['stanko', 'ana', 'mislav']
+)) # => 'mislav'
