5c75227668c9a8b3213cc390d1e706cc6127bcef
춤추는망고 <ensia96@gmail.com>
2021-03-07 15:40:55 +0900

    [add] maximum variable

--- programmers/practice/hash/전화번호 목록(lv.2).py
+++ programmers/practice/hash/전화번호 목록(lv.2).py
@@ -5,11 +5,13 @@ def solution(phone_book):
     output
         - answer     : 임의의 번호가 다른 번호의 접두어인 경우의 진리값
     '''
-    table = dict()
-    index = 0
+    table   = dict()
+    index   = 0
+    maximum = len(max(phone_book, key=len))
 
     for number in phone_book:
         table[number] = True
+
 
 print(solution(["119", "97674223", "1195524421"])) # False
 print(solution(["123", "456", "789"])) # True
