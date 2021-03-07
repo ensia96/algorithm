02a49e74958492164661d3a98f88d1b66af21c08
춤추는망고 <ensia96@gmail.com>
2021-03-07 12:45:04 +0900

    [add] docstring to solution function
    [add] docstring to solution function

--- programmers/practice/hash/전화번호 목록(lv.2).py
+++ programmers/practice/hash/전화번호 목록(lv.2).py
@@ -1,7 +1,13 @@
 def solution(phone_book):
+    '''
+    input
+        - phone_book : [전화번호] (1 <= [] <= 1000000, 1 <= i <= 20)
+    output
+        - answer     : 임의의 번호가 다른 번호의 접두어인 경우의 진리값
+    '''
     answer = True
     return answer
 
 print(solution(["119", "97674223", "1195524421"])) # False
-print(solution(["123","456","789"])) # True
-print(solution(["12","123","1235","567","88"])) # False
+print(solution(["123", "456", "789"])) # True
+print(solution(["12", "123", "1235", "567", "88"])) # False
