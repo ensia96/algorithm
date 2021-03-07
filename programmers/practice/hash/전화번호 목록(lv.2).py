4a7735103a6dab662f741d12d7d5fd6793e3a65c
춤추는망고 <ensia96@gmail.com>
2021-03-07 16:49:10 +0900

    [change] function

--- programmers/practice/hash/전화번호 목록(lv.2).py
+++ programmers/practice/hash/전화번호 목록(lv.2).py
@@ -4,19 +4,43 @@ def solution(phone_book):
         - phone_book : [전화번호] (1 <= [] <= 1000000, 1 <= i <= 20)
     output
         - answer     : 임의의 번호가 다른 번호의 접두어인 경우의 진리값
+    result
+        - 정확성 : 54.2/100
+        - 효율성 : 12.5/100
     '''
     table   = dict()
     index   = 0
     maximum = len(max(phone_book, key=len))
 
     for number in phone_book:
-        table[number] = True
+        table[number] = False
 
     while index < maximum:
         count = dict()
 
-        index += 1
+        for number in phone_book:
+            if table[number]:
+                continue
+            digit = number[index]
 
+            if digit not in count:
+                count[digit] = 0
+            count[digit] += 1
+
+        if max(count.values()) == 1:
+            return True
+
+        for number in phone_book:
+            if table[number]:
+                continue
+
+            if count[number[index]] >= 1:
+                if len(number) - 1 == index:
+                    return False
+            else:
+                table[number] = False
+
+        index += 1
 
 print(solution(["119", "97674223", "1195524421"])) # False
 print(solution(["123", "456", "789"])) # True
