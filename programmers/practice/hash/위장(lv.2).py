2bd7be0699cfb80b6cb12e167e78708d6a962d8f
춤추는망고 <ensia96@gmail.com>
2021-03-12 11:15:13 +0900

    [add] code base & examples

---
+++ programmers/practice/hash/위장(lv.2).py
@@ -0,0 +1,14 @@
+def solution(clothes):
+    answer = 0
+    return answer
+
+print(solution([
+    ["yellowhat", "headgear"],
+    ["bluesunglasses", "eyewear"],
+    ["green_turban", "headgear"]
+])) # 5
+print(solution([
+    ["crowmask", "face"],
+    ["bluesunglasses", "face"],
+    ["smoky_makeup", "face"]
+])) # 3
