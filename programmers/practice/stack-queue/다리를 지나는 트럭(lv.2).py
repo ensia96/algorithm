697d27b93437eb241a45852876b415d46854f69a
춤추는망고 <ensia96@gmail.com>
2021-03-19 14:51:28 +0900

    [add] docstring to solution function

--- programmers/practice/stack-queue/다리를 지나는 트럭(lv.2).py
+++ programmers/practice/stack-queue/다리를 지나는 트럭(lv.2).py
@@ -1,4 +1,12 @@
 def solution(bridge_length, weight, truck_weights):
+    '''
+    input
+        - bridge_length : 다리의 길이 (1 <= i <= 10000)
+        - weight        : 다리가 견딜 수 있는 무게 (1 <= i <= 10000)
+        - truck_weights : [트럭의 무게] (1 <= [] <= 10000, 1 <= truck <= weight)
+    output
+        - answer        : 모든 트럭이 다리를 건너는데 걸리는 최소 시간
+    '''
     answer = 0
     return answer
 
