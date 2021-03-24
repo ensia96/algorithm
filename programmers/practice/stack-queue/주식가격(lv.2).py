aba48701aa1acdf12d83e4ffc12a424724006e59
춤추는망고 <ensia96@gmail.com>
2021-03-24 12:06:33 +0900

    [add] necessary variables

--- programmers/practice/stack-queue/주식가격(lv.2).py
+++ programmers/practice/stack-queue/주식가격(lv.2).py
@@ -5,7 +5,9 @@ def solution(prices):
     output
         - answer : 각 시점마다 주식이 떨어지지 않은 기간
     '''
-    answer = []
+    answer = [0]
+    time   = 0
+
     return answer
 
 print(solution([1, 2, 3, 2, 3])) # [4, 3, 1, 1, 0]
