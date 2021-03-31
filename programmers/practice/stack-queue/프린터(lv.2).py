62401b88a9071a248f859a5900ec8e7d4406e470
춤추는망고 <ensia96@gmail.com>
2021-03-31 10:48:40 +0900

    [add] docstring to solution function

--- programmers/practice/stack-queue/프린터(lv.2).py
+++ programmers/practice/stack-queue/프린터(lv.2).py
@@ -1,4 +1,11 @@
 def solution(priorities, location):
+    '''
+    input
+        - priorities : [인쇄 중요도] (1 <= [] <= 100, 1 <= i <= 9)
+        - location : 요청한 문서의 인덱스 (0 <= i <= len(priorities) - 1)
+    output
+        - answer : 요청된 문서가 출력되는 순서
+    '''
     answer = 0
     return answer
 
