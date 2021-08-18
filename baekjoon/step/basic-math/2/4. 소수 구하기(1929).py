b270ebfa3cdff76f1ffce70d431cde8720bd1f43
춤추는망고 <ensia96@gmail.com>
2021-08-18 23:20:40 +0900

    [update] 소수 구하기(1929) - 시간 초과 해결

--- baekjoon/step/basic-math/2/4. 소수 구하기(1929).py
+++ baekjoon/step/basic-math/2/4. 소수 구하기(1929).py
@@ -1,4 +1,5 @@
 m, n = map(int, input().split())
 
-for i in (i for i in range(m, n + 1) if all(i % j for j in range(2, i))):
-    print(i)
+for i in range(m, n + 1):
+    if all(i % j for j in range(2, i)):
+        print(i)
