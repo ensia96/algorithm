0787292f20ee4ed1aa94dd554e00b0e99dbc524e
춤추는망고 <ensia96@gmail.com>
2021-08-19 23:11:54 +0900

    [update] 골드바흐의 추측(9020) - 틀렸습니다 해결

--- baekjoon/step/basic-math/2/6. 골드바흐의 추측(9020).py
+++ baekjoon/step/basic-math/2/6. 골드바흐의 추측(9020).py
@@ -1,17 +1,19 @@
 l = map(int, [*open(0)][1:])
 
 m = max(l) + 1
-a = [1 for _ in range(0, m)]
+a = [0, 0] + [1 for _ in range(m)]
 
 for i in range(2, m):
     if a[i]:
-        for j in range(2 * i, m, i):
+        for j in range(2 * i, m + 1, i):
             a[j] = 0
 
 for n in l:
-    for i in range(2, n // 2 + 1):
-        if a[i]:
-            b = n - i
-            if b > 1 and a[b]:
-                x, y = i, b
-    print(x, y)
+    x = n // 2
+    y = x
+    for i in range(n):
+        if a[x] and a[y]:
+            print(x, y)
+            break
+        x -= 1
+        y += 1
