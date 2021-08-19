987ebcbfbb0ef813246c050d862041cbf9b042f7
춤추는망고 <ensia96@gmail.com>
2021-08-19 21:20:21 +0900

    [update] 소수 구하기(1929) - 시간 초과 해결

--- baekjoon/step/basic-math/2/4. 소수 구하기(1929).py
+++ baekjoon/step/basic-math/2/4. 소수 구하기(1929).py
@@ -1,24 +1,15 @@
 m, n = map(int, input().split())
-x = {2}
 
-for i in range(m, n + 1):
-    a = 1
+n += 1
+prime = [True] * n
 
-    for y in x:
-        if not i % y:
-            x.add(i)
-            a = 0
-            break
+for i in range(2, int(n ** 0.5) + 1):
+    if prime[i]:
+        for j in range(2 * i, n, i):
+            prime[j] = False
 
-    if not a:
-        continue
-
-    for j in range(2, int(i ** 0.5)):
-        if not i % j:
-            a = 0
-            break
-    if a:
-        x.add(i)
+for i in range(m, n):
+    if i > 1 and prime[i]:
         print(i)
 
 # 에라토스테네스의 체 풀이 관련 글 : https://leejunggae.tistory.com/3
