3e34021d59ed04612dd3e90ac5d544134dd953ff
춤추는망고 <ensia96@gmail.com>
2021-08-19 21:57:14 +0900

    [update] 베르트랑 공준(4948) - 런타임 에러(TypeError) 해결

--- baekjoon/step/basic-math/2/5. 베르트랑 공준(4948).py
+++ baekjoon/step/basic-math/2/5. 베르트랑 공준(4948).py
@@ -1,5 +1,5 @@
 l = [*open(0)][:-1]
-a = max(l) + 1
+a = 2 * max(l) + 2
 b = [1 for _ in range(a)]
 
 for i in range(2, int(a ** 0.5) + 1):
@@ -8,4 +8,4 @@ for i in range(2, int(a ** 0.5) + 1):
             b[i] = 0
 
 for n in l:
-    sum(1 if i > 1 and b[i] else 0 for i in range(n, 2 * n + 1))
+    print(sum(b[i] for i in range(n, 2 * n + 1) if i > 1))
