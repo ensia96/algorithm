87f6e4ccc3a4a91a6f6ae6b0ca06cb07c5105aa8
춤추는망고 <ensia96@gmail.com>
2021-08-19 22:24:52 +0900

    [update] 베르트랑 공준(4948) - 틀렸습니다 해결

--- baekjoon/step/basic-math/2/5. 베르트랑 공준(4948).py
+++ baekjoon/step/basic-math/2/5. 베르트랑 공준(4948).py
@@ -1,11 +1,17 @@
-l = map(int, [*open(0)][:-1])
-a = 2 * max(l) + 2
-b = [1 for _ in range(a)]
+x, l = True, []
 
-for i in range(2, int(a ** 0.5) + 1):
-    if b[i]:
-        for j in range(2 * i, a, i):
-            b[i] = 0
+while x:
+    x = int(input())
+    if x:
+        l.append(x)
+
+m = 2 * max(l) + 1
+a = [1 for _ in range(m + 1)]
+
+for i in range(2, m):
+    if a[i]:
+        for j in range(2 * i, m + 1, i):
+            a[j] = 0
 
 for n in l:
-    print(sum(b[i] for i in range(n, 2 * n + 1) if i > 1))
+    print(sum(a[i] for i in range(n + 1, 2 * n + 1) if i > 1))
