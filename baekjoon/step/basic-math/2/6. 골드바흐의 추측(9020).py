7a8d9debb1e9eb9b820a29332126308de9f9ec15
춤추는망고 <ensia96@gmail.com>
2021-08-19 22:44:50 +0900

    [update] 골드바흐의 추측(9020) - 런타임 에러 (IndexError) 해결

--- baekjoon/step/basic-math/2/6. 골드바흐의 추측(9020).py
+++ baekjoon/step/basic-math/2/6. 골드바흐의 추측(9020).py
@@ -5,12 +5,13 @@ a = [1 for _ in range(0, m)]
 
 for i in range(2, m):
     if a[i]:
-        for j in range(2 * i, m + 1, i):
+        for j in range(2 * i, m, i):
             a[j] = 0
 
 for n in l:
     for i in range(2, n // 2):
-        b = n - i
-        if b > 1 and a[b]:
-            x, y = i, b
+        if a[i]:
+            b = n - i
+            if a[b]:
+                x, y = i, b
     print(x, y)
