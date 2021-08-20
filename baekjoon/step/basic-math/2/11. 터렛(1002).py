939add434e132b6411170df49ac098274e3c3958
춤추는망고 <ensia96@gmail.com>
2021-08-20 21:44:29 +0900

    [update] 터렛(1002) - 틀렸습니다 해결

--- baekjoon/step/basic-math/2/11. 터렛(1002).py
+++ baekjoon/step/basic-math/2/11. 터렛(1002).py
@@ -1,14 +1,25 @@
-for _ in range(int(input())):
+def check():
     x1, y1, r1, x2, y2, r2 = map(int, input().split())
-
-    if x1 == x2 and y1 == y2:
-        if r1 == r2:
-            print(-1)
-            break
-        print(0)
-        break
-
     l = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-    r = r1 + r2
+    s, d = r1 + r2, abs(r1 - r2)
 
-    print(0 if l > r else 2 if l < r else 1)
+    if l == 0:
+        if r1 == r2:
+            return -1
+        return 0
+
+    if s < l or l < d:
+        return 0
+
+    if s == l or d == l:
+        return 1
+
+    if d < l < s:
+        return 2
+
+
+for _ in range(int(input())):
+    print(check())
+
+# 두 원의 위치 관계 참고 글 : https://mathbang.net/101
+# 파이썬 절대값 참고 글 : https://blockdmask.tistory.com/380
