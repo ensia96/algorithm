a80a73aa85667f48f9f0f72b4f04307a004ee414
춤추는망고 <ensia96@gmail.com>
2021-08-22 21:56:55 +0900

    [update] 하노이 탑 이동 순서(11729) - 런타임 에러 (RecursionError) 해결

--- baekjoon/step/recursion/4. 하노이 탑 이동 순서(11729).py
+++ baekjoon/step/recursion/4. 하노이 탑 이동 순서(11729).py
@@ -1,86 +1,15 @@
+def h(n, s, e):
+    if n == 1:
+        print(s, e)
+        return
+
+    h(n - 1, s, 6 - s - e)
+    print(s, e)
+    h(n - 1, 6 - s - e, e)
+
+
 n = int(input())
-x = sum(2 ** _ for _ in range(n))
-t = [[21, *(i for i in range(n, 0, -1))], [21], [21]]
-r = [[21], [21], list(t[0])]
+print(2 ** n - 1)
+h(n, 1, 3)
 
-
-def m(t, s, g):
-    return (
-        not (t[g].append(t[s].pop()))
-        if t[s] and t[g] and t[s][-1] < t[g][-1]
-        else False
-    )
-
-
-def s(a=[]):
-    if len(a) > x:
-        return False
-    if t == r:
-        return a
-
-    for i in range(3):
-        for j in range(3):
-            if i != j and m(t, i, j):
-                a.append((f"{i + 1} {j + 1}"))
-                if s(a):
-                    return a
-                else:
-                    a.pop()
-                    m(t, j, i)
-
-
-print(x)
-print("\n".join(s()))
-
-
-# 1
-# 1 3
-
-# 3
-# 1 2
-# 1 3
-# 2 3
-
-# 7
-# 1 3 | 3 1
-# 1 2 | 3 2
-# 3 2 | 1 2
-# 1 3 | 3 1
-# 2 1 | 2 3
-# 2 3 | 2 1
-# 1 3 | 3 1
-
-# 15
-# 1 2
-# 1 3
-# 2 3
-# 1 2
-# 3 1
-# 3 2
-# 1 2
-# 1 3
-# 2 3
-# 2 1
-# 3 1
-# 2 3
-# 1 2
-# 1 3
-# 2 3
-
-[
-    (1, 2),
-    (1, 3),
-    (2, 3),
-    (1, 2),
-    (3, 1),
-    (3, 2),
-    (1, 2),
-    (1, 3),
-    (2, 3),
-    (2, 1),
-    (3, 1),
-    (2, 3),
-    (1, 2),
-    (1, 3),
-    (2, 3),
-]
+# 풀이 참고 : https://study-all-night.tistory.com/6
