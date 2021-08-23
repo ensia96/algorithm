741447b75069e39abdce7b48bf4423f4a5a3c1f3
춤추는망고 <ensia96@gmail.com>
2021-08-23 23:42:04 +0900

    baekjoon/baaarkingdog/0x04

---
+++ baekjoon/baaarkingdog/0x04/키로거(5397).py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    p, n = [], []
+    for c in input():
+        if c not in "<>-":
+            p.append(c)
+        if p:
+            if c == "<":
+                n.append(p.pop())
+            if c == "-":
+                p.pop()
+        if n and c == ">":
+            p.append(n.pop())
+    print("".join(p + n[::-1]))
