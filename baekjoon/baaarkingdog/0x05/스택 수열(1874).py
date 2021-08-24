9f820adfa7e70804ecf60b1fd58aa83b1100505e
춤추는망고 <ensia96@gmail.com>
2021-08-24 23:14:45 +0900

    [update] 스택 수열(1874) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x05/스택 수열(1874).py
+++ baekjoon/baaarkingdog/0x05/스택 수열(1874).py
@@ -4,14 +4,14 @@ n, s, a, c, f = int(input()), [], [], 1, 1
 
 for _ in range(n):
     i = int(sys.stdin.readline().strip())
-    if not s or s[-1] > i:
-        f = 0
-        continue
-    while l and s[-1] < i:
-        s.append(l.pop())
+    while c <= i:
+        s.append(c)
         a.append("+")
-    while s and s[-1] >= i:
+        c += 1
+    if s[-1] == i:
         s.pop()
         a.append("-")
+    else:
+        f = 0
 
 print("\n".join(a) if f else "NO")
