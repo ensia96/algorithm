615474d79b19c598459f990a42a76bad642035a1
춤추는망고 <ensia96@gmail.com>
2021-08-24 21:08:11 +0900

    [update] 스택(10828) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x05/스택(10828).py
+++ baekjoon/baaarkingdog/0x05/스택(10828).py
@@ -3,14 +3,14 @@ import sys
 s = []
 
 for _ in range(int(input())):
-    c = sys.stdin.readline()
-    if c == "pop":
-        print(s.pop() if s else "-1")
-    elif c == "size":
+    c = sys.stdin.readline().split()
+    if c[0] == "pop":
+        print(s.pop() if s else -1)
+    elif c[0] == "size":
         print(str(len(s)))
-    elif c == "empty":
-        print("0" if s else "1")
-    elif c == "top":
-        print(s[-1] if s else "-1")
+    elif c[0] == "empty":
+        print(0 if s else 1)
+    elif c[0] == "top":
+        print(s[-1] if s else -1)
     else:
-        s.append(c.split().pop())
+        s.append(c.pop())
