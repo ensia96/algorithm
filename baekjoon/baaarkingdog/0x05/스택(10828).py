2cab391397f6a305dfa6cfc53bf3dd319f267351
춤추는망고 <ensia96@gmail.com>
2021-08-24 20:56:05 +0900

    [update] 스택(10828) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x05/스택(10828).py
+++ baekjoon/baaarkingdog/0x05/스택(10828).py
@@ -1,14 +1,16 @@
-s = []
+s, r = [], []
 
 for _ in range(int(input())):
     c = input()
     if "push" in c:
         s.append(c.split().pop())
     if c == "pop":
-        print(s.pop() if s else -1)
+        r.append(s.pop() if s else "-1")
     if c == "size":
-        print(len(s))
+        r.append(len(s))
     if c == "empty":
-        print(0 if s else 1)
+        r.append("0" if s else "1")
     if c == "top":
-        print(s[-1] if s else -1)
+        r.append(s[-1] if s else "-1")
+
+print("\n".join(r))
