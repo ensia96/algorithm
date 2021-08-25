32602f4fbe9c5a63858d3c4fe2a73840ac229e19
춤추는망고 <ensia96@gmail.com>
2021-08-25 23:38:26 +0900

    [update] 옥상 정원 꾸미기(6198) - 출력 초과 해결

--- baekjoon/baaarkingdog/0x05/옥상 정원 꾸미기(6198).py
+++ baekjoon/baaarkingdog/0x05/옥상 정원 꾸미기(6198).py
@@ -1,15 +1,15 @@
 n = int(input())
-s, a = [(10e8, 0)], [0 for _ in range(n)]
+s, a = [(10e8, 0)], 0
 
 for i in range(n):
     h = int(input())
     while s[-1][0] < h:
         v = s.pop()[1]
-        a[v] = i - v - 1
+        a += i - v - 1
     s += [(h, i)]
 
 while len(s) > 1:
     v = s.pop()[1]
-    a[v] = i - v
+    a += i - v
 
-print(*map(str, a))
+print(a)
