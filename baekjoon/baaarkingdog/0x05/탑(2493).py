bcbb02603320547d706b13e4cb76f3ff62f1b89d
춤추는망고 <ensia96@gmail.com>
2021-08-25 21:25:10 +0900

    [update] 탑(2493) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x05/탑(2493).py
+++ baekjoon/baaarkingdog/0x05/탑(2493).py
@@ -1,13 +1,13 @@
 import sys
 
-n = int(input())
-t, a = [*map(int, sys.stdin.readline().split())], [0 for _ in range(n)]
+n, t = int(input()), [*map(int, sys.stdin.readline().split()[::-1])]
+a, i, m, r = [], 0, 10e8, 0
 
-for i in range(n - 1, -1, -1):
-    h = t[i]
-    for j in range(i - 1, -1, -1):
-        if h < t[j]:
-            a[i] = j + 1
-            break
+while t:
+    c = t.pop()
+    if c < m:
+        m, r = c, i
+    a.append(r)
+    i += 1
 
 print(*map(str, a))
