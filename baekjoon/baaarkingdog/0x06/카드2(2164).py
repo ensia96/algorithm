42ddd4d810e6d6efb089d15cffe4be420a8c4a1c
춤추는망고 <ensia96@gmail.com>
2021-08-26 18:44:43 +0900

    [update] 카드2(2164) - 런타임 에러 (IndexError) 해결

--- baekjoon/baaarkingdog/0x06/카드2(2164).py
+++ baekjoon/baaarkingdog/0x06/카드2(2164).py
@@ -1,11 +1,12 @@
 import collections as c
 
 n, i = int(input()), 1
-q = c.deque(range(1, n + 1))
+q = c.deque(i + 1 for i in range(n))
 
 while q:
     if len(q) == 1:
         print(q.pop())
+        break
     if i % 2:
         q.popleft()
     else:
