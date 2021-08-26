9eee7f534b4960fe7b8f20401b9b2a5039e3c69b
춤추는망고 <ensia96@gmail.com>
2021-08-26 17:01:22 +0900

    [update] 큐(10845) - 런타임 에러 (IndexError) 해결

--- baekjoon/baaarkingdog/0x06/큐(10845).py
+++ baekjoon/baaarkingdog/0x06/큐(10845).py
@@ -6,8 +6,8 @@ for _ in range(int(input())):
         if h == t:
             p(-1)
         else:
-            p(q[h])
-            h += 1
+            p(q[t])
+            t += 1
     elif c == "size":
         p(t - h)
     elif c == "empty":
@@ -19,5 +19,3 @@ for _ in range(int(input())):
     else:
         q += [c.split().pop()]
         t += 1
-        if h > t:
-            h = t
