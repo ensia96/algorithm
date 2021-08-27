7c43ab70c02aad09d720dceca416f1a669d9e61f
춤추는망고 <ensia96@gmail.com>
2021-08-27 23:05:49 +0900

    [update] 괄호의 값(2504) - 런타임 에러 (IndexError) 해결

--- baekjoon/baaarkingdog/0x08/괄호의 값(2504).py
+++ baekjoon/baaarkingdog/0x08/괄호의 값(2504).py
@@ -1,20 +1,23 @@
 s, b, o, c, f = [], {')': '(', ']': '['}, '([', ')]', 1
 
-for i in input().replace('()', '2').replace('[]', '3'):
-    if not f:
-        break
-    if i in o:
-        s.append(i)
-    elif i in c:
-        v = 0
-        while s[-1] != b[i]:
-            d = s.pop()
-            if type(d) != int:
-                f = 0
-                break
-            v += d
-        s.append(v * (2 if s.pop() == '(' else 3))
-    else:
-        s.append(int(i))
+try:
+    for i in input().replace('()', '2').replace('[]', '3'):
+        if not f:
+            break
+        if i in o:
+            s.append(i)
+        elif i in c:
+            v = 0
+            while s[-1] != b[i]:
+                d = s.pop()
+                if type(d) != int:
+                    f = 0
+                    break
+                v += d
+            s.append(v * (2 if s.pop() == '(' else 3))
+        else:
+            s.append(int(i))
 
-print(sum(s) if f else 0)
+    print(sum(s) if f else 0)
+except:
+    print(0)
