1f8e076784d148db9be09191395cce105dce57ce
춤추는망고 <ensia96@gmail.com>
2021-08-27 16:59:03 +0900

    [update] AC(5430) 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x07/AC(5430).py
+++ baekjoon/baaarkingdog/0x07/AC(5430).py
@@ -6,12 +6,13 @@ i = sys.stdin.readline
 for _ in range(int(i())):
     p, _, d, r, f = i().strip(), i(), c.deque(i()[1:-2].split(',')), 0, 0
     for a in p:
-        if not d or not d[0]:
-            f = 1
-            break
         if a == 'R':
             r = not r
         else:
-            d.pop() if r else d.popleft()
+            if not d or not d[0]:
+                f = 1
+                break
+            else:
+                d.pop() if r else d.popleft()
 
     print('error' if f else f"[{','.join(reversed(d) if r else d)}]")
