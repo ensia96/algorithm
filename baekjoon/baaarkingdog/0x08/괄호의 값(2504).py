981c2ee26a95720f668a45cad20d5a1661ca5b21
춤추는망고 <ensia96@gmail.com>
2021-08-27 22:58:53 +0900

    괄호의 값(2504)

---
+++ baekjoon/baaarkingdog/0x08/괄호의 값(2504).py
@@ -0,0 +1,20 @@
+s, b, o, c, f = [], {')': '(', ']': '['}, '([', ')]', 1
+
+for i in input().replace('()', '2').replace('[]', '3'):
+    if not f:
+        break
+    if i in o:
+        s.append(i)
+    elif i in c:
+        v = 0
+        while s[-1] != b[i]:
+            d = s.pop()
+            if type(d) != int:
+                f = 0
+                break
+            v += d
+        s.append(v * (2 if s.pop() == '(' else 3))
+    else:
+        s.append(int(i))
+
+print(sum(s) if f else 0)
