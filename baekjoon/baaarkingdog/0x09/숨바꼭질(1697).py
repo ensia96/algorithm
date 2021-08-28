0bc9c6e7a57572233e7500e5487f39dc609f012d
춤추는망고 <ensia96@gmail.com>
2021-08-28 22:09:13 +0900

    [update] 숨바꼭질(1697) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/숨바꼭질(1697).py
+++ baekjoon/baaarkingdog/0x09/숨바꼭질(1697).py
@@ -1,14 +1,14 @@
 import collections as c
 
 n, k = map(int, input().split())
-q, l = c.deque([(n, 0)]), [0] * (k + 2)
+q, l = c.deque([n]), [0] * 100000
 
 while q:
-    c, v = q.popleft()
+    c = q.popleft()
     if n == k or l[k]:
         print(l[k])
         break
     for m in [c - 1, c + 1, c * 2]:
-        if 0 <= m < k + 2 and not l[m]:
-            l[m] = v + 1
-            q.append((m, v + 1))
+        if 0 <= m < 100000 and not l[m]:
+            l[m] = l[c] + 1
+            q.append(m)
