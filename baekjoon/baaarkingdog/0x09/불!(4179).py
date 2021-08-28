e1318db3dfa4c403f246d5ce0c2b488bcb9fb9e7
춤추는망고 <ensia96@gmail.com>
2021-08-28 20:27:11 +0900

    [update] 불!(4179) - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x09/불!(4179).py
+++ baekjoon/baaarkingdog/0x09/불!(4179).py
@@ -2,32 +2,45 @@ import sys
 import collections as c
 
 i = sys.stdin.readline
-j, f, d = c.deque(), c.deque(), [(1, 0), (0, 1), (-1, 0), (0, -1)]
+q, d = c.deque(), [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 r, c = map(int, i().split())
-b, a = [[*i().strip()] for _ in range(r)], 1
+b = [[*i().strip()] for _ in range(r)]
+a = s = 'IMPOSSIBLE'
 
 for n in range(r):
     for m in range(c):
         if b[n][m] == 'J':
-            b[n][m] = 1
-            j.append((n, m))
-        if b[n][m] == 'F':
-            f.append((n, m))
-        if b[n][m] == '#':
             b[n][m] = 0
+            j = (n, m)
+        if b[n][m] == 'F':
+            b[n][m] = 0
+            q.append((n, m))
 
-while j and f:
-    n, m = j.popleft()
-    v, w = f.popleft()
+while q:
+    n, m = q.popleft()
     for x, y in d:
-        p, q = n + y, m + x
-        g, h = v + y, w + x
-        if 0 <= g < r and 0 <= h < c and b[g][h]:
-            b[g][h] = 'F'
-            f.append((g, h))
-        if 0 <= p < r and 0 <= q < c and b[p][q] == '.':
-            a += 1
-            j.append((p, q))
+        v, w = n + y, m + x
+        if 0 <= v < r and 0 <= w < c and b[v][w] == '.':
+            b[v][w] = b[n][m] + 1
+            q.append((v, w))
 
-print(a if any('.' in l for l in b) else 'IMPOSSIBLE')
+q.append(j)
+
+while q:
+    if a != s:
+        break
+    n, m = q.popleft()
+    f = b[n][m]
+    for x, y in d:
+        v, w = n + y, m + x
+        if 0 <= v < r and 0 <= w < c:
+            e = b[v][w]
+            if e != '#' and f + 1 < e:
+                b[v][w] = f + 1
+                q.append((v, w))
+        else:
+            a = f + 1
+            break
+
+print(a)
