3b4baad2fcdfade8b0a71151a74ce8b3c0b81e18
춤추는망고 <ensia96@gmail.com>
2021-08-28 22:03:57 +0900

    [update] 숨바꼭질(1697) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/숨바꼭질(1697).py
+++ baekjoon/baaarkingdog/0x09/숨바꼭질(1697).py
@@ -1,15 +1,14 @@
 import collections as c
 
 n, k = map(int, input().split())
-q, l = c.deque([(n, 0)]),  [0] * (2 * k + 1)
-a = k
+q, l = c.deque([(n, 0)]), [0] * (k + 2)
 
 while q:
     c, v = q.popleft()
-    if c == k:
-        a = min(a, v)
-    for m in [c * 2, c + 1, c - 1]:
-        if 0 < m <= 2 * k and not l[m]:
-            l[m] = 1
+    if n == k or l[k]:
+        print(l[k])
+        break
+    for m in [c - 1, c + 1, c * 2]:
+        if 0 <= m < k + 2 and not l[m]:
+            l[m] = v + 1
             q.append((m, v + 1))
-print(a)
