f1dec64db28e275b28a09608fdd44a7eb6e03d8a
춤추는망고 <ensia96@gmail.com>
2021-08-28 21:34:10 +0900

    [update] 숨바꼭질(1697) - 런타임 에러 (IndexError) 해결

--- baekjoon/baaarkingdog/0x09/숨바꼭질(1697).py
+++ baekjoon/baaarkingdog/0x09/숨바꼭질(1697).py
@@ -1,15 +1,14 @@
 import collections as c
 
 n, k = map(int, input().split())
-q, v = c.deque([(n, 0)]), 0
+q, v, l = c.deque([(n, 0)]), 0, [0] * (k + 1)
 
-while 1:
+while q:
     if c == k:
         print(v)
         break
     c, v = q.popleft()
-    if c + 1 > k:
-        continue
-    q.append((c * 2, v + 1))
-    q.append((c - 1, v + 1))
-    q.append((c + 1, v + 1))
+    for a in [c * 2, c + 1, c - 1]:
+        if 0 < a <= k and not l[a]:
+            l[a] = 1
+            q.append((a, v + 1))
