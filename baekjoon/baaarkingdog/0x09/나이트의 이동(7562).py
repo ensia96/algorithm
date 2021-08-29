4734de9657c6095cb1b811219fd12dca784f85c4
춤추는망고 <ensia96@gmail.com>
2021-08-29 13:12:52 +0900

    [update] 나이트의 이동(7562) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/나이트의 이동(7562).py
+++ baekjoon/baaarkingdog/0x09/나이트의 이동(7562).py
@@ -20,7 +20,7 @@ for _ in r(t(i())):
     m[a][b] = 1
     for c, d in q:
         if m[g[0]][g[1]]:
-            print('answer : ', m[g[0]][g[1]] - 1)
+            print(m[g[0]][g[1]] - 1)
             break
         for x, y in v(c, d):
             if 0 <= x < n and 0 <= y < n and not m[x][y]:
