bc106a58ec187e8bb2294fa999c0c93a7c1eb42a
춤추는망고 <ensia96@gmail.com>
2021-08-29 18:27:19 +0900

    [update] 벽 부수고 이동하기(2206) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/벽 부수고 이동하기(2206).py
+++ baekjoon/baaarkingdog/0x09/벽 부수고 이동하기(2206).py
@@ -1,20 +1,24 @@
 import sys
 
-i = sys.stdin.readline
-n, m = map(int, i().split())
-a, q = [], [(0, 0, 0)]
-b = [[*map(int, [*i().strip()])] for _ in range(n)]
-b[0][0] = 1
+i, r, t, p = sys.stdin.readline, range, int, map
+n, m = p(t, i().split())
+a, q = -1, [(0, 0, 0)]
+b = [[*p(t, [*i().strip()])] for _ in r(n)]
+v = [[[0, 0] for _ in r(m)] for _ in r(n)]
 
 for i, j, f in q:
-    if b[n - 1][m - 1]:
-        a.append(b[n - 1][m - 1])
+    if i == n - 1 and j == m - 1:
+        a = v[i][j][f] + 1
+        break
     for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
         if 0 <= x < n and 0 <= y < m:
-            if b[x][y] == 1 and not f:
-                b[x][y] = b[i][j] + 1
-                q += [(x, y, 1)]
-            if not b[x][y]:
-                b[x][y] = b[i][j] + 1
+            if b[x][y] == 0 and v[x][y][f] == 0:
+                v[x][y][f] = v[i][j][f] + 1
                 q += [(x, y, f)]
-print(min(a) if a else -1)
+            if b[x][y] == 1 and not f:
+                v[x][y][1] = v[i][j][f] + 1
+                q += [(x, y, 1)]
+
+print(a)
+
+# 풀이 참고 : https://tmdrl5779.tistory.com/15
