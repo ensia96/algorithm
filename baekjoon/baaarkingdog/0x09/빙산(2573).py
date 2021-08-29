ae65479f140e97fe4698d1d76bceb250b0aefdfd
춤추는망고 <ensia96@gmail.com>
2021-08-29 22:59:30 +0900

    [update] 빙산(2573) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/빙산(2573).py
+++ baekjoon/baaarkingdog/0x09/빙산(2573).py
@@ -3,30 +3,29 @@ import sys
 i = sys.stdin.readline
 l, r = lambda: map(int, i().split()), range
 n, m = l()
-b, g = [[*l()] for _ in r(n)], lambda x: r(1, x - 1)
-p, a, c = lambda: ((i, j) for j in g(m) for i in g(n)), 0, 1
+a, b, c = 0, [[*l()] for _ in r(n)], 1
 
 
 def s():
     v, o = [[0] * m for _ in r(n)], 0
-    for i, j in p():
-        if b[i][j] and not v[i][j]:
-            o += 1
-            v[i][j], q = 1, [(i, j)]
-            for u, w in q:
-                for x, y in [(u+1, w), (u-1, w), (u, w+1), (u, w-1)]:
-                    if b[x][y] and not v[x][y]:
-                        v[x][y] = 1
-                        q += [(x, y)]
+    for i in r(1, n - 1):
+        for j in r(1, m - 1):
+            if b[i][j] and not v[i][j]:
+                o += 1
+                v[i][j], q = 1, [(i, j)]
+                for u, w in q:
+                    for x, y in [(u+1, w), (u-1, w), (u, w+1), (u, w-1)]:
+                        if b[x][y] and not v[x][y]:
+                            v[x][y] = 1
+                            q += [(x, y)]
     return o
 
 
 def f():
-    for i, j in p():
-        if b[i][j]:
-            for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-                if b[i][j] and not b[x][y]:
-                    b[i][j] -= 1
+    for i, j in [(i, j) for i in r(n) for j in r(m) if not b[i][j]]:
+        for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+            if 0 <= x < n and 0 <= y < n and b[x][y]:
+                b[x][y] -= 1
 
 
 while 1:
