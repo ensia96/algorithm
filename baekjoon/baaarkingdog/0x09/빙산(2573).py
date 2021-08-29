47c78239ce61dec236b45395994c45e24a321407
춤추는망고 <ensia96@gmail.com>
2021-08-30 00:16:58 +0900

    [update] 빙산(2573) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x09/빙산(2573).py
+++ baekjoon/baaarkingdog/0x09/빙산(2573).py
@@ -3,27 +3,34 @@ import sys
 i = sys.stdin.readline
 l, r = lambda: map(int, i().split()), range
 n, m = l()
-a, b = 0, [[*l()] for _ in r(n)]
+b, c = [[*l()] for _ in r(n)], 0
 
-while 1:
-    v, o = [[0] * m for _ in r(n)], 0
+
+def f():
+    a, v = 0, [[0] * m for _ in r(n)]
     for i in r(1, n - 1):
         for j in r(1, m - 1):
-            if not o > 1 and b[i][j] and not v[i][j]:
-                o += 1
-                v[i][j], q = 1, [(i, j)]
-                for u, w in q:
-                    for x, y in [(u+1, w), (u-1, w), (u, w+1), (u, w-1)]:
-                        if b[x][y] and not v[x][y]:
-                            v[x][y] = 1
-                            q += [(x, y)]
-    if o != 1:
-        print(a if o else 0)
+            if a > 1:
+                return a
+            if b[i][j] and not v[i][j]:
+                a += 1
+                v[i][j] = 1
+                q = [(i, j)]
+                for e, f in q:
+                    for x, y in [(e+1, f), (e-1, f), (e, f+1), (e, f-1)]:
+                        if not v[x][y]:
+                            if b[x][y]:
+                                v[x][y] = 1
+                                q += [(x, y)]
+                            else:
+                                if b[e][f] > 0:
+                                    b[e][f] -= 1
+    return a
+
+
+while 1:
+    z = f()
+    if z != 1:
+        print(c if z > 1 else 0)
         break
-    a += 1
-    for i in r(n):
-        for j in r(m):
-            if not v[i][j] and not b[i][j]:
-                for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-                    if 0 <= x < n and 0 <= y < m and b[x][y] > 0:
-                        b[x][y] -= 1
+    c += 1
