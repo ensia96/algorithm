de56ffd1b24c0047bc24a296e30910d82eb2c9f2
춤추는망고 <ensia96@gmail.com>
2021-08-30 14:15:39 +0900

    [update] 불(5427) - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x09/불(5427).py
+++ baekjoon/baaarkingdog/0x09/불(5427).py
@@ -1,48 +1,39 @@
 import sys
 i, r = sys.stdin.readline, range
 
-
-def g(s, f):
-    a = []
-    f = e(f)
-    for p, q in s:
-        for x, y in [(p+1, q), (p-1, q), (p, q+1), (p, q-1)]:
-            if 0 <= x < h and 0 <= y < w:
-                if not v[x][y]:
-                    v[x][y] = 1
-                    m[x][y] = m[p][q] + 1
-                    a += [(x, y)]
-            else:
-                return m[p][q] + 1
-    if a:
-        return g(a, f)
-
-
-def e(f):
-    a = []
-    for p, q in f:
-        for x, y in [(p+1, q), (p-1, q), (p, q+1), (p, q-1)]:
-            if 0 <= x < h and 0 <= y < w and m[x][y] == '.':
-                v[x][y] = 1
-                a += [(x, y)]
-    return a
-
-
 for _ in r(int(i())):
     w, h = map(int, i().split())
-    m, v = [[*i().strip()] for _ in r(h)], [[0] * w for _ in r(h)]
-    f = []
+    m, v, f = [[*i().strip()] for _ in r(h)], [[0]*w for _ in r(h)], []
+    o = 'IMPOSSIBLE'
 
     for a in r(h):
         for b in r(w):
-            if m[a][b] == '@':
+            p = m[a][b]
+            if p == '@':
                 s = [(a, b)]
                 m[a][b] = 0
-            if m[a][b] == '*':
+            if p in '#*':
+                m[a][b] = 0
                 v[a][b] = 1
-                f += [(a, b)]
-            if m[a][b] == '#':
-                v[a][b] = 1
+                if p == '*':
+                    f += [(a, b)]
 
-    o = g(s, f)
-    print(o if o else 'IMPOSSIBLE')
+    for p, q in f:
+        for x, y in [(p+1, q), (p-1, q), (p, q+1), (p, q-1)]:
+            if 0 <= x < h and 0 <= y < w and m[x][y] == '.':
+                m[x][y] = m[p][q] + 1
+                v[x][y] = 1
+                f += [(x, y)]
+
+    for p, q in s:
+        for x, y in [(p+1, q), (p-1, q), (p, q+1), (p, q-1)]:
+            if 0 <= x < h and 0 <= y < w:
+                e, g = m[p][q], m[x][y]
+                if not v[x][y] and g == '.' or e + 1 < g:
+                    v[x][y], m[x][y] = 1, e + 1
+                    s += [(x, y)]
+                continue
+            o, s = m[p][q] + 1, []
+            break
+
+    print(o)
