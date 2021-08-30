410a6ad2783cf6ad88541cfab560d935c23a6865
춤추는망고 <ensia96@gmail.com>
2021-08-30 17:14:24 +0900

    [update] 스타트링크(5014) - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x09/스타트링크(5014).py
+++ baekjoon/baaarkingdog/0x09/스타트링크(5014).py
@@ -1,12 +1,13 @@
 f, s, g, u, d = map(int, input().split())
-a, l, b, q = 'use the stairs', [], [0] * (f + 1), [(s, 0)]
+a, b, q = 'use the stairs', [0] * (f + 1), [(s, 0)]
 
 for p, c in q:
     if p == g:
-        l.append(c)
+        a = c
+        break
     b[p] = 1
     for m in [p + u, p - d]:
         if 0 < m <= f and not b[m]:
             q += [(m, c + 1)]
 
-print(min(l) if l else a)
+print(a)
