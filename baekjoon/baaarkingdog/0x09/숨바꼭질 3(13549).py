3c27f4f2804feee8ac150ea02333756b8c27eade
춤추는망고 <ensia96@gmail.com>
2021-08-30 19:23:52 +0900

    [update] 숨바꼭질 3(13549) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x09/숨바꼭질 3(13549).py
+++ baekjoon/baaarkingdog/0x09/숨바꼭질 3(13549).py
@@ -1,13 +1,14 @@
 n, k = map(int, input().split())
 q, l = [n], [0] * 100002
+l[n] = 1
 
 for p in q:
     if l[k]:
-        print(l[k])
+        print(l[k] - 1)
         break
     for a in [p-1, p+1, p*2]:
         if 0 <= a < 100002 and not l[a]:
             l[a] = l[p]
-            if a == p*2:
+            if a != p*2:
                 l[a] += 1
             q += [a]
