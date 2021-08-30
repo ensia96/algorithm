7fd83df049d986b94ed2689fe0bf1ad1f2777d7e
춤추는망고 <ensia96@gmail.com>
2021-08-30 23:15:01 +0900

    [update] 말이 되고픈 원숭이(1600) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x09/말이 되고픈 원숭이(1600).py
+++ baekjoon/baaarkingdog/0x09/말이 되고픈 원숭이(1600).py
@@ -9,18 +9,16 @@ for a, b, t in q:
     if (a, b) == (h-1, w-1):
         e = v[a][b][t]
         break
-    for c, d in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+    for c, d in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
         x, y = a+c, b+d
         if 0 <= x < h and 0 <= y < w and not m[x][y] and not v[x][y][t]:
             v[x][y][t] = v[a][b][t] + 1
             q += [(x, y, t)]
     if t > 0:
-        for c, d in [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]:
+        for c, d in [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, 2), (1, -2), (2, 1), (2, -1)]:
             x, y = a+c, b+d
             if 0 <= x < h and 0 <= y < w and not m[x][y] and not v[x][y][t-1]:
                 v[x][y][t-1] = v[a][b][t] + 1
                 q += [(x, y, t-1)]
 
 print(e)
-
-# 풀이 참고 : https://pacific-ocean.tistory.com/393
