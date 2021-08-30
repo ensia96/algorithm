309d756997fb2ff9620eac36a7df52ef62b6fb14
춤추는망고 <ensia96@gmail.com>
2021-08-30 16:22:36 +0900

    [update] 다리 만들기(2146) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/다리 만들기(2146).py
+++ baekjoon/baaarkingdog/0x09/다리 만들기(2146).py
@@ -12,21 +12,21 @@ for c in r(n):
             for u, v in t:
                 for x, y in [(u+1, v), (u-1, v), (u, v+1), (u, v-1)]:
                     if 0 <= x < n and 0 <= y < n and not s[x][y]:
-                        s[x][y] = p
                         if b[x][y]:
+                            s[x][y] = p
                             t += [(x, y)]
                         else:
-                            b[x][y] = 1
-                            q += [(x, y, p)]
+                            q += [(u, v, p)]
 
 for c, d, p in q:
     if f:
         break
     s[c][d] = p
     for x, y in [(c+1, d), (c-1, d), (c, d+1), (c, d-1)]:
-        if 0 <= x < n and 0 <= y < n and s[c][d] != s[x][y]:
+        if 0 <= x < n and 0 <= y < n and p != s[x][y]:
             if b[x][y]:
-                f = b[c][d] + b[x][y]
+                f = b[c][d] + b[x][y] - 2
             else:
                 b[x][y], s[x][y] = b[c][d] + 1, p
+                q += [(x, y, p)]
 print(f)
