d3700e46cdcedbfc87d15df9b21b1c74b48c0727
춤추는망고 <ensia96@gmail.com>
2021-08-30 19:36:55 +0900

    [update] 숨바꼭질 3(13549) - 런타임 에러 (ValueError) 해결

--- baekjoon/baaarkingdog/0x09/숨바꼭질 3(13549).py
+++ baekjoon/baaarkingdog/0x09/숨바꼭질 3(13549).py
@@ -3,13 +3,13 @@ q, l = [n], [0] * 100002
 l[n], w = 1, []
 
 for p in q:
-    for a in [p-1, p+1, p*2]:
+    for a, t in [(p-1, 1), (p+1, 1), (p*2, 0)]:
         if 0 <= a < 100002 and not l[a]:
-            v = l[p]
-            if a != p*2:
-                v += 1
+            v = l[p] + t
             if a == k:
-                w.append(v)
-            l[a] = v
-            q += [a]
+                print(v - 1)
+                w += [v]
+            else:
+                l[a] = v
+                q += [a]
 print(min(w) - 1)
