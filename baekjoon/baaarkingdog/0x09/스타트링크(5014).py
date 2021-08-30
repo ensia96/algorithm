968f83cecf5780238e2c6ffb4950f2208362a66e
춤추는망고 <ensia96@gmail.com>
2021-08-30 17:15:44 +0900

    [update] 스타트링크(5014) - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x09/스타트링크(5014).py
+++ baekjoon/baaarkingdog/0x09/스타트링크(5014).py
@@ -1,7 +1,9 @@
+import collections as c
 f, s, g, u, d = map(int, input().split())
-a, b, q = 'use the stairs', [0] * (f + 1), [(s, 0)]
+a, b, q = 'use the stairs', [0] * (f + 1), c.deque([(s, 0)])
 
-for p, c in q:
+while q:
+    p, c = q.popleft()
     if p == g:
         a = c
         break
