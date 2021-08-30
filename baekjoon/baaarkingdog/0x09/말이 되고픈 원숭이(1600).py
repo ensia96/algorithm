8418786b02d4b600be727e29b738146a76d46f5f
춤추는망고 <ensia96@gmail.com>
2021-08-30 22:51:54 +0900

    [update] 말이 되고픈 원숭이(1600) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x09/말이 되고픈 원숭이(1600).py
+++ baekjoon/baaarkingdog/0x09/말이 되고픈 원숭이(1600).py
@@ -1,28 +1,21 @@
 import sys
-i = sys.stdin.readline
+i, r = sys.stdin.readline, range
 k, l = int(i()), lambda: [*map(int, i().split())]
 w, h = l()
-m, r, q = [l() for _ in range(h)], -1, [(0, 0, 0)]
-v = [[[0] * (k+1) for __ in range(w)] for _ in range(h)]
+m, q = [l() for _ in r(h)], [(0, 0, 0, 0)]
+v = [[300 for __ in r(w)] for _ in r(h)]
 
-for a, b, t in q:
-    if r != -1:
-        break
-    for c, d in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-        x, y = a + c, b + d
-        if 0 <= x < h and 0 <= y < w and not m[x][y] and not v[x][y][t]:
-            if (x, y) == (h-1, w-1):
-                r = v[a][b][t] + 1
-                break
-            v[x][y][t] = v[a][b][t] + 1
-            q += [(x, y, t)]
+for a, b, c, t in q:
+    for p, d in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        x, y = a+p, b+d
+        if 0 <= x < h and 0 <= y < w and not m[x][y] and c+1 < v[x][y]:
+            v[x][y] = c+1
+            q += [(x, y, c+1, t)]
     if t < k:
-        for c, d in [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]:
-            x, y = a + c, b + d
-            if 0 <= x < h and 0 <= y < w and not m[x][y] and not v[x][y][t]:
-                if (x, y) == (h-1, w-1):
-                    r = v[a][b][t] + 1
-                    break
-                v[x][y][t + 1] = v[a][b][t] + 1
-                q += [(x, y, t+1)]
-print(r)
+        for p, d in [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]:
+            x, y = a+p, b+d
+            if 0 <= x < h and 0 <= y < w and not m[x][y] and c+1 < v[x][y]:
+                v[x][y] = c+1
+                q += [(x, y, c+1, t+1)]
+a = v[w - 1][h - 1]
+print(a if a != 300 else -1)
