1a46afef56738c4e33dabea29174931c264f69d0
춤추는망고 <ensia96@gmail.com>
2021-08-30 20:18:12 +0900

    [update] 숨바꼭질 4(13913) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x09/숨바꼭질 4(13913).py
+++ baekjoon/baaarkingdog/0x09/숨바꼭질 4(13913).py
@@ -1,21 +1,21 @@
+import collections as c
 n, k = map(int, input().split())
 q, m = [(n, k, 0)], [0] * 100001
-m[n], l = (n, k, 0), []
+m[n], l = (n, k, 0), c.deque([k])
 
 for c, p, t in q:
     if m[k]:
         break
-    for x in [c*2, c+1, c-1]:
+    for x in [c-1, c*2, c+1]:
         if 0 <= x < 100001 and not m[x]:
             m[x] = (x, c, m[c][2] + 1)
             q += [(x, c, m[c][2] + 1)]
-
 c = k
 while 1:
     _, c, __ = m[c]
     if c == k:
         break
-    l = [c] + l
+    l.appendleft(c)
 
 print(m[k][2])
-print(*(l + [k]))
+print(*l)
