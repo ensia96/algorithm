9b7bd9085a9df3001ef548ebddd538226736c24f
춤추는망고 <ensia96@gmail.com>
2021-08-30 13:22:29 +0900

    [update] 불(5427) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/불(5427).py
+++ baekjoon/baaarkingdog/0x09/불(5427).py
@@ -1,10 +1,33 @@
 import sys
 i, r = sys.stdin.readline, range
 
+
+def g(s, f):
+    for p, q in s:
+        f = e(f)
+        for x, y in [(p+1, q), (p-1, q), (p, q+1), (p, q-1)]:
+            if 0 <= x < h and 0 <= y < w:
+                u, v = m[p][q], m[x][y]
+                if v == '.' or (v != '#' and u + 1 < v):
+                    m[x][y] = u + 1
+                    s += [(x, y)]
+            else:
+                return m[p][q] + 1
+
+
+def e(a):
+    for p, q in a:
+        for x, y in [(p+1, q), (p-1, q), (p, q+1), (p, q-1)]:
+            if 0 <= x < h and 0 <= y < w and m[x][y] == '.':
+                m[x][y] = m[p][q] + 1
+                a += [(x, y)]
+    return a
+
+
 for _ in r(int(i())):
     w, h = map(int, i().split())
     m = [[*i().strip()] for _ in r(h)]
-    f, o = [], 'IMPOSSIBLE'
+    f = []
 
     for a in r(h):
         for b in r(w):
@@ -15,21 +38,5 @@ for _ in r(int(i())):
                 m[a][b] = 0
                 f += [(a, b)]
 
-    for p, q in f:
-        for x, y in [(p+1, q), (p-1, q), (p, q+1), (p, q-1)]:
-            if 0 <= x < h and 0 <= y < w and m[x][y] == '.':
-                m[x][y] = m[p][q] + 1
-                f += [(x, y)]
-
-    for p, q in s:
-        for x, y in [(p+1, q), (p-1, q), (p, q+1), (p, q-1)]:
-            if 0 <= x < h and 0 <= y < w:
-                u, v = m[p][q], m[x][y]
-                if v == '.' or (v != '#' and u + 1 < v):
-                    m[x][y] = u + 1
-                    s += [(x, y)]
-            else:
-                o = m[p][q] + 1
-                s = []
-                break
-    print(o)
+    o = g(s, f)
+    print(o if o else 'IMPOSSIBLE')
