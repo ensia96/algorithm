fe5c2951ce0b938411eafeb41a503234031bea83
춤추는망고 <ensia96@gmail.com>
2021-08-30 19:28:54 +0900

    [update] 숨바꼭질 3(13549) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/숨바꼭질 3(13549).py
+++ baekjoon/baaarkingdog/0x09/숨바꼭질 3(13549).py
@@ -1,14 +1,15 @@
 n, k = map(int, input().split())
 q, l = [n], [0] * 100002
-l[n] = 1
+l[n], w = 1, []
 
 for p in q:
-    if l[k]:
-        print(l[k] - 1)
-        break
     for a in [p-1, p+1, p*2]:
         if 0 <= a < 100002 and not l[a]:
-            l[a] = l[p]
+            v = l[p]
             if a != p*2:
-                l[a] += 1
+                v += 1
+            if a == k:
+                w.append(v)
+            l[a] = v
             q += [a]
+print(min(w) - 1)
