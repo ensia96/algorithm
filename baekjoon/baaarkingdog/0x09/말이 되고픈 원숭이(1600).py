9c779d37a58618484d6ddb522bf2057d06014394
춤추는망고 <ensia96@gmail.com>
2021-08-30 22:27:25 +0900

    [update] 말이 되고픈 원숭이(1600) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x09/말이 되고픈 원숭이(1600).py
+++ baekjoon/baaarkingdog/0x09/말이 되고픈 원숭이(1600).py
@@ -6,19 +6,27 @@ m, r, q = [l() for _ in range(h)], -1, [(0, 0, 0)]
 v = [[[0] * (k+1) for __ in range(w)] for _ in range(h)]
 
 for a, b, t in q:
-    if any(v[h - 1][w - 1]):
-        r = min(filter(None, (v[h - 1][w - 1])))
+    print()
+    for z in v:
+        print(z)
+    print()
+    if r != -1:
         break
     for c, d in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
         x, y = a + c, b + d
         if 0 <= x < h and 0 <= y < w and not m[x][y] and not v[x][y][t]:
+            if (x, y) == (h-1, w-1):
+                r = v[a][b][t] + 1
+                break
             v[x][y][t] = v[a][b][t] + 1
             q += [(x, y, t)]
     if t < k:
         for c, d in [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]:
             x, y = a + c, b + d
             if 0 <= x < h and 0 <= y < w and not m[x][y] and not v[x][y][t]:
+                if (x, y) == (h-1, w-1):
+                    r = v[a][b][t] + 1
+                    break
                 v[x][y][t + 1] = v[a][b][t] + 1
                 q += [(x, y, t+1)]
-
 print(r)
