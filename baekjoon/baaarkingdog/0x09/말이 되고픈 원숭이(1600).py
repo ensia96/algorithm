250881c4b64e99caffa9f9ec5b8cc6d6a14a4740
춤추는망고 <ensia96@gmail.com>
2021-08-30 23:02:20 +0900

    [update] 말이 되고픈 원숭이(1600) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/말이 되고픈 원숭이(1600).py
+++ baekjoon/baaarkingdog/0x09/말이 되고픈 원숭이(1600).py
@@ -2,20 +2,25 @@ import sys
 i, r = sys.stdin.readline, range
 k, l = int(i()), lambda: [*map(int, i().split())]
 w, h = l()
-m, q = [l() for _ in r(h)], [(0, 0, 0, 0)]
-v = [[300 for __ in r(w)] for _ in r(h)]
+m, q = [l() for _ in r(h)], [(0, 0, k)]
+e, v = -1, [[[0 for ___ in r(k+1)] for __ in r(w)] for _ in r(h)]
 
-for a, b, c, t in q:
-    for p, d in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-        x, y = a+p, b+d
-        if 0 <= x < h and 0 <= y < w and not m[x][y] and c+1 < v[x][y]:
-            v[x][y] = c+1
-            q += [(x, y, c+1, t)]
-    if t < k:
-        for p, d in [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]:
-            x, y = a+p, b+d
-            if 0 <= x < h and 0 <= y < w and not m[x][y] and c+1 < v[x][y]:
-                v[x][y] = c+1
-                q += [(x, y, c+1, t+1)]
-a = v[w - 1][h - 1]
-print(a if a != 300 else -1)
+for a, b, t in q:
+    if (a, b) == (h-1, w-1):
+        e = v[a][b][t]
+        break
+    for c, d in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        x, y = a+c, b+d
+        if 0 <= x < h and 0 <= y < w and not m[x][y] and not v[x][y][t]:
+            v[x][y][t] = v[a][b][t] + 1
+            q += [(x, y, t)]
+    if t > 0:
+        for c, d in [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]:
+            x, y = a+c, b+d
+            if 0 <= x < h and 0 <= y < w and not m[x][y] and not v[x][y][t-1]:
+                v[x][y][t-1] = v[a][b][t] + 1
+                q += [(x, y, t-1)]
+
+print(e)
+
+# 풀이 참고 : https://pacific-ocean.tistory.com/393
