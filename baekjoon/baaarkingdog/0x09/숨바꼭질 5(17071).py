61b8822e7e3015bd283b08d636db74860ee31bdf
춤추는망고 <ensia96@gmail.com>
2021-08-31 20:18:11 +0900

    [update] 숨바꼭질 5 - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/숨바꼭질 5(17071).py
+++ baekjoon/baaarkingdog/0x09/숨바꼭질 5(17071).py
@@ -1,17 +1,14 @@
 n, k = map(int, input().split())
 m = int(5 * 10e4)
 l = [0] * (m + 1)
-q, l[n] = [n], 1
+q, l[n], r = [n], 1, -1
 
 for p in q:
-    v = k + sum(range(l[p]))
-    if v > m:
-        print(-1)
-        break
-    elif l[v]:
-        print(l[v] - 1)
+    if p == k + sum(range(l[p])):
+        r = l[p]
         break
     for a in [p-1, p+1, p*2]:
         if 0 < a <= m and not l[a]:
             l[a] = l[p] + 1
             q += [a]
+print(r)
