8380a7b5732d135436388ba3b8e9937e1f2fb23a
춤추는망고 <ensia96@gmail.com>
2021-08-31 16:58:03 +0900

    [update] 불켜기(11967) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/불켜기(11967).py
+++ baekjoon/baaarkingdog/0x09/불켜기(11967).py
@@ -1,8 +1,8 @@
 import sys
 i, r = sys.stdin.readline, range
-l, d, q = lambda: map(int, i().split()), {}, [(1, 1)]
+l, d = lambda: map(int, i().split()), {}
 n, m = l()
-g, v = [[0] * (n+1) for _ in r(n+1)], [[0] * (n+1) for _ in r(n+1)]
+g = [[0] * (n+1) for _ in r(n+1)]
 g[1][1] = 1
 
 for _ in r(m):
@@ -12,14 +12,24 @@ for _ in r(m):
         d[c] = []
     d[c].append((a, b))
 
-for m in q:
-    if m in d:
-        for x, y in d[m]:
-            g[x][y] = 1
-    a, b = m
-    for x, y in [(a+1, b), (a-1, b), (a, b+1), (a, b-1)]:
-        if 0 < x <= n and 0 < y <= n and g[x][y] and not v[x][y]:
-            q += [(x, y)]
-            v[x][y] = 1
+
+def c(s, f):
+    v = [[0] * (n+1) for _ in r(n+1)]
+    q = [s]
+    for m in q:
+        if m in d:
+            for x, y in d.pop(m):
+                g[x][y] = 1
+                c((x, y), (1, 1))
+        a, b = m
+        for x, y in [(a+1, b), (a-1, b), (a, b+1), (a, b-1)]:
+            if 0 < x <= n and 0 < y <= n and g[x][y] and not v[x][y]:
+                if (x, y) == f:
+                    return
+                q += [(x, y)]
+                v[x][y] = 1
+
+
+c((1, 1), None)
 
 print(sum(sum(g[i]) for i in r(n+1)))
