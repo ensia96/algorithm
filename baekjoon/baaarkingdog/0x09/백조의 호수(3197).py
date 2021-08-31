50de3f298f8a036f992f0374ccd03509d00787c4
춤추는망고 <ensia96@gmail.com>
2021-08-31 15:32:51 +0900

    [update] 백조의 호수(3197) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x09/백조의 호수(3197).py
+++ baekjoon/baaarkingdog/0x09/백조의 호수(3197).py
@@ -2,7 +2,7 @@ import sys
 i, g = sys.stdin.readline, range
 r, c = map(int, i().split())
 m = [[*i().strip()] for _ in g(r)]
-w, l, k = [], [], 1
+w, l, k, t = [], [], 1, 0
 
 for i in g(r):
     for j in g(c):
@@ -14,16 +14,8 @@ for i in g(r):
         else:
             m[i][j] = [1 if u == 'X' else 0, 0]
 
-
-def f(p):
-    for a, b in p:
-        for x, y in [(a+1, b), (a-1, b), (a, b+1), (a, b-1)]:
-            if 0 <= x < r and 0 <= y < c and m[x][y][0]:
-                m[x][y][0] = 0
-    return []
-
-
-def s(p, w, t):
+while l:
+    p = l
     l = []
     for a, b, k in p:
         for x, y in [(a+1, b), (a-1, b), (a, b+1), (a, b-1)]:
@@ -35,10 +27,12 @@ def s(p, w, t):
                     continue
                 if q != k:
                     if q:
-                        return t
+                        exit(print(t))
                     m[x][y][1] = k
                     p += [(x, y, k)]
-    return s(l, f(w), t + 1)
-
-
-print(s(l, [], 0))
+    for a, b in w:
+        for x, y in [(a+1, b), (a-1, b), (a, b+1), (a, b-1)]:
+            if 0 <= x < r and 0 <= y < c and m[x][y][0]:
+                m[x][y][0] = 0
+    w = []
+    t += 1
