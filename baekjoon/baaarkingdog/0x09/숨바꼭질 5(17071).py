7a9ceeb6ed300c854f664f56046fd2a2db74e0f0
춤추는망고 <ensia96@gmail.com>
2021-08-31 23:12:51 +0900

    [update] 숨바꼭질 5 - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/숨바꼭질 5(17071).py
+++ baekjoon/baaarkingdog/0x09/숨바꼭질 5(17071).py
@@ -1,18 +1,21 @@
-import collections as c
 n, k = map(int, input().split())
-m = int(5 * 10e4)
-l = [0] * (m + 1)
-q, r = c.deque([(n, 0)]), -1
+m = int(5e5)
+l = [[0, 0] for _ in range(m + 1)]
+d, r, c = [n], -1, 0
 
-while q:
-    p, t = q.popleft()
-    x = k + (t*(1+t)//2)
-    if p == x:
-        exit(print(t))
-    for a in [p-1, p+1, p*2]:
-        if a == x + t + 1:
-            exit(print(t + 1))
-        if 0 < a <= m and x + t + 1 <= m:
-            q.append((a, t+1))
+while 1:
+    q, x = d, k + (c*(1+c)//2)
+    d = []
+    if x > m:
+        exit(print(-1))
+    for p in q:
+        i = (c+1) % 2
+        if p == x or l[x][c % 2]:
+            exit(print(c))
+        for a in [p-1, p+1, p*2]:
+            if 0 <= a <= m and not l[a][i]:
+                d += [a]
+                l[a][i] = 1
+    c += 1
 
-print(r)
+# 풀이 참고 : https://ijsilver.tistory.com/30
