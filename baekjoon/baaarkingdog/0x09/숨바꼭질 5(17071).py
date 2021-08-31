3a06543838ddfc6dc3cd53f92cfc70e04e833b2f
춤추는망고 <ensia96@gmail.com>
2021-08-31 21:22:56 +0900

    [update] 숨바꼭질 5 - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x09/숨바꼭질 5(17071).py
+++ baekjoon/baaarkingdog/0x09/숨바꼭질 5(17071).py
@@ -1,9 +1,11 @@
+import collections as c
 n, k = map(int, input().split())
 m = int(5 * 10e4)
 l = [0] * (m + 1)
-q, r = [(n, 0)], -1
+q, r = c.deque([(n, 0)]), -1
 
-for p, t in q:
+while q:
+    p, t = q.popleft()
     x = k + (t*(1+t)//2)
     if p == x:
         exit(print(t))
