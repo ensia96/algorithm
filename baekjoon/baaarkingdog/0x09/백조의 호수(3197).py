2c8e5e7797baa815206524b175e2dd664927f0b6
춤추는망고 <ensia96@gmail.com>
2021-08-31 14:47:28 +0900

    [update] 백조의 호수(3197) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x09/백조의 호수(3197).py
+++ baekjoon/baaarkingdog/0x09/백조의 호수(3197).py
@@ -16,11 +16,15 @@ for i in g(r):
 
 
 def f(p):
+    v = [[0] * c for _ in g(r)]
     n = []
     for a, b in p:
+        if v[a][b]:
+            continue
         for x, y in [(a + 1, b), (a-1, b), (a, b+1), (a, b-1)]:
             if 0 <= x < r and 0 <= y < c and m[x][y] == 'X':
                 m[x][y] = '.'
+                v[x][y] = 1
                 n += [(x, y)]
     return n
 
