b5625e972437a21f66952b28cb745697d1ac0c14
춤추는망고 <ensia96@gmail.com>
2021-08-31 19:32:32 +0900

    [update] 불켜기(11967) - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x09/불켜기(11967).py
+++ baekjoon/baaarkingdog/0x09/불켜기(11967).py
@@ -1,35 +1,29 @@
 import sys
-sys.setrecursionlimit(10**6)
+import collections as c
 i, r = sys.stdin.readline, range
-l, d = lambda: map(int, i().split()), {}
+l, d, q = lambda: map(int, i().split()), c.defaultdict(list), c.deque([(1, 1)])
 n, m = l()
 g, v = [[0] * (n+1) for _ in r(n+1)], [[0] * (n+1) for _ in r(n+1)]
-g[1][1] = 1
 
 for _ in r(m):
     x, y, a, b = l()
-    c = x, y
-    if c not in d:
-        d[c] = []
+    c = (x, y)
     d[c].append((a, b))
 
+g[1][1], v[1][1], c = 1, 1, 1
 
-def c(s, f):
-    q = [s]
-    for m in q:
-        if m in d:
-            for x, y in d.pop(m):
-                g[x][y] = 1
-                c((x, y), (1, 1))
-        a, b = m
-        for x, y in [(a+1, b), (a-1, b), (a, b+1), (a, b-1)]:
-            if 0 < x <= n and 0 < y <= n and g[x][y] and not v[x][y]:
-                if (x, y) == f:
-                    return
-                q += [(x, y)]
-                v[x][y] = 1
+while q:
+    a, b = q.popleft()
+    for x, y in d[(a, b)]:
+        if not g[x][y]:
+            g[x][y], c = 1, c+1
+            for i, j in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]:
+                if 0 < i <= n and 0 < j <= n and v[i][j]:
+                    q.append((i, j))
+    for x, y in [(a+1, b), (a-1, b), (a, b+1), (a, b-1)]:
+        if 0 < x <= n and 0 < y <= n and g[x][y] and not v[x][y]:
+            q.append((x, y))
+            v[x][y] = 1
+print(c)
 
-
-c((1, 1), None)
-
-print(sum(sum(g[i]) for i in r(n+1)))
+# 풀이 참고 : https://chelseashin.tistory.com/91
