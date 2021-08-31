54d0a4d69b8e32cba4d79f07beabb68b418f78c1
춤추는망고 <ensia96@gmail.com>
2021-08-31 15:36:12 +0900

    [update] 백조의 호수(3197) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x09/백조의 호수(3197).py
+++ baekjoon/baaarkingdog/0x09/백조의 호수(3197).py
@@ -2,37 +2,43 @@ import sys
 i, g = sys.stdin.readline, range
 r, c = map(int, i().split())
 m = [[*i().strip()] for _ in g(r)]
-w, l, k, t = [], [], 1, 0
+w, l, p, d = [], [], ['A', 'B'], 'AB'
 
 for i in g(r):
     for j in g(c):
-        u = m[i][j]
-        if u == 'L':
-            l += [(i, j, k)]
-            m[i][j] = [1, k]
-            k += 1
-        else:
-            m[i][j] = [1 if u == 'X' else 0, 0]
+        if m[i][j] == 'L':
+            u = p.pop()
+            l += [(i, j, u)]
+            w += [(i, j)]
+            m[i][j] = u
+        if m[i][j] == '.':
+            w += [(i, j)]
 
-while l:
-    p = l
-    l = []
+
+def f(p):
+    n = []
+    for a, b in p:
+        for x, y in [(a + 1, b), (a-1, b), (a, b+1), (a, b-1)]:
+            if 0 <= x < r and 0 <= y < c and m[x][y] == 'X':
+                m[x][y] = '.'
+                n += [(x, y)]
+    return n
+
+
+def s(p, w, t):
+    v = [[0] * c for _ in g(r)]
     for a, b, k in p:
-        for x, y in [(a+1, b), (a-1, b), (a, b+1), (a, b-1)]:
-            if 0 <= x < r and 0 <= y < c:
-                d, q = m[x][y]
-                if d:
-                    w += [(a, b)]
-                    l += [(a, b, k)]
-                    continue
-                if q != k:
-                    if q:
-                        exit(print(t))
-                    m[x][y][1] = k
+        for x, y in [(a + 1, b), (a-1, b), (a, b+1), (a, b-1)]:
+            if 0 <= x < r and 0 <= y < c and not v[x][y]:
+                u = m[x][y]
+                if u != k and u in d:
+                    return t
+                if u == '.':
                     p += [(x, y, k)]
-    for a, b in w:
-        for x, y in [(a+1, b), (a-1, b), (a, b+1), (a, b-1)]:
-            if 0 <= x < r and 0 <= y < c and m[x][y][0]:
-                m[x][y][0] = 0
-    w = []
-    t += 1
+                    m[x][y] = k
+                if u == 'X':
+                    w += [(a, b)]
+    return s(l, f(w), t + 1)
+
+
+print(s(l, w, 0))
