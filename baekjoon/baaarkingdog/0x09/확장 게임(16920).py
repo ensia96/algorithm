57027f4407cde4b9090a772cab7b5fea795ac3e1
춤추는망고 <ensia96@gmail.com>
2021-09-01 11:55:45 +0900

    [update] 확장 게임(16920) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x09/확장 게임(16920).py
+++ baekjoon/baaarkingdog/0x09/확장 게임(16920).py
@@ -8,8 +8,9 @@ l = [[] for _ in r(p)]
 c = [l[int(b[i][j]) - 1].append((int(b[i][j]) - 1, i, j))
      for i in r(n) for j in r(m) if b[i][j] in '123456789']
 c, l = [len(l[i]) for i in r(p)], d(d(q) for q in l)
+a = sum(w.count('.') for w in b)
 
-while l:
+while l and a:
     q, w = l.popleft(), t.popleft()
     e = w
     while w:
@@ -18,7 +19,7 @@ while l:
             p, i, j = q.popleft()
             for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
                 if 0 <= x < n and 0 <= y < m and b[x][y] == '.':
-                    c[p], b[x][y] = c[p] + 1, p
+                    c[p], b[x][y], a = c[p] + 1, p, a-1
                     v += [(p, x, y)]
         q = d(v)
     if q:
