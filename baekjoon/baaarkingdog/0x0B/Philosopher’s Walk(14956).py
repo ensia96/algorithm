45cfd1fd24f94c97d6fd57cf40d01025a788d324
춤추는망고 <ensia96@gmail.com>
2021-09-02 17:25:55 +0900

    [update] Philosopher’s Walk(14956) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x0B/Philosopher’s Walk(14956).py
+++ baekjoon/baaarkingdog/0x0B/Philosopher’s Walk(14956).py
@@ -3,13 +3,13 @@ n, m = map(int, input().split())
 
 def s(n, m):
     if n == 2:
-        return (1, 1) if m == 0 else (1, 2) if m == 1 else (2, 2) if m == 2 else (2, 1)
-    k = int(n/2)
-    p = int(m / (k*k))
-    a, b = s(n >> 1, m % (k*k))
-    return (b, a) if p == 0 else (a, b+k) if p == 1 else (a+k, b+k) if p == 2 else (k-b+1+k, k-a+1)
+        return ((1, 1), (1, 2), (2, 2), (2, 1))[m]
+    k = n//2
+    p = m // (k*k)
+    a, b = s(n//2, m % (k*k))
+    return ((b, a), (a, b+k), (a+k, b+k), (k-b+1+k, k-a+1))[p]
 
 
-print(*s(n, m))
+print(*s(n, m-1))
 
 # 풀이 참고 : https://www.programmerall.com/article/9390577247/
