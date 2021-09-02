20985c185018c7613e055443b96ac67b93d1c4e2
춤추는망고 <ensia96@gmail.com>
2021-09-02 20:35:06 +0900

    N과 M (6)(15655)

---
+++ baekjoon/baaarkingdog/0x0C/N과 M (6)(15655).py
@@ -0,0 +1,13 @@
+l, _ = lambda: map(int, input().split()), 0
+
+
+def b(n, m, l, s=[]):
+    if len(s) == m:
+        print(*map(str, s))
+        return
+    for i in l:
+        if not s or s[-1] < i:
+            b(n, m, l, s+[i])
+
+
+b(*l(), sorted(l()))
