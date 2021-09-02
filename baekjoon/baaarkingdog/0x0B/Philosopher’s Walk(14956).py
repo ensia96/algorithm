599cbe9e8d5f7a28d5fd80bf0d40a49469a9c2d6
춤추는망고 <ensia96@gmail.com>
2021-09-02 17:19:10 +0900

    [update] Philosopher’s Walk(14956) - 런타임 에러 (IndexError) 해결

--- baekjoon/baaarkingdog/0x0B/Philosopher’s Walk(14956).py
+++ baekjoon/baaarkingdog/0x0B/Philosopher’s Walk(14956).py
@@ -3,11 +3,11 @@ n, m = map(int, input().split())
 
 def s(n, m):
     if n == 2:
-        return ((1, 1), (1, 2), (2, 2), (2, 1))[m-1]
-    k = n//2
-    p = m // (k*k)
-    a, b = s(n//2, m % (k*k))
-    return [(b, a), (a, b+k), (a+k, b+k), (k-b+1+k, k-a+1)][p]
+        return (1, 1) if m == 0 else (1, 2) if m == 1 else (2, 2) if m == 2 else (2, 1)
+    k = n/2
+    p = m / (k*k)
+    a, b = s(n >> 1, m % (k*k))
+    return (b, a) if p == 0 else (a, b+k) if p == 1 else (a+k, b+k) if p == 2 else (k-b+1+k, k-a+1)
 
 
 print(*s(n, m))
