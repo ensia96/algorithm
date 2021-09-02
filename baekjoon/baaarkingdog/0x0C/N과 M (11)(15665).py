c982d799a4a12a46680e8228f981d35fca042277
춤추는망고 <ensia96@gmail.com>
2021-09-02 22:34:24 +0900

    N과 M (11)(15665)

---
+++ baekjoon/baaarkingdog/0x0C/N과 M (11)(15665).py
@@ -0,0 +1,19 @@
+l, c = lambda: map(int, input().split()), 0
+n, m = l()
+l = sorted(l())
+s, d = [0] * m,  {}
+
+
+def b(c):
+    if c == m:
+        t = ' '.join(map(str, s))
+        if t not in d:
+            d[t] = 0
+        return
+    for i in range(n):
+        s[c] = l[i]
+        b(c+1)
+
+
+b(c)
+print('\n'.join(d))
