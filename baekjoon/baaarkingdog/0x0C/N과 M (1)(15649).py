bb530d6858bd52be588c627cdad0d4d180eb7f58
춤추는망고 <ensia96@gmail.com>
2021-09-02 19:57:47 +0900

    [update] N과 M (1)(15649) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0C/N과 M (1)(15649).py
+++ baekjoon/baaarkingdog/0x0C/N과 M (1)(15649).py
@@ -2,7 +2,7 @@ def b(n, m, l=[]):
     if len(l) == m:
         print(*map(str, l))
     for i in range(1, n+1):
-        if i not in l and len(l) <= m:
+        if i not in l:
             b(n, m, l + [i])
 
 
