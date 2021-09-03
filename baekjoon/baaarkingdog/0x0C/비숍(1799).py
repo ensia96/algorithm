66f3d093751da993b8eef4c7612f32b59bfea0da
춤추는망고 <ensia96@gmail.com>
2021-09-03 18:35:50 +0900

    [update] 비숍(1799) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x0C/비숍(1799).py
+++ baekjoon/baaarkingdog/0x0C/비숍(1799).py
@@ -1,24 +1,25 @@
-import sys
-i, r = sys.stdin.readline, range
+i, a = input, [0]*2
 n = int(i())
-m = [[*map(int, i().split())] for _ in r(n)]
-s = [(i, j) for i in r(n) for j in r(n) if m[i][j]]
-l = len(s)
-a, v = set(), [0] * l
+m = [[*map(int, i().split())] for _ in range(n)]
 e, f = [0] * (2*n-1), [0] * (2*n-1)
 
 
-def b(c, p):
-    if p == l-1:
-        a.add(c)
+def t(x, y, c, o):
+    if y >= n:
+        x, y = x+1, y % 2 == 0
+    if x >= n:
+        a[o] = max(a[o], c)
         return
-    for i in r(p, l):
-        x, y = s[i]
-        if not v[i] and m[x][y] and not e[x+y] and not f[x-y+n-1]:
-            v[i], m[x][y], e[x+y], f[x-y+n-1] = 1, 0, 1, 1
-            b(c+1, i)
-            v[i], m[x][y], e[x+y], f[x-y+n-1] = 0, 1, 0, 0
+    if m[x][y] and not e[x+y] and not f[y-x+n-1]:
+        e[x+y] = f[y-x+n-1] = 1
+        t(x, y+2, c+1, o)
+        e[x+y] = f[y-x+n-1] = 0
+    t(x, y+2, c, o)
 
 
-b(0, 0)
-print(max(a))
+t(0, 0, 0, 0)
+t(0, 1, 0, 1)
+
+print(sum(a))
+
+# 풀이 참고 : https://j2wooooo.tistory.com/80
