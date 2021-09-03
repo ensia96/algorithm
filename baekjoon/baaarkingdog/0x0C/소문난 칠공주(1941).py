c5ea11f518e559402656861ffaae38011829c13a
춤추는망고 <ensia96@gmail.com>
2021-09-03 23:39:44 +0900

    [update] 소문난 칠공주(1941) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0C/소문난 칠공주(1941).py
+++ baekjoon/baaarkingdog/0x0C/소문난 칠공주(1941).py
@@ -1,30 +1,20 @@
-m = [input().strip() for _ in range(5)]
-w, v, p = 0, [[0]*5 for _ in range(5)], []
+import itertools as t
+r = range
+m = [input().strip() for _ in r(5)]
 
 
 def c(s):
-    x, y = s % 5, s//5
-    for a, b in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]:
-        if 0 <= a < 5 and 0 <= b < 5 and not v[b][a] and (b*5+a) in p:
-            v[b][a] = 1
-            c((b*5+a))
+    l = [(i % 5, i//5) for i in s]
+    if sum(m[y][x] == 'S' for x, y in l) < 4:
+        return 0
+    v, q = [0] * 7, [l[0]]
+    for x, y in q:
+        for i in r(7):
+            a, b = l[i]
+            if not v[i] and abs(a - x) + abs(b - y) == 1:
+                v[i] = 1
+                q += [l[i]]
+    return all(v)
 
 
-def d(l, i, n):
-    global w, v, p
-    if n >= 4 or 25-i < 7-l:
-        return
-    if l == 7:
-        c(p[0])
-        w += sum(sum(v, [])) == 7
-        v = [[0]*5 for _ in range(5)]
-        return
-    x, y = i % 5, i // 5
-    p += [i]
-    d(l+1, i+1, n+1 if m[y][x] == 'Y' else n)
-    p.pop()
-    d(l, i+1, n)
-
-
-d(0, 0, 0)
-print(w)
+print(sum(c(k) for k in t.combinations(r(25), 7)))
