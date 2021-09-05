142ed7fba9e4a7619c8e9fce5665d3e3208aafcd
춤추는망고 <ensia96@gmail.com>
2021-09-05 19:27:15 +0900

    [update] 2048 (Easy)(12100) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0D/2048 (Easy)(12100).py
+++ baekjoon/baaarkingdog/0x0D/2048 (Easy)(12100).py
@@ -1,26 +1,26 @@
-i, g = input, range
-n = int(i())
-m = [[*map(int, i().split())] for _ in g(n)]
+n, g = int(input()), range
+m = [[*map(int, input().split())] for _ in g(n)]
 r, a = lambda m: [[m[n-1-j][i] for j in g(n)] for i in g(n)], 0
 
 
 def c(l):
-    a = [i for i in l if i]
+    a = [*filter(None, l)]
     for i in g(len(a)-1):
         if a[i] == a[i+1]:
             a[i], a[i+1] = a[i]*2, 0
-    a = [i for i in a if i]
+    a = [*filter(None, a)]
     return a+[0]*(len(l)-len(a))
 
 
 def e(t, m):
     global a
     if t == 0:
-        return max(map(max, m))
+        a = max(a, max(map(max, m)))
+        return
     for z in g(4):
-        a = max(a, e(t-1, [c(l) for l in m]))
+        e(t-1, [c(l) for l in m])
         m = r(m)
-    return a
 
 
-print(e(5, m))
+e(5, m)
+print(a)
