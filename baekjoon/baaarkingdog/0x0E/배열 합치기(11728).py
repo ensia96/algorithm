5fdf8795b3cfb5f1eefd132857f709a134d223b7
춤추는망고 <ensia96@gmail.com>
2021-09-05 22:03:15 +0900

    [update] 배열 합치기(11728) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0E/배열 합치기(11728).py
+++ baekjoon/baaarkingdog/0x0E/배열 합치기(11728).py
@@ -1,13 +1,3 @@
-l, c = lambda: map(int, input().split()), []
-n, m = l()
-a, b, i, j = [*l()], [*l()], 0, 0
-
-while i < n and j < m:
-    if j == m or a[i] <= b[j]:
-        c += [a[i]]
-        i += 1
-    else:
-        c += [b[j]]
-        j += 1
-
-print(*(c+a[i:]+b[j:]))
+l, _ = lambda: map(int, input().split()), 0
+_ = l()
+print(*sorted([*l(), *l()]))
