afcfc4aadd039a636cf5fd4d54158d5b8f098b12
춤추는망고 <ensia96@gmail.com>
2021-09-05 14:40:45 +0900

    [update] 스티커 붙이기(18808) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x0D/스티커 붙이기(18808).py
+++ baekjoon/baaarkingdog/0x0D/스티커 붙이기(18808).py
@@ -4,35 +4,32 @@ i = sys.stdin.readline
 l, g = lambda: map(int, i().split()), range
 n, m, k = l()
 b = [[0]*m for _ in g(n)]
-s, t = [], []
 
 
-def h(f, r, c):
-    a = [[] for _ in g(4)]
-    q = [a[q].append([(i, j), (j, r-1-i), (r-1-i, c-1-j), (c-1-j, i)][q])
-         for q in g(4) for i in g(r) for j in g(c) if f[i][j]]
-    return a
+def t(r, c):
+    a = [[0] * r for _ in g(c)]
+    for u in g(c):
+        for v in g(r):
+            a[u][v] = p[r-1-v][u]
+    return c, r, a
 
 
-def u(f, e, d, r, c):
-    for i in g(4):
-        if all(0 <= y+e < n and 0 <= x+d < m and not b[y+e][x+d] for y, x in f[i]):
-            for y, x in f[i]:
-                b[y+e][x+d] = 1
-            return 1
-    if not(d or e):
-        r = max(n, m)
-        for i in g(r):
-            for j in g(r):
-                if i+j > 0:
-                    if u(f, e+i, d+j, r, c):
-                        return
+def e(x, y):
+    if not any(b[x+u][y+v] and p[u][v] for u in g(r) for v in g(c)):
+        for u in g(r):
+            for v in g(c):
+                if p[u][v]:
+                    b[x+u][y+v] = 1
+        return 1
 
 
-for _ in g(k):
+while k:
+    k -= 1
     r, c = l()
-    s, t = s+[(r, c)], t + [[[*l()] for __ in g(r)]]
+    p = [[*l()] for __ in g(r)]
+    for _ in g(4):
+        if any(e(x, y) for x in g(n-r+1) for y in g(m-c+1)):
+            break
+        r, c, p = t(r, c)
 
-t = [h(t[i], *s[i]) for i in g(k)]
-t = [u(t[p], 0, 0, *s[p]) for p in g(k)]
 print(sum(sum(l) for l in b))
