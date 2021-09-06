5f6c74cf6c70aa51397138d39fb0a92e67b9bb27
춤추는망고 <ensia96@gmail.com>
2021-09-06 23:24:47 +0900

    [update] 먹을 것인가 먹힐 것인가(7795) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x0F/먹을 것인가 먹힐 것인가(7795).py
+++ baekjoon/baaarkingdog/0x0F/먹을 것인가 먹힐 것인가(7795).py
@@ -3,5 +3,10 @@ i = sys.stdin.readline
 l, _ = lambda: [*map(int, i().split())], 0
 
 for _ in range(int(i())):
-    _, a, b = i(), l(), l()
-    print(sum(i > j for i in a for j in b))
+    i()
+    a, b = {}, {}
+    for k in l():
+        a[k] = a.get(k, 0)+1
+    for k in l():
+        b[k] = b.get(k, 0)+1
+    print(sum(a[i] * b[j] for i in a for j in b if i > j))
