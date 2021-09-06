561fd3586396569b0b2977c07727b0ab846362e2
춤추는망고 <ensia96@gmail.com>
2021-09-06 22:44:35 +0900

    [update] 역원소 정렬(5648) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0F/역원소 정렬(5648).py
+++ baekjoon/baaarkingdog/0x0F/역원소 정렬(5648).py
@@ -1,5 +1,3 @@
-import sys
-_, *n = input().split()
-for l in sys.stdin:
-    n += l.split()
-print(*sorted(map(int, [i[::-1] for i in n])))
+n = []
+_ = [n.append(l.split()) for l in open(0)]
+print(*sorted(map(int, [i[::-1] for i in n[1:]])))
