b39b74de769e74576e3f3997784fc68e21546f06
춤추는망고 <ensia96@gmail.com>
2021-09-06 16:36:07 +0900

    [update] 다른 사람이 구현한 병합 정렬 코드 추가

--- baekjoon/baaarkingdog/0x0E/merge_sort.py
+++ baekjoon/baaarkingdog/0x0E/merge_sort.py
@@ -23,3 +23,37 @@ def merge_sort(l):
 
 print(merge_sort([3, 4, 1, 2, 3, 45, 1, 2, 3, 45, 7, 9, 6, 1]))
 print(sorted([3, 4, 1, 2, 3, 45, 1, 2, 3, 45, 7, 9, 6, 1]))
+
+# 다른 사람의 풀이를 보다가 인상 깊어서 가져온 코드
+# 출처 : https://www.acmicpc.net/source/29188108
+
+
+def merge_sort(arr):
+    def sort(start, end):
+        if end - start < 2:
+            return arr
+        mid = (start + end) // 2
+        sort(start, mid)
+        sort(mid, end)
+        return merge(start, mid, end)
+
+    def merge(start, mid, end):
+        temp = []
+        a = start
+        b = mid
+        while a < mid and b < end:
+            if arr[a] < arr[b]:
+                temp.append(arr[a])
+                a += 1
+            else:
+                temp.append(arr[b])
+                b += 1
+        while a < mid:
+            temp.append(arr[a])
+            a += 1
+        while b < end:
+            temp.append(arr[b])
+            b += 1
+        for i in range(start, end):
+            arr[i] = temp[i - start]
+    return sort(0, len(arr))
