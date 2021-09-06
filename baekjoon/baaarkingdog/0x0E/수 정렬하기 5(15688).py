5ace782b46b378f6d7a38061f87dc4710f447d2b
춤추는망고 <ensia96@gmail.com>
2021-09-06 16:24:05 +0900

    [update] 수 정렬하기 5(15688) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x0E/수 정렬하기 5(15688).py
+++ baekjoon/baaarkingdog/0x0E/수 정렬하기 5(15688).py
@@ -1,10 +1,1 @@
-import sys
-i, m = sys.stdin.readline, 10**6+1
-l = [0]*m
-
-for _ in range(int(i())):
-    l[int(i())] += 1
-
-for i in range(m):
-    for _ in range(l[i]):
-        sys.stdout.write(f'{i}\n')
+print(*sorted([*map(int, [*open(0)][1:])]))
