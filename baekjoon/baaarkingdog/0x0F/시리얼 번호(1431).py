e6fb2d700dca2e6a2fcf11e04e53370fd4f66e65
춤추는망고 <ensia96@gmail.com>
2021-09-06 21:16:09 +0900

    [update] 시리얼 번호(1431) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0F/시리얼 번호(1431).py
+++ baekjoon/baaarkingdog/0x0F/시리얼 번호(1431).py
@@ -1,2 +1,2 @@
-print('\n'.join(sorted([input() for _ in range(int(input()))], key=lambda x: (
-    len(x), sum(int(c) for c in x if c.isdigit()), x))))
+print(*sorted([input() for _ in range(int(input()))],
+      key=lambda x: (len(x), sum(int(c) for c in x if c.isdigit()), x)))
