ed01977aef99aa919b51b9213287dd196902aa0a
춤추는망고 <ensia96@gmail.com>
2021-09-07 22:55:40 +0900

    [update] 구간 합 구하기 4(11659) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x10/구간 합 구하기 4(11659).py
+++ baekjoon/baaarkingdog/0x10/구간 합 구하기 4(11659).py
@@ -1,6 +1,4 @@
 import sys
 d, l, a = lambda: map(int, sys.stdin.readline().split()), [0], 0
 n, m, *_ = [*d()] + [exec('a += i;l += [a]') for i in d()]
-for _ in range(m):
-    i, j = d()
-    print(l[j]-l[i-1])
+exec('i,j=d();print(l[j]-l[i-1]);'*m)
