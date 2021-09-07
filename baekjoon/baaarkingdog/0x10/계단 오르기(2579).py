7edebe221be190a52682bbb5a4731a2947945841
춤추는망고 <ensia96@gmail.com>
2021-09-07 19:34:32 +0900

    [update] 계단 오르기(2579) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x10/계단 오르기(2579).py
+++ baekjoon/baaarkingdog/0x10/계단 오르기(2579).py
@@ -1,7 +1,7 @@
 n = int(input())
-s = [int(input()) for _ in range(n)]
+s = [0]+[int(input()) for _ in range(n)]
 if n < 3:
     exit(print(sum(s)))
-d = s[:3]
-_ = [d.append(min(d[i-2], d[i-3])+s[i]) for i in range(3, n)]
+d = s[:5]
+_ = [d.append(min(d[i-2], d[i-3])+s[i]) for i in range(4, n)]
 print(sum(s)-min(d[-1], d[-2]))
