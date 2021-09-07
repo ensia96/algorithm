dd69e80a0c75f2b93c8efb5e2672a63468dde796
춤추는망고 <ensia96@gmail.com>
2021-09-07 22:44:40 +0900

    [update] 구간 합 구하기 4(11659) - 컴파일 에러 해결

--- baekjoon/baaarkingdog/0x10/구간 합 구하기 4(11659).py
+++ baekjoon/baaarkingdog/0x10/구간 합 구하기 4(11659).py
@@ -1,6 +1,6 @@
 import sys
-d, l, a = lambda: [*map(int, sys.stdin.readline().split())], [0], 0
-n, m, *_ = d() + [l.append(a := a+i) for i in d()]
+d, l, a = lambda: map(int, sys.stdin.readline().split()), [0], 0
+n, m, *_ = [*d()] + [exec('a += i;l += [a]') for i in d()]
 for _ in range(m):
     i, j = d()
     print(l[j]-l[i-1])
