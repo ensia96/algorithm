1a6108598d949efc035d2df81531a6d828589e75
춤추는망고 <ensia96@gmail.com>
2021-09-07 16:07:23 +0900

    [update] 1, 2, 3 더하기(9095) - 런타임 에러 (TypeError) 해결

--- baekjoon/baaarkingdog/0x10/1, 2, 3 더하기(9095).py
+++ baekjoon/baaarkingdog/0x10/1, 2, 3 더하기(9095).py
@@ -2,6 +2,6 @@ i, t, g = input, int, range
 for _ in g(t(i())):
     n = t(i())
     d = [0, 1, 2, 4] + [0]*n
-    for i in g(4, n+1):
-        d[i] = d[i-1]+d[i-2]+d[i-3]
+    for _ in g(4, n+1):
+        d[_] = d[_-1]+d[_-2]+d[_-3]
     print(d[n])
