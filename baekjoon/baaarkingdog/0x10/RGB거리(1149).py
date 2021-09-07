3c12d4c2ae2ad20f2697288e460466b3d38e06cc
춤추는망고 <ensia96@gmail.com>
2021-09-07 21:35:56 +0900

    [update] RGB거리(1149) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x10/RGB거리(1149).py
+++ baekjoon/baaarkingdog/0x10/RGB거리(1149).py
@@ -1,5 +1,5 @@
 x = y = z = 0
 for _ in range(int(input())):
     r, g, b = map(int, input().split())
-    x, y, z = r+min(y, z), g+min(x, z)+y, b+min(x, y)
+    x, y, z = r+min(y, z), g+min(x, z), b+min(x, y)
 print(min(x, y, z))
