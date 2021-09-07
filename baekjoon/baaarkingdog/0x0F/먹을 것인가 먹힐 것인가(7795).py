5fa0448c4978c580710391c440aefff740f2f1a9
춤추는망고 <ensia96@gmail.com>
2021-09-07 10:20:17 +0900

    [update] 먹을 것인가 먹힐 것인가(7795) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0F/먹을 것인가 먹힐 것인가(7795).py
+++ baekjoon/baaarkingdog/0x0F/먹을 것인가 먹힐 것인가(7795).py
@@ -1,21 +1,7 @@
-import sys
-i = sys.stdin.readline
-l, _ = lambda: sorted([*map(int, i().split())]), 0
+import bisect as b
+i = input
+l, c = lambda: sorted([*map(int, i().split())]), b.bisect_left
 
 for _ in range(int(i())):
-    i()
-    a, b = l(), l()
-    _ = p = q = 0
-    v, w = len(a), len(b)
-    while 1:
-        q = d = 0
-        c = p
-        if v == p or w == q:
-            break
-        while q < w and b[q] < a[p]:
-            q += 1
-            d += 1
-        while p < v and a[c] == a[p]:
-            p += 1
-        _ += (p-c)*d
-    print(_)
+    _, a, b = i(), l(), l()
+    print(sum(c(b, i) for i in a))
