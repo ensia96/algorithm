671476fa7f52b390934301d632d7fe6bf675a273
춤추는망고 <ensia96@gmail.com>
2021-09-07 19:56:10 +0900

    [update] 계단 오르기(2579) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x10/계단 오르기(2579).py
+++ baekjoon/baaarkingdog/0x10/계단 오르기(2579).py
@@ -1,12 +1,7 @@
 n = int(input())
-s, d = [0]*305, [0]*305
-for i in range(1, n+1):
-    s[i] = int(input())
+s = [0] + [int(input()) for _ in range(n)]
 if n <= 2:
     exit(print(sum(s)))
-d[1] = s[1]
-d[2] = s[2]
-d[3] = s[3]
-for i in range(4, n):
-    d[i] = min(d[i-2], d[i-3])+s[i]
+d = s[:4]
+_ = [d.append(min(d[i-2], d[i-3])+s[i]) for i in range(4, n)]
 print(sum(s)-min(d[n-1], d[n-2]))
