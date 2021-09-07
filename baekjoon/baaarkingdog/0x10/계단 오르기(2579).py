e9bea6378a8fd09dbccf77a958ff4de8ad48fbdc
춤추는망고 <ensia96@gmail.com>
2021-09-07 17:27:44 +0900

    계단 오르기(2579)

---
+++ baekjoon/baaarkingdog/0x10/계단 오르기(2579).py
@@ -0,0 +1,5 @@
+n = int(input())
+s = [int(input()) for _ in range(n)]
+d = [(s[0], 0), (s[1], s[0]+s[1])]
+_ = [d.append((max(d[i-2])+s[i], d[i-1][0]+s[i])) for i in range(2, n)]
+print(max(d[n-1]))
