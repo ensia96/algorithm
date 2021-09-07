aaf192b7ba8c8fd12976bd16e1d9bb6cafb9416b
춤추는망고 <ensia96@gmail.com>
2021-09-07 11:58:34 +0900

    [update] 국영수(10825) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0F/국영수(10825).py
+++ baekjoon/baaarkingdog/0x0F/국영수(10825).py
@@ -1,3 +1,3 @@
 i, t = input, int
-print(*map(lambda x: x[0], sorted([i().split() for _ in range(t(i()))],
-      key=lambda x: (-t(x[1]), t(x[2]), -t(x[3]), x[0]))))
+print(*[t for*_, t in sorted((-t(b), t(c), -t(d), a)
+      for a, b, c, d in eval('i().split(),'*t(i())))])
