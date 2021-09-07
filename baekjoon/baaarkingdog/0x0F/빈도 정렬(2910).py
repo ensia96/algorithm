f029b9201888a0e665e1bb6404b2ada4a02aa9fa
춤추는망고 <ensia96@gmail.com>
2021-09-07 11:27:10 +0900

    [update] 빈도 정렬(2910) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0F/빈도 정렬(2910).py
+++ baekjoon/baaarkingdog/0x0F/빈도 정렬(2910).py
@@ -1,12 +1,3 @@
-import sys
-i = sys.stdin.readline
-x, d = lambda: map(int, i().split()), {}
-n, c = x()
-l = [*x()]
-
-for i in range(n):
-    d[l[i]] = d.get(l[i], [0, n])
-    d[l[i]][0] += 1
-    d[l[i]][1] = min(d[l[i]][1], i)
-print(*[k for k in sorted(d, key=lambda x: (-d[x][0], d[x][1]))
-      for _ in range(d[k][0])])
+i = input
+_, l = i(), i().split()
+print(*sorted(l, key=lambda x: (-l.count(x), l.index(x))))
