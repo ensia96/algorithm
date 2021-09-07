0b3e4287ff20d6fce91e6ff182efacc83c0d6b45
춤추는망고 <ensia96@gmail.com>
2021-09-07 17:32:33 +0900

    [update] 계단 오르기(2579) - 런타임 에러 (IndexError) 해결

--- baekjoon/baaarkingdog/0x10/계단 오르기(2579).py
+++ baekjoon/baaarkingdog/0x10/계단 오르기(2579).py
@@ -1,5 +1,7 @@
 n = int(input())
 s = [int(input()) for _ in range(n)]
+if n == 1:
+    exit(print(s.pop()))
 d = [(s[0], 0), (s[1], s[0]+s[1])]
 _ = [d.append((max(d[i-2])+s[i], d[i-1][0]+s[i])) for i in range(2, n)]
 print(max(d[n-1]))
