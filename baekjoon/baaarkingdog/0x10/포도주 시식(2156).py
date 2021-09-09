775e3bac463ada9eafbca43c6ac4e9c57e7d4f0d
춤추는망고 <ensia96@gmail.com>
2021-09-09 22:30:01 +0900

    [update] 포도주 시식(2156) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x10/포도주 시식(2156).py
+++ baekjoon/baaarkingdog/0x10/포도주 시식(2156).py
@@ -2,9 +2,9 @@ n = int(input())
 w = [int(input()) for _ in range(n)]
 if n < 3:
     exit(print(sum(w)))
-d = [0, w[0], w[0]+w[1], max(w[0]+w[1], w[1]+w[2], w[2]+w[0])]
+d = [w[0], w[0]+w[1], max(w[0]+w[1], w[1]+w[2], w[2]+w[0])]
 _ = [d.append(max(d[i-3]+w[i-1]+w[i], d[i-2]+w[i], d[i-1]))
      for i in range(3, n)]
-print(d[n])
+print(d[-1])
 
 # 도움 받은 글 : https://www.acmicpc.net/board/view/73217
