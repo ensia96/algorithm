0b4057e7c09fb9425eab361a0e965991ab967814
춤추는망고 <ensia96@gmail.com>
2021-09-10 11:32:09 +0900

    [update] 가장 긴 증가하는 부분 수열(11053) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x10/가장 긴 증가하는 부분 수열(11053).py
+++ baekjoon/baaarkingdog/0x10/가장 긴 증가하는 부분 수열(11053).py
@@ -1,6 +1,9 @@
 n = int(input())
 a = [*map(int, input().split())]
-d = [(a[0], 1)]
-_ = [d.append((max(d[i-1][0], a[i]), d[i-1][1]+(d[i-1][0] < a[i])))
-     for i in range(1, n)]
-print(d[-1][1])
+d = [[0]*n for _ in range(n)]
+for i in range(n):
+    d[i][i], v = 1, a[i]
+    for j in range(i+1, n):
+        b = v < a[j]
+        v, d[i][j] = [v, a[j]][b], d[i][j-1]+b
+print(max(map(max, d)))
