7b9847b36c8f09f70cbb54790e1971e1f9751ac0
춤추는망고 <ensia96@gmail.com>
2021-09-10 15:06:00 +0900

    [update] 쉬운 계단 수(10844) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x10/쉬운 계단 수(10844).py
+++ baekjoon/baaarkingdog/0x10/쉬운 계단 수(10844).py
@@ -1,5 +1,9 @@
 n = int(input())
-d = [[0]+[1]*9]+[[] for _ in range(n-1)]
-_ = [d[i].append((d[i-1][j-1] + d[i-1][j+1] if 0 < j < 9 else d[i-1]
-                 [[8, 1][not j]]) % 10**9) for i in range(1, n) for j in range(10)]
-print(sum(d[n-1]))
+d = [[0]+[1]*9]+[[0]*10 for _ in range(n-1)]
+for i in range(1, n):
+    for j in range(10):
+        if 0 < j < 9:
+            d[i][j] = d[i-1][j-1] + d[i-1][j+1]
+        else:
+            d[i][j] = d[i-1][[8, 1][not j]]
+print(sum(d[-1]) % 10**9)
