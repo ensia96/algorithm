d67c1c6207b32c911443b4d4e7eb3256c494ffb4
춤추는망고 <ensia96@gmail.com>
2021-09-10 18:15:19 +0900

    [update] 퇴사(14501) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x10/퇴사(14501).py
+++ baekjoon/baaarkingdog/0x10/퇴사(14501).py
@@ -1,8 +1,8 @@
 n = int(input())
 d = [[0]*(n+1) for _ in range(n)]
+
 for i in range(n):
     t, p = map(int, input().split())
-    if i+t <= n:
-        for j in range(i+t, n+1):
-            d[i][j] = p+d[i-1][i]
+    for j in range(n+1):
+        d[i][j] = [max(d[i-1][i] + p, d[i-1][j]), d[i-1][j]][i+t > j]
 print(max(map(max, d)))
