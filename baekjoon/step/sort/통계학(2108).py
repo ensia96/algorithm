e3cf3e0cb09610116766ee3607c86dd9c39c9a7e
춤추는망고 <ensia96@gmail.com>
2021-09-11 22:07:59 +0900

    [update] 통계학(2108) - 숏코딩에서 본 코드 추가

--- baekjoon/step/sort/통계학(2108).py
+++ baekjoon/step/sort/통계학(2108).py
@@ -7,3 +7,8 @@ print(round(sum(b)/n))
 print(b[len(b)//2])
 print(a[1][0] if len(a) > 1 and a[0][1] == a[1][1] else a[0][0])
 print(max(b)-min(b))
+
+# 숏코딩에서 statistics 모듈 사용한 코드를 봄
+# from statistics import*
+# n,*l=map(int,open(0))
+# print('%.0f'%mean(l),median(l),sorted(multimode(l))[:2][-1],max(l)-min(l))
