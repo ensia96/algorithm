3aeceb6bd856a8239a0b902a6c7af4f8aac110cc
춤추는망고 <ensia96@gmail.com>
2021-09-11 22:06:38 +0900

    [update] 통계학(2108) - 틀렸습니다 해결

--- baekjoon/step/sort/통계학(2108).py
+++ baekjoon/step/sort/통계학(2108).py
@@ -3,7 +3,7 @@ n = int(input())
 b = sorted([int(input()) for _ in range(n)])
 a = c.Counter(b).most_common()
 
-print(sum(b)//n)
+print(round(sum(b)/n))
 print(b[len(b)//2])
 print(a[1][0] if len(a) > 1 and a[0][1] == a[1][1] else a[0][0])
 print(max(b)-min(b))
