8230a1717ea657be9c845f4e92e81e87243664f6
춤추는망고 <ensia96@gmail.com>
2021-09-11 22:00:01 +0900

    [update] 통계학(2108) - 틀렸습니다 해결

--- baekjoon/step/sort/통계학(2108).py
+++ baekjoon/step/sort/통계학(2108).py
@@ -4,6 +4,6 @@ b = sorted([int(input()) for _ in range(n)])
 a = c.Counter(b).most_common()
 
 print(sum(b)//n)
-print(b[n//2])
+print(b[len(b)//2])
 print(a[1][0] if len(a) > 1 and a[0][1] == a[1][1] else a[0][0])
 print(max(b)-min(b))
