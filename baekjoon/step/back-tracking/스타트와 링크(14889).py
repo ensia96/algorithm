a0c2472c35aad5fbfb25d526a3bef2da12eb9c1f
춤추는망고 <ensia96@gmail.com>
2021-09-15 12:02:03 +0900

    [update] 스타트와 링크(14889) - 시간 초과 해결

--- baekjoon/step/back-tracking/스타트와 링크(14889).py
+++ baekjoon/step/back-tracking/스타트와 링크(14889).py
@@ -1,13 +1,3 @@
-# from itertools import combinations as c
-# n = int(input())
-# s = [[*map(int, input().split())] for _ in range(n)]
-# b, r = map(set, c(range(n), n//2)), {*range(n)}
-
-# print(min(abs(sum(s[i][j] for i in c for j in c) -
-# abs(sum(s[i][j] for i in r-c for j in r-c))) for c in b))
-
-########################################################################
-
 n = int(input())
 s = [[*map(int, input().split())] for _ in range(n)]
 g = {*range(n)}
@@ -16,14 +6,14 @@ l, v = lambda x: sum(s[i][j] for i in x for j in x), [1]*n
 
 def f(a, c):
     if c == n:
-        return abs(l(g-a)-l(a))
+        return abs(l(g-set(a))-l(a))
     r = 10**8
     for i in range(n):
         if v[i]:
             v[i] = 0
-            r = min(r, f(a.union({i}), c+2))
+            r = min(r, f(a+[i], c+2))
             v[i] = 1
     return r
 
 
-print(f(set(), 0))
+print(f([], 0))
