da6d246f3a14de080eb71bdfb0f81d4373c71768
춤추는망고 <ensia96@gmail.com>
2021-09-15 15:42:38 +0900

    [update] 스타트와 링크(14889) - 시간 초과 해결

--- baekjoon/step/back-tracking/스타트와 링크(14889).py
+++ baekjoon/step/back-tracking/스타트와 링크(14889).py
@@ -4,16 +4,16 @@ g = {*range(n)}
 l, v = lambda x: sum(s[i][j] for i in x for j in x), [1]*n
 
 
-def f(a, c):
+def f(a, i, c):
     if c == n:
         return abs(l(g-set(a))-l(a))
     r = 10**8
-    for i in range(n):
-        if v[i]:
-            v[i] = 0
-            r = min(r, f(a+[i], c+2))
-            v[i] = 1
+    for d in range(i, n):
+        if v[d]:
+            v[d] = 0
+            r = min(r, f(a+[d], d, c+2))
+            v[d] = 1
     return r
 
 
-print(f([], 0))
+print(f([], 0, 0))
