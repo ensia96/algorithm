3b78577d129c65c86298b3595e6e8ee5d8965336
춤추는망고 <ensia96@gmail.com>
2021-09-15 11:54:14 +0900

    [update] 스타트와 링크(14889) - 백트래킹 풀이 추가

--- baekjoon/step/back-tracking/.스타트와 링크(14889).py@neomake_1804_209.py
+++ baekjoon/step/back-tracking/.스타트와 링크(14889).py@neomake_1804_209.py
@@ -1,13 +1,3 @@
-# from itertools import combinations as c
-# n = int(input())
-# s = [[*map(int, input().split())] for _ in range(n)]
-# b, r = map(set, c(range(n), n//2)), {*range(n)}
-
-# print(min(abs(sum(s[i][j] for i in c for j in c) -
-# abs(sum(s[i][j] for i in r-c for j in r-c))) for c in b))
-
-########################################################################
-
 n = int(input())
 s = [[*map(int, input().split())] for _ in range(n)]
 g = {*range(n)}
