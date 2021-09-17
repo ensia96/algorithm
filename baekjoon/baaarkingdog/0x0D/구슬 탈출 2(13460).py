05f8e45c65dd49557879ff452bbedb85b5a3d654
춤추는망고 <ensia96@gmail.com>
2021-09-17 18:25:32 +0900

    [update] 구슬 탈출 2(13460) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x0D/구슬 탈출 2(13460).py
+++ baekjoon/baaarkingdog/0x0D/구슬 탈출 2(13460).py
@@ -1,62 +1,48 @@
 n, m = map(int, input().split())
-a = [[*input()] for _ in range(n)]
-_, u = lambda i, j, d: [(i+1, j), (i-1, j), (i, j+1), (i, j-1)][d], 11
+dx, dy, s, g = [1, -1, 0, 0], [0, 0, -1, 1], [], range
+v = [[[[0] * m for i in g(n)] for i in g(m)] for i in g(n)]
+
+for i in g(n):
+    a = [*input()]
+    s += [a]
+    for j in g(m):
+        if a[j] == "R":
+            ri, rj = i, j
+        if a[j] == "B":
+            bi, bj = i, j
+
+q = [(ri, rj, bi, bj, 1)]
+v[ri][rj][bi][bj] = 1
 
 
-for i in range(n):
-    for j in range(m):
-        if a[i][j] == 'R':
-            r = (i, j, 0)
-        if a[i][j] == 'B':
-            b = (i, j, 1)
+def e(i, j, x, y):
+    c = 0
+    while s[i + x][j + y] != "#" and s[i][j] != "O":
+        i += x
+        j += y
+        c += 1
+    return i, j, c
 
 
-def check(i, j):
-    v = []
-    for d in range(4):
-        y, x = _(i, j, d)
-        if a[y][x] != '#':
-            v += [d]
-    return v
+for ri, rj, bi, bj, d in q:
+    if d > 10:
+        break
+    for i in g(4):
+        nri, nrj, rc = e(ri, rj, dx[i], dy[i])
+        nbi, nbj, bc = e(bi, bj, dx[i], dy[i])
+        if s[nbi][nbj] != "O":
+            if s[nri][nrj] == "O":
+                exit(print(d))
+            if (nri, nrj) == (nbi, nbj):
+                if rc > bc:
+                    nri -= dx[i]
+                    nrj -= dy[i]
+                else:
+                    nbi -= dx[i]
+                    nbj -= dy[i]
+            if not v[nri][nrj][nbi][nbj]:
+                v[nri][nrj][nbi][nbj] = 1
+                q += [(nri, nrj, nbi, nbj, d + 1)]
+print(-1)
 
-
-def move(y, x, l, d):
-    q = [(y, x)]
-    for i, j in q:
-        v, h = _(i, j, d)
-        if a[v][h] == '.':
-            q += [(v, h)]
-        elif a[v][h] == 'O':
-            a[y][x] = '.'
-            return v, h
-        elif a[v][h] == '#':
-            a[y][x] = '.'
-            a[i][j] = 'RB'[l]
-            return i, j
-        else:
-            return i, j
-
-
-def t(r, b, c):
-    global a, u
-    if u <= c:
-        return
-    for d in range(4):
-        i, j = move(*r, d)
-        h, k = move(*b, d)
-        if (i, j) == (r[0], r[1]):
-            i, j = move(*r, d)
-        if a[h][k] == 'O':
-            a[b[0]][b[1]] = 'B'
-            return
-        if a[i][j] == 'O':
-            a[r[0]][r[1]] = 'R'
-            u = c
-            return
-        t((i, j, 0), (h, k, 1), c+1)
-        a[i][j] = a[h][k] = '.'
-        a[r[0]][r[1]], a[b[0]][b[1]] = 'R', 'B'
-
-
-t(r, b, 1)
-print(u if u != 11 else -1)
+# 풀이 참고 : https://pacific-ocean.tistory.com/346
