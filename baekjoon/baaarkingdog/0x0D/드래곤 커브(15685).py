8662a37edf7d0d9c7dd040094c53688c7ca8824d
춤추는망고 <ensia96@gmail.com>
2021-09-23 15:32:20 +0900

    [update] 드래곤 커브(15685) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0D/드래곤 커브(15685).py
+++ baekjoon/baaarkingdog/0x0D/드래곤 커브(15685).py
@@ -1,22 +1,12 @@
-a = [[*map(int, input().split())]for _ in range(int(input()))]
-b, r = [[0]*101 for _ in range(101)], [(1, 0), (0, -1), (-1, 0), (0, 1)]
-
-
-def f(x, y, c, g, q):
-    if c > g:
-        return
-    Q = [*map(lambda x: (x+1) % 4, q[::-1])]
-    for p in Q:
+a, r = range, ((1, 0), (0, -1), (-1, 0), (0, 1))
+b = [[0]*101 for _ in a(101)]
+for _ in a(int(input())):
+    x, y, d, g = map(int, input().split())
+    b[y][x], q = 1, [d]
+    for _ in a(g):
+        q += [(i+1) % 4 for i in q[::-1]]
+    for p in q:
         x, y = x+r[p][0], y+r[p][1]
         b[y][x] = 1
-    f(x, y, c+1, g, q+Q)
-
-
-for x, y, d, g in a:
-    b[y][x] = 1
-    x, y = x+r[d][0], y+r[d][1]
-    b[y][x] = 1
-    f(x, y, 1, g, [d])
-
-print(sum(all(b[i+y][j+x] for y in range(2) for x in range(2))
-      for i in range(100) for j in range(100) if b[i][j]))
+print(sum(all(b[i+y][j+x] for y in a(2) for x in a(2))
+      for i in a(100) for j in a(100)))
