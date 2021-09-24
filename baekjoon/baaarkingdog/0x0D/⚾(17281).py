c73a473462cc8351393c46c87ea73d8c64aa5ebb
춤추는망고 <ensia96@gmail.com>
2021-09-24 15:52:14 +0900

    [update] ⚾(17281) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x0D/⚾(17281).py
+++ baekjoon/baaarkingdog/0x0D/⚾(17281).py
@@ -1,4 +1,3 @@
-import collections as l
 import itertools as t
 import sys
 i, g = sys.stdin.readline, range
@@ -7,17 +6,18 @@ a, u = 0, [*t.permutations(g(1, 9))]
 for m in u:
     i, p, e = 0, 0, m[:3]+(0,)+m[3:]
     for t in n:
-        b, o = l.deque([0]*3), 0
+        x = y = z = o = 0
         while o < 3:
-            c = t[e[i]]
-            if c:
-                p, c = p+b.popleft(), c-1
-                b.append(1)
-                while c:
-                    p, c = p+b.popleft(), c-1
-                    b.append(0)
-            else:
+            if not t[e[i]]:
                 o += 1
+            elif t[e[i]] == 1:
+                p, x, y, z = p+z, 1, x, y
+            elif t[e[i]] == 2:
+                p, x, y, z = p+y+z, 0, 1, x
+            elif t[e[i]] == 3:
+                p, x, y, z = p+x+y+z, 0, 0, 1
+            else:
+                p, x, y, z = p+x+y+z+1, 0, 0, 0
             i = (i+1) % 9
     a = max(a, p)
 print(a)
