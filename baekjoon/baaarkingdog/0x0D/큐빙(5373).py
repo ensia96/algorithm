035690ff02ac861302c7599456eaca2cdf5cc4bf
춤추는망고 <ensia96@gmail.com>
2021-09-25 18:06:17 +0900

    [update] 큐빙(5373) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0D/큐빙(5373).py
+++ baekjoon/baaarkingdog/0x0D/큐빙(5373).py
@@ -5,11 +5,9 @@ for _ in range(int(input())):
         t, x, y, z, w = [(u, l, f, r, b), (d, b, r, f, l), (f, u, l, d, r),
                          (b, r, d, l, u), (l, f, u, b, d), (r, d, b, u, f)]['UDFBLR'.find(i)]
         for _ in range([3, 1][j == '+']):
-            t[2], t[5], t[8], t[7], t[6], t[3], t[0], t[1] = \
+            t[2], t[5], t[8], t[7], t[6], t[3], t[0], t[1] =\
                 t[0], t[1], t[2], t[5], t[8], t[7], t[6], t[3]
-            x[8], x[7], x[6], y[6], y[3], y[0], z[2], z[5], z[8], w[0], w[1], w[2] = \
+            x[8], x[7], x[6], y[6], y[3], y[0], z[2], z[5], z[8], w[0], w[1], w[2] =\
                 y[6], y[3], y[0], z[2], z[5], z[8], w[0], w[1], w[2], x[8], x[7], x[6]
     for i in range(3):
         print(''.join(c[0][i*3:(i+1)*3]))
-
-# 풀이 참고 : https://conak-diary.tistory.com/99
