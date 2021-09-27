8debb2a4d011ca937214f0af7aa229b092d18bf1
춤추는망고 <ensia96@gmail.com>
2021-09-27 14:37:14 +0900

    [update] 나무 재테크(16235) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x0D/나무 재테크(16235).py
+++ baekjoon/baaarkingdog/0x0D/나무 재테크(16235).py
@@ -1,30 +1,27 @@
 import sys
-import collections as c
+from collections import deque
 l, r = lambda: map(int, sys.stdin.readline().split()), range
 n, m, k = l()
-i, g = [[*l()]for _ in r(n)], [[5]*n for _ in r(n)]
-t = {(p, q): c.deque()for p in r(n) for q in r(n)}
-for z, y, x in sorted([[*l()][::-1] for _ in r(m)]):
-    t[(y-1, x-1)].append(z)
-while k:
-    for y, x in t:
-        dt, u = 0, len(t[(y, x)])
+i, g, t = [[*l()]for _ in r(n)], [5]*n*n, [deque()for _ in r(n*n)]
+for x, y, z in sorted([[*l()]for _ in r(m)]):
+    t[(x-1)*n+y-1].append(z)
+for _ in r(k):
+    for p in r(n*n):
+        dt, u = 0, len(t[p])
         while u:
-            a, u = t[(y, x)].popleft(), u-1
-            if g[y][x] >= a:
-                g[y][x] -= a
-                t[(y, x)].append(a+1)
-            else:
+            a, u = t[p].popleft(), u-1
+            if g[p] < a:
                 dt += a//2
-        g[y][x] += dt
-    for y, x in [*t]:
-        for a in t[(y, x)]:
+            else:
+                g[p] -= a
+                t[p].append(a+1)
+        g[p] += dt
+    for z in r(n*n):
+        for a in t[z]:
             if not a % 5:
                 for p, q in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
-                    if 0 <= y+p < n and 0 <= x+q < n:
-                        t[(y+p, x+q)].appendleft(1)
-    for y in r(n):
-        for x in r(n):
-            g[y][x] += i[y][x]
-    k -= 1
-print(sum(map(lambda x: len(t[x]), t)))
+                    if 0 <= z//n+p < n and 0 <= z % n+q < n:
+                        t[(z//n+p)*n+z % n+q].appendleft(1)
+    for p in r(n*n):
+        g[p] += i[p//n][p % n]
+print(sum(len(t[p])for p in r(n*n)))
