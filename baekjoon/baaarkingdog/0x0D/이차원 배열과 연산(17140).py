f8f693c7c509f4faae22f6b01481723bee6f2106
춤추는망고 <ensia96@gmail.com>
2021-09-29 18:05:59 +0900

    [update] 이차원 배열과 연산(17140) - 런타임 에러 (IndexError) 해결

--- baekjoon/baaarkingdog/0x0D/이차원 배열과 연산(17140).py
+++ baekjoon/baaarkingdog/0x0D/이차원 배열과 연산(17140).py
@@ -7,23 +7,23 @@ while t <= 100:
         exit(print(t))
     if v < h:
         for i in g(h):
-            m = sorted(C.Counter([*filter(None, [b[j][i] for j in range(v)])]
-                                 ).most_common(), key=lambda x: (x[1], x[0]))[:50]
-            p = len(m)
-            m += [(0, 0)]*(50-len(m))
-            for j in g(49):
-                b[2*j][i], b[2*j+1][i] = m[j][0], m[j][1]
-            b[98][i] = m[49][0]
-            v = min(max(v, p*2), 99)
+            m = C.Counter([*filter(None, [b[j][i]
+                          for j in range(v)])]).most_common()
+            m.sort(key=lambda x: x[::-1])
+            l = len(m)
+            m = [p[a] for p in m for a in g(2)][:99] + [0]*(99-l)
+            for j in g(99):
+                b[j][i] = m[j]
+            v = min(max(v, l*2), 99)
     else:
         for i in g(v):
-            m = sorted(C.Counter([*filter(None, [b[i][j] for j in range(h)])]
-                                 ).most_common(), key=lambda x: (x[1], x[0]))[:50]
-            p = len(m)
-            m += [(0, 0)]*(50-len(m))
-            for j in g(49):
-                b[i][2*j], b[i][2*j+1] = m[j][0], m[j][1]
-            b[i][98] = m[49][0]
-            h = min(max(h, p*2), 99)
+            m = C.Counter([*filter(None, [b[i][j]
+                          for j in range(h)])]).most_common()
+            m.sort(key=lambda x: x[::-1])
+            l = len(m)
+            m = [p[a] for p in m for a in g(2)][:99] + [0]*(99-l)
+            for j in g(99):
+                b[i][j] = m[j]
+            h = min(max(h, l*2), 99)
     t += 1
 print(-1)
