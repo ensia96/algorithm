7dca1f3d509cbc7bf6347d27860512482acdd7bd
춤추는망고 <ensia96@gmail.com>
2021-09-29 18:53:19 +0900

    [update] 이차원 배열과 연산(17140) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0D/이차원 배열과 연산(17140).py
+++ baekjoon/baaarkingdog/0x0D/이차원 배열과 연산(17140).py
@@ -6,15 +6,13 @@ for t in g(101):
     v, h = len(b), len(b[0])
     if r <= v and c <= h and b[r-1][c-1] == k:
         exit(print(t))
-    f = v < h
-    b = [b, [*zip(*b)]][f]
-    l = len(b)
+    b, l = [b, [*zip(*b)]][v < h], max(v, h)
     for j in g(l):
         a, b[j] = C.Counter([*filter(None, b[j])]).most_common(), []
         a.sort(key=lambda x: x[::-1])
         for x, y in a:
             b[j] += [x, y]
     m = max(map(len, b))
-    b = [(b[j]+[0]*(m-len(b[j])))[:100] for j in g(l)]
-    b = [b, [*zip(*b)]][f]
+    b = [(b[j]+[0]*(m-len(b[j])))[:100]for j in g(l)]
+    b = [b, [*zip(*b)]][v < h]
 print(-1)
