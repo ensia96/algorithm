848cb9ab826bdf38c79a4e16b5153b25244b1fb6
춤추는망고 <ensia96@gmail.com>
2021-10-04 16:22:39 +0900

    [update] 로봇 청소기(4991) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x0D/로봇 청소기(4991).py
+++ baekjoon/baaarkingdog/0x0D/로봇 청소기(4991).py
@@ -1,27 +1,34 @@
-from itertools import permutations
+import itertools as e
 u, g = input, range
+
+
+def k(p, i):
+    q, v, o = [(*l[p], 0)], [0]*w*h, l[i]
+    for r, c, d in q:
+        for nr, nc in [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]:
+            if (nr, nc) == o:
+                t[p][i] = t[i][p] = d+1
+                return
+            if 0 <= nr < h and 0 <= nc < w and not v[nr*w+nc] and b[nr][nc] != 'x':
+                v[nr*w+nc] = 1
+                q += [(nr, nc, d+1)]
+
+
 while 1:
     w, h = map(int, u().split())
     if not w+h:
         break
-    a, b = 10**9, [[*u().strip()]for _ in g(h)]
-    l = [(p//w, p % w)for p in g(h*w)if b[p//w][p % w] in 'o*']
-    s = len(l)
-    t = [[-1]*s for _ in g(s)]
-    for p in g(s):
-        for i in g(p, s):
-            q, v, o = [(*l[p], -1)], [0]*w*h, l[i]
-            for r, c, d in q:
-                for nr, nc in [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]:
-                    if (nr, nc) == o:
-                        t[p][i] = t[i][p] = d
-                    if 0 <= nr < h and 0 <= nc < w and not v[nr*w+nc] and b[nr][nc] != 'x':
-                        v[nr*w+nc] = 1
-                        q += [(nr, nc, d+1)]
-    n = permutations(g(1, s), s-1)
-    for p in n:
+    a, b, t, l = 10**9, [[*u().strip()]for _ in g(h)], 0, []
+    for p in g(h*w):
+        i, j = p//w, p % w
+        t = [t, [0, (i, j)][b[i][j] == 'o']][not t]
+        l = [l, l+[(i, j)]][b[i][j] == '*']
+    l, s = [t]+l, len(l)+1
+    t = [[0]*s for _ in g(s)]
+    _ = [k(p, i)for p, i in ((x, y)for x in g(s)for y in g(x+1, s))]
+    for p in e.permutations(g(1, s), s-1):
         l = r = 0
         for i in p:
             l, r = l+t[r][i], i
         a = min(a, l)
-    print(a)
+    print(a if a else -1)
