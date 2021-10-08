7561bf53fbcabd8efdfe6c3c9be6279851886c85
춤추는망고 <ensia96@gmail.com>
2021-10-08 14:03:09 +0900

    [update] 새로운 게임 2(17837) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x0D/새로운 게임 2(17837).py
+++ baekjoon/baaarkingdog/0x0D/새로운 게임 2(17837).py
@@ -5,21 +5,19 @@ e, o = lambda y, x: (0 <= y < n)*(0 <= x < n)-1 or b[y][x] == 2, []
 for i in r(k):
     y, x, d = l()
     m[y-1][x-1] += [i]
-    o += [(y-1, x-1, d-1)]
+    o += [[y-1, x-1, d-1]]
 for t in r(1000):
     for i in r(k):
         y, x, d = o[i]
         c, v = m[y][x], [(y, x+1), (y, x-1), (y-1, x), (y+1, x)]
-        h = c.index(i)
-        u, ny, nx = c[h:], *v[d]
-        d = [d, (d//2)*2+[1, 0][d % 2]][e(ny, nx)]
-        ny, nx, o[i] = *v[d], (y, x, d)
-        if e(ny, nx):
+        d = [d, (d//2)*2+[1, 0][d % 2]][e(*v[d])]
+        p, q, o[i][2] = *v[d], d
+        if e(p, q):
             continue
-        u = u[::[1, -1][b[ny][nx] == 1]]
-        m[y][x], m[ny][nx] = c[:h], m[ny][nx]+u
-        if len(m[ny][nx]) > 3:
+        h = c.index(i)
+        u = c[h:][::[1, -1][b[p][q] == 1]]
+        m[y][x], m[p][q] = c[:h], m[p][q]+u
+        if len(m[p][q]) > 3:
             exit(print(t+1))
-        for p in u:
-            o[p] = (ny, nx, o[p][2])
+        o = [[o[_], [p, q, o[_][2]]][_ in u]for _ in r(k)]
 print(-1)
