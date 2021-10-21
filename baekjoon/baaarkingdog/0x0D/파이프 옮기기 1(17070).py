ae8ff37f0f9671f082f4451a2b29482422fdc2fd
춤추는망고 <ensia96@gmail.com>
2021-10-21 16:32:29 +0900

    [update] 파이프 옮기기 1(17070) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x0D/파이프 옮기기 1(17070).py
+++ baekjoon/baaarkingdog/0x0D/파이프 옮기기 1(17070).py
@@ -1,20 +1,18 @@
-n, r, a, z = int(input()), range, ((0, 1), (1, 0), (1, 1)), 0
-m, g = [[*map(int, input().split())]for _ in r(n)], (n-1, n-1)
-d = (a[0:1], a[1:2], a)
-if m[n-1][n-1]:
-    exit(print(0))
+n, r, a = int(input()), range, ((0, 1), (1, 0), (1, 1))
+m = [[*map(int, input().split())]for _ in r(n)]
+d, v = (a[0:1], a[1:2], a), [[[-1]*3 for _ in r(n)]for _ in r(n)]
 
 
 def b(y, x, s):
-    global z
-    i, j = y+a[s][0], x+a[s][1]
-    if ((i, j) == g):
-        z += 1
-        return
+    if (y, x) == (n-1, n-1):
+        return 1
+    if v[y][x][s] != -1:
+        return v[y][x][s]
+    v[y][x][s] = 0
     for S in ((0, 2), (1, 2), (0, 1, 2))[s]:
-        if all((i+h < n)*(j+w < n) and not m[i+h][j+w]for h, w in d[S]):
-            b(i, j, S)
+        if all((y+h < n)*(x+w < n) and not m[y+h][x+w]for h, w in d[S]):
+            v[y][x][s] += b(y+a[S][0], x+a[S][1], S)
+    return v[y][x][s]
 
 
-b(0, 0, 0)
-print(z)
+print(b(0, 1, 0))
