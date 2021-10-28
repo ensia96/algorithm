0216929582bde5f7a92c18f111f60159a5ebd55f
춤추는망고 <ensia96@gmail.com>
2021-10-28 18:57:11 +0900

    [update] 자두나무(2240) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x10/자두나무(2240).py
+++ baekjoon/baaarkingdog/0x10/자두나무(2240).py
@@ -1,7 +1,9 @@
 I, R = input, range
 T, W = map(int, I().split())
 D = [[[0] * (W + 1) for _ in R(2)] for _ in R(T + 1)]
-for t in R(T):
+p = int(I()) == 1
+D[0][0][W], D[0][1][W - 1] = p, not (p)
+for t in R(1, T):
     p = int(I()) == 1
     for w in R(W + 1):
         D[t][0][w] = max(D[t - 1][0][w], w < W and D[t - 1][1][w + 1]) + p
