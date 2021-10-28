2f5151f7ee1158a05acdb04693d21ac564903129
춤추는망고 <ensia96@gmail.com>
2021-10-28 19:58:43 +0900

    [update] 자두나무(2240) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x10/자두나무(2240).py
+++ baekjoon/baaarkingdog/0x10/자두나무(2240).py
@@ -1,9 +1,8 @@
 I, R = input, range
 T, W = map(int, I().split())
-P = [int(I()) for _ in R(T)]
 D = [[0] * (W + 1) for _ in range(T)]
 for t in R(T):
-    p = P[t] == 1
+    p = int(I()) == 1
     D[t][0] = D[t - 1][0] + p
     for w in R(1, W + 1):
         a = bool((p * (not w % 2)) + ((not p) * (w % 2)))
