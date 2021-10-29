dc641c1e49e67a056fe10a306fe9b9bbb5c4ed0c
춤추는망고 <ensia96@gmail.com>
2021-10-29 14:50:32 +0900

    [update] 가장 긴 증가하는 부분 수열 4(14002) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x10/가장 긴 증가하는 부분 수열 4(14002).py
+++ baekjoon/baaarkingdog/0x10/가장 긴 증가하는 부분 수열 4(14002).py
@@ -1,12 +1,13 @@
 I, R = input, range
 n, A = int(I()), [*map(int, I().split())]
-D, P = [1]*n, [0]*n
+D, P = [1]*n, [-1]*n
 for i in R(n):
     for j in R(i):
         if A[j] < A[i]:
-            D[i] = max(D[i], D[j]+1)
-            if D[i] >= max(D):
-                P[D[j]] = A[j]
-a, P = D.index(max(D)), [*filter(None, P)]
+            C = D[j]+1
+            D[i] = max(D[i], C)
+            if (P[C] == -1)+((j < P[C])*(A[j] < A[C])):
+                P[D[j]] = j
+a = D.index(max(D))
 print(D[a])
-print(*P, A[a])
+print(*[A[p]for p in P if p > -1], A[a])
