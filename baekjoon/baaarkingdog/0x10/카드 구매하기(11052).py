6dc4a507dd881ad0bc76dcab3d620d811dc84ad8
춤추는망고 <ensia96@gmail.com>
2021-10-30 15:49:17 +0900

    [update] 카드 구매하기(11052) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x10/카드 구매하기(11052).py
+++ baekjoon/baaarkingdog/0x10/카드 구매하기(11052).py
@@ -1,8 +1,5 @@
 i, r = input, range
-n = int(i())+1
-p, d = [0]+[*map(int, i().split())], [[0]*(n) for _ in r(n)]
-for i in r(1, n):
-    for j in r(n):
-        for k in range(j//i+1):
-            d[i][j] = max(d[i][j], d[i-1][j], p[i]*k+d[i-1][j-i*k])
-print(d[-1][-1])
+n, p, d = int(i()), [*map(int, i().split())], [0]
+for i in r(n):
+    d += [max(d[i-j]+p[j]for j in r(i+1))]
+print(d[-1])
