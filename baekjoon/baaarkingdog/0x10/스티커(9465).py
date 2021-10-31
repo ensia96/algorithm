950053e8eed4fd8ad7c6b7cecc11047c78704248
춤추는망고 <ensia96@gmail.com>
2021-10-31 13:28:26 +0900

    [update] 스티커(9465) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x10/스티커(9465).py
+++ baekjoon/baaarkingdog/0x10/스티커(9465).py
@@ -2,9 +2,8 @@ import sys
 I, R, T = sys.stdin.readline, range, int
 for _ in R(T(I())):
     n, s = T(I()), [*zip(*[[*map(T, I().split())]for _ in R(2)])]
-    d = [[0]*2 for _ in R(n+2)]
+    a = b = c = d = 0
     for i in R(n):
-        m = max(d[i-2])
-        d[i][0] = max(m, d[i-1][1])+s[i][0]
-        d[i][1] = max(m, d[i-1][0])+s[i][1]
-    print(max(d[-3]))
+        m, a, b = max(a, b), c, d
+        c, d = max(m, d)+s[i][0], max(m, c)+s[i][1]
+    print(max(c, d))
