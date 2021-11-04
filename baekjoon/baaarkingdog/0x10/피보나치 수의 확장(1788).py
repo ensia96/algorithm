6312cd11122793457f78bcb74f8bd29bd9e34e8d
춤추는망고 <ensia96@gmail.com>
2021-11-04 14:25:39 +0900

    [update] 피보나치 수의 확장(1788) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x10/피보나치 수의 확장(1788).py
+++ baekjoon/baaarkingdog/0x10/피보나치 수의 확장(1788).py
@@ -1,6 +1,6 @@
 N = int(input())
 n, f = abs(N), N >= 0
-a, b, c, d = *[0, 1][::f or -1], 0, 10**9
+a, b, c, d = *[0, 1][::f or -1], n, 10**9
 for i in range(2, n+2-f):
     c = [a-b, a+b][f]
     c = [c % d, -(-c % d)][c < 0]
