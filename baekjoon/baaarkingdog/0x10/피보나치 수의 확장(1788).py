38a62628ea5267e8193042ab26d49302d3717eef
춤추는망고 <ensia96@gmail.com>
2021-11-04 14:46:06 +0900

    [update] 피보나치 수의 확장(1788) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x10/피보나치 수의 확장(1788).py
+++ baekjoon/baaarkingdog/0x10/피보나치 수의 확장(1788).py
@@ -1,9 +1,6 @@
-N = int(input())
-n, f = abs(N), N >= 0
-a, b, c, d = *[0, 1][::f or -1], n, 10**9
-for i in range(2, n+2-f):
-    c = [a-b, a+b][f]
-    c = [c % d, -(-c % d)][c < 0]
-    a, b = b, c
-print(int(c > 0 or (c < 0)*-1))
-print(abs(c))
+n = int(input())
+print([0, int(n > 0 or n % 2 or -1)][bool(n)])
+a, b = 0, 1
+for i in ' '*abs(n):
+    a, b = b, a+b % 10**9
+print(a)
