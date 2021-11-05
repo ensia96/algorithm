7b6f5dd4e2747c87e44eb9837d3e9856a9d45731
춤추는망고 <ensia96@gmail.com>
2021-11-05 13:48:40 +0900

    [update] 삼각 그래프(4883) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x10/삼각 그래프(4883).py
+++ baekjoon/baaarkingdog/0x10/삼각 그래프(4883).py
@@ -1,11 +1,11 @@
-l, t = lambda: [*map(int, input().split())], 0
+l, t = lambda: map(int, input().split()), 0
 while 1:
     n, t = int(input()), t+1
     if not n:
         exit()
-    a, b, c = l()
-    a, c = 10**9, b+c
-    for _ in range(n-1):
+        a, b, c = l()
+        a, c = 10**9, b+c
+    for _ in ' '*(n-1):
         x, y, z = l()
         x += min(a, b)
         y += min(x, a, b, c)
