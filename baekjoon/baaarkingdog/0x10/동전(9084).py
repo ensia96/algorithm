f8efcd58972fa2ced0dc38eaf4bafc83602a723f
춤추는망고 <ensia96@gmail.com>
2021-11-08 15:50:30 +0900

    [update] 동전(9084) - 런타임 에러 (IndexError) 해결

--- baekjoon/baaarkingdog/0x10/동전(9084).py
+++ baekjoon/baaarkingdog/0x10/동전(9084).py
@@ -2,11 +2,10 @@ I, T, R = input, int, range
 N, L = lambda: T(I()), lambda: [*map(T, I().split())]
 for _ in R(N()):
     n, C, m = N(), [0]+L(), N()
-    D = [[0]*(n+1) for _ in R(m+1)]
-    for j in R(1, n+1):
-        if C[j] > m:
-            D[i][j] = D[i][j-1]
-        D[C[j]][j] = 1
+    D = [0]*(m+1)
+    for c in C:
+        if c > m:
+            break
         for i in R(1, m+1):
-            D[i][j] += D[i][j-1]+D[i-C[j]][j]
-    print(D)
+            D[i] += (i == c)+D[i-c]*(i > c)
+    print(D[m])
