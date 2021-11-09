7c1448814d58f6995d3501db6ac48fab9a3c4f0a
춤추는망고 <ensia96@gmail.com>
2021-11-09 14:34:44 +0900

    [update] 가장 큰 정사각형(1915) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x10/가장 큰 정사각형(1915).py
+++ baekjoon/baaarkingdog/0x10/가장 큰 정사각형(1915).py
@@ -1,9 +1,7 @@
-I, R = input, range
-n, m = map(int, I().split())
-D = [[*map(int, I())]for _ in R(n)]
+R = range
+n, m = map(int, input().split())
+D = [[*map(int, input())]for _ in R(n)]
 for i in R(1, n):
     for j in R(1, m):
-        if D[i][j]:
-            t = [D[i-1][j-1], D[i-1][j], D[i][j-1]]
-            D[i][j] += min(t)*all(t)
+        D[i][j] += min(D[i-1][j-1], D[i-1][j], D[i][j-1])*D[i][j]
 print(max(map(max, D))**2)
