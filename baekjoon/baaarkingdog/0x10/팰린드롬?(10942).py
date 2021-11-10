6896f6b74a69ec69121260d68535c7cb0739f2b7
춤추는망고 <ensia96@gmail.com>
2021-11-10 13:59:48 +0900

    [update] 팰린드롬?(10942) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x10/팰린드롬?(10942).py
+++ baekjoon/baaarkingdog/0x10/팰린드롬?(10942).py
@@ -5,9 +5,8 @@ n, a = int(I()), [*L()]
 D = [[0]*n for _ in R(n)]
 for d in R(n):
     for s in R(n-d):
-        e = s+d
-        m = a[s] == a[e]
-        D[s][e] = +(s == e) or (s+1 == e)*m or m*D[s+1][e-1]
+        m = a[s] == a[s+d]
+        D[s][s+d] = not d or (d == 1)*m or m*D[s+1][s+d-1]
 for _ in R(int(I())):
     s, e = L()
-    print(D[s-1][e-1])
+    print(+D[s-1][e-1])
