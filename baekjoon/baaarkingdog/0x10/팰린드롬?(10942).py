62242586daea7c67b5471cc25bfad85cea3ac8fc
춤추는망고 <ensia96@gmail.com>
2021-11-10 13:53:56 +0900

    [update] 팰린드롬?(10942) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x10/팰린드롬?(10942).py
+++ baekjoon/baaarkingdog/0x10/팰린드롬?(10942).py
@@ -1,13 +1,13 @@
 import sys
-i = sys.stdin.readline
-I, L, R = lambda: int(i()), lambda: map(int, i().split()), range
-n, a = I(), [*L()]
+I = sys.stdin.readline
+L, R = lambda: map(int, I().split()), range
+n, a = int(I()), [*L()]
 D = [[0]*n for _ in R(n)]
-for i in R(n):
-    c = d = ()
-    for j in R(i, n):
-        c, d = (*c, a[j]), (a[j], *d)
-        D[i][j] = 1*(c == d)
-for _ in R(I()):
+for d in R(n):
+    for s in R(n-d):
+        e = s+d
+        m = a[s] == a[e]
+        D[s][e] = +(s == e) or (s+1 == e)*m or m*D[s+1][e-1]
+for _ in R(int(I())):
     s, e = L()
     print(D[s-1][e-1])
