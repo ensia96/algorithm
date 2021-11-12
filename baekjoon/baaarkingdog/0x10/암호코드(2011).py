d3f2f9bed0c3765b6db31be81e90ea8f79df567c
춤추는망고 <ensia96@gmail.com>
2021-11-12 12:12:18 +0900

    암호코드(2011)

---
+++ baekjoon/baaarkingdog/0x10/암호코드(2011).py
@@ -0,0 +1,8 @@
+n = [*map(int, input())]
+L, l = lambda x: n[x-1] and n[x-1]*10+n[x] <= 26, len(n)
+if not all(n):
+    exit(print(0))
+D = [1]*l
+for i in range(1, l):
+    D[i] = D[i-2]*L(i)+D[i-1]
+print(D[-1])
