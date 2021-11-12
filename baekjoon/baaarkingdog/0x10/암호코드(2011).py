1d6bfb16329a99c3042afd8518d19e51f8fcd307
춤추는망고 <ensia96@gmail.com>
2021-11-12 12:38:06 +0900

    [update] 암호코드(2011) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x10/암호코드(2011).py
+++ baekjoon/baaarkingdog/0x10/암호코드(2011).py
@@ -2,7 +2,7 @@ n, m = [*map(int, input())], 10**6
 L, l = lambda x: n[x-1] and n[x-1]*10+n[x] <= 26, len(n)
 if not n[0]:
     exit(print(0))
-D = [1]*l
+a = b = 1
 for i in range(1, l):
-    D[i] = (D[i-2]*L(i)+D[i-1]*bool(n[i])) % m
-print(D[-1])
+    a, b = b, (a*L(i)+b*bool(n[i])) % m
+print(b)
