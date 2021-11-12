580fca95d831d59e86e6eb81c2fbd44848282fb9
춤추는망고 <ensia96@gmail.com>
2021-11-12 12:16:35 +0900

    [update] 암호코드(2011) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x10/암호코드(2011).py
+++ baekjoon/baaarkingdog/0x10/암호코드(2011).py
@@ -1,8 +1,8 @@
-n = [*map(int, input())]
+n, m = [*map(int, input())], 10**6
 L, l = lambda x: n[x-1] and n[x-1]*10+n[x] <= 26, len(n)
 if not all(n):
     exit(print(0))
 D = [1]*l
 for i in range(1, l):
-    D[i] = D[i-2]*L(i)+D[i-1]
+    D[i] = (D[i-2]*L(i)+D[i-1]) % m
 print(D[-1])
