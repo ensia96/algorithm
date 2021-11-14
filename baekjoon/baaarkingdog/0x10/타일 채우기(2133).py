a1242e2592420b267d706f9ce17a7789bd2d5237
춤추는망고 <ensia96@gmail.com>
2021-11-14 13:34:52 +0900

    [update] 타일 채우기(2133) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x10/타일 채우기(2133).py
+++ baekjoon/baaarkingdog/0x10/타일 채우기(2133).py
@@ -1,5 +1,7 @@
+n = int(input())
 D = [1, 0, 3]
 P = [0, 1, 0]
-for i in range(3, int(input())+1):
+for i in range(3, n+1):
     D, P = D+[D[i-2]+2*P[i-1]], P+[D[i-1]+P[i-2]]
-print(D[-1])
+print(D)
+print(D[n])
