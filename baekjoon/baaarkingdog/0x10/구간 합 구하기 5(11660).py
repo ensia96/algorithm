6b10839ad24da97ac0a9d6fec7b5747d6b4cc525
춤추는망고 <ensia96@gmail.com>
2021-11-17 12:46:13 +0900

    [update] 구간 합 구하기 5(11660) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x10/구간 합 구하기 5(11660).py
+++ baekjoon/baaarkingdog/0x10/구간 합 구하기 5(11660).py
@@ -1,10 +1,11 @@
 l, r = lambda: map(int, input().split()), range
 n, m = l()
-b, N = [0]+[[*l()]for _ in r(n)], n+1
+N = n+1
+b = [[0]*N]+[[0, *l()]for _ in r(n)]
 d = [[0]*N for _ in r(N)]
 for i in r(1, N):
     for j in r(1, N):
-        d[i][j] = d[i][j-1]+b[i][j-1]
+        d[i][j] = d[i][j-1]+d[i-1][j]-d[i-1][j-1]+b[i][j]
 for _ in r(m):
     h, i, j, k = l()
-    print(sum(d[p][k]-d[p][i-1]for p in r(h, j+1)))
+    print(d[j][k]+d[h-1][i-1]-d[h-1][k]-d[j][i-1])
