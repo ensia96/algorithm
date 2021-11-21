12435a717ac5d73d62c72178ca1fc70bf62f9262
춤추는망고 <ensia96@gmail.com>
2021-11-21 14:01:20 +0900

    [update] 잃어버린 괄호(1541) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x11/잃어버린 괄호(1541).py
+++ baekjoon/baaarkingdog/0x11/잃어버린 괄호(1541).py
@@ -1,7 +1,2 @@
-f, F = input(), 0
-n = [*map(int, f.replace('+', ' ', -1).replace('-', ' ', -1).split())]
-o = [*filter(lambda x: x in '+-', f)]
-for i in range(len(o)):
-    n[i+1] *= -1 if F*(o[i] == '+') or o[i] == '-' else 1
-    F = o[i] == '-' or F
-print(sum(n))
+a, *b = [sum(map(int, s.split('+')))for s in input().split('-')]
+print(a-sum(b))
