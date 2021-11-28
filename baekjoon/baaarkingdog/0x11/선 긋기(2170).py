de11fb764762617f155364da487e07fb6eb94ef5
춤추는망고 <ensia96@gmail.com>
2021-11-28 13:58:05 +0900

    [update] 선 긋기(2170) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x11/선 긋기(2170).py
+++ baekjoon/baaarkingdog/0x11/선 긋기(2170).py
@@ -1,7 +1,7 @@
 import sys
 I = sys.stdin.readline
-l = sorted((*map(int, I().split()),)for _ in ' '*int(I()))
-a, s, e = 0, *l[0]
-for x, y in l:
+a = 0
+s = e = int(-1e10)
+for x, y in sorted((*map(int, I().split()),)for _ in ' '*int(I())):
     a, s, e = [(a, s, max(e, y)), (a+e-s, x, y)][e < x]
 print(a+e-s)
