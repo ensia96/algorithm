a846d7f332cb3659ba946fd0fca288829ab1aa6c
춤추는망고 <ensia96@gmail.com>
2021-11-29 13:52:26 +0900

    멀티탭 스케줄링(1700)

---
+++ baekjoon/baaarkingdog/0x11/멀티탭 스케줄링(1700).py
@@ -0,0 +1,13 @@
+l, a = lambda: map(int, input().split()), 0
+n, k = l()
+E, D, M = [*l()], {}, []
+for i in range(k)[::-1]:
+    e = E[i]
+    D[e] = D.get(e, [])+[i]
+for e in E:
+    D[e].pop()
+    M.sort(key=lambda e: min(D[e] or [101]))
+    if e in M:
+        continue
+    a, M = a+bool(len(M) == n and M.pop()), M+[e]
+print(a)
