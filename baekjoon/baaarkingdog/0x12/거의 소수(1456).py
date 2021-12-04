9c486f559900efc445d9b75f0bf89dee779b35b3
춤추는망고 <ensia96@gmail.com>
2021-12-04 18:47:08 +0900

    [update] 거의 소수(1456) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x12/거의 소수(1456).py
+++ baekjoon/baaarkingdog/0x12/거의 소수(1456).py
@@ -4,12 +4,9 @@ d = [1]*(c+1)
 for i in range(2, c+1):
     if not d[i]:
         continue
-    for j in range(i+i, c+1, i):
-        d[j] = 0
-for i in range(2, c+1):
-    if not d[i]:
-        continue
     t = i*i
     while t <= b:
         A, t = A+(a <= t), t*i
+    for j in range(i+i, c+1, i):
+        d[j] = 0
 print(A)
