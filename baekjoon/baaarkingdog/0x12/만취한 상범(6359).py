5d6c3b38081bd231d69c3c59522b58234590ce9a
춤추는망고 <ensia96@gmail.com>
2021-12-04 13:29:32 +0900

    [update] 만취한 상범(6359) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x12/만취한 상범(6359).py
+++ baekjoon/baaarkingdog/0x12/만취한 상범(6359).py
@@ -1,7 +1,2 @@
-for _ in range(int(input())):
-    n = int(input())
-    R = [0]*(n+1)
-    for i in range(1, n+1):
-        for j in range(i, n+1, i):
-            R[j] = not R[j]
-    print(sum(R))
+for _ in ' '*int(input()):
+    print(int((int(input())**.5)))
