3caae629ccf0edbfe8b07d858e00fce3b7680686
춤추는망고 <ensia96@gmail.com>
2021-12-05 12:39:16 +0900

    [update] 골드바흐 파티션(17103) - 런타임 에러 (IndexError) 해결

--- baekjoon/baaarkingdog/0x12/골드바흐 파티션(17103).py
+++ baekjoon/baaarkingdog/0x12/골드바흐 파티션(17103).py
@@ -1,8 +1,8 @@
-m = 10**2
+m = 10**6
 P = [0, 0]+[1]*m
 for i in range(m+1):
     if P[i]:
-        P[i+i::i] = [0]*(m//i-1)
+        P[i+i::i] = [0]*(m//i)
 for _ in ' '*int(input()):
     n = int(input())
     print(sum(P[i]*P[n-i]for i in range(2, n//2+1)))
