661aa99221a6dffb343c1a19acb6ce9cf45d9c44
춤추는망고 <ensia96@gmail.com>
2021-12-05 14:44:14 +0900

    [update] 날짜 계산(1476) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x12/날짜 계산(1476).py
+++ baekjoon/baaarkingdog/0x12/날짜 계산(1476).py
@@ -1,4 +1,4 @@
 e, s, m = map(int, input().split())
 s, m = s % 28, m % 19
 for i in range(e, 7981, 15):
-    (i % 28 == s)*(i % 19 == m) and exit(prinat(i))
+    (i % 28 == s)*(i % 19 == m) and exit(print(i))
