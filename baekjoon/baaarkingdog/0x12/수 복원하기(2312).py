6e5f11dd1d6ebdc0f6f796342bba950aa6b9b1de
춤추는망고 <ensia96@gmail.com>
2021-12-05 13:22:07 +0900

    [update] 수 복원하기(2312) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x12/수 복원하기(2312).py
+++ baekjoon/baaarkingdog/0x12/수 복원하기(2312).py
@@ -1,9 +1,9 @@
 M = 10**5
-P, m = [0, 0]+[1]*(M-1), int(M**.5)
-for i in range(m):
+P = [0, 0]+[1]*(M-1)
+for i in range(M):
     if P[i]:
         P[i+i::i] = [0]*(M//i-1)
-P = [i for i in range(m+1)if P[i]]
+P = [i for i in range(M+1)if P[i]]
 for _ in ' '*int(input()):
     n = int(input())
     for p in P:
