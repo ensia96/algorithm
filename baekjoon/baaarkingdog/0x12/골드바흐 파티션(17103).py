5d62d6e36f37e3b89fa7864683c89c1fe590ad12
춤추는망고 <ensia96@gmail.com>
2021-12-05 12:47:09 +0900

    [update] 골드바흐 파티션(17103) - 런타임 에러 (ValueError) 해결

--- baekjoon/baaarkingdog/0x12/골드바흐 파티션(17103).py
+++ baekjoon/baaarkingdog/0x12/골드바흐 파티션(17103).py
@@ -1,8 +1,8 @@
 m = 10**6
-P = [0, 0]+[1]*m
-for i in range(m+1):
+P = [0, 0]+[1]*(m-1)
+for i in range(2, m+1):
     if P[i]:
-        P[i+i::i] = [0]*(m//i)
+        P[i+i::i] = [0]*(m//i-1)
 for _ in ' '*int(input()):
     n = int(input())
     print(sum(P[i]*P[n-i]for i in range(2, n//2+1)))
