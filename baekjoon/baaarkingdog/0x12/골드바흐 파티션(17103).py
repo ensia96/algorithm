b32cbc40eed2bd03a780f384e9c46c4ab707c4bf
춤추는망고 <ensia96@gmail.com>
2021-12-05 12:37:42 +0900

    [update] 골드바흐 파티션(17103) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x12/골드바흐 파티션(17103).py
+++ baekjoon/baaarkingdog/0x12/골드바흐 파티션(17103).py
@@ -1,10 +1,8 @@
-m = 10**6
+m = 10**2
 P = [0, 0]+[1]*m
 for i in range(m+1):
-    if not P[i]:
-        continue
-    for j in range(i+i, m+1, i):
-        P[j] = 0
+    if P[i]:
+        P[i+i::i] = [0]*(m//i-1)
 for _ in ' '*int(input()):
     n = int(input())
     print(sum(P[i]*P[n-i]for i in range(2, n//2+1)))
