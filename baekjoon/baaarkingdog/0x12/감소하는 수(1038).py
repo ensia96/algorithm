e34b91a3382890d5e4ad826eb7747a993ac27085
춤추는망고 <ensia96@gmail.com>
2021-12-06 15:19:12 +0900

    감소하는 수(1038)

---
+++ baekjoon/baaarkingdog/0x12/감소하는 수(1038).py
@@ -0,0 +1,16 @@
+l = []
+
+
+def solve(A):
+    if A//10**9:
+        return
+    l.append(A)
+    for i in range(A % 10):
+        solve(A*10+i)
+
+
+for i in range(10):
+    solve(i)
+l.sort()
+n = int(input())
+print(-(len(l) < n) or l[n])
