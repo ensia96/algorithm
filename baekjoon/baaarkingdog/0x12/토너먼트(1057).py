cdbdcc8f46a4f7d082b8bc3d1a15fe8843ac9524
춤추는망고 <ensia96@gmail.com>
2021-12-06 18:54:00 +0900

    [update] 토너먼트(1057) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x12/토너먼트(1057).py
+++ baekjoon/baaarkingdog/0x12/토너먼트(1057).py
@@ -1,8 +1,6 @@
 n, k, l = map(int, input().split())
-L = [0]*n
-L[k-1] = L[l-1] = 1
-c = 0
+k, l, c = k-1, l-1, 1
 while 1:
-    L, c = [L[i]+((i+1 < n) and L[i+1])for i in range(0, n, 2)], c+1
-    n = len(L)
-    max(L) == 2 and exit(print(c))
+    if k-l in [1, -1]:
+        exit(print(c))
+    k, l, c = k//2, l//2, c+1
