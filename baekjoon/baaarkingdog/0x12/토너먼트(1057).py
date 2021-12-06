987510f0a03a37433a5bd317f03e57b16377c77c
춤추는망고 <ensia96@gmail.com>
2021-12-06 18:55:38 +0900

    [update] 토너먼트(1057) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x12/토너먼트(1057).py
+++ baekjoon/baaarkingdog/0x12/토너먼트(1057).py
@@ -1,6 +1,4 @@
-n, k, l = map(int, input().split())
-k, l, c = k-1, l-1, 1
-while 1:
-    if k-l in [1, -1]:
-        exit(print(c))
-    k, l, c = k//2, l//2, c+1
+n, k, l, c = *map(int, input().split()), 0
+while k != l:
+    k, l, c = k-k//2, l-l//2, c+1
+print(c)
