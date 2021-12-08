8f07d92980b907ccf5b678f53ccb0a5a2d528395
춤추는망고 <ensia96@gmail.com>
2021-12-08 15:33:10 +0900

    [update] 장미(3343) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x12/장미(3343).py
+++ baekjoon/baaarkingdog/0x12/장미(3343).py
@@ -1,5 +1,2 @@
-import math
 n, a, b, c, d = map(int, input().split())
-g = math.gcd(a, c)
-q, m = divmod(n, g)
-print(min(q*g//a*b, q*g//c*d)+min((m//a or 1)*b, (m//c or 1)*d))
+print(min(n//a*b+min(b, (n % a//c or 1)*d), n//c*d+min((n % c//a or 1)*b, d)))
