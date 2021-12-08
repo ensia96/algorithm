fae70f83201558649aa9d4544214c21f7fa7d374
춤추는망고 <ensia96@gmail.com>
2021-12-08 16:53:13 +0900

    [update] 장미(3343) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x12/장미(3343).py
+++ baekjoon/baaarkingdog/0x12/장미(3343).py
@@ -1,3 +1,3 @@
 n, a, b, c, d = map(int, input().split())
 a, b, c, d = [(a, b, c, d), (c, d, a, b)][b*c > a*d]
-print(min(d*i + ((n-c*i)//a+bool((n-c*i) % a))*b for i in range(a) if c*i < n))
+print(min(d*i+((n-c*i)//a+bool((n-c*i) % a))*b for i in range(a)if c*i <= n))
