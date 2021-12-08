ef026bbe2554d31d63ce97f8c92e3971d067292b
춤추는망고 <ensia96@gmail.com>
2021-12-08 16:17:41 +0900

    [update] 장미(3343) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x12/장미(3343).py
+++ baekjoon/baaarkingdog/0x12/장미(3343).py
@@ -3,7 +3,7 @@ n, a, b, c, d = map(int, input().split())
 
 def s(i, j, k):
     x, y = divmod(i, j)
-    return (x+bool(y))*k
+    return k * (i < 0 or (x+bool(y)))
 
 
 print(min(s(n, a, b), s(n-c, a, b)+d, s(n, c, d), s(n-a, c, d)+b))
