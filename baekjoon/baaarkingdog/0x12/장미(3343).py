3641e91ab6c823698313556b078b358cb2e056c6
춤추는망고 <ensia96@gmail.com>
2021-12-08 15:38:00 +0900

    [update] 장미(3343) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x12/장미(3343).py
+++ baekjoon/baaarkingdog/0x12/장미(3343).py
@@ -1,2 +1,3 @@
 n, a, b, c, d = map(int, input().split())
-print(min(n//a*b+min(b, (n % a//c or 1)*d), n//c*d+min((n % c//a or 1)*b, d)))
+print(min(n//a*b+bool(n % a)*min(b, (n % a//c or 1)*d),
+      n//c*d+bool(n % c)*min((n % c//a or 1)*b, d)))
