812ab55fcc1ec1f2775bbc87b207d7144f8f77f5
춤추는망고 <ensia96@gmail.com>
2021-12-08 22:56:57 +0900

    [update] 장미(3343) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x12/장미(3343).py
+++ baekjoon/baaarkingdog/0x12/장미(3343).py
@@ -3,6 +3,5 @@ if a*d <= b*c:
     a, b, c, d = c, d, a, b
 A = b*(n//a+bool(n % a))
 for i in range(a):
-    if n-c*i > 0:
-        A = min(A, d*i+((n-c*i)//a+bool((n-c*i) % a))*b)
+    A = min(A, d*i+max(0, ((n-c*i)//a+bool((n-c*i) % a)))*b)
 print(A)
