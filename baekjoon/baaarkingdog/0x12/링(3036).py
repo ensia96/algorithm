f4851b5138a29e701c60158f23c4ef57c2a95489
춤추는망고 <ensia96@gmail.com>
2021-12-08 12:45:27 +0900

    [update] 링(3036) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x12/링(3036).py
+++ baekjoon/baaarkingdog/0x12/링(3036).py
@@ -1,5 +1,5 @@
-g, n = lambda a, b: (a == 0)*b or g(b % a, a), input()
-m, *l = map(int, input().split())
+import math
+m, *l = map(int, open(0)[1].split())
 for x in l:
-    y = g(m, x)
+    y = math.gcd(m, x)
     print(f"{m//y}/{x//y}")
