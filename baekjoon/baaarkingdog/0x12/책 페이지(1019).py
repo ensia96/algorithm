6412cd933327f34cbacaf1d58349bbade2d199a7
춤추는망고 <ensia96@gmail.com>
2021-12-12 14:46:46 +0900

    [update] 책 페이지(1019) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x12/책 페이지(1019).py
+++ baekjoon/baaarkingdog/0x12/책 페이지(1019).py
@@ -2,11 +2,7 @@ N = n = int(input())
 A, c = [0]*10, 1
 while n:
     n, r = divmod(n, 10)
-    A[0] += n*c
-    for i in range(r):
-        A[i+1] += (n+1)*c
-    A[r] -= c-(N % c)-1
-    for i in range(r+1, 10):
-        A[i] += n*c
+    for i in range(10):
+        A[i] += (n+(1 <= i <= r))*c-(-c-(N % c)-1)*(i == r)
     c *= 10
 print(*A)
