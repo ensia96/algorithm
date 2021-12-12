235f53cbc02ae40541a9c7e7663b85ef3f508504
춤추는망고 <ensia96@gmail.com>
2021-12-12 14:40:09 +0900

    책 페이지(1019)

---
+++ baekjoon/baaarkingdog/0x12/책 페이지(1019).py
@@ -0,0 +1,8 @@
+N = n = int(input())
+A, c = [0]*10, 1
+while n:
+    n, r = divmod(n, 10)
+    for i in range(10):
+        A[i] += (n+(1 <= i < r))*c-(-c-(N % c)-1)*(i == r)
+    c *= 10
+print(A)
