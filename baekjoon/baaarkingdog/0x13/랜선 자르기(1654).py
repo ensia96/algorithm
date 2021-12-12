42885c80b1510540e76afbeb5a5f92555980535f
춤추는망고 <ensia96@gmail.com>
2021-12-12 23:05:01 +0900

    [update] 랜선 자르기(1654) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x13/랜선 자르기(1654).py
+++ baekjoon/baaarkingdog/0x13/랜선 자르기(1654).py
@@ -3,7 +3,8 @@ K = [int(input())for _ in ' '*k]
 s, e = 1, 2**31-1
 while s < e:
     m = (s+e+1)//2
-    if sum(map(lambda x: x//m, K)) >= n: s = m
+    if sum(i//m for i in K) >= n:
+        s = m
     else:
         e = m-1
 print(s)
