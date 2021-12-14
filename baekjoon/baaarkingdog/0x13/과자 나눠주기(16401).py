55520958ba1b15b5d97bd4d5b8f8527f24fef08f
춤추는망고 <ensia96@gmail.com>
2021-12-14 13:00:25 +0900

    [update] 과자 나눠주기(16401) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x13/과자 나눠주기(16401).py
+++ baekjoon/baaarkingdog/0x13/과자 나눠주기(16401).py
@@ -1,10 +1,10 @@
 M, _ = map(int, input().split())
 L = [*map(int, input().split())]
-s, e = 0, max(L)
+s, e = 1, max(L)
 while s < e:
     m = (s+e+1)//2
     if sum(l//m for l in L) >= M:
-        s = m+1
+        s = m
     else:
         e = m-1
 print(e)
