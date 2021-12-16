7cbc9c7d7ee3db390f0eb7088714e8bc1c78c1df
춤추는망고 <ensia96@gmail.com>
2021-12-16 19:25:52 +0900

    [update] 합이 0(3151) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x13/합이 0(3151).py
+++ baekjoon/baaarkingdog/0x13/합이 0(3151).py
@@ -1,11 +1,10 @@
 import sys
 import bisect as B
 I = sys.stdin.readline
-l, r = B.bisect_left, B.bisect_right
 n, a = int(I()), 0
 A = sorted(map(int, I().split()))
 for i in range(n-1):
     for j in range(1, n):
         s = A[i]+A[j]
-        a += r(A, -s, i+1, j) != l(A, -s, i+1, j)
+        a += B.bisect_right(A, -s, i+1, j)-B.bisect_left(A, -s, i+1, j)
 print(a)
