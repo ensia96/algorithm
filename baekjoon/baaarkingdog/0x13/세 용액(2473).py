c891a97c3fbebf73d04e359c1f0fe9fb07762e0a
춤추는망고 <ensia96@gmail.com>
2021-12-19 14:43:48 +0900

    [update] 세 용액(2473) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x13/세 용액(2473).py
+++ baekjoon/baaarkingdog/0x13/세 용액(2473).py
@@ -5,8 +5,8 @@ a = p = 2**32
 for i in range(n):
     for j in range(i+1, n):
         s = A[i]+A[j]
-        l, r = B.bisect_left(A, -s), B.bisect_right(A, -s)
-        for k in [l-1, l, r, r+1]:
+        l = B.bisect_left(A, -s)
+        for k in [l-1, l, l+1]:
             if not 0 <= k < n or k in [i, j]:
                 continue
             t = abs(A[k]+s)
