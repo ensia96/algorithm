3beb8235e7e2c463406e85f9386238e41fd03f78
춤추는망고 <ensia96@gmail.com>
2021-12-20 13:40:19 +0900

    [update] 합이 0인 네 정수(7453) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x13/합이 0인 네 정수(7453).py
+++ baekjoon/baaarkingdog/0x13/합이 0인 네 정수(7453).py
@@ -1,14 +1,14 @@
 import sys
-import bisect as T
+import bisect as X
+import collections as Y
 I, R = sys.stdin.readline, range
 n = int(I())
 A, B, C, D = [], [], [], []
 for _ in ' '*n:
     a, b, c, d = map(int, I().split())
     A, B, C, D = A+[a], B+[b], C+[c], D+[d]
-a, E, F = 0, sorted(C[i]+D[j]for i in R(n)for j in R(n)), {}
+a, E, F = 0, sorted(C[i]+D[j]for i in R(n)for j in R(n)), Y.defaultdict(int)
 for i in R(n):
     for j in R(n):
-        s = A[i]+B[j]
-        F[s] = F.get(s, 0)+1
-print(sum(F[f]*(T.bisect_right(E, -f)-T.bisect_left(E, -f))for f in F))
+        F[A[i]+B[j]] += 1
+print(sum(F[f]*(X.bisect_right(E, -f)-X.bisect_left(E, -f))for f in F))
