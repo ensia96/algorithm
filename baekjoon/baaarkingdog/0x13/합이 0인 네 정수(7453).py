95f9529324e8a4e09360abfbcdb120da29cc24f6
춤추는망고 <ensia96@gmail.com>
2021-12-20 13:32:21 +0900

    합이 0인 네 정수(7453)

---
+++ baekjoon/baaarkingdog/0x13/합이 0인 네 정수(7453).py
@@ -0,0 +1,14 @@
+import sys
+import bisect as T
+I, R = sys.stdin.readline, range
+n = int(I())
+A, B, C, D = [], [], [], []
+for _ in ' '*n:
+    a, b, c, d = map(int, I().split())
+    A, B, C, D = A+[a], B+[b], C+[c], D+[d]
+a, E = 0, sorted(C[i]+D[j]for i in R(n)for j in R(n))
+for i in R(n):
+    for j in R(n):
+        s = A[i]+B[j]
+        a += T.bisect_right(E, -s)-T.bisect_left(E, -s)
+print(a)
