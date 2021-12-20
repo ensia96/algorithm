861fd6f455cb4894b06eecb6f0452613f8cbb3ce
춤추는망고 <ensia96@gmail.com>
2021-12-20 13:44:02 +0900

    [update] 합이 0인 네 정수(7453) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x13/합이 0인 네 정수(7453).py
+++ baekjoon/baaarkingdog/0x13/합이 0인 네 정수(7453).py
@@ -6,9 +6,14 @@ n = int(I())
 A, B, C, D = [], [], [], []
 for _ in ' '*n:
     a, b, c, d = map(int, I().split())
-    A, B, C, D = A+[a], B+[b], C+[c], D+[d]
-a, E, F = 0, sorted(C[i]+D[j]for i in R(n)for j in R(n)), Y.defaultdict(int)
+    A.append(a)
+    B.append(b)
+    C.append(c)
+    D.append(d)
+a, E, F = 0, [], Y.defaultdict(int)
 for i in R(n):
     for j in R(n):
         F[A[i]+B[j]] += 1
+        E.append(C[i]+D[j])
+E.sort()
 print(sum(F[f]*(X.bisect_right(E, -f)-X.bisect_left(E, -f))for f in F))
