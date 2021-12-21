a07f9f5bdbbe05c98aa9406c74d1a78812137e6f
춤추는망고 <ensia96@gmail.com>
2021-12-21 22:09:51 +0900

    부분합(1806)

---
+++ baekjoon/baaarkingdog/0x14/부분합(1806).py
@@ -0,0 +1,10 @@
+n, s = map(int, input().split())
+A = [*map(int, input().split())]
+a, c, j = 2**17, A[0], 1
+for i in range(n):
+    while j < n and c < s:
+        c, j = c+A[j], j+1
+    if c >= s:
+        a = min(a, j-i)
+    c -= A[i]
+print(a)
