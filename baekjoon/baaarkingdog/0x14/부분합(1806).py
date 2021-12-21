c3654b814f561165acd2c68e6776e7b317f2f9e8
춤추는망고 <ensia96@gmail.com>
2021-12-21 22:17:57 +0900

    [update] 부분합(1806) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x14/부분합(1806).py
+++ baekjoon/baaarkingdog/0x14/부분합(1806).py
@@ -1,10 +1,12 @@
 n, s = map(int, input().split())
 A = [*map(int, input().split())]
-a, c, j = 2**17, A[0], 1
+a, c, j = 2**17, A[0], 0
 for i in range(n):
     while j < n and c < s:
-        c, j = c+A[j], j+1
-    if c >= s:
-        a = min(a, j-i)
-    c -= A[i]
+        j += 1
+        if j != n:
+            c += A[j]
+    if j == n:
+        break
+    a, c = min(a, j-i+1), c-A[i]
 print(a)
