9d3ff0bac2dde3ef213e88c05b0228d9f7822b0a
춤추는망고 <ensia96@gmail.com>
2021-12-22 12:11:11 +0900

    소수의 연속합(1644)

---
+++ baekjoon/baaarkingdog/0x14/소수의 연속합(1644).py
@@ -0,0 +1,12 @@
+n = int(input())
+A = [0, 0]+[1]*(n-1)
+for i in range(2, n+1):
+    if A[i]:
+        A[i+i::i] = [0]*(n//i-1)
+A = [i for i in range(n+1)if A[i]]
+r, s, c, j = len(A), A[0], 0, 1
+for i in range(r):
+    while j < r and s < n:
+        s, j = s+A[j], j+1
+    c, s = c+(s == n), s-A[i]
+print(c)
