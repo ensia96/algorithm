e9e99838af6c70b08ce4eb7f1d61b30866dca266
춤추는망고 <ensia96@gmail.com>
2021-12-29 13:18:57 +0900

    [update] 문자열 지옥에 빠진 호석(20166) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x15/문자열 지옥에 빠진 호석(20166).py
+++ baekjoon/baaarkingdog/0x15/문자열 지옥에 빠진 호석(20166).py
@@ -1,15 +1,14 @@
 import collections as c
 n, m, k = map(int, input().split())
 A, D, R = [input()for _ in ' '*n], c.defaultdict(int), range
-A = [a*5 for a in A]*5
-for i in R(n*2, n*3):
-    for j in R(m*2, m*3):
+for i in R(n):
+    for j in R(m):
         Q = [(i, j, 1, A[i][j])]
         D[A[i][j]] += 1
         for x, y, d, s in Q:
             if d == 5:
                 continue
-            for a, b in [(x+p-1, y+q-1)for p in R(3)for q in R(3)if (p, q) != (1, 1)]:
+            for a, b in [((x+p-1) % n, (y+q-1) % m)for p in R(3)for q in R(3)if (p, q) != (1, 1)]:
                 D[s+A[a][b]] += 1
                 Q += [(a, b, d+1, s+A[a][b])]
 for _ in ' '*k:
