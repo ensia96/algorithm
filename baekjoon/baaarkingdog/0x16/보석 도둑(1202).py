24bd3d8754d0532383259cf94209646225b3e6fe
춤추는망고 <ensia96@gmail.com>
2021-12-30 19:36:03 +0900

    [update] 보석 도둑(1202) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x16/보석 도둑(1202).py
+++ baekjoon/baaarkingdog/0x16/보석 도둑(1202).py
@@ -1,18 +1,19 @@
 import sys
 import heapq as h
 I, i, d = sys.stdin.readline, h.heappush, h.heappop
-a, J, C, l = 0, [], [], lambda: map(int, I().split())
+a, J, l = 0, [], lambda: map(int, I().split())
 n, k = l()
 for _ in ' '*n:
     m, v = l()
-    i(J, (m, -v))
-for _ in ' '*k:
-    i(C, int(I()))
-while J and C:
-    m, v = d(J)
-    c = d(C)
-    while C and c < m:
-        c = d(C)
-    if c >= m:
-        a += -v
+    i(J, (-v, m))
+for c in sorted(int(I())for _ in ' '*k):
+    T = []
+    while J:
+        v, m = d(J)
+        if m <= c:
+            a += -v
+            break
+        else:
+            i(T, (v, m))
+    J = [*h.merge(J, T)]
 print(a)
