e50b79be6fb9b79813c21defd393438f8f98959b
춤추는망고 <ensia96@gmail.com>
2021-12-30 17:08:25 +0900

    [update] 이중 우선순위 큐(7662) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x16/이중 우선순위 큐(7662).py
+++ baekjoon/baaarkingdog/0x16/이중 우선순위 큐(7662).py
@@ -1,25 +1,26 @@
 import heapq as h
-import collections as c
 i, d = h.heappush, h.heappop
 
 
-def D(L, f, a):
-    while L and not E[L[0]*(f and -1 or 1)]:
+def D(L, f):
+    while L and L[0]*f not in E:
         d(L)
-    if a and L:
-        E[L[0]*(f and -1 or 1)] -= 1
-        d(L)
+    if E:
+        t = d(L)*f
+        if E[t]:
+            E[t] -= 1
+        else:
+            E.pop(t)
 
 
 for _ in ' '*int(input()):
-    E, Q, q, k = c.defaultdict(int), [], [], int(input())
-    for _ in ' '*k:
+    E, Q, q = {}, [], []
+    for _ in ' '*int(input()):
         o, n = input().split()
         n = int(n)
         if o == 'I':
-            E[n] += 1
+            E[n] = E.get(n, -1)+1
             i(Q, -n), i(q, n)
-        elif Q:
-            D(Q, 1, 1) if n > 0 else D(q, 0, 1)
-    D(Q, 1, 0), D(q, 0, 0)
-    print(Q and q and f"{-Q[0]} {q[0]}" or 'EMPTY')
+        else:
+            D(Q, -1) if n > 0 else D(q, 1)
+    print(f"{max(E)} {min(E)}"if E else'EMPTY')
