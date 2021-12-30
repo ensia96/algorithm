b616da4c0f833816a11560b55657259e44838550
춤추는망고 <ensia96@gmail.com>
2021-12-30 19:51:34 +0900

    [update] 보석 도둑(1202) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x16/보석 도둑(1202).py
+++ baekjoon/baaarkingdog/0x16/보석 도둑(1202).py
@@ -1,19 +1,13 @@
 import sys
 import heapq as h
-I, i, d = sys.stdin.readline, h.heappush, h.heappop
-a, J, l = 0, [], lambda: map(int, I().split())
-n, k = l()
-for _ in ' '*n:
-    m, v = l()
-    i(J, (-v, m))
+I, L, T = sys.stdin.readline, lambda: map(int, I().split()), []
+n, k = L()
+J = sorted((*L(),)for _ in ' '*n)
+a = j = 0
 for c in sorted(int(I())for _ in ' '*k):
-    T = []
-    while J:
-        v, m = d(J)
-        if m <= c:
-            a += -v
-            break
-        else:
-            i(T, (v, m))
-    J = [*h.merge(J, T)]
+    while j < n and c >= J[j][0]:
+        h.heappush(T, -J[j][1])
+        j += 1
+    if T:
+        a += -h.heappop(T)
 print(a)
