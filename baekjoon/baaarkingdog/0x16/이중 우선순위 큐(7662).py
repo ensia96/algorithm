1da4fea28ae72625db42ff602c8767d8a898011a
춤추는망고 <ensia96@gmail.com>
2021-12-30 14:47:03 +0900

    [update] 이중 우선순위 큐(7662) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x16/이중 우선순위 큐(7662).py
+++ baekjoon/baaarkingdog/0x16/이중 우선순위 큐(7662).py
@@ -1,11 +1,24 @@
 import heapq as h
+import collections as c
+i, d = h.heappush, h.heappop
+
+
+def D(L, f):
+    while L and not E[L[0]*(f and -1 or 1)]:
+        d(L)
+    if L:
+        E[L[0]*(f and -1 or 1)] -= 1
+        d(L)
+
+
 for _ in ' '*int(input()):
-    Q, k = [], int(input())
+    E, Q, q, k = c.defaultdict(int), [], [], int(input())
     for _ in ' '*k:
         o, n = input().split()
         n = int(n)
         if o == 'I':
-            h.heappush(Q, n)
+            E[n] += 1
+            i(Q, -n), i(q, n)
         elif Q:
-            h.heappop(Q) if n < 0 else Q.pop()
-    print(Q and f"{max(Q)} {min(Q)}" or 'EMPTY')
+            D(Q, 1) if n > 0 else D(q, 0)
+    print(Q and q and f"{d(Q)} {d(q)}" or 'EMPTY')
