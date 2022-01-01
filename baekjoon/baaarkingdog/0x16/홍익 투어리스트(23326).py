76d03acc5e8cbb2a516ddf109de4a909c2ade2dc
춤추는망고 <ensia96@gmail.com>
2022-01-01 14:34:26 +0900

    [update] 홍익 투어리스트(23326) - 다른 언어로 제출

--- baekjoon/baaarkingdog/0x16/홍익 투어리스트(23326).py
+++ baekjoon/baaarkingdog/0x16/홍익 투어리스트(23326).py
@@ -30,3 +30,56 @@ for _ in ' '*q:
                 break
         if not H:
             print(-1)
+
+# (아무리 생각해봐도 다른 풀이가 안 떠올라서, 아래 블로그에 올라온 C++ 코드 다듬어서 제출...)
+# https://blog.naver.com/PostView.naver?blogId=pasdfq&logNo=222554198268&parentCategoryNo=&categoryNo=15&viewDate=&isShowPopularPosts=true&from=search
+
+# #include <bits/stdc++.h>
+
+# using namespace std;
+
+# int n, q;
+# set<int> s;
+# int pivot = 1;
+
+# int query()
+# {
+    # if (s.empty())
+        # return -1;
+    # auto it = s.lower_bound(pivot);
+    # if (it != s.end())
+        # return *it - pivot;
+    # return *s.begin() - pivot + n;
+# }
+
+# int main()
+# {
+    # ios_base::sync_with_stdio(0);
+    # cin.tie(0);
+
+    # cin >> n >> q;
+
+    # for (int i = 1; i <= n; ++i) {
+        # int x;
+        # cin >> x;
+        # if (x)
+            # s.insert(i);
+    # }
+
+    # while (q--) {
+        # int t, x;
+        # cin >> t;
+        # if (t == 3)
+            # cout << query() << '\n';
+        # else {
+            # cin >> x;
+            # if (t == 1) {
+            # if (s.count(x))
+            # s.erase(x);
+            # else
+            # s.insert(x);
+            # } else
+            # pivot = (pivot - 1 + x) % n + 1;
+        # }
+    # }
+# }
