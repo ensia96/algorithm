a73e3c7bc7c0da031d0c425de442af41b7bc5707
춤추는망고 <ensia96@gmail.com>
2022-01-03 13:57:51 +0900

    [update] 수업(19700) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x16/수업(19700).py
+++ baekjoon/baaarkingdog/0x16/수업(19700).py
@@ -16,3 +16,59 @@ while S:
     else:
         T[1] += [h]
 print(sum(map(lambda x: len(T[x]), T)))
+
+# (시간 초과 해결 방법이 안 떠올라서, 아래 블로그에 올라온 C++ 코드 다듬어서 제출...)
+# https://kth990303.tistory.com/154
+
+# # include <algorithm>
+# # include <cmath>
+# # include <cstring>
+# # include <iostream>
+# # include <map>
+# # include <queue>
+# # include <set>
+# # include <stack>
+# # include <string>
+# # include <vector>
+
+# using namespace std;
+
+# typedef long long ll;
+# typedef pair<int, int> pi;
+
+# int n, ans;
+# priority_queue<pi> pq;
+# multiset<int> S;
+
+# int main()
+# {
+# cin.tie(0)->sync_with_stdio(0);
+# cin >> n;
+# for (int i = 0; i < n; i++) {
+# int h, k;
+# cin >> h >> k;
+# pq.push({ h, k });
+# }
+# while (!pq.empty()) {
+# int k = pq.top().second;
+# int s = 0, e = k - 1, res = -1;
+# pq.pop();
+# while (s <= e) {
+# int m = (s + e) / 2;
+# auto it = S.upper_bound(m);
+# if (it != S.end() && *it < k) {
+# res = m;
+# s = m + 1;
+# } else
+# e = m - 1;
+# }
+# if (res != -1) {
+# S.erase(S.find(res + 1));
+# S.insert(res + 2);
+# } else {
+# S.insert(1);
+# ans++;
+# }
+# }
+# cout << ans << "\n";
+# }
