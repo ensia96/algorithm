fe1122a1730bfd408423a0b86741ad9e89de5da6
춤추는망고 <ensia96@gmail.com>
2022-01-13 14:37:44 +0900

    [update] 이분 그래프(1707) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x18/이분 그래프(1707).py
+++ baekjoon/baaarkingdog/0x18/이분 그래프(1707).py
@@ -1,29 +1,30 @@
 import sys
 I = sys.stdin.readline
+
+
+def B(i):
+    q, S[i] = [(i, 0)], 0
+    for p, d in q:
+        for c in C[p]:
+            if S[c] == d:
+                return 0
+            elif S[c] < 0:
+                q += [(c, not d)]
+                S[c] = not d
+    return 1
+
+
 for _ in ' '*int(I()):
     v, e = map(int, I().split())
     v += 1
-    C, S = [[]for _ in ' '*v], [-1]*v
+    C, S, A = [[]for _ in ' '*v], [-1]*v, 0
     for _ in ' '*e:
         a, b = map(int, I().split())
         C[a], C[b] = C[a]+[b], C[b]+[a]
     for i in range(1, v):
         if S[i] >= 0:
             continue
-        q, S[i] = [(i, 0)], 0
-        for p, d in q:
-            for c in C[p]:
-                if S[c] == d:
-                    print('NO')
-                    break
-                elif S[c] < 0:
-                    q += [(c, not d)]
-                    S[c] = not d
-            else:
-                continue
+        A = B(i)
+        if not A:
             break
-        else:
-            continue
-        break
-    else:
-        print('YES')
+    print(['NO', 'YES'][A])
