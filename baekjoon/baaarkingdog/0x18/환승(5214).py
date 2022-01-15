033ca83d20d726a398b67b87aa71168b62aa3021
춤추는망고 <ensia96@gmail.com>
2022-01-15 15:17:44 +0900

    환승(5214)

---
+++ baekjoon/baaarkingdog/0x18/환승(5214).py
@@ -0,0 +1,17 @@
+n, k, m = map(int, input().split())
+L, A = [[]for _ in ' '*(n+1)], 10**11
+for i in range(m):
+    I = [*map(int, input().split())]
+    L += [I]
+    for s in I:
+        L[s] += [n+i+1]
+Q, V = [(1, 1)], [0, 1]+[0]*(n+m-1)
+for i, d in Q:
+    if i == n:
+        A = min(A, d)
+    for s in L[i]:
+        if not V[s]:
+            Q += [(s, d+(s <= n))]
+            if s <= n:
+                V[s] = 1
+print([A, -1][A == 10**11])
