c7b61f7e9f21b203cd91bf7d5417c7d1dfaf4e4a
춤추는망고 <ensia96@gmail.com>
2022-01-15 15:20:40 +0900

    [update] 환승(5214) - 런타임 에러 (TypeError) 해결

--- baekjoon/baaarkingdog/0x18/환승(5214).py
+++ baekjoon/baaarkingdog/0x18/환승(5214).py
@@ -4,16 +4,16 @@ n, k, m = map(int, I().split())
 L, A = [[]for _ in ' '*(n+1)], 10**11
 for i in range(m):
     I = [*map(int, I().split())]
-    L += [I]
+    L.append(I)
     for s in I:
-        L[s] += [n+i+1]
+        L[s].append(n+i+1)
 Q, V = [(1, 1)], [0, 1]+[0]*(n+m-1)
 for i, d in Q:
     if i == n:
         A = min(A, d)
     for s in L[i]:
         if not V[s]:
-            Q += [(s, d+(s <= n))]
+            Q.append((s, d+(s <= n)))
             if s <= n:
                 V[s] = 1
 print([A, -1][A == 10**11])
