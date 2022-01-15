e93b1706ea0986834e991acf1e357b3d9e618d10
춤추는망고 <ensia96@gmail.com>
2022-01-15 15:38:59 +0900

    [update] 환승(5214) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x18/환승(5214).py
+++ baekjoon/baaarkingdog/0x18/환승(5214).py
@@ -1,19 +1,17 @@
 import sys
 I = sys.stdin.readline
 n, k, m = map(int, I().split())
-L, A = [[]for _ in ' '*(n+1)], 10**11
+L = [[]for _ in ' '*(n+m+1)]
 for i in range(m):
-    T = [*map(int, I().split())]
-    L.append(T)
-    for s in T:
-        L[s].append(n+i+1)
-Q, V = [(1, 1)], [0, 1]+[0]*(n+m-1)
-for i, d in Q:
+    for j in map(int, I().split()):
+        L[j].append(n+i+1)
+        L[n+i+1].append(j)
+Q, V = [1], [0, 1]+[0]*(n+m-1)
+for i in Q:
     if i == n:
-        A = min(A, d)
+        exit(print(V[i]))
     for s in L[i]:
         if not V[s]:
-            Q.append((s, d+(s <= n)))
-            if s <= n:
-                V[s] = 1
-print([A, -1][A == 10**11])
+            Q.append(s)
+            V[s] = V[i]+(s > n)
+print(-1)
