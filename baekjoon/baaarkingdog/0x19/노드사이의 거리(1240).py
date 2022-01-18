72099d6ee24fd11846451a86c5ff3dd7e86195c1
춤추는망고 <ensia96@gmail.com>
2022-01-18 13:13:46 +0900

    [update] 노드사이의 거리(1240) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x19/노드사이의 거리(1240).py
+++ baekjoon/baaarkingdog/0x19/노드사이의 거리(1240).py
@@ -4,15 +4,14 @@ D = [[0]*n for _ in ' '*n]
 for _ in ' '*(n-2):
     u, v, d = map(int, input().split())
     D[u][v] = D[v][u] = d
-for i in range(1, n):
-    q, v = [i], [0]*n
-    v[i] = 1
-    for j in q:
-        for k in range(n):
-            if not v[k] and D[j][k]:
-                v[k] = 1
-                D[i][k] = D[k][i] = min(D[i][k] or 10001, D[i][j]+D[j][k])
-                q += [k]
 for _ in ' '*m:
     a, b = map(int, input().split())
-    print(D[a][b])
+    q, v = [(a, 0)], [0]*n
+    for i, d in q:
+        if i == b:
+            print(d)
+            break
+        for j in range(1, n):
+            if D[i][j] and not v[j]:
+                v[j] = 1
+                q += [(j, D[i][j]+d)]
