2dca9293fe37083be75ce396f8addf3c3b6fb089
춤추는망고 <ensia96@gmail.com>
2022-01-18 13:46:02 +0900

    [update] 노드사이의 거리(1240) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x19/노드사이의 거리(1240).py
+++ baekjoon/baaarkingdog/0x19/노드사이의 거리(1240).py
@@ -4,14 +4,19 @@ D = [[0]*n for _ in ' '*n]
 for _ in ' '*(n-2):
     u, v, d = map(int, input().split())
     D[u][v] = D[v][u] = d
+
+
+def F(i, d):
+    if i == b:
+        print(d)
+        return
+    for j in range(1, n):
+        if D[i][j] and not v[j]:
+            v[j] = 1
+            F(j, D[i][j]+d)
+
+
 for _ in ' '*m:
     a, b = map(int, input().split())
-    q, v = [(a, 0)], [0]*n
-    for i, d in q:
-        if i == b:
-            print(d)
-            break
-        for j in range(1, n):
-            if D[i][j] and not v[j]:
-                v[j] = 1
-                q += [(j, D[i][j]+d)]
+    v = [0]*n
+    F(a, 0)
