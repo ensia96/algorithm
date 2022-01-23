ad83e6e59c0752377aff6083d6251b9fdaf3731d
춤추는망고 <ensia96@gmail.com>
2022-01-23 14:14:30 +0900

    [update] 트리의 지름(1967) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x19/트리의 지름(1967).py
+++ baekjoon/baaarkingdog/0x19/트리의 지름(1967).py
@@ -1,15 +1,6 @@
 n = int(input())
-C, D = [[]for _ in ' '*-~n], [0]*-~n
-for _ in ' '*~-n:
-    p, c, w = map(int, input().split())
-    C[p] += (c, w),
-    C[c] += (p, w),
-for i in range(1, -~n):
-    q, v = [(i, 0)], [0]*-~n
-    v[i] = 1
-    for p, d in q:
-        for c, w in C[p]:
-            if not v[c]:
-                v[c], D[i] = 1, max(D[i], w+d)
-                q += [(c, d+w)]
-print(max(D))
+A, D = 0, [0]*-~n
+for p, c, w in [(*map(int, input().split()),)for _ in ' '*~-n][::-1]:
+    x, y = D[p], D[c]+w
+    A, D[p] = max(A, x+y), max(x, y)
+print(A)
