91ab97d4979a4af39580c2973caf13d45f8a30f4
춤추는망고 <ensia96@gmail.com>
2022-01-28 13:55:31 +0900

    [update] 작업(2056) - 런타임 에러 (ValueError) 해결

--- baekjoon/baaarkingdog/0x1A/작업(2056).py
+++ baekjoon/baaarkingdog/0x1A/작업(2056).py
@@ -1,9 +1,9 @@
 n = int(input())
 n += 1
-W, A = [0]*n, [()]
+W, A = [0]*n, [0]
 for i in range(1, n):
     t, a, *T = map(int, input().split())
     W[i], A = t, A+[T]
-for i in range(2, n):
-    W[i] += max(W[j]for j in A[i])
+for i in range(n):
+    W[i] += max(W[j]for j in A[i] or [0])
 print(max(W))
