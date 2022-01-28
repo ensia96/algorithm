014e0c1ee00cf6531c65becc60b2d1f966e5cdd8
춤추는망고 <ensia96@gmail.com>
2022-01-28 13:59:04 +0900

    [update] 작업(2056) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x1A/작업(2056).py
+++ baekjoon/baaarkingdog/0x1A/작업(2056).py
@@ -1,9 +1,6 @@
 n = int(input())
-n += 1
-W, A = [0]*n, [0]
-for i in range(1, n):
-    t, a, *T = map(int, input().split())
-    W[i], A = t, A+[T]
-for i in range(n):
-    W[i] += max(W[j]for j in A[i] or [0])
+W = [0]
+for _ in ' '*n:
+    w = [*map(int, input().split())]
+    W += w[0]+max(W[j]for j in w[2:]+[0]),
 print(max(W))
