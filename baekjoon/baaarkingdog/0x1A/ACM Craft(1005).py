98feba40fd5dae0e9898efa593626630a8b336cb
춤추는망고 <ensia96@gmail.com>
2022-01-29 15:39:13 +0900

    [update] ACM Craft(1005) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x1A/ACM Craft(1005).py
+++ baekjoon/baaarkingdog/0x1A/ACM Craft(1005).py
@@ -3,13 +3,13 @@ I = sys.stdin.readline
 L, T = lambda: map(int, I().split()), int(I())
 
 
-def F(x): A[x] = max(A[i] > ~0 and A[i] or F(i)
+def F(x): A[x] = max(F(i)if A[i] < 0 else A[i]
                      for i in C[x] or [0])+D[x]if C[x]else D[x]; return A[x]
 
 
 for _ in ' '*T:
     n, k = L()
-    A, C, D = [-1]*-~n, [0]+[[]for _ in ' '*n], [0]+[*L()]
+    A, C, D = [-1]*-~n, [[]for _ in ' '*-~n], [0]+[*L()]
     for _ in ' '*k:
         a, b = L()
         C[b] += a,
