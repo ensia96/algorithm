eac30813c5068344bd7e0b969e6a84e825f05a9e
춤추는망고 <ensia96@gmail.com>
2022-01-29 15:45:12 +0900

    [update] ACM Craft(1005) - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x1A/ACM Craft(1005).py
+++ baekjoon/baaarkingdog/0x1A/ACM Craft(1005).py
@@ -1,11 +1,14 @@
 import sys
 I = sys.stdin.readline
-sys.setrecursionlimit(10**9)
 L, T = lambda: map(int, I().split()), int(I())
 
 
-def F(x): A[x] = max(F(i)if A[i] < 0 else A[i]
-                     for i in C[x] or [0])+D[x]if C[x]else D[x]; return A[x]
+def F(x):
+    for i in C[x]:
+        if A[i] < 0:
+            A[i] = F(i)
+        A[x] = max(A[x], A[i])
+    return A[x]+D[x]
 
 
 for _ in ' '*T:
@@ -14,4 +17,4 @@ for _ in ' '*T:
     for _ in ' '*k:
         a, b = L()
         C[b] += a,
-    print(F(int(I())))
+    print(F(int(I()))+1)
