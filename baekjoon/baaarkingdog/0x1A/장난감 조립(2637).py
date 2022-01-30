e890b1b2e360446aaac180fb5f161c8cb5220161
춤추는망고 <ensia96@gmail.com>
2022-01-30 12:03:37 +0900

    [update] 장난감 조립(2637) - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x1A/장난감 조립(2637).py
+++ baekjoon/baaarkingdog/0x1A/장난감 조립(2637).py
@@ -1,6 +1,7 @@
+import collections as c
 l, A = lambda: map(int, input().split()), {}
 n = next(l())
-C, Q = [{}for _ in ' '*-~n], [(n, 1)]
+C, Q = c.defaultdict(dict), [(n, 1)]
 for _ in ' '*next(l()):
     x, y, k = l()
     C[x][y] = k
