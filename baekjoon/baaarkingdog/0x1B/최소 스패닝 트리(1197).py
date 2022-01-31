12f157ff1ccfaf78d739c2e7227fe16230c01c0e
춤추는망고 <ensia96@gmail.com>
2022-01-31 18:15:43 +0900

    [update] 최소 스패닝 트리(1197) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x1B/최소 스패닝 트리(1197).py
+++ baekjoon/baaarkingdog/0x1B/최소 스패닝 트리(1197).py
@@ -5,22 +5,14 @@ G = [*range(v+1)]
 
 
 def F(x):
-    if G[x] == x:
-        return x
-    G[x] = F(G[x])
+    if G[x] != x:
+        G[x] = F(G[x])
     return G[x]
 
 
-def D(u, v):
-    u, v = F(u), F(v)
-    if u < v:
-        G[v] = u
-    else:
-        G[u] = v
-
-
 for c, a, b in sorted((*map(int, l.split()[::-1]),)for l in L):
-    if F(a) != F(b):
-        D(a, b)
+    a, b = F(a), F(b)
+    if f(a) != f(b):
+        G[max(a, b)] = min(a, b)
         A += c
 print(A)
