f79fcaff1c89c62d1732408b7312938da953077b
춤추는망고 <ensia96@gmail.com>
2022-01-31 18:12:31 +0900

    [update] 최소 스패닝 트리(1197) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x1B/최소 스패닝 트리(1197).py
+++ baekjoon/baaarkingdog/0x1B/최소 스패닝 트리(1197).py
@@ -1,11 +1,11 @@
 I, *L = open(0)
 v, e = map(int, I.split())
 A = C = 0
-G = [-1]*-~v
+G = [*range(v+1)]
 
 
 def F(x):
-    if G[x] < 0:
+    if G[x] == x:
         return x
     G[x] = F(G[x])
     return G[x]
@@ -13,22 +13,14 @@ def F(x):
 
 def D(u, v):
     u, v = F(u), F(v)
-    if u == v:
-        return 0
-    if G[u] == G[v]:
-        G[u] -= 1
-    if G[u] < G[v]:
+    if u < v:
         G[v] = u
     else:
         G[u] = v
-    return 1
 
 
 for c, a, b in sorted((*map(int, l.split()[::-1]),)for l in L):
-    if D(a, b):
-        continue
-    A += c
-    C += 1
-    if not C+1-v:
-        break
+    if F(a) != F(b):
+        D(a, b)
+        A += c
 print(A)
