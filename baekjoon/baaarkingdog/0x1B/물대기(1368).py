64daef4bbadd7187afc6ed55bd75488b4fa58e7f
춤추는망고 <ensia96@gmail.com>
2022-02-02 13:14:43 +0900

    [update] 물대기(1368) - 파이썬 풀이 추가

--- baekjoon/baaarkingdog/0x1B/물대기(1368).py
+++ baekjoon/baaarkingdog/0x1B/물대기(1368).py
@@ -1,35 +1,16 @@
-# 설명을 듣고도 전혀 감이 안 와서, 인터넷에서 풀이 찾아봄..
-# https://lastknight00.tistory.com/123
-
-# #include <algorithm>
-# #include <cstdio>
-# #include <vector>
-
-# using namespace std;
-# typedef pair<int, pair<int, int>> P;
-# int n, t, m, i, j, s, g[301];
-# vector<P> v;
-# int f(int a) { return a == g[a] ? a : g[a] = f(g[a]); }
-# void u(int a, int b) { g[f(a)] = f(b); }
-
-# int main()
-# {
-# scanf("%d", &n);
-# for (i = 1; i <= n; i++)
-# scanf("%d", &t), v.push_back({ t, { i, 0 } }), g[i] = i;
-# for (i = 1; i <= n; i++) {
-# for (j = 1; j <= n; j++) {
-# scanf("%d", &t);
-# if (i < j)
-# v.push_back({ t, { i, j } });
-# }
-# }
-# sort(v.begin(), v.end());
-# for (P p : v) {
-# i = p.second.first;
-# j = p.second.second;
-# if (f(i) != f(j))
-# s += p.first, u(i, j);
-# }
-# printf("%d", s);
-# }
+import heapq as h
+n = int(input())
+G = [int(input())for _ in ' '*n]
+G = [(0, *G)]+[(G[i], *map(int, input().split()))for i in range(n)]
+V, Q = [0]*(n+1), [(0, 1)]
+E = A = 0
+while Q:
+    w, i = h.heappop(Q)
+    if V[i]:
+        continue
+    V[i], A, E = 1, A+w, E+1
+    if E == n-1:
+        break
+    for j in range(n+1):
+        h.heappush(Q, (G[i][j], j))
+print(A)
