b02f1dad1ad8e747a21ed163f5cd6c920c439777
춤추는망고 <ensia96@gmail.com>
2022-02-04 14:04:38 +0900

    [update] 도시 분할 계획(1647) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x1B/도시 분할 계획(1647).py
+++ baekjoon/baaarkingdog/0x1B/도시 분할 계획(1647).py
@@ -6,14 +6,15 @@ for l in L:
     i, j, w = map(int, l.split())
     C[i] += (w, j),
     C[j] += (w, i),
-A, Q, V = [], [(0, 1)], [0]*-~n
+Q, V = [(0, 1)], [0]*-~n
+A = E = W = 0
 while Q:
     w, i = h.heappop(Q)
     if V[i]:
         continue
-    V[i], A = 1, A+[w]
-    if len(A) == n:
+    V[i], A, E, W = 1, A+w, E+1, max(W, w)
+    if E == n:
         break
     for w, j in C[i]:
         h.heappush(Q, (w, j))
-print(sum(A[:-1]))
+print(A-W)
