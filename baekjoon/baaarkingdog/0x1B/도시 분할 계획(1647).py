02db145b403a7d1e72c48447c3d05955baf499e0
춤추는망고 <ensia96@gmail.com>
2022-02-04 14:00:47 +0900

    [update] 도시 분할 계획(1647) - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x1B/도시 분할 계획(1647).py
+++ baekjoon/baaarkingdog/0x1B/도시 분할 계획(1647).py
@@ -6,14 +6,14 @@ for l in L:
     i, j, w = map(int, l.split())
     C[i] += (w, j),
     C[j] += (w, i),
-A, Q, V = [], [(0, 1, 1)], [0]*-~n
+A, Q, V = [], [(0, 1)], [0]*-~n
 while Q:
-    w, i, _ = h.heappop(Q)
+    w, i = h.heappop(Q)
     if V[i]:
         continue
     V[i], A = 1, A+[w]
-    if len(A) == n-1:
+    if len(A) == n:
         break
     for w, j in C[i]:
-        h.heappush(Q, (w, j, i))
-print(sum(A))
+        h.heappush(Q, (w, j))
+print(sum(A[:-1]))
