91ebae9aef891f358897a760fb7f919cd2b99a3b
춤추는망고 <ensia96@gmail.com>
2022-02-05 12:48:42 +0900

    학교 탐방하기(13418)

---
+++ baekjoon/baaarkingdog/0x1B/학교 탐방하기(13418).py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+n += 1
+E = [(*map(int, input().split()[::-1]),)for _ in ' '*-~m]
+
+
+def F(r):
+    def f(x):
+        if x != P[x]:
+            P[x] = f(P[x])
+        return P[x]
+    P, L = [*range(n+1)], [0]*n
+    a = e = 0
+    for w, i, j in sorted(E, reverse=r):
+        i, j = f(i), f(j)
+        if i == j:
+            continue
+        a, e = a+w, e+1
+        if e == n-1:
+            return a**2
+        if L[i] < L[j]:
+            i, j = j, i
+        P[i], L[i] = j, L[i]+1
+
+
+print(F(1)-F(0))
