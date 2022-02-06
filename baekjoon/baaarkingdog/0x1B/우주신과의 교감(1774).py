50e2c0dbbdd69b001123725d05d97ee5bf36d3c0
춤추는망고 <ensia96@gmail.com>
2022-02-06 15:47:24 +0900

    [update] 우주신과의 교감(1774) - 프림 알고리즘 풀이 추가

--- baekjoon/baaarkingdog/0x1B/우주신과의 교감(1774).py
+++ baekjoon/baaarkingdog/0x1B/우주신과의 교감(1774).py
@@ -1,32 +1,28 @@
+import heapq as h
 l, t = lambda: map(int, input().split()), lambda a, b: (a**2+b**2)**.5
 n, m = l()
 n += 1
+D = [[0]*n for _ in ' '*n]
 L = {i: (*l(),)for i in range(1, n)}
-Q = [(0, *l())for _ in ' '*m]
 for i in range(1, n):
-    for j in range(i+1, n):
+    for j in range(1, n):
+        if i == j:
+            continue
         a, b = L[i]
         c, d = L[j]
-        Q.append((t(a-c, b-d), i, j))
-Q.sort()
-P, L = [*range(n)], [0]*n
+        D[i][j] = t(a-c, b-d)
+for _ in ' '*m:
+    i, j = l()
+    D[i][j] = D[j][i] = 0
+Q, V = [(0, 1)], [0]*n
 A = E = 0
-
-
-def f(x):
-    if P[x] != x:
-        P[x] = f(P[x])
-    return P[x]
-
-
-for w, i, j in Q:
-    i, j = f(i), f(j)
-    if i == j:
+while Q:
+    w, i = h.heappop(Q)
+    if V[i]:
         continue
-    A, E = A+w, E+1
-    if E == n-2:
+    V[i], A, E = 1, A+w, E+1
+    if E == n-1:
         break
-    if L[i] < L[j]:
-        i, j = j, i
-    P[j], L[i] = P[i], L[i]+1
+    for j in range(1, n):
+        h.heappush(Q, (D[i][j], j))
 print(f"{A:.2f}")
