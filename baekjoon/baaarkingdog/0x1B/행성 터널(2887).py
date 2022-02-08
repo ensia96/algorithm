2111c93c430b3ef977aab679765e55724cde5c02
춤추는망고 <ensia96@gmail.com>
2022-02-08 15:56:23 +0900

    [update] 행성 터널(2887) - 프림 알고리즘 풀이 추가

--- baekjoon/baaarkingdog/0x1B/행성 터널(2887).py
+++ baekjoon/baaarkingdog/0x1B/행성 터널(2887).py
@@ -1,27 +1,23 @@
 import sys
+import heapq as h
 I = sys.stdin.readline
 n = int(I())
 P = [(*map(int, I().split()), i)for i in range(n)]
-R, E = [i for i in range(n)], []
-
-
-def f(x):
-    if R[x] != x:
-        R[x] = f(R[x])
-    return R[x]
-
-
+G = [[]for _ in ' '*n]
 for j in range(3):
     P.sort(key=lambda x: x[j])
     for i in range(1, n):
-        E += (abs(P[i][j]-P[i-1][j]), P[i-1][3], P[i][3]),
-E.sort()
-a = e = 0
-for w, i, j in E:
-    i, j = f(i), f(j)
-    if i == j:
+        w = abs(P[i][j]-P[i-1][j])
+        G[P[i-1][3]] += (w, P[i][3]),
+Q, V = [(0, 0)], [0]*n
+A = E = 0
+while Q:
+    w, i = h.heappop(Q)
+    if V[i]:
         continue
-    a, e, R[j] = a+w, e+1, i
-    if e == n-1:
+    V[i], A, E = 1, A+w, E+1
+    if E == n:
         break
-print(a)
+    for D in G[i]:
+        h.heappush(Q, D)
+print(A)
