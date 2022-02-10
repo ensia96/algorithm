9d14a255d93527b29ffde30573c796c042061668
춤추는망고 <ensia96@gmail.com>
2022-02-10 14:08:34 +0900

    [update] 0x0D/상어 초등학교(21608) - 컴파일 에러 해결

--- baekjoon/baaarkingdog/0x0D/상어 초등학교(21608).py
+++ baekjoon/baaarkingdog/0x0D/상어 초등학교(21608).py
@@ -12,7 +12,8 @@ for _ in ' '*N:
         a = b = 0
         for x, y in g(i, j):
             if f(x)*f(y):
-                a += D[x][y] in Lb += not D[x][y]
+                a += D[x][y] in L
+                b += not D[x][y]
         A += (-a, -b, i, j),
     a, b, c, d = sorted(A)[0]
     D[c][d] = s
