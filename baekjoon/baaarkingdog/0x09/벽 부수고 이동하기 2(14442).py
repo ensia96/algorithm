e0506709a8fa1a125b6c9d6f91d77e915d132ed4
춤추는망고 <ensia96@gmail.com>
2022-02-18 15:40:17 +0900

    [update] 0x09/벽 부수고 이동하기 2(14442) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x09/벽 부수고 이동하기 2(14442).py
+++ baekjoon/baaarkingdog/0x09/벽 부수고 이동하기 2(14442).py
@@ -1,19 +1,18 @@
-import collections as c
-M = 2**20
 n, m, k = map(int, input().split())
 B = [[*map(int, input())]for _ in ' '*n]
-D = [[[M]*-~k for _ in ' '*m]for _ in ' '*n]
-D[0][0][0], Q = 1, c.deque([(1, 0, 0, 0)])
+D, d = [[0]*m for _ in ' '*n], 1
+D[0][0], Q = 1, [(0, 0, k)]
 while Q:
-    d, i, j, h = Q.popleft()
-    for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-        if not (0 <= x < n)*(0 <= y < m):
-            continue
-        z = h+B[x][y]
-        if z > k:
-            continue
-        if D[x][y][z] == M:
-            D[x][y][z] = d+1
-            Q.append((d+1, x, y, z))
-A = min(D[-1][-1])
-print(-(A == M) or A)
+    q = []
+    for i, j, h in Q:
+        if (i, j) == (n-1, m-1):
+            exit(print(d))
+        for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+            if not (0 <= x < n)*(0 <= y < m):
+                continue
+            z = h-B[x][y]
+            if D[x][y] < z:
+                D[x][y] = z
+                q += (x, y, z),
+    Q, d = q, d+1
+print(-1)
