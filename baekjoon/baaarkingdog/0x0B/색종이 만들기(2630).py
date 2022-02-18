bd867906ab22385d2a6ff16e33070fc3ba054bd6
춤추는망고 <ensia96@gmail.com>
2022-02-18 12:58:57 +0900

    [update] 0x0B/색종이 만들기(2630) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x0B/색종이 만들기(2630).py
+++ baekjoon/baaarkingdog/0x0B/색종이 만들기(2630).py
@@ -3,17 +3,13 @@ A = [0]*2
 
 
 def S(n, P):
-    if n == 1:
-        A[P[0]] += 1
+    if n == 1 or sum(map(sum, P)) in [0, n**2]:
+        A[P[0][0]] += 1
         return
     for i in range(2):
         T = P[i*(n//2):(i+1)*(n//2)]
         for j in range(2):
-            t = [t[j*(n//2):(j+1)*(n//2)]for t in T]
-            if sum(map(sum, t)) in [0, (n//2)**2]:
-                A[t[0][0]] += 1
-                continue
-            S(n//2, t)
+            S(n//2, [t[j*(n//2):(j+1)*(n//2)]for t in T])
 
 
 S(n, [[*map(int, input().split())]for _ in ' '*n])
