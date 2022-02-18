898be16ccc777e13436f0f8710bf2e91b9cc1329
춤추는망고 <ensia96@gmail.com>
2022-02-18 15:05:50 +0900

    [update] 0x09/벽 부수고 이동하기 2(14442) - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x09/벽 부수고 이동하기 2(14442).py
+++ baekjoon/baaarkingdog/0x09/벽 부수고 이동하기 2(14442).py
@@ -1,9 +1,11 @@
+import collections as c
 M = 2**20
 n, m, k = map(int, input().split())
 B = [[*map(int, input())]for _ in ' '*n]
 D = [[[M]*-~k for _ in ' '*m]for _ in ' '*n]
-R, Q = lambda x, y: 0 <= x < n and 0 <= y < m, [(1, 0, 0, 0)]
-for d, i, j, h in Q:
+R, Q = lambda x, y: 0 <= x < n and 0 <= y < m, c.deque((1, 0, 0, 0),)
+while Q:
+    d, i, j, h = Q.popleft()
     for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
         if not R(x, y):
             continue
