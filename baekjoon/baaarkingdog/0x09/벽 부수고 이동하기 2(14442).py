9c09ae82b86a49a985d050d9ead420a303ec877a
춤추는망고 <ensia96@gmail.com>
2022-02-18 15:00:27 +0900

    [update] 0x09/벽 부수고 이동하기 2(14442) - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x09/벽 부수고 이동하기 2(14442).py
+++ baekjoon/baaarkingdog/0x09/벽 부수고 이동하기 2(14442).py
@@ -2,7 +2,6 @@ M = 2**20
 n, m, k = map(int, input().split())
 B = [[*map(int, input())]for _ in ' '*n]
 D = [[[M]*-~k for _ in ' '*m]for _ in ' '*n]
-V = [[[0]*-~k for _ in ' '*m]for _ in ' '*n]
 R, Q = lambda x, y: 0 <= x < n and 0 <= y < m, [(1, 0, 0, 0)]
 for d, i, j, h in Q:
     for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
@@ -11,9 +10,8 @@ for d, i, j, h in Q:
         z = h+B[x][y]
         if z > k:
             continue
-        D[x][y][z] = min(d+1, D[x][y][z])
-        if not V[x][y][z]:
-            V[x][y][z] = 1
+        if D[x][y][z] > d+1:
+            D[x][y][z] = d+1
             Q += [(d+1, x, y, z)]
 A = min(D[-1][-1])
 print(-(A == M) or A)
