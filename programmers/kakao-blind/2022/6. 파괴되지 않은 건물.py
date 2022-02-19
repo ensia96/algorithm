a7a5555da95d3b86b4913c08dea2dc2e591a58e3
춤추는망고 <ensia96@gmail.com>
2022-02-19 19:13:15 +0900

    programmers/kakao-blind/2022/6. 파괴되지 않은 건물

--- programmers/kakao-blind/2022/6. 파괴되지 않은 건물.py
+++ programmers/kakao-blind/2022/6. 파괴되지 않은 건물.py
@@ -1,3 +1,24 @@
+def solution(board, skill):
+    R = range
+    n, m = len(board), len(board[0])
+    C = [[0]*-~m for _ in ' '*-~n]
+
+    for t, a, b, c, d, e in skill:
+        f = (2*(t-1)-1)*e
+        C[a][b] += f
+        C[a][d+1] -= f
+        C[c+1][b] -= f
+        C[c+1][d+1] += f
+
+    for i in R(1, n+1):
+        for j in R(m+1):
+            C[i][j] += C[i-1][j]
+    for i in R(n+1):
+        for j in R(1, m+1):
+            C[i][j] += C[i][j-1]
+
+    return sum(board[i][j]+C[i][j] > 0 for i in R(n)for j in R(m))
+
 # 신입 공채 당시에 제출했던 코드
 #
 # # 정확성 통과, 효율성 0
