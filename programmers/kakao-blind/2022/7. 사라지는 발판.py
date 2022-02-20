5f5dd46c0db1404ee5556597b94c5ac8fc933ee9
춤추는망고 <ensia96@gmail.com>
2022-02-20 22:13:27 +0900

    programmers/kakao-blind/2022/7. 사라지는 발판

--- programmers/kakao-blind/2022/7. 사라지는 발판.py
+++ programmers/kakao-blind/2022/7. 사라지는 발판.py
@@ -1,3 +1,24 @@
+def solution(board, aloc, bloc):
+    n, m = len(board), len(board[0])
+    visited = [[0]*m for _ in ' '*n]
+
+    def simulate(a, b, c, d):
+        if visited[a][b]:
+            return 0
+        result = 0
+        for x, y in [(a, b-1), (a, b+1), (a-1, b), (a+1, b)]:
+            if (0 <= x < n)*(0 <= y < m) and not visited[x][y] and board[x][y]:
+                visited[a][b] = 1
+                value = simulate(c, d, x, y)+1
+                visited[a][b] = 0
+                if result % 2:
+                    result = min(result, value) if value % 2 else result
+                else:
+                    result = value if value % 2 else max(result, value)
+        return result
+
+    return simulate(*aloc, *bloc)
+
 # 신입 공채 당시에 제출했던 코드
 #
 # def simulate(f, aloc, bloc, n, m, board, count):
