5749d72124f4ea8b9d103f4b241ab4e71bf9deba
춤추는망고 <ensia96@gmail.com>
2022-02-25 22:55:58 +0900

    [update] 플로이드 2(11780) - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x1C/플로이드 2(11780).py
+++ baekjoon/baaarkingdog/0x1C/플로이드 2(11780).py
@@ -1,10 +1,11 @@
 M = 2**27
-n = int(input())+1
-N = range(1, n)
+n = int(input())
+N = range(n)
 C = [[M]*n for _ in ' '*n]
 R = [[0]*n for _ in ' '*n]
 for _ in ' '*int(input()):
     a, b, c = map(int, input().split())
+    a, b = a-1, b-1
     C[a][b], R[a][b] = min(C[a][b], c), b
 for k in N:
     C[k][k] = 0
@@ -13,11 +14,11 @@ for k in N:
             t = C[i][k]+C[k][j]
             if C[i][j] > t:
                 C[i][j], R[i][j] = t, R[i][k]
-for l in C[1:]:
-    print(*l[1:])
+for l in C:
+    print(*l)
 for i in N:
     for j in N:
-        r = [i]
-        while r[-1] != j:
-            r += R[r[-1]][j],
+        r = [i+1]
+        while r[-1]-1 != j:
+            r += R[r[-1]-1][j]+1,
         print(len(r), *r)if len(r) > 1 else print(0)
