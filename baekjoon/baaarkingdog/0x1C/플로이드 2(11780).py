1d4a2823645f8bace2f6c66d827e5e74a5a34aad
춤추는망고 <ensia96@gmail.com>
2022-02-25 23:06:03 +0900

    [update] 플로이드 2(11780) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x1C/플로이드 2(11780).py
+++ baekjoon/baaarkingdog/0x1C/플로이드 2(11780).py
@@ -1,11 +1,10 @@
 M = 2**27
-n = int(input())
-N = range(n)
+n = int(input())+1
+N = range(1, n)
 C = [[M]*n for _ in ' '*n]
 R = [[0]*n for _ in ' '*n]
 for _ in ' '*int(input()):
     a, b, c = map(int, input().split())
-    a, b = a-1, b-1
     C[a][b], R[a][b] = min(C[a][b], c), b
 for k in N:
     C[k][k] = 0
@@ -14,14 +13,14 @@ for k in N:
             t = C[i][k]+C[k][j]
             if C[i][j] > t:
                 C[i][j], R[i][j] = t, R[i][k]
-for l in C:
-    print(*l)
+for l in C[1:]:
+    print(*l[1:])
 for i in N:
     for j in N:
         if not R[i][j]:
             print(0)
             continue
-        r = [i+1]
-        while r[-1]-1 != j:
-            r += R[r[-1]-1][j]+1,
+        r = [i]
+        while r[-1] != j:
+            r += R[r[-1]][j],
         print(len(r), *r)
