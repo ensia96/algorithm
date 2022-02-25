d1ce24bf2ded3dfa3a8634721f3d26334df9faf9
춤추는망고 <ensia96@gmail.com>
2022-02-25 22:49:36 +0900

    플로이드 2(11780)

---
+++ baekjoon/baaarkingdog/0x1C/플로이드 2(11780).py
@@ -0,0 +1,23 @@
+M = 2**27
+n = int(input())+1
+N = range(1, n)
+C = [[M]*n for _ in ' '*n]
+R = [[0]*n for _ in ' '*n]
+for _ in ' '*int(input()):
+    a, b, c = map(int, input().split())
+    C[a][b], R[a][b] = min(C[a][b], c), b
+for k in N:
+    C[k][k] = 0
+    for i in N:
+        for j in N:
+            t = C[i][k]+C[k][j]
+            if C[i][j] > t:
+                C[i][j], R[i][j] = t, R[i][k]
+for l in C[1:]:
+    print(*l[1:])
+for i in N:
+    for j in N:
+        r = [i]
+        while r[-1] != j:
+            r += R[r[-1]][j],
+        print(len(r), *r)if len(r) > 1 else print(0)
