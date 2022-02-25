01fd99c8bb8b57934b331972e371d035d699295c
춤추는망고 <ensia96@gmail.com>
2022-02-25 23:14:12 +0900

    [update] 플로이드 2(11780) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x1C/플로이드 2(11780).py
+++ baekjoon/baaarkingdog/0x1C/플로이드 2(11780).py
@@ -1,26 +1,35 @@
+import sys
+I = sys.stdin.readline
 M = 2**27
-n = int(input())+1
+n = int(I())+1
 N = range(1, n)
 C = [[M]*n for _ in ' '*n]
 R = [[0]*n for _ in ' '*n]
-for _ in ' '*int(input()):
-    a, b, c = map(int, input().split())
-    C[a][b], R[a][b] = min(C[a][b], c), b
+for _ in ' '*int(I()):
+    a, b, c = map(int, I().split())
+    C[a][b] = min(C[a][b], c)
 for k in N:
     C[k][k] = 0
     for i in N:
         for j in N:
             t = C[i][k]+C[k][j]
             if C[i][j] > t:
-                C[i][j], R[i][j] = t, R[i][k]
+                C[i][j], R[i][j] = t, k
 for l in C[1:]:
-    print(*l[1:])
+    print(*(0 if i == M else i for i in l[1:]))
+
+
+def f(i, j):
+    if not R[i][j]:
+        return []
+    k = R[i][j]
+    return f(i, k)+[k]+f(k, j)
+
+
 for i in N:
     for j in N:
         if C[i][j] in [0, M]:
             print(0)
             continue
-        r = [i]
-        while r[-1] != j:
-            r += R[r[-1]][j],
+        r = [i]+f(i, j)+[j]
         print(len(r), *r)
