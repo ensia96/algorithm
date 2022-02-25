9b8f2b2d864c8a8785b10359d3f43caeef9750ba
춤추는망고 <ensia96@gmail.com>
2022-02-25 16:14:07 +0900

    [update] 플로이드(11404) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x1C/플로이드(11404).py
+++ baekjoon/baaarkingdog/0x1C/플로이드(11404).py
@@ -1,16 +1,14 @@
 M = 2**27
 I, R = input, range
 n = int(I())
-m = int(I())
 C = [[M]*n for _ in ' '*n]
-for _ in ' '*m:
+for _ in ' '*int(I()):
     a, b, c = map(int, I().split())
     C[a-1][b-1] = min(C[a-1][b-1], c)
-for i in R(n):
-    C[i][i] = 0
 for k in R(n):
+    C[k][k] = 0
     for i in R(n):
         for j in R(n):
             C[i][j] = min(C[i][j], C[i][k]+C[k][j])
-for l in [[0 if C[i][j] == M else C[i][j]for j in R(n)]for i in R(n)]:
-    print(*l)
+for l in C:
+    print(*(i % M for i in l))
