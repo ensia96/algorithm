6311d03b0662d80bb962df7b229289a7596aba10
춤추는망고 <ensia96@gmail.com>
2022-02-26 14:33:33 +0900

    [update] 서강그라운드(14938) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x1C/서강그라운드(14938).py
+++ baekjoon/baaarkingdog/0x1C/서강그라운드(14938).py
@@ -1,27 +1,12 @@
-L, M, A = lambda: map(int, input().split()), 8**4, 0
+L, M = lambda: map(int, input().split()), 8**4
 n, m, r = L()
 I, N = [*L()], range(n)
 C = [[M]*n for _ in ' '*n]
-R = [[0]*n for _ in ' '*n]
 for _ in ' '*r:
     a, b, l = L()
-    a, b = a-1, b-1
-    C[a][b] = C[b][a] = l
-    R[a][b], R[b][a] = b, a
+    C[a-1][b-1] = C[b-1][a-1] = l
 for k in N:
     for i in N:
         for j in N:
-            t = C[i][k]+C[k][j]
-            if C[i][j] > t:
-                C[i][j], R[i][j] = t, R[i][k]
-for i in N:
-    V = {i}
-    for j in N:
-        if not C[i][j] % M or C[i][j] > m:
-            continue
-        r = [i]
-        while r[-1] != j:
-            r += R[r[-1]][j],
-        V = {*V, *r}
-    A = max(A, sum(I[c]for c in V))
-print(A)
+            C[i][j] = min(C[i][j], C[i][k]+C[k][j])
+print(max(I[i]+sum(I[j]for j in N if(C[i][j] % M)*(C[i][j] <= m))for i in N))
