9268e8085b986b2a1e37a4e1d4419a4a999ed109
춤추는망고 <ensia96@gmail.com>
2022-02-26 14:40:52 +0900

    [update] 서강그라운드(14938) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x1C/서강그라운드(14938).py
+++ baekjoon/baaarkingdog/0x1C/서강그라운드(14938).py
@@ -6,7 +6,8 @@ for _ in ' '*r:
     a, b, l = L()
     C[a-1][b-1] = C[b-1][a-1] = l
 for k in N:
+    C[k][k] = 0
     for i in N:
         for j in N:
             C[i][j] = min(C[i][j], C[i][k]+C[k][j])
-print(max(I[i]+sum(I[j]for j in N if(C[i][j] % M)*(C[i][j] <= m))for i in N))
+print(max(sum(I[j]for j in N if C[i][j] <= m)for i in N))
