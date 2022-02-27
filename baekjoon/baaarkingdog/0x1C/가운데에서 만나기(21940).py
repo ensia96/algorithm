8994bc3b08270f6d811626c88b3c3695c270706f
춤추는망고 <ensia96@gmail.com>
2022-02-27 13:50:57 +0900

    [update] 가운데에서 만나기(21940) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x1C/가운데에서 만나기(21940).py
+++ baekjoon/baaarkingdog/0x1C/가운데에서 만나기(21940).py
@@ -6,13 +6,12 @@ C = [[M]*n for _ in N]
 for _ in ' '*m:
     a, b, t = L()
     C[a-1][b-1] = min(C[a-1][b-1], t)
-k, A = next(L()), [i-1 for i in L()]
+k, A = L(), [i-1 for i in L()]
 for k in N:
+    C[k][k] = 0
     for i in N:
         for j in N:
             C[i][j] = min(C[i][j], C[i][k]+C[k][j])
-for i in N:
-    C[i][i] = 0
 for j in N:
     t = max(C[i][j]+C[j][i]for i in A)
     D[t] = D.get(t, [])+[j]
