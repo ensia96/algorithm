e7d94e0fa87e6c8352846d43502d5be4b2e8092b
춤추는망고 <ensia96@gmail.com>
2022-03-05 15:31:25 +0900

    [update] 허들 넘기(23286) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x1C/허들 넘기(23286).py
+++ baekjoon/baaarkingdog/0x1C/허들 넘기(23286).py
@@ -9,7 +9,8 @@ for k in N:
     for i in N:
         for j in N:
             a, b, c = D[i][j], D[i][k], D[k][j]
-            D[i][j] = a > 0 and b > 0 and c > 0 and min(a, max(b, c)) or a
+            if a > 0 and b > 0 and c > 0:
+                D[i][j] = min(a, max(b, c))
 for _ in R(t):
     s, e = L()
     print(D[s-1][e-1])
