528333679bfb842d4f7c93a7ff1818abe12216a0
춤추는망고 <ensia96@gmail.com>
2022-03-05 15:45:52 +0900

    [update] 허들 넘기(23286) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x1C/허들 넘기(23286).py
+++ baekjoon/baaarkingdog/0x1C/허들 넘기(23286).py
@@ -1,16 +1,15 @@
-L, R = lambda: map(int, input().split()), range
+L, M, R = lambda: map(int, input().split()), 8**8, range
 n, m, t = L()
 N = R(n)
-D = [[-1]*n for _ in N]
+D = [[M]*n for _ in N]
 for _ in R(m):
     u, v, h = L()
     D[u-1][v-1] = h
 for k in N:
     for i in N:
         for j in N:
-            a, b, c = D[i][j], D[i][k], D[k][j]
-            if b > 0 and c > 0:
-                D[i][j] = min(a, max(b, c))
+            D[i][j] = min(D[i][j], max(D[i][k], D[k][j]))
 for _ in R(t):
     s, e = L()
-    print(D[s-1][e-1])
+    t = D[s-1][e-1]
+    print(t if t-M else -1)
