c755cea628dd08b2680ac1fdad62f512b436fcf2
춤추는망고 <ensia96@gmail.com>
2022-03-07 20:13:06 +0900

    [update] 밤편지(23258) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x1C/밤편지(23258).py
+++ baekjoon/baaarkingdog/0x1C/밤편지(23258).py
@@ -1,20 +1,19 @@
 import sys
-I = sys.stdin.readline
-L, M = lambda: map(int, I().split()), 8**8
+L, M = lambda: map(int, sys.stdin.readline().split()), 8**8
 n, q = L()
 N = range(1, n+1)
 D = [[[0]*-~n for _ in ' '*-~n]for _ in ' '*-~n]
 for i in N:
     l = [*L()]
     for j in N:
-        D[i][j][0] = l[j-1]
-        if not D[i][j][0] and i-j:
-            D[i][j][0] = M
+        D[0][i][j] = l[j-1]
+        if not D[0][i][j] and i-j:
+            D[0][i][j] = M
 for k in N:
     for i in N:
         for j in N:
-            D[i][j][k] = min(D[i][j][k-1], D[i][k][k-1]+D[k][j][k-1])
+            D[k][i][j] = min(D[k-1][i][j], D[k-1][i][k]+D[k-1][k][j])
 for _ in ' '*q:
     c, s, e = L()
-    t = D[s][e][c-1]
+    t = D[c-1][s][e]
     print(t if t-M else -1)
