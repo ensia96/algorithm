9b19214f88005955d9a3b730787a10ab14ce1430
춤추는망고 <ensia96@gmail.com>
2022-03-07 20:00:31 +0900

    밤편지(23258)

---
+++ baekjoon/baaarkingdog/0x1C/밤편지(23258).py
@@ -0,0 +1,18 @@
+L, M = lambda: map(int, input().split()), 8**8
+n, q = L()
+N = range(1, n+1)
+D = [[[0]*-~n for _ in ' '*-~n]for _ in ' '*-~n]
+for i in N:
+    l = [*L()]
+    for j in N:
+        D[i][j][0] = l[j-1]
+        if not D[i][j][0] and i-j:
+            D[i][j][0] = M
+for k in N:
+    for i in N:
+        for j in N:
+            D[i][j][k] = min(D[i][j][k-1], D[i][k][k-1]+D[k][j][k-1])
+for _ in ' '*q:
+    c, s, e = L()
+    t = D[s][e][c-1]
+    print(t if t-M else -1)
