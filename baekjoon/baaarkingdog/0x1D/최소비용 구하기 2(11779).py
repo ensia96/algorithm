ce8e95b0b3b930ccc88769a94c60a44138ed6b8a
춤추는망고 <ensia96@gmail.com>
2022-03-11 11:45:51 +0900

    [update] 최소비용 구하기 2(11779) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x1D/최소비용 구하기 2(11779).py
+++ baekjoon/baaarkingdog/0x1D/최소비용 구하기 2(11779).py
@@ -1,42 +1,23 @@
-import sys
-import heapq
-input = sys.stdin.readline
-
-n = int(input())
-m = int(input())
-graph = [[] for _ in range(n + 1)]
-
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-
-start, end = map(int, input().split())
-
-# 가장 가까운 노드를 기록
-nearnest = [start] * (n + 1)
-distance = [1e9] * (n + 1)
-
-q = [(0, start)]
-while q:
-    dist, now = heapq.heappop(q)
-    if dist > distance[now]:
+import heapq as h
+L, M = lambda: map(int, input().split()), 8**9
+n = next(L())+1
+D, E = [M]*n, [[]for _ in ' '*n]
+for _ in ' '*next(L()):
+    s, e, d = L()
+    E[s] += (d, e),
+s, e = L()
+R, D[s], Q = [s]*n, 0, [(0, s)]
+while Q:
+    x, y = h.heappop(Q)
+    if D[y] != x:
         continue
-
-    for next, nextDist in graph[now]:
-        cost = nextDist + dist
-        if cost < distance[next]:
-            distance[next], nearnest[next] = cost, now
-            heapq.heappush(q, (cost, next))
-
-ans = []
-tmp = end
-while tmp != start:
-    ans.append(str(tmp))
-    tmp = nearnest[tmp]
-
-ans.append(str(start))
-ans.reverse()
-
-print(distance[end])
-print(len(ans))
-print(" ".join(ans))
+    for i, j in E[y]:
+        if D[j] > D[y]+i:
+            D[j], R[j] = D[y]+i, y
+            h.heappush(Q, (D[j], j))
+r = [e]
+while r[-1]-s:
+    r += R[r[-1]],
+print(D[e])
+print(len(r))
+print(*r[::-1])
