1000fbef89ccfdf23e61e5ce89c9c51a4f6203ae
춤추는망고 <ensia96@gmail.com>
2022-03-16 13:42:39 +0900

    횡단보도(24042)

---
+++ baekjoon/baaarkingdog/0x1D/횡단보도(24042).py
@@ -0,0 +1,23 @@
+import sys
+import heapq as h
+L, M = lambda: map(int, sys.stdin.readline().split()), 8**8
+n, m = L()
+n += 1
+E = [[]for _ in ' '*n]
+for i in range(m):
+    a, b = L()
+    E[a] += (i, b),
+    E[b] += (i, a),
+D, Q = [M]*n, [(0, 1)]
+D[1] = 0
+while Q:
+    x, y = h.heappop(Q)
+    if D[y]-x:
+        continue
+    for i, j in E[y]:
+        t = i-x % m
+        t = x+t+(t <= 0)*m+1
+        if D[j] > t:
+            D[j] = t
+            h.heappush(Q, (t, j))
+print(D[n-1])
