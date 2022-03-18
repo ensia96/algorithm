f29607a3cf5c1ef75e277a43dbc0a4ff55fd3988
춤추는망고 <ensia96@gmail.com>
2022-03-18 13:45:20 +0900

    [update] K번째 최단경로 찾기(1854) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x1D/K번째 최단경로 찾기(1854).py
+++ baekjoon/baaarkingdog/0x1D/K번째 최단경로 찾기(1854).py
@@ -1,7 +1,5 @@
-import sys
 import heapq as h
 L, M = lambda: map(int, input().split()), 20**9
-P = h.heappush
 n, m, k = L()
 n += 1
 E = [[]for _ in ' '*n]
@@ -9,16 +7,12 @@ for _ in ' '*m:
     a, b, c = L()
     E[a] += (c, b),
 D, Q = [[] for _ in ' '*n], [(0, 1)]
-D[1] += 0,
 while Q:
     x, y = h.heappop(Q)
+    if not len(D[y])-k:
+        continue
+    D[y] += x,
     for i, j in E[y]:
-        if len(D[j]) < k:
-            P(D[j], -x-i)
-            P(Q, (x+i, j))
-        elif D[j][0] < -x-i:
-            h.heappop(D[j])
-            P(D[j], -x-i)
-            P(Q, (x+i, j))
+        h.heappush(Q, (x+i, j))
 for d in D[1:]:
-    print(-1 if len(d) != k else -d[0])
+    print(-1 if len(d)-k else d[k-1])
