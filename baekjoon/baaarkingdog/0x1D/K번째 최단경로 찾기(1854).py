49982836f1ef3b524ae8b5605a775b1d71b1f1dd
춤추는망고 <ensia96@gmail.com>
2022-03-18 13:29:39 +0900

    [update] K번째 최단경로 찾기(1854) - 메모리 초과 해결

--- baekjoon/baaarkingdog/0x1D/K번째 최단경로 찾기(1854).py
+++ baekjoon/baaarkingdog/0x1D/K번째 최단경로 찾기(1854).py
@@ -8,13 +8,14 @@ for _ in ' '*m:
     a, b, c = L()
     E[a] += (c, b),
 D, Q = [[] for _ in ' '*n], [(0, 1)]
+D[1] += 0,
 while Q:
     x, y = h.heappop(Q)
     for i, j in E[y]:
-        if len(D[j]) < k or D[j][0] < -x-i:
+        if len(D[j]) < k:
             h.heappush(D[j], -x-i)
             h.heappush(Q, (x+i, j))
-            if len(D[k]) > k:
+            if len(D[j]) > k:
                 h.heappop(D[j])
 for d in D[1:]:
     print(-1 if len(d) != k else -d[0])
