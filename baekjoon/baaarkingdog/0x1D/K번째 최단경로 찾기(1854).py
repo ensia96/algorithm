794a40d01f3e309300ac44bc90be610ec5ac80db
춤추는망고 <ensia96@gmail.com>
2022-03-18 13:14:01 +0900

    K번째 최단경로 찾기(1854)

---
+++ baekjoon/baaarkingdog/0x1D/K번째 최단경로 찾기(1854).py
@@ -0,0 +1,19 @@
+import sys
+import heapq as h
+L, M = lambda: map(int, input().split()), 11**9
+n, m, k = L()
+n += 1
+E = [[]for _ in ' '*n]
+for _ in ' '*m:
+    a, b, c = L()
+    E[a] += (c, b),
+D, Q = [[-M]*k for _ in ' '*n], [(0, 1)]
+while Q:
+    x, y = h.heappop(Q)
+    for i, j in E[y]:
+        if D[j][0] < -x-i:
+            h.heappush(D[j], -x-i)
+            h.heappop(D[j])
+            h.heappush(Q, (x+i, j))
+for d in D[1:]:
+    print(-1 if -d[0] == M else -d[0])
