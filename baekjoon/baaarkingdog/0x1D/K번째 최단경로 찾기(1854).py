a5eff9d84874c1cdac266e804a991c15b2162c54
춤추는망고 <ensia96@gmail.com>
2022-03-18 13:38:49 +0900

    [update] K번째 최단경로 찾기(1854) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x1D/K번째 최단경로 찾기(1854).py
+++ baekjoon/baaarkingdog/0x1D/K번째 최단경로 찾기(1854).py
@@ -1,6 +1,7 @@
 import sys
 import heapq as h
 L, M = lambda: map(int, input().split()), 20**9
+P = h.heappush
 n, m, k = L()
 n += 1
 E = [[]for _ in ' '*n]
@@ -13,9 +14,11 @@ while Q:
     x, y = h.heappop(Q)
     for i, j in E[y]:
         if len(D[j]) < k:
-            h.heappush(D[j], -x-i)
-            h.heappush(Q, (x+i, j))
-            if len(D[j]) > k:
-                h.heappop(D[j])
+            P(D[j], -x-i)
+            P(Q, (x+i, j))
+        elif D[j][0] < -x-i:
+            h.heappop(D[j])
+            P(D[j], -x-i)
+            P(Q, (x+i, j))
 for d in D[1:]:
     print(-1 if len(d) != k else -d[0])
