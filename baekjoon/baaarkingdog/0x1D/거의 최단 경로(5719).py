bbeb849ee04c1a5d5aa8823af92c73971a687bf1
춤추는망고 <ensia96@gmail.com>
2022-03-19 19:06:34 +0900

    [update] 거의 최단 경로(5719) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x1D/거의 최단 경로(5719).py
+++ baekjoon/baaarkingdog/0x1D/거의 최단 경로(5719).py
@@ -1,29 +1,43 @@
 import heapq as h
 L, M = lambda: map(int, input().split()), 8**8
+
+
+def F():
+    Q, D[s] = [(0, s)], 0
+    while Q:
+        x, y = h.heappop(Q)
+        if D[y]-x:
+            continue
+        for i in E[y]:
+            t = D[y]+E[y][i]
+            if D[i] > t:
+                D[i] = t
+                h.heappush(Q, (t, i))
+
+
+def X():
+    Q = [d]
+    for a in Q:
+        if a == s:
+            continue
+        for b in R[a]:
+            if not D[b]+E[b][a]-D[a] and E[b][a]:
+                del E[b][a]
+                Q += b,
+
+
 while 1:
     n, m = L()
     if not n:
         break
     s, d = L()
-    E, t = [{}for _ in ' '*n], 0
+    E, R = [{}for _ in ' '*n], [{}for _ in ' '*n]
     for _ in ' '*m:
         u, v, p = L()
-        E[u][v] = p
-    while 1:
-        R, D, Q = [s]*n, [M]*n, [(0, s)]
-        D[s] = 0
-        while Q:
-            x, y = h.heappop(Q)
-            if D[y]-x:
-                continue
-            for i in E[y]:
-                if D[i] > D[y]+E[y][i]:
-                    D[i], R[i] = D[y]+E[y][i], y
-                    h.heappush(Q, (D[i], i))
-        if D[d] == M or t and t < D[d]:
-            break
-        r, t = [d], D[d]
-        while r[-1]-s:
-            r += R[r[-1]],
-            E[r[-1]].pop(r[-2])
-    print(-(not D[d]-M) or D[d])
+        E[u][v] = R[v][u] = p
+    D = [M]*n
+    F()
+    X()
+    D = [M]*n
+    F()
+    print(-(D[d] == M) or D[d])
