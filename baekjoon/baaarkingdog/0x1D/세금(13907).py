3ac6298006750e3ec7ee6c2b9f956a28f9c28c67
춤추는망고 <ensia96@gmail.com>
2022-03-20 15:30:49 +0900

    [update] 세금(13907) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x1D/세금(13907).py
+++ baekjoon/baaarkingdog/0x1D/세금(13907).py
@@ -9,19 +9,20 @@ for _ in ' '*m:
     a, b, w = L()
     E[a] += (w, b),
     E[b] += (w, a),
-D, Q = [{}for _ in ' '*n], [(0, 0, s)]
+D, Q = [[M]*n for _ in ' '*n], [(0, 0, s)]
 D[s][0] = 0
 while Q:
     x, y, z = h.heappop(Q)
-    if y > n:
+    if y > n-2:
         continue
     for i, j in E[z]:
-        if D[j].get(y+1, M) > D[z][y]+i:
+        if D[j][y+1] > D[z][y]+i:
             D[j][y+1] = D[z][y]+i
             h.heappush(Q, (D[j][y+1], y+1, j))
-print(min(D[d].values()))
+print(min(D[d]))
 for _ in ' '*k:
     a = next(L())
-    for i in D[d]:
-        D[d][i] += a*i
-    print(min(D[d].values()))
+    for i in range(n):
+        if D[d][i] != M:
+            D[d][i] += a*i
+    print(min(D[d]))
