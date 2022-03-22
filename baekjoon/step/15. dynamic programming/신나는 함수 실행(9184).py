a73885070aaa529ac83fe07a3229010b40d2afbe
춤추는망고 <ensia96@gmail.com>
2022-03-22 13:21:52 +0900

    [update] 신나는 함수 실행(9184) - 코드 길이 줄이기

--- baekjoon/step/15. dynamic programming/신나는 함수 실행(9184).py
+++ baekjoon/step/15. dynamic programming/신나는 함수 실행(9184).py
@@ -1,4 +1,6 @@
 M = 8**8
+T = [[[M]*21 for _ in ' '*21]for _ in ' '*21]
+T[0][0][0] = 1
 
 
 def w(a, b, c):
@@ -6,12 +8,11 @@ def w(a, b, c):
         a = b = c = 0
     if max(a, b, c) > 20:
         a = b = c = 20
-    if T[a][b][c] != M:
-        return T[a][b][c]
-    f = (a < b)*(b < c)
-    F = not f
-    T[a][b][c] = w(a-F, b, c-f)+w(a-F, b-1, c-f) - \
-        w(a-F, b-1, c-F)+w(a-1, b, c-1)*F
+    if T[a][b][c] == M:
+        f = a < b < c
+        F = not f
+        T[a][b][c] = w(a-F, b, c-f)+w(a-F, b-1, c-f) - \
+            w(a-F, b-1, c-F)+w(a-1, b, c-1)*F
     return T[a][b][c]
 
 
@@ -19,6 +20,4 @@ while 1:
     a, b, c = map(int, input().split())
     if (a == -1)*(b == -1)*(c == -1):
         break
-    T = [[[M]*21 for _ in ' '*21]for _ in ' '*21]
-    T[0][0][0] = 1
     print(f"w({a}, {b}, {c}) = {w(a,b,c)}")
