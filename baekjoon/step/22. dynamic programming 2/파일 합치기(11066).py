644d8dea998904595da9b04f3315ae70bc62485f
춤추는망고 <ensia96@gmail.com>
2022-04-06 17:29:36 +0900

    [update] 파일 합치기(11066) - 코드 길이 줄이기

--- baekjoon/step/22. dynamic programming 2/파일 합치기(11066).py
+++ baekjoon/step/22. dynamic programming 2/파일 합치기(11066).py
@@ -1,16 +1,18 @@
+I, R = input, range
+
+
 def f(x, y):
-    if D[x][y]+(x == y):
-        return D[x][y]
-    D[x][y] = min(f(x, i)+f(i+1, y)for i in range(x, y))+F[y]-F[x-1]
+    if not D[x][y]+(x == y):
+        D[x][y] = min(f(x, i)+f(i+1, y)for i in R(x, y))+F[y]-F[x-1]
     return D[x][y]
 
 
-for _ in ' '*int(input()):
-    k, t = int(input()), 0
-    F = [*map(int, input().split())]+[0]
-    D = [[0]*-~k for _ in ' '*-~k]
-    for i in range(1, k):
+for _ in ' '*int(I()):
+    k = int(I())
+    F = [*map(int, I().split())]+[0]
+    D = [[0]*k for _ in ' '*k]
+    for i in R(1, k):
         D[i-1][i] = F[i-1]+F[i]
-    for i in range(1, k):
+    for i in R(1, k):
         F[i] += F[i-1]
     print(f(0, k-1))
