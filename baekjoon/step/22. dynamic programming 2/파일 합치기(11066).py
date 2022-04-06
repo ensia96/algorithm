6b0129d9e7ce84b6f3d78e37da1fe25d7a42ffce
춤추는망고 <ensia96@gmail.com>
2022-04-06 17:20:50 +0900

    [update] 파일 합치기(11066) - 틀렸습니다 해결

--- baekjoon/step/22. dynamic programming 2/파일 합치기(11066).py
+++ baekjoon/step/22. dynamic programming 2/파일 합치기(11066).py
@@ -1,11 +1,16 @@
-import heapq as h
-d = h.heappop
+def f(x, y):
+    if D[x][y]+(x == y):
+        return D[x][y]
+    D[x][y] = min(f(x, i)+f(i+1, y)for i in range(x, y))+F[y]-F[x-1]
+    return D[x][y]
+
+
 for _ in ' '*int(input()):
     k, t = int(input()), 0
-    F = [*map(int, input().split())]
-    h.heapify(F)
-    for _ in ' '*~-k:
-        x, y = d(F), d(F)
-        h.heappush(F, x+y)
-        t += x+y
-    print(t)
+    F = [*map(int, input().split())]+[0]
+    D = [[0]*-~k for _ in ' '*-~k]
+    for i in range(1, k):
+        D[i-1][i] = F[i-1]+F[i]
+    for i in range(1, k):
+        F[i] += F[i-1]
+    print(f(0, k-1))
