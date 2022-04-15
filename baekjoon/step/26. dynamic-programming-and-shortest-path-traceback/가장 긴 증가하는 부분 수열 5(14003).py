fe3ef7532793a641e2f4494907b3dbe227b0e1e2
춤추는망고 <ensia96@gmail.com>
2022-04-15 23:43:45 +0900

    가장 긴 증가하는 부분 수열 5(14003)

---
+++ baekjoon/step/26. dynamic-programming-and-shortest-path-traceback/가장 긴 증가하는 부분 수열 5(14003).py
@@ -0,0 +1,21 @@
+import bisect as B
+n = int(input())
+A = [0]+[*map(int, input().split())]
+D = [0]*-~n
+C = [0]
+R = []
+M = 0
+
+for i in range(1, n+1):
+    if C[-1] < A[i]:
+        C += A[i],
+        M = D[i] = len(C) - 1
+        continue
+    D[i] = B.bisect_left(C, A[i])
+    C[D[i]] = A[i]
+print(M)
+for i in range(n, 0, -1):
+    if D[i] == M:
+        R += A[i],
+        M -= 1
+print(*R[::-1])
