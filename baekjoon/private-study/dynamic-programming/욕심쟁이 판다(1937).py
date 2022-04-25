64e4a7e2b6398934f9e677cc9b6be7cba8981be9
춤추는망고 <ensia96@gmail.com>
2022-04-25 22:45:03 +0900

    욕심쟁이 판다(1937)

---
+++ baekjoon/private-study/dynamic-programming/욕심쟁이 판다(1937).py
@@ -0,0 +1,19 @@
+import sys
+sys.setrecursionlimit(10**6)
+n = int(input())
+A = [[*map(int, input().split())]for _ in ' '*n]
+D = [[0]*n for _ in ' '*n]
+N = range(n)
+
+
+def f(i, j):
+    if D[i][j]:
+        return D[i][j]
+    D[i][j] = 1
+    for x, y in [(i-1, j), (i+1, j), (i, j+1), (i, j-1)]:
+        if (0 <= x < n)*(0 <= y < n) and A[i][j] < A[x][y]:
+            D[i][j] = max(D[i][j], f(x, y)+1)
+    return D[i][j]
+
+
+print(max(f(i, j)for i in N for j in N))
