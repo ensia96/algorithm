8f41d9c9692306461195c0f7161355658ea687c4
춤추는망고 <ensia96@gmail.com>
2022-04-25 22:51:45 +0900

    [update] 욕심쟁이 판다(1937) - 메모리 초과 해결

--- baekjoon/private-study/dynamic-programming/욕심쟁이 판다(1937).py
+++ baekjoon/private-study/dynamic-programming/욕심쟁이 판다(1937).py
@@ -1,26 +1,19 @@
 import sys
-I, R = sys.stdin.readline, range
 sys.setrecursionlimit(10**6)
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
-def f(x, y):
-    if D[x][y]:
-        return D[x][y]
-    D[x][y] = 1
-    for i in R(4):
-        nx, ny = x + dx[i], y + dy[i]
-        if (0 <= nx < n)*(0 <= ny < n) and A[x][y] < A[nx][ny]:
-            D[x][y] = max(D[x][y], f(nx, ny) + 1)
-    return D[x][y]
-
-
-n = int(I())
-A = [[*map(int, I().split())]for _ in ' '*n]
+n = int(input())
+A = [[*map(int, input().split())]for _ in ' '*n]
 D = [[0]*n for _ in ' '*n]
-M = 0
-for i in R(n):
-    for j in R(n):
-        M = max(M, f(i, j))
-print(M)
+N = range(n)
+
+
+def f(i, j):
+    if D[i][j]:
+        return D[i][j]
+    D[i][j] = 1
+    for x, y in [(i-1, j), (i+1, j), (i, j+1), (i, j-1)]:
+        if (0 <= x < n)*(0 <= y < n) and A[i][j] < A[x][y]:
+            D[i][j] = max(D[i][j], f(x, y)+1)
+    return D[i][j]
+
+
+print(max(f(i, j)for i in N for j in N))
