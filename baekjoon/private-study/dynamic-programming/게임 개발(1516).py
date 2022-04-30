f64961506f45e531e51bffb74437a8aae7bc178e
춤추는망고 <ensia96@gmail.com>
2022-04-30 22:42:03 +0900

    [update] 게임 개발(1516) - 틀렸습니다 해결

--- baekjoon/private-study/dynamic-programming/게임 개발(1516).py
+++ baekjoon/private-study/dynamic-programming/게임 개발(1516).py
@@ -1,11 +1,23 @@
+import collections as C
 n = int(input())+1
-D, E = [0]*-~n, [0]
+C, I, E, R, Q = [0]*n, [0]*n, [[]for _ in ' '*n], [0]*n, C.deque()
 N = range(1, n)
 for i in N:
     c, *A = map(int, input().split())
-    D[i] = c
-    E += A,
-for i in N[::-1]:
-    D[i] += max(D[e]for e in E[i])
+    C[i] = c
+    for a in A[:-1]:
+        E[a] += i,
+        I[i] += 1
 for i in N:
-    print(D[i])
+    if not I[i]:
+        Q += i,
+while Q:
+    c = Q.popleft()
+    R[c] += C[c]
+    for e in E[c]:
+        I[e] -= 1
+        R[e] = max(R[e], R[c])
+        if not I[e]:
+            Q += e,
+for r in R:
+    print(r)
