1025a9553edaa9bbd8d6e6143dda8b104dfbba2e
춤추는망고 <ensia96@gmail.com>
2022-05-03 10:29:42 +0900

    [update] 기타리스트(1495) - 메모리 초과 해결

--- baekjoon/private-study/dynamic-programming/기타리스트(1495).py
+++ baekjoon/private-study/dynamic-programming/기타리스트(1495).py
@@ -1,15 +1,16 @@
-import collections as C
 n, s, m = map(int, input().split())
 V = [*map(int, input().split())]
-Q = C.deque([(0, s)])
+D = [0]*-~m
+Q = [(0, s)]
 M = -1
-while Q:
-    i, v = Q.popleft()
+for i, v in Q:
     if i == n:
         M = max(M, v)
         continue
-    if v-V[i] >= 0:
+    if v-V[i] >= 0 and D[v] == i:
+        D[v-V[i]] = i+1
         Q += (i+1, v-V[i]),
-    if v+V[i] <= m:
+    if v+V[i] <= m and D[v] == i:
+        D[v+V[i]] = i+1
         Q += (i+1, v+V[i]),
 print(M)
