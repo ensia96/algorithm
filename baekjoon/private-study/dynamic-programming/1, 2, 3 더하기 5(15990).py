04d864110156dea444571f2e174542205958d39c
춤추는망고 <ensia96@gmail.com>
2022-05-07 19:40:13 +0900

    1, 2, 3 더하기 5(15990)

---
+++ baekjoon/private-study/dynamic-programming/1, 2, 3 더하기 5(15990).py
@@ -0,0 +1,12 @@
+A = [int(input())for _ in ' '*int(input())]
+n = max(A)
+D = [[0]*4 for _ in ' '*-~n]
+D[1] = [0, 1, 0, 0]
+D[2] = [0, 0, 1, 0]
+D[3] = [0, 1, 1, 1]
+for i in range(4, n+1):
+    D[i][1] = D[i-1][2]+D[i-1][3]
+    D[i][2] = D[i-2][1]+D[i-2][3]
+    D[i][3] = D[i-3][1]+D[i-3][2]
+for a in A:
+    print(sum(D[a]))
