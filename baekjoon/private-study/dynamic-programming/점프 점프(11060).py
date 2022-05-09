7731a8fb43f9a410e473be0913f637e8831df484
춤추는망고 <ensia96@gmail.com>
2022-05-09 11:02:21 +0900

    점프 점프(11060)

---
+++ baekjoon/private-study/dynamic-programming/점프 점프(11060).py
@@ -0,0 +1,9 @@
+n, m = int(input()), 8**8
+A = [*map(int, input().split())]
+D = [m]*n
+D[0] = 0
+for i in range(n):
+    for j in range(A[i]+1):
+        if i+j < n:
+            D[i+j] = min(D[i+j], D[i]+1)
+print(D[-1]if D[-1]-m else -1)
