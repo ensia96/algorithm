34304973eb91a73f73c158627015949ad8f929da
춤추는망고 <ensia96@gmail.com>
2022-05-10 13:08:13 +0900

    [update] Four Squares(17626) - 코드 길이 줄이기

--- baekjoon/private-study/dynamic-programming/Four Squares(17626).py
+++ baekjoon/private-study/dynamic-programming/Four Squares(17626).py
@@ -1,6 +1,5 @@
 n = int(input())
 D = [0, 1]+[4]*n
 for i in range(1, n+1):
-    for j in range(1, int(i**.5)+1):
-        D[i] = min(D[i], D[i-j**2]+1)
+    D[i] = min(D[i-j*j]+1 for j in range(1, int(i**.5)+1))
 print(D[n])
