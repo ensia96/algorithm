2e632d14190bf42320dc023237ebf4bc27a34789
춤추는망고 <ensia96@gmail.com>
2022-05-12 14:49:32 +0900

    [update] 공통 부분 문자열(5582) - 코드 길이 줄이기

--- baekjoon/private-study/dynamic-programming/공통 부분 문자열(5582).py
+++ baekjoon/private-study/dynamic-programming/공통 부분 문자열(5582).py
@@ -1,10 +1,8 @@
-A = input()
-B = input()
+A, B = open(0)
 n = len(A)
 m = len(B)
 D = [[0]*-~m for _ in ' '*n]
 for i in range(n):
     for j in range(m):
-        if A[i] == B[j]:
-            D[i][j] = D[i-1][j-1]+1
+        D[i][j] = (D[i-1][j-1]+1)*(A[i] == B[j])
 print(max(map(max, D)))
