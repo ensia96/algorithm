dae60bfdd05123cd5320d559eb31696b782a155a
춤추는망고 <ensia96@gmail.com>
2022-05-14 21:50:58 +0900

    [update] D번 - 결전의 금요일 - 틀렸습니다 해결

--- baekjoon/contest/gomgomcup/D번 - 결전의 금요일.py
+++ baekjoon/contest/gomgomcup/D번 - 결전의 금요일.py
@@ -1,7 +1,9 @@
 n = int(input())
 A = [*map(int, input().split())]
-r = 1
-for a in A:
-    r += a
-    r % 7 == 5 and exit(print('YES'))
+D = [0]
+for i in range(n):
+    D += D[i]+A[i],
+for i in range(n+1):
+    for j in range(i):
+        (1+D[i]-D[j]) % 7 == 5 and exit(print('YES'))
 print('NO')
