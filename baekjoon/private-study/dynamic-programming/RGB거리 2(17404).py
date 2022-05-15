8cf44c5c636ce7d1d320b44430d1d76050afbb49
춤추는망고 <ensia96@gmail.com>
2022-05-15 14:42:58 +0900

    [update] RGB거리 2(17404) - 틀렸습니다 해결

--- baekjoon/private-study/dynamic-programming/RGB거리 2(17404).py
+++ baekjoon/private-study/dynamic-programming/RGB거리 2(17404).py
@@ -2,7 +2,7 @@ n = int(input())
 C = [[*map(int, input().split())]for _ in ' '*n]
 C += [C[0]]
 x = y = z = 0
-for i in range(n+1):
+for i in range(1, n+1):
     x, y, z = min(y, z)+C[i][0], min(x, z)+C[i][1], min(x, y)+C[i][2]
 t = min(C[0])
-print(min(x-t, y-t, z-t))
+print(min(x, y, z))
