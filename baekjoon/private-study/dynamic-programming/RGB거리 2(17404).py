6018bf6a06515c6437aa03c81bcfde3c16220930
춤추는망고 <ensia96@gmail.com>
2022-05-15 17:46:26 +0900

    [update] RGB거리 2(17404) - 코드 길이 줄이기

--- baekjoon/private-study/dynamic-programming/RGB거리 2(17404).py
+++ baekjoon/private-study/dynamic-programming/RGB거리 2(17404).py
@@ -1,9 +1,9 @@
-n, m = int(input()), 8**8
+n, m, r = int(input()), 8**8, range(3)
 C = [[*map(int, input().split())]for _ in ' '*n]
 A = m
-for j in range(3):
-    D = [m*(i != j)+C[0][i] for i in range(3)]
+for j in r:
+    D = [m*(i != j)+C[0][i]for i in r]
     for i in range(1, n):
         D = min(D[1:])+C[i][0], min(D[0], D[2])+C[i][1], min(D[:2])+C[i][2]
-    A = min([D[k]for k in range(3)if j-k]+[A])
+    A = min([D[k]for k in r if j-k]+[A])
 print(A)
