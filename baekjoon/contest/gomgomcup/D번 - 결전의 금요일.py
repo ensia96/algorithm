0f51ae5961aa77999c0f67e2274fe73f5c161377
춤추는망고 <ensia96@gmail.com>
2022-05-17 17:45:16 +0900

    [update] D번 - 결전의 금요일 - 틀렸습니다 해결

--- baekjoon/contest/gomgomcup/D번 - 결전의 금요일.py
+++ baekjoon/contest/gomgomcup/D번 - 결전의 금요일.py
@@ -1,9 +1,19 @@
+def f(x):
+    if x % 7 == 4:
+        exit(print('YES'))
+    for i in range(1, 7):
+        if C[i]:
+            C[i] -= 1
+            f(x+i)
+            C[i] += 1
+
+
 n = int(input())
 A = [*map(int, input().split())]
-C = [0]*7
+C, c = [0]*7, 0
 for a in A:
-    for i in range(7):
-        if C[i]:
-            C[(i+a) % 7] = 1
-    C[a % 7] = 1
-print(['NO', 'YES'][C[4]])
+    if a % 7:
+        C[a % 7] += 1
+        c += 1
+f(0+4*bool(C[4]+(c > 5)))
+print('NO')
