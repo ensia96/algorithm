af936e29372dbee5890f46ac57ecfb6fa09e7330
춤추는망고 <ensia96@gmail.com>
2022-05-17 18:10:37 +0900

    [update] D번 - 결전의 금요일 - 코드 길이 줄이기

--- baekjoon/contest/gomgomcup/D번 - 결전의 금요일.py
+++ baekjoon/contest/gomgomcup/D번 - 결전의 금요일.py
@@ -1,19 +1,9 @@
-def f(x):
-    if x % 7 == 4:
-        exit(print('YES'))
-    for i in range(1, 7):
+input()
+C = [1]+[0]*6
+for a in map(int, input().split()):
+    c = [0]*7
+    for i in range(7):
         if C[i]:
-            C[i] -= 1
-            f(x+i)
-            C[i] += 1
-
-
-n = int(input())
-A = [*map(int, input().split())]
-C, c = [0]*7, 0
-for a in A:
-    if a % 7:
-        C[a % 7] += 1
-        c += 1
-f(0+4*bool(C[4]+(c > 5)))
-print('NO')
+            c[(i+a) % 7] = c[i] = 1
+    C = c
+print(['NO', 'YES'][C[4]])
