caf559680a4dea25860031dba601b62f51f2330d
춤추는망고 <ensia96@gmail.com>
2022-05-22 12:19:16 +0900

    [update] 팰린드롬 분할(1509) - 코드 길이 줄이기

--- baekjoon/private-study/dynamic-programming/팰린드롬 분할(1509).py
+++ baekjoon/private-study/dynamic-programming/팰린드롬 분할(1509).py
@@ -1,21 +1,9 @@
-import heapq as H
 A = input()
 n = len(A)
-C = [[0]*n for _ in ' '*n]
-for j in range(n):
-    for i in range(n-j):
-        m = A[i] == A[i+j]
-        C[i][i+j] = not j or (j == 1)*m or m*C[i+1][i+j-1]
-E = {i: [j+1 for j in range(i, n)if C[i][j]]for i in range(n)}
-Q = [(0, 0)]
-m = 8**8
-D = [0]+[m]*n
-while Q:
-    x, y = H.heappop(Q)
-    if D[y]-x:
-        continue
-    for i in E.get(y, []):
-        if D[i] > D[y]+1:
-            D[i] = D[y]+1
-            H.heappush(Q, (D[i], i))
+D = [*range(n+1)]
+for i in range(n):
+    for j in range(i+1, n+1):
+        t = A[i:j]
+        if t == t[::-1]:
+            D[j] = min(D[j], D[i]+1)
 print(D[-1])
