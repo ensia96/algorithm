e52b0da827532abb0814c3cd92b2b1db0001b134
춤추는망고 <ensia96@gmail.com>
2022-05-27 13:43:28 +0900

    [update] 안녕(1535) - 틀렸습니다 해결

--- baekjoon/private-study/dynamic-programming/안녕(1535).py
+++ baekjoon/private-study/dynamic-programming/안녕(1535).py
@@ -1,7 +1,11 @@
 n = int(input())
 L = [*map(int, input().split())]
 H = [*map(int, input().split())]
-D = [0]*100
+D = [[0]*100 for _ in ' '*n]
 for i in range(n):
-    D = [max((H[i]+D[j-L[i]])*(L[i] < j), D[j])for j in range(100)]
-print(D[99])
+    for j in range(100):
+        if j < L[i-1]:
+            D[i][j] = D[i-1][j]
+        else:
+            D[i][j] = max(D[i-1][j], D[i-1][j-L[i-1]]+H[i-1])
+print(D[-1][99])
