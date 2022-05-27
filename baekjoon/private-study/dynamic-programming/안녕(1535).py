2b2fd046218e61ea9c7e4ae737d2fd2bfa35abb7
춤추는망고 <ensia96@gmail.com>
2022-05-27 14:01:12 +0900

    [update] 안녕(1535) - 코드 길이 줄이기

--- baekjoon/private-study/dynamic-programming/안녕(1535).py
+++ baekjoon/private-study/dynamic-programming/안녕(1535).py
@@ -1,11 +1,7 @@
 n = int(input())
-L = [0]+[*map(int, input().split())]
-J = [0]+[*map(int, input().split())]
-D = [[0]*101 for _ in range(n+1)]
-for i in range(1, n+1):
-    for j in range(1, 101):
-        if L[i] <= j:
-            D[i][j] = max(D[i-1][j], D[i-1][j-L[i]] + J[i])
-        else:
-            D[i][j] = D[i-1][j]
-print(D[n][99])
+L = [*map(int, input().split())]
+H = [*map(int, input().split())]
+D = [0]*100
+for i in range(n):
+    D = [max(D[j], (D[j-L[i]]+H[i])*(j >= L[i]))for j in range(100)]
+print(max(D))
