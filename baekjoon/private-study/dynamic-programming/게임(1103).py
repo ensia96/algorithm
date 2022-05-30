5ff43fd98414ea4ed06acf0e16d80c0e0b260a5e
춤추는망고 <ensia96@gmail.com>
2022-05-30 14:51:44 +0900

    [update] 게임(1103) - 코드 길이 줄이기

--- baekjoon/private-study/dynamic-programming/게임(1103).py
+++ baekjoon/private-study/dynamic-programming/게임(1103).py
@@ -1,20 +1,17 @@
 n, m = map(int, input().split())
 B = [[*input()]for _ in ' '*n]
-D, V = [[0]*m for _ in ' '*n], [[0]*m for _ in ' '*n]
-V[0][0] = 1
+D = [[0]*m for _ in ' '*n]
 
 
-def f(x, y):
+def f(x, y, v):
     if D[x][y]:
-        V[x][y] and exit(print(-1))
+        (v & 1 << (x*m+y)) and exit(print(-1))
         return D[x][y]
-    D[x][y] = V[x][y] = 1
-    t = int(B[x][y])
+    D[x][y], v, t = 1, v | (1 << x*m+y), int(B[x][y])
     for i, j in [(x+t, y), (x-t, y), (x, y+t), (x, y-t)]:
         if (0 <= i < n)*(0 <= j < m) and B[i][j] != 'H':
-            D[x][y] = max(D[x][y], f(i, j)+1)
-    V[x][y] = 0
+            D[x][y] = max(D[x][y], f(i, j, v)+1)
     return D[x][y]
 
 
-print(f(0, 0))
+print(f(0, 0, 0))
