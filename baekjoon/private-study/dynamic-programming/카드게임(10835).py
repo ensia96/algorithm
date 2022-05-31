b4e8bc3adb9bebed5c649435517aaf635b60197b
춤추는망고 <ensia96@gmail.com>
2022-05-31 18:46:08 +0900

    [update] 카드게임(10835) - 틀렸습니다 해결

--- baekjoon/private-study/dynamic-programming/카드게임(10835).py
+++ baekjoon/private-study/dynamic-programming/카드게임(10835).py
@@ -1,8 +1,16 @@
 n = int(input())
 L = [*map(int, input().split())]
 R = [*map(int, input().split())]
-D = [[0]*-~n for _ in ' '*-~n]
-for i in range(n):
-    for j in range(n):
-        D[i+1][j+1] = max(D[i][j], (L[i] > R[j])*(D[i+1][j]+R[j]))
-print(D[n][n])
+D = [[-1]*n for _ in ' '*n]
+
+
+def f(i, j):
+    if i == n or j == n:
+        return 0
+    if D[i][j]+1:
+        return D[i][j]
+    D[i][j] = max(f(i+1, j+1), f(i+1, j), (L[i] > R[j]) and (f(i, j+1)+R[j]))
+    return D[i][j]
+
+
+print(f(0, 0))
