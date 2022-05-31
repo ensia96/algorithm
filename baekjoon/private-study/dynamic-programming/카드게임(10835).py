9c256d26589d0c27f787ba7b0ffb08848c341c44
춤추는망고 <ensia96@gmail.com>
2022-05-31 19:15:13 +0900

    [update] 카드게임(10835) - 코드 길이 줄이기

--- baekjoon/private-study/dynamic-programming/카드게임(10835).py
+++ baekjoon/private-study/dynamic-programming/카드게임(10835).py
@@ -1,18 +1,8 @@
-import sys
-sys.setrecursionlimit(10**6)
 n = int(input())
 L = [*map(int, input().split())]
 R = [*map(int, input().split())]
-D = [[-1]*n for _ in ' '*n]
-
-
-def f(i, j):
-    if i == n or j == n:
-        return 0
-    if D[i][j]+1:
-        return D[i][j]
-    D[i][j] = f(i, j+1)+R[j]if L[i] > R[j]else max(f(i+1, j+1), f(i+1, j))
-    return D[i][j]
-
-
-print(f(0, 0))
+D = [[0]*-~n for _ in ' '*-~n]
+for i in range(n)[::-1]:
+    for j in range(n)[::-1]:
+        D[i][j] = max(D[i+1][j+1], D[i+1][j], (L[i] > R[j])*(D[i][j+1]+R[j]))
+print(D[0][0])
