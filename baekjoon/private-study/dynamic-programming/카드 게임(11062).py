41ec94d857a2ea228933a17c24cb3419b40fbb4e
춤추는망고 <ensia96@gmail.com>
2022-06-02 22:22:26 +0900

    [update] 카드 게임(11062) - 코드 길이 줄이기

--- baekjoon/private-study/dynamic-programming/카드 게임(11062).py
+++ baekjoon/private-study/dynamic-programming/카드 게임(11062).py
@@ -1,15 +1,13 @@
 def f(x, y, z):
-    if x <= y and not sum(D[x][y]):
+    if x <= y and not D[x][y]:
         Z = not z
         T, t = f(x+1, y, Z), f(x, y-1, Z)
-        i, j = [(t, y), (T, x)][T[z]+A[x] > t[z]+A[y]]
-        a, b = i
-        D[x][y] = (a+A[j]*Z, b+A[j]*z)
+        D[x][y] = max(T+A[x], t+A[y])*Z+min(T, t)*z
     return D[x][y]
 
 
 for _ in ' '*int(input()):
     n = int(input())
     A = [*map(int, input().split())]
-    D = [[(0, 0)]*-~n for _ in ' '*-~n]
-    print(f(0, n-1, 0)[0])
+    D = [[0]*-~n for _ in ' '*-~n]
+    print(f(0, n-1, 0))
