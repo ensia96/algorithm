bf3a68db2d67318db8da968946352034091f8781
춤추는망고 <ensia96@gmail.com>
2022-06-03 21:31:20 +0900

    [update] 고층 빌딩(1328) - 틀렸습니다 해결

--- baekjoon/private-study/dynamic-programming/고층 빌딩(1328).py
+++ baekjoon/private-study/dynamic-programming/고층 빌딩(1328).py
@@ -4,6 +4,6 @@ D[0][0][0] = 1
 for i in range(1, n):
     for j in range(n):
         for k in range(n):
-            D[i][j][k] = D[i-1][j-1][k]+D[i-1][j][k-1] + \
-                D[i-1][j][k]*(i-1) % 1000000007
+            D[i][j][k] = (D[i-1][j-1][k]+D[i-1][j][k-1] +
+                          D[i-1][j][k]*(i-1)) % 1000000007
 print(D[n-1][l-1][r-1])
