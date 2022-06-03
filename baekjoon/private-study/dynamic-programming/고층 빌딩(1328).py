6baa82d6d1bebf16cd16527eb0e752ab5d4e2dbf
춤추는망고 <ensia96@gmail.com>
2022-06-03 21:29:32 +0900

    고층 빌딩(1328)

---
+++ baekjoon/private-study/dynamic-programming/고층 빌딩(1328).py
@@ -0,0 +1,8 @@
+n, l, r = map(int, input().split())
+D = [[[0]*n for _ in ' '*n]for _ in ' '*n]
+D[0][0][0] = 1
+for i in range(1, n):
+    for j in range(n):
+        for k in range(n):
+            D[i][j][k] = D[i-1][j-1][k]+D[i-1][j][k-1]+D[i-1][j][k]*(i-1)
+print(D[n-1][l-1][r-1])
