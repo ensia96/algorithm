4806dc59a3819f42ab3f630ccf350586729f1ef8
춤추는망고 <ensia96@gmail.com>
2022-06-17 21:05:35 +0900

    벽장문의 이동(2666)

---
+++ baekjoon/private-study/dynamic-programming/벽장문의 이동(2666).py
@@ -0,0 +1,17 @@
+n = int(input())
+a, b = map(int, input().split())
+m = int(input())
+A = [~-int(input())for _ in ' '*m]
+D = [[m*[-1]for _ in ' '*n]for _ in ' '*n]
+
+
+def f(x, y, z):
+    if z == m:
+        D[x][y][z] = 0
+    elif D[x][y][z] == -1:
+        D[x][y][z] = min(abs(x-A[z])+f(A[z], y, z+1),
+                         abs(y-A[z])+f(x, A[z], z+1))
+    return D[x][y][z]
+
+
+print(f(a-1, b-1, 0))
