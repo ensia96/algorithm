aa7bad05553093448a2cd4745cabbb4c67c4feb5
춤추는망고 <ensia96@gmail.com>
2022-06-17 21:08:45 +0900

    [update] 벽장문의 이동(2666) - 코드 길이 줄이기

--- baekjoon/private-study/dynamic-programming/벽장문의 이동(2666).py
+++ baekjoon/private-study/dynamic-programming/벽장문의 이동(2666).py
@@ -2,16 +2,12 @@ n = int(input())
 a, b = map(int, input().split())
 m = int(input())
 A = [~-int(input())for _ in ' '*m]
-D = [[m*[-1]for _ in ' '*n]for _ in ' '*n]
 
 
 def f(x, y, z):
     if z == m:
         return 0
-    elif D[x][y][z] == -1:
-        D[x][y][z] = min(abs(x-A[z])+f(A[z], y, z+1),
-                         abs(y-A[z])+f(x, A[z], z+1))
-    return D[x][y][z]
+    return min(abs(x-A[z])+f(A[z], y, z+1), abs(y-A[z])+f(x, A[z], z+1))
 
 
 print(f(a-1, b-1, 0))
