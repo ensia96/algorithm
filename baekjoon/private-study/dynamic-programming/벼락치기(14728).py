80bcbaf3c4e2e38773e921ff06a920f613a18e59
춤추는망고 <ensia96@gmail.com>
2022-06-21 14:06:47 +0900

    [update] 벼락치기(14728) - 런타임 에러 (IndexError) 해결

--- baekjoon/private-study/dynamic-programming/벼락치기(14728).py
+++ baekjoon/private-study/dynamic-programming/벼락치기(14728).py
@@ -2,5 +2,5 @@ n, t = map(int, input().split())
 D = -~t*[0]
 for _ in ' '*n:
     k, s = map(int, input().split())
-    D = [max(D[j], (D[j-k]+s)*(j >= k))for j in range(t+1)]
+    D = k > t and D or [max(D[j], (D[j-k]+s)*(j >= k))for j in range(t+1)]
 print(D[-1])
