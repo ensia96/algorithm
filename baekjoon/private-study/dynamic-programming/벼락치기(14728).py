b240b299d31eace09d4a7e7b1787ed1e559fd7c5
춤추는망고 <ensia96@gmail.com>
2022-06-21 13:48:54 +0900

    [update] 벼락치기(14728) - 틀렸습니다 해결

--- baekjoon/private-study/dynamic-programming/벼락치기(14728).py
+++ baekjoon/private-study/dynamic-programming/벼락치기(14728).py
@@ -2,5 +2,5 @@ n, t = map(int, input().split())
 D = t*[0]
 for _ in ' '*n:
     k, s = map(int, input().split())
-    D = [max(D[j], (D[j-k]+s)*(j > k))for j in range(t)]
+    D = [max(D[j], (D[j-k]+s)*(j >= k))for j in range(t)]
 print(D[-1])
