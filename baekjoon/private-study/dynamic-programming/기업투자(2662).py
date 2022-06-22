26a3d29ef34ef964fdd2adbaf9b905bbce934dbf
춤추는망고 <ensia96@gmail.com>
2022-06-22 15:57:23 +0900

    [update] 기업투자(2662) - 코드 길이 줄이기

--- baekjoon/private-study/dynamic-programming/기업투자(2662).py
+++ baekjoon/private-study/dynamic-programming/기업투자(2662).py
@@ -1,15 +1,14 @@
 n, m = map(int, input().split())
 A = [[*map(int, input().split())]for _ in ' '*n]
-D, P, R = [-~n*[0]for _ in ' '*m], [-~n*[0]for _ in ' '*m], []
+D, P, R = -~n*[0], [-~n*[0]for _ in ' '*m], []
 for i in range(m):
-    for j in range(1, n+1):
-        D[i][j] = D[i-1][j]
+    for j in range(n, 0, -1):
         for k in range(j):
-            if D[i][j] < D[i-1][j-k-1]+A[k][i+1]:
-                D[i][j], P[i][j] = D[i-1][j-k-1]+A[k][i+1], k+1
+            if D[j] < D[j-k-1]+A[k][i+1]:
+                D[j], P[i][j] = D[j-k-1]+A[k][i+1], k+1
 while m:
     m -= 1
     R += P[m][n],
     n -= P[m][n]
-print(D[-1][-1])
+print(D[-1])
 print(*R[::-1])
