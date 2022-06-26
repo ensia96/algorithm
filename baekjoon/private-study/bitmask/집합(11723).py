f6a9a0d4705cfd62ea7ddd729d40ed96d3b8cd5c
춤추는망고 <ensia96@gmail.com>
2022-06-26 14:17:17 +0900

    [update] 집합(11723) - 메모리 초과 해결

--- baekjoon/private-study/bitmask/집합(11723).py
+++ baekjoon/private-study/bitmask/집합(11723).py
@@ -1,6 +1,8 @@
+import sys
+i = sys.stdin.readline
 S = 0
-for l in [*open(0)][1:]:
-    c, *A = l.split()
+for _ in ' '*int(i()):
+    c, *A = i().split()
     x, y = c[2], A and 1 << int(A[0])
     S = {'d': S | y, 'm': S & ~y, 'g': S ^ y, 'e': x ==
          'e' and print(+bool(S & y)) or S}[x]if x in 'dmge'else x == 'l' and (1 << 21)-1
