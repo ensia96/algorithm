8854124a73eaa4c3a88b8d631c70356991cf25ae
춤추는망고 <ensia96@gmail.com>
2022-06-26 14:03:33 +0900

    [update] 집합(11723) - 시간 초과 해결

--- baekjoon/private-study/bitmask/집합(11723).py
+++ baekjoon/private-study/bitmask/집합(11723).py
@@ -2,5 +2,13 @@ S = 0
 for _ in ' '*int(input()):
     c, *A = input().split()
     x, y = c[2], A and 1 << int(A[0])
-    S = {'d': S | y, 'm': S & ~y, 'g': S ^ y, 'e': x ==
-         'e' and print(+bool(S & y)) or S}[x]if x in 'dmge'else x == 'l' and (1 << 21)-1
+    if x == 'd':
+        S |= y
+    elif x == 'm':
+        S &= ~y
+    elif x == 'e':
+        print(+bool(S & y))
+    elif x == 'g':
+        S ^= y
+    else:
+        S = ((1 << 21)-1)*(x == 'l')
