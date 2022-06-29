9c8a66239788841e7e09809c949df33b0c77d945
춤추는망고 <ensia96@gmail.com>
2022-06-29 16:23:15 +0900

    도영이가 만든 맛있는 음식(2961)

---
+++ baekjoon/private-study/bitmask/도영이가 만든 맛있는 음식(2961).py
@@ -0,0 +1,12 @@
+n = int(input())
+A = [[*map(int, input().split())]for _ in ' '*n]
+
+
+def f(x, y, z):
+    if x == n:
+        return abs(y-z)
+    s, b = A[x]
+    return min(f(x+1, y, z), f(x+1, y*s, z+b))
+
+
+print(f(0, 1, 0))
