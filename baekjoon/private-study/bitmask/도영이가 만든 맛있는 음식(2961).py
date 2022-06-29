452d0a408cff12da517d441a418e67db81611943
춤추는망고 <ensia96@gmail.com>
2022-06-29 17:03:50 +0900

    [update] 도영이가 만든 맛있는 음식(2961) - 틀렸습니다 해결

--- baekjoon/private-study/bitmask/도영이가 만든 맛있는 음식(2961).py
+++ baekjoon/private-study/bitmask/도영이가 만든 맛있는 음식(2961).py
@@ -2,5 +2,5 @@ n = int(input())
 A = [(1, 0)]
 for _ in ' '*n:
     s, b = map(int, input().split())
-    A += [(x*s, b-y)for x, y in A]
-print(min(abs(x-y)for x, y in A))
+    A += [(x*s, b+y)for x, y in A]
+print(min(abs(x-y)for x, y in A[1:]))
