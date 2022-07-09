1917a7042c0473977f5e1018431b2adefb9b08fe
춤추는망고 <ensia96@gmail.com>
2022-07-09 18:40:38 +0900

    [update] 개똥벌레(3020) - 코드 길이 줄이기

--- baekjoon/private-study/binary search/개똥벌레(3020).py
+++ baekjoon/private-study/binary search/개똥벌레(3020).py
@@ -1,24 +1,9 @@
+import bisect as B
 n, h = map(int, input().split())
-A, x, c = [[], []], 0, n//2
+A, f, c = [[], []], B.bisect_left, n//2
 for i in range(n):
     A[i % 2] += int(input()),
 A[0].sort()
 A[1].sort()
-
-
-def f(x, y):
-    l, r = 0, c
-    while l <= r:
-        m = (l+r)//2
-        if m == c:
-            t = m
-            break
-        if A[x][m] >= y:
-            r, t = m-1, m
-        else:
-            l = m+1
-    return t
-
-
-T = [c-f(0, i+1)+c-f(1, h-i)for i in range(h)]
+T = [c-f(A[0], i+1)+c-f(A[1], h-i)for i in range(h)]
 print(min(T), T.count(min(T)))
