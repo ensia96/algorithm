b88f3604ab274941070560b5c004d9c73e7bb41c
춤추는망고 <ensia96@gmail.com>
2022-07-16 16:58:22 +0900

    [update] 사냥꾼(8983) - 코드 길이 줄이기

--- baekjoon/private-study/binary search/사냥꾼(8983).py
+++ baekjoon/private-study/binary search/사냥꾼(8983).py
@@ -1,17 +1,7 @@
+import bisect as B
 m, n, l = map(int, input().split())
 A, C = sorted(map(int, input().split())), 0
 for _ in ' '*n:
     x, y = map(int, input().split())
-    if y > l:
-        continue
-    s, e, z = 0, len(A)-1, l-y
-    while s <= e:
-        m = (s+e)//2
-        if A[m] < x-z:
-            s = m+1
-        elif A[m] > x+z:
-            e = m-1
-        else:
-            C += 1
-            break
+    C += (y <= l)*bool(B.bisect_left(A, x-l+y) < B.bisect_right(A, x+l-y))
 print(C)
