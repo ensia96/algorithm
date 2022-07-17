8b41e8f6ffce58a7aac0a6f9a22692bf3d3d47fc
춤추는망고 <ensia96@gmail.com>
2022-07-17 16:56:15 +0900

    [update] 꼬인 전깃줄(1365) - 코드 길이 줄이기

--- baekjoon/private-study/binary search/꼬인 전깃줄(1365).py
+++ baekjoon/private-study/binary search/꼬인 전깃줄(1365).py
@@ -1,14 +1,9 @@
+import bisect
 n = int(input())
 A = [*map(int, input().split())]
 D = []
 for a in A:
-    l, r, t = 0, len(D)-1, 0
-    while l <= r:
-        m = (l+r)//2
-        if D[m] < a:
-            l = t = m+1
-        else:
-            r = m-1
+    t = bisect.bisect_left(D, a)
     if t == len(D):
         D += a,
     D[t] = a
