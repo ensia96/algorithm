d30728060ae3bdffc567a5ebb9daf95b4f42f7d3
춤추는망고 <ensia96@gmail.com>
2022-07-20 17:31:14 +0900

    [udpate] 오름세(3745) - 컴파일 에러 해결

--- baekjoon/private-study/binary search/오름세(3745).py
+++ baekjoon/private-study/binary search/오름세(3745).py
@@ -2,5 +2,6 @@ import bisect
 for l in [*open(0)][1::2]:
     D = []
     for a in map(int, l.split()):
-        t = bisect.bisect_left(D, a)D[t:t+1] = a,
+        t = bisect.bisect_left(D, a)
+        D[t:t+1] = a,
     print(len(D))
