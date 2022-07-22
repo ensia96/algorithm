4ea85c84637817f3ef5c0e7ee2a81927ced1ae14
춤추는망고 <ensia96@gmail.com>
2022-07-22 18:19:38 +0900

    [update] K번째 수(7469) - 코드 길이 줄이기

--- baekjoon/private-study/binary search/K번째 수(7469).py
+++ baekjoon/private-study/binary search/K번째 수(7469).py
@@ -7,22 +7,21 @@ T = [[]for _ in ' '*2*S]
 A = [*map(int, input().split())]
 
 
-def U(h, i, j, k, l):
-    if l > 2*S:
-        return
-    T[l] += h,
-    t, l = (i+j)//2, l*2
-    if k < t:
-        U(h, i, t, k, l)
+def U(i, l, r):
+    if l == r:
+        T[i] = [A[l-1]]
     else:
-        U(h, t+1, j, k, l+1)
+        m = (l+r)//2
+        U(i*2, l, m)
+        U(i*2+1, m+1, r)
+        T[i] = sorted(T[i*2]+T[i*2+1])
 
 
 def F(h, i, j, k, l, x):
     if l < i or j < k:
         return 0
     if k <= i and j <= l:
-        return bisect.bisect_left(T[h], h)
+        return bisect.bisect_left(T[h], x)
     m = (i+j)//2
     return F(h*2, i, m, k, l, x)+F(h*2+1, m+1, j, k, l, x)
 
@@ -38,9 +37,6 @@ def Q(i, j, k):
     print(r)
 
 
-for i in range(n):
-    U(A[i], 0, S, i, 1)
-for i in range(2*S):
-    T[i].sort()
+U(1, 1, n)
 for _ in ' '*m:
     Q(*map(int, input().split()))
