3041d2498a52bd63a097271abc98b28fd7119573
춤추는망고 <ensia96@gmail.com>
2022-07-25 15:25:12 +0900

    [udpate] 수열(2559) - 런타임 에러 (ValueError) 해결

--- baekjoon/private-study/two pointer/수열(2559).py
+++ baekjoon/private-study/two pointer/수열(2559).py
@@ -2,4 +2,4 @@ n, k = map(int, input().split())
 A = [*map(int, input().split())]
 for i in range(1, n):
     A[i] += A[i-1]
-print(max(A[i+k]-A[i]for i in range(n-k)))
+print(max(A[i+k]-A[i]for i in range(n-k))if n-k else A[-1])
