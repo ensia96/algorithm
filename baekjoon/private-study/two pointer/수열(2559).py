295b4b00c56740998e80e9812b234dcad29cdb80
춤추는망고 <ensia96@gmail.com>
2022-07-25 15:29:40 +0900

    [udpate] 수열(2559) - 틀렸습니다 해결

--- baekjoon/private-study/two pointer/수열(2559).py
+++ baekjoon/private-study/two pointer/수열(2559).py
@@ -2,4 +2,4 @@ n, k = map(int, input().split())
 A = [0]+[*map(int, input().split())]
 for i in range(n):
     A[i+1] += A[i]
-print(max(A[i+k]-A[i]for i in range(n-k))if n-k else A[-1])
+print(max(A[i+k]-A[i]for i in range(n-k+1))if n-k else A[-1])
