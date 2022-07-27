fb1c2c9f00706ae4a05610bc2f152beab4fc33a9
춤추는망고 <ensia96@gmail.com>
2022-07-27 13:35:02 +0900

    [update] 회문(17609) - 런타임 에러 (RecursionError) 해결

--- baekjoon/private-study/two pointer/회문(17609).py
+++ baekjoon/private-study/two pointer/회문(17609).py
@@ -1,12 +1,13 @@
 def f(l, r, c):
-    if l >= r:
-        return c
-    if A[l] == A[r]:
-        return f(l+1, r-1, c)
-    elif not c:
-        return min(f(l+1, r, 1), f(l, r-1, 1))
-    else:
-        return 2
+    while l < r:
+        if A[l] == A[r]:
+            l += 1
+            r -= 1
+        elif not c:
+            return 1+(f(l+1, r, 1)-1)*(f(l, r-1, 1)-1)
+        else:
+            return 2
+    return c
 
 
 for _ in ' '*int(input()):
