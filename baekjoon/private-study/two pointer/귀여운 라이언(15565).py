b9373cb3a63e3b61368fb2643bcabb415cc90cd2
춤추는망고 <ensia96@gmail.com>
2022-07-31 14:59:07 +0900

    귀여운 라이언(15565)

---
+++ baekjoon/private-study/two pointer/귀여운 라이언(15565).py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+A = [*map(int, input().split())]
+l = r = 0
+c, s = A[0] < 2, n-1
+while l < n:
+    if c < k and r < n-1:
+        r += 1
+        c += A[r] < 2
+    else:
+        c -= A[l] < 2
+        l += 1
+        s = min(s, r-l+1+s*(c != k))
+print(s)
