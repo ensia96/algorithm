f0e4b617d3e58aa311396f631dc7131363bf5f65
춤추는망고 <ensia96@gmail.com>
2022-08-07 15:59:40 +0900

    선수과목 (Prerequisite)(14567) - 코드 길이 줄이기

--- baekjoon/private-study/topological sorting/선수과목 (Prerequisite)(14567).py
+++ baekjoon/private-study/topological sorting/선수과목 (Prerequisite)(14567).py
@@ -1,18 +1,9 @@
 n, m = map(int, input().split())
 E = [[]for _ in ' '*-~n]
-I, A = [0]*-~n, [1]*-~n
+A = [1]*-~n
 for _ in ' '*m:
     a, b = map(int, input().split())
-    E[a] += b,
-    I[b] += 1
-Q = [i for i in range(1, n+1)if not I[i]]
-while Q:
-    q = []
-    for x in Q:
-        for e in E[x]:
-            I[e] -= 1
-            if not I[e]:
-                A[e] = max(A[e], A[x]+1)
-                q += e,
-    Q = q
+    E[b] += a,
+for i in range(1, n+1):
+    A[i] = max([A[j]for j in E[i]]+[0])+1
 print(*A[1:])
