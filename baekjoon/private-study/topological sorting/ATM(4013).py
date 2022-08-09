fb5ee264893c2cb4654be7975ce8d19a2e502a9c
춤추는망고 <ensia96@gmail.com>
2022-08-09 22:07:49 +0900

    [update] ATM(4013) - 메모리 초과 해결

--- baekjoon/private-study/topological sorting/ATM(4013).py
+++ baekjoon/private-study/topological sorting/ATM(4013).py
@@ -1,63 +1,76 @@
-from collections import deque
 import sys
+from collections import deque
 sys.setrecursionlimit(650001)
-I = sys.stdin.readline
-L, S = lambda: map(int, I().split()), []
+def input(): return sys.stdin.readline().rstrip()
+def read(): return map(int, input().split())
 
 
-def f(x):
-    V[x] = 1
-    for a in E[x]:
-        V[a] or f(a)
-    S.append(x)
+def dfs(node):
+    visit[node] = 1
+    for now in graph[node]:
+        if not visit[now]:
+            dfs(now)
+    stack.append(node)
 
 
-def r(x, y):
-    H[x] = y
-    M[y] += W[x]
-    for a in R[x]:
-        if H[a] == -1:
-            r(a, y)
-        elif H[x]-H[a]:
-            G[H[a]].append(H[x])
+def reverse_dfs(node, num):
+    scc_num[node] = num
+    scc_arr[num] += 1
+    scc_val[num] += cash[node]
+    for now in reverse_graph[node]:
+        if scc_num[now] == -1:
+            reverse_dfs(now, num)
+        elif scc_num[node] != scc_num[now]:
+            group[scc_num[now]].append(scc_num[node])
 
 
-n, m = L()
-E = [[]for i in range(n)]
-R = [[]for i in range(n)]
-V = [0]*n
-S = []
-H = [-1]*n
-G = []
-for i in range(m):
-    a, b = L()
-    E[a-1].append(b-1)
-    R[b-1].append(a-1)
-W = [int(I())for i in range(n)]
-for i in range(n):
-    V[i] or f(i)
-M = []
+N, M = read()
+graph = [[] for i in range(N)]
+reverse_graph = [[] for i in range(N)]
+visit = [0] * N
+stack = []
+scc_num = [-1] * N
+scc_arr = []
+group = []
+
+for i in range(M):
+    a, b = read()
+    graph[a-1].append(b-1)
+    reverse_graph[b-1].append(a-1)
+
+cash = [int(input()) for i in range(N)]
+
+for i in range(N):
+    if not visit[i]:
+        dfs(i)
+
+scc_val = []
 k = 0
-while S:
-    x = S.pop()
-    if H[x] == -1:
-        G.append([])
-        M.append(0)
-        r(x, k)
+while stack:
+    now = stack.pop()
+    if scc_num[now] == -1:
+        group.append([])
+        scc_arr.append(0)
+        scc_val.append(0)
+        reverse_dfs(now, k)
         k += 1
-s, p = L()
-s -= 1
-del E, R
-Q = deque([H[s]])
-D = [0]*k
-D[H[s]] = M[H[s]]
-while Q:
-    x = Q.popleft()
-    for n in G[x]:
-        if D[n] < D[x]+M[n]:
-            D[n] = D[x]+M[n]
-            Q.append(n)
-A = 0
-for r in L():
-    A = max(A, D[H[r-1]])
-print(A)
+
+S, P = read()
+S -= 1
+result = list(read())
+
+del graph, reverse_graph
+
+que = deque([scc_num[S]])
+dp = [0] * k
+dp[scc_num[S]] = scc_val[scc_num[S]]
+while que:
+    now = que.popleft()
+    for n in group[now]:
+        if dp[n] < dp[now] + scc_val[n]:
+            dp[n] = dp[now] + scc_val[n]
+            que.append(n)
+answer = 0
+for r in result:
+    answer = max(answer, dp[scc_num[r-1]])
+print(answer)
