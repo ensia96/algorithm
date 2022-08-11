81e72cb9f898e9b5f2f1aa4ad113a53e24672292
춤추는망고 <ensia96@gmail.com>
2022-08-11 16:23:47 +0900

    과제(13904)

---
+++ baekjoon/private-study/priority queue/과제(13904).py
@@ -0,0 +1,13 @@
+import heapq as H
+n = int(input())
+Q, l = [], 0
+for _ in ' '*n:
+    d, w = map(int, input().split())
+    H.heappush(Q, (d, -w))
+    l = max(l, d)
+D = [-~l*[0]for _ in ' '*n]
+for i in range(n):
+    x, y = H.heappop(Q)
+    for j in range(x):
+        D[i][j] = max(D[i-1][j], D[i-1][j-1]-y)
+print(max(D[-1]))
