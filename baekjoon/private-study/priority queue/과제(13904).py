0c2587687f8aa46b2199b8c7feabdd9744055df8
춤추는망고 <ensia96@gmail.com>
2022-08-11 18:29:52 +0900

    [update] 과제(13904) - 코드 길이 줄이기

--- baekjoon/private-study/priority queue/과제(13904).py
+++ baekjoon/private-study/priority queue/과제(13904).py
@@ -1,18 +1,6 @@
-import heapq as H
-n = int(input())
+from heapq import*
 Q = []
-l = p = 0
-for _ in ' '*n:
-    d, w = map(int, input().split())
-    H.heappush(Q, (-w, -d))
-    l = max(l, d+1)
-C = [1]*l
-while Q:
-    x, y = H.heappop(Q)
-    while y:
-        y += 1
-        if C[y]:
-            p -= x
-            C[y] = 0
-            break
-print(p)
+for d, w in sorted((*map(int, input().split()),)for _ in ' '*int(input())):
+    heappush(Q, w)
+    d < len(Q) and heappop(Q)
+print(sum(Q))
