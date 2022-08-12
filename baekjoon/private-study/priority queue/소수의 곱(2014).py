d0bdbf68fcc1713f73022ad86a2f939ec0de5e5b
춤추는망고 <ensia96@gmail.com>
2022-08-12 21:45:38 +0900

    [update] 소수의 곱(2014) - 메모리 초과 해결

--- baekjoon/private-study/priority queue/소수의 곱(2014).py
+++ baekjoon/private-study/priority queue/소수의 곱(2014).py
@@ -1,12 +1,17 @@
 import heapq as H
 k, n = map(int, input().split())
 A = [*map(int, input().split())]
-Q = [(A[i], i)for i in range(k)]
-s = set()
-f = 1
-while f:
-    x, y = H.heappop(Q)
-    s.add(x)
-    for i in range(y, k):
-        x*A[i] in s or H.heappush(Q, (x*A[i], i))
-        n > len(s) or exit(print(sorted([*s])[n-1]))
+Q = [1]
+C = set([1])
+M = 0
+while n:
+    x = H.heappop(Q)
+    for a in A:
+        t = a*x
+        if t in C or len(Q) > n and M <= t:
+            continue
+        H.heappush(Q, t)
+        M = max(M, t)
+        C.add(t)
+    n -= 1
+print(Q[0])
