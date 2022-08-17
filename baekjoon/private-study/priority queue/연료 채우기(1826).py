a7dc7221b397e1eb01c213a4c15ef4a87930dae9
춤추는망고 <ensia96@gmail.com>
2022-08-17 14:31:49 +0900

    [update] 연료 채우기(1826) - 메모리 초과 해결

--- baekjoon/private-study/priority queue/연료 채우기(1826).py
+++ baekjoon/private-study/priority queue/연료 채우기(1826).py
@@ -1,15 +1,14 @@
 import heapq as H
 n = int(input())
 A = [(*map(int, input().split()),)for _ in ' '*n]
+H.heapify(A)
 l, p = map(int, input().split())
-Q = [(0, 0, p)]
-f = 1
-while Q:
-    x, y, z = H.heappop(Q)
-    if l <= y+z:
-        f = 0
-        break
-    for i in range(n):
-        a, b = A[i]
-        y < a <= y+z and H.heappush(Q, (x+1, a, z-a+y+b))
-print(-1 if f else x)
+Q = []
+a = 0
+while p < l:
+    while A and A[0][0] <= p:
+        H.heappush(Q, -H.heappop(A)[1])
+    Q or exit(print(-1))
+    p -= H.heappop(Q)
+    a += 1
+print(a)
