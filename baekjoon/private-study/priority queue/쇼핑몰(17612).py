84e659ec79528f8dc2b7b0890e802a0a074be669
춤추는망고 <ensia96@gmail.com>
2022-08-21 13:14:44 +0900

    [update] 쇼핑몰(17612) - 시간 초과 해결

--- baekjoon/private-study/priority queue/쇼핑몰(17612).py
+++ baekjoon/private-study/priority queue/쇼핑몰(17612).py
@@ -1,19 +1,16 @@
 import heapq as H
 n, k = map(int, input().split())
-Q, A, C, x = [], [], [0]*k, 0
+Q, A, C, x = [], [], [*range(k)], 0
+H.heapify(C)
 for _ in ' '*n:
     i, w = map(int, input().split())
     if len(Q) == k:
         x, y, z = Q[0]
         while Q and Q[0][0] == x:
             a, b, c = H.heappop(Q)
-            C[-b] = 0
+            H.heappush(C, -b)
             A += c,
-    for c in range(k):
-        if not C[c]:
-            C[c] = 1
-            H.heappush(Q, (x+w, -c, i))
-            break
+    H.heappush(Q, (x+w, -H.heappop(C), i))
 while Q:
     A += H.heappop(Q)[2],
 print(sum(-~i*A[i]for i in range(n)))
