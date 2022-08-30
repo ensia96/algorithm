5033e25ebef49a446aa4625e4fa05395c98e608b
춤추는망고 <ensia96@gmail.com>
2022-08-30 15:52:32 +0900

    세부(13905)

---
+++ baekjoon/private-study/minimum spanning tree/세부(13905).py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+s, e = map(int, input().split())
+E = []
+G = [*range(n+1)]
+for _ in ' '*m:
+    h1, h2, k = map(int, input().split())
+    E += (-k, h1, h2),
+
+
+def f(x):
+    if G[x]-x:
+        G[x] = f(G[x])
+    return G[x]
+
+
+for x, y, z in sorted(E):
+    y, z = f(y), f(z)
+    if y-z:
+        G[max(y, z)] = min(y, z)
+        G[s]-G[e] or exit(print(-x))
+print(0)
