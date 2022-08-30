8e31eb25fe93d53d3f85a5170deabb7d0393bfcd
춤추는망고 <ensia96@gmail.com>
2022-08-30 16:34:35 +0900

    [update] 세부(13905) - 코드 길이 줄이기

--- baekjoon/private-study/minimum spanning tree/세부(13905).py
+++ baekjoon/private-study/minimum spanning tree/세부(13905).py
@@ -13,21 +13,9 @@ def f(x):
     return G[x]
 
 
-T = [[]for _ in ' '*-~n]
 for x, y, z in sorted(E):
-    a, b = f(y), f(z)
-    if a-b:
-        G[max(a, b)] = min(a, b)
-        T[y] += (z, -x),
-        T[z] += (y, -x),
-Q, A, V, C = [s], 0, [1]*-~n, [0]*-~n
-V[s], C[s] = 0, 8**8
-while Q:
-    q = []
-    for c in Q:
-        for x, y in T[c]:
-            if V[x]:
-                V[x], C[x] = 0, min(C[c], y)
-                q += x,
-    Q = q
-print(C[e])
+    y, z = f(y), f(z)
+    if y-z:
+        G[max(y, z)] = min(y, z)
+        f(s)-f(e) or exit(print(-x))
+print(0)
