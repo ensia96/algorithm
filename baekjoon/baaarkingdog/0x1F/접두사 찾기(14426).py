f60f3a2947b0f49ea0f4b3e352b5d4c01b37d33a
춤추는망고 <ensia96@gmail.com>
2022-09-10 13:42:00 +0900

    [update] 접두사 찾기(14426) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x1F/접두사 찾기(14426).py
+++ baekjoon/baaarkingdog/0x1F/접두사 찾기(14426).py
@@ -3,31 +3,23 @@ M = n*500+1
 C = [0]*M
 N = [26*[-1]for _ in ' '*M]
 U = 1
-def T(c): return ord(c)-ord('a')
-
-
-def I():
-    global U
+A = 0
+for _ in ' '*n:
     c = 0
     for s in input():
-        t = T(s)
+        t = ord(s)-ord('a')
         if N[c][t] == -1:
             N[c][t] = U
             U += 1
         c = N[c][t]
     C[c] = 1
-
-
-def F():
+for _ in ' '*m:
     c = 0
     for s in input():
-        t = T(s)
+        t = ord(s)-ord('a')
         if N[c][t] == -1:
-            return 0
+            break
         c = N[c][t]
-    return 1
-
-
-for _ in ' '*n:
-    I()
-print(sum(F()for _ in ' '*m))
+    else:
+        A += 1
+print(A)
