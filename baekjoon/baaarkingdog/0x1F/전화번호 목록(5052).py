c54598e0eda7245db0f686d1a7196e0d85e41dc4
춤추는망고 <ensia96@gmail.com>
2022-09-11 19:24:33 +0900

    [update] 전화번호 목록(5052) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x1F/전화번호 목록(5052).py
+++ baekjoon/baaarkingdog/0x1F/전화번호 목록(5052).py
@@ -1,17 +1,22 @@
 for _ in ' '*int(input()):
     n = int(input())
     M = 10*n+1
-    C, N = [0]*M, [10*[-1]for _ in ' '*M]
+    I, C, N = [], [0]*M, [10*[-1]for _ in ' '*M]
     A = U = 1
     for _ in ' '*n:
         c = 0
-        for i in input():
-            t = int(i)
+        I += input(),
+        for s in I[-1]:
+            t = int(s)
             if N[c][t] == -1:
                 N[c][t] = U
                 U += 1
             c = N[c][t]
+        C[c] = 1
+    for S in I:
+        c = 0
+        for s in S[:-1]:
+            c = N[c][int(s)]
             if C[c]:
                 A = 0
-        C[c] = 1
     print('YES'if A else'NO')
