3e57ccd711ac880c69a8f9c191e37b11d78d99f7
춤추는망고 <ensia96@gmail.com>
2022-09-12 17:38:46 +0900

    [update] 디스크 트리(7432) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x1F/디스크 트리(7432).py
+++ baekjoon/baaarkingdog/0x1F/디스크 트리(7432).py
@@ -1,27 +1,14 @@
-S = "\\!#$%&'()-0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ^_`{}~"
-D = {S[i]: i for i in range(53)}
-n = int(input())
-M = 80*n+1
-C = [0]*M
-N = [53*[-1]for _ in ' '*M]
-U = 1
-for _ in ' '*n:
-    c = 0
-    for s in input():
-        t = D[s]
-        if N[c][t] == -1:
-            N[c][t] = U
-            U += 1
-        if t == 0:
-            C[c] = 1
-        c = N[c][t]
-    C[c] = 1
+T = {}
+for _ in ' '*int(input()):
+    C = T
+    for s in input().split('\\'):
+        C[s] = C = C.get(s, {})
 
 
-def f(x, y, z):
-    C[x] and print(' '*y+z)
-    for i in range(53):
-        0 < N[x][i] and f(N[x][i], y+(not i), z+S[i]if i else'')
+def f(x, y):
+    for k in sorted(x):
+        print(' '*y+k)
+        x[k] and f(x[k], y+1)
 
 
-f(0, 0, '')
+f(T, 0)
