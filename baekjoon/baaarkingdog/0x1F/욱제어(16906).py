ac00b0e53d4839fea63bd98f989da016a541be62
춤추는망고 <ensia96@gmail.com>
2022-09-16 14:59:34 +0900

    [update] 욱제어(16906) - 틀렸습니다 해결

--- baekjoon/baaarkingdog/0x1F/욱제어(16906).py
+++ baekjoon/baaarkingdog/0x1F/욱제어(16906).py
@@ -1,5 +1,4 @@
-input()
-A, D = [], {}
+A, D = [0]*int(input()), {}
 
 
 def f(S):
@@ -20,13 +19,14 @@ def p(S):
 
 def e(x, y):
     if not y:
-        p(x), A.append(x)
+        p(x)
+        A[I] = x
         return 1
     for i in '01':
         if not f(x+i) and e(x+i, y-1):
             return 1
 
 
-for i in sorted(map(int, input().split())):
-    e('', i) or exit(print(-1))
+for s, I in sorted((j, i)for i, j in enumerate(map(int, input().split()))):
+    e('', s) or exit(print(-1))
 print(1, *A, sep='\n')
