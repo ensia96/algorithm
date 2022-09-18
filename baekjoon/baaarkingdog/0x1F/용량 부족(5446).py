4511b3137af1137a6c76a459652dce72b1fbf242
춤추는망고 <ensia96@gmail.com>
2022-09-18 16:19:19 +0900

    [update] 용량 부족(5446) - 코드 길이 줄이기

--- baekjoon/baaarkingdog/0x1F/용량 부족(5446).py
+++ baekjoon/baaarkingdog/0x1F/용량 부족(5446).py
@@ -1,8 +1,8 @@
 def f(x):
     A = 0
     for k in x:
-        if x[k] and 1 in x[k]:
-            A += (2 in x[k])+f(x[k])if 0 in x[k]else 1
+        if x[k]:
+            A += (1 in x[k])+f(x[k])if 0 in x[k]else 1
     return A
 
 
@@ -12,8 +12,7 @@ for _ in ' '*int(input()):
         T = D
         for s in input():
             T[s] = T = T.get(s, {})
-            T[1] = 0
-        T[2] = 0
+        T[1] = 0
     m = int(input())
     for _ in ' '*m:
         T = D
