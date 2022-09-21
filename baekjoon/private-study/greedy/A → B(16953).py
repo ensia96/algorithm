7da17270f7ed1defba28dd03aa54b79f070bdb2b
춤추는망고 <ensia96@gmail.com>
2022-09-21 15:45:30 +0900

    [update] A → B(16953) - 그리디 풀이 추가

--- baekjoon/private-study/greedy/A → B(16953).py
+++ baekjoon/private-study/greedy/A → B(16953).py
@@ -1,13 +1,9 @@
 a, b = map(int, input().split())
-Q, c = [a], 0
-while Q:
-    q = []
-    for i in Q:
-        if i == b:
-            exit(print(c+1))
-        for j in [i*2, 10*i+1]:
-            if j <= b:
-                q += j,
-    Q = q
+c = 1
+while a < b:
+    if not b % 2:
+        b //= 2
+    elif b % 10 == 1:
+        b //= 10
     c += 1
-print(-1)
+print(-1 if a-b else c)
