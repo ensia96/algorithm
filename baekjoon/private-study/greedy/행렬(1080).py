3b7a059115228e4d74494134745556dacbe57cee
춤추는망고 <ensia96@gmail.com>
2022-09-27 13:42:43 +0900

    [update] 행렬(1080) - 틀렸습니다 해결

--- baekjoon/private-study/greedy/행렬(1080).py
+++ baekjoon/private-study/greedy/행렬(1080).py
@@ -3,30 +3,14 @@ n, m = map(int, I().split())
 A = [[*map(int, I())]for _ in ' '*n]
 B = [[*map(int, I())]for _ in ' '*n]
 c = 0
-
-
-def f(i, j):
-    for k in R(3):
-        for l in R(3):
-            A[i+k][j+l] = not A[i+k][j+l]
-    return 1
-
-
-def g(i):
-    for j in R(m-2, m):
-        if A[i][j] != B[i][j]:
-            print(-1)
-            return 1
-
-
 for i in R(n-2):
     for j in R(m-2):
-        c += A[i][j]-B[i][j] and f(i, j)
-    if g(i):
-        break
-else:
-    for i in R(n-2, n):
-        if g(i):
-            break
-    else:
-        print(c)
+        if A[i][j]-B[i][j]:
+            for k in R(3):
+                for l in R(3):
+                    A[i+k][j+l] = not A[i+k][j+l]
+                    c += 1
+for i in R(n):
+    for j in R(m):
+        A[i][j]-B[i][j] and exit(print(-1))
+print(c)
