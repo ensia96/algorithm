55daadec2d2fe10ef7c2cbea806a9d4d6d8939cd
춤추는망고 <ensia96@gmail.com>
2022-09-27 13:48:21 +0900

    [update] 행렬(1080) - 코드 길이 줄이기

--- baekjoon/private-study/greedy/행렬(1080).py
+++ baekjoon/private-study/greedy/행렬(1080).py
@@ -1,29 +1,16 @@
-def change(r, c):
-    for i in range(3):
-        for j in range(3):
-            if a[r+i][c+j] == 1:
-                a[r+i][c+j] = 0
-            else:
-                a[r+i][c+j] = 1
-
-
-n, m = map(int, input().split())
-a = [list(map(int, input()))for _ in range(n)]
-b = [list(map(int, input()))for _ in range(n)]
-
-
-count = 0
-for i in range(0, n-2):
-    for j in range(0, m-2):
-        if a[i][j] != b[i][j]:
-            change(i, j)
-            count += 1
-
-
-for i in range(n):
-    for j in range(m):
-        if a[i][j] != b[i][j]:
-            print(-1)
-            exit()
-
-print(count)
+I, R = input, range
+n, m = map(int, I().split())
+A = [[*map(int, I())]for _ in ' '*n]
+B = [[*map(int, I())]for _ in ' '*n]
+c = 0
+for i in R(n-2):
+    for j in R(m-2):
+        if A[i][j]-B[i][j]:
+            for k in R(3):
+                for l in R(3):
+                    A[i+k][j+l] = not A[i+k][j+l]
+            c += 1
+for i in R(n):
+    for j in R(m):
+        A[i][j]-B[i][j] and exit(print(-1))
+print(c)
