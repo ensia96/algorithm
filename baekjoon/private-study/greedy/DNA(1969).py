3d6ef197f72c9c1cdf4b17e0ce41a1af70198034
춤추는망고 <ensia96@gmail.com>
2022-10-08 20:15:36 +0900

    [update] DNA(1969) - 틀렸습니다 해결

--- baekjoon/private-study/greedy/DNA(1969).py
+++ baekjoon/private-study/greedy/DNA(1969).py
@@ -1,8 +1,8 @@
 import collections as C
 n, m = map(int, input().split())
 R, c = '', 0
-for a in [*zip(*[input()for _ in ' '*n])]:
-    a, *b = sorted(C.Counter(a).most_common())
+for a in zip(*[input()for _ in ' '*n]):
+    a, *b = sorted(C.Counter(a).most_common(), key=lambda x: (-x[1], x[0]))
     R += a[0]
     c += sum([c for _, c in b]+[0])
 print(R)
