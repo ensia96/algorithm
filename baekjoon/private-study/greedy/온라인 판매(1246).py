07ded3d5c52bd5f12103ef391e807dc360eb7273
춤추는망고 <ensia96@gmail.com>
2022-11-04 12:48:48 +0900

    [update] 온라인 판매(1246) - 코드 길이 줄이기

--- baekjoon/private-study/greedy/온라인 판매(1246).py
+++ baekjoon/private-study/greedy/온라인 판매(1246).py
@@ -1,3 +1,7 @@
 n, m = map(int, input().split())
-print(*max(((j, j*min(m-i, n))for i, j in enumerate(sorted(int(input())
-      for _ in ' '*m))), key=lambda x: (x[1], -x[0])))
+p = a = 0
+for i, j in enumerate(sorted(int(input())for _ in ' '*m)):
+    x = j*min(m-i, n)
+    if x > p:
+        a, p = j, x
+print(a, p)
