a220807dbf0ac1bb04c87e62ee5ce6b1ba4cbdd8
춤추는망고 <ensia96@gmail.com>
2022-11-08 19:25:30 +0900

    [update] 꿀 따기(21758) - 틀렸습니다 해결

--- baekjoon/private-study/greedy/꿀 따기(21758).py
+++ baekjoon/private-study/greedy/꿀 따기(21758).py
@@ -1,8 +1,11 @@
 n = int(input())
 *A, = map(int, input().split())
-a, b, x, y = abs(A[-1]-A[-2]), abs(A[0]-A[1]), A[-1], A[0]
+a, t = max(A[-1]-A[-2], A[-2]-A[-1]), A[-1]
 for i in range(n-2, 0, -1):
-    a, b = max(a, x-A[i]), max(b, y-A[-i])
-    x += A[i]
-    y += A[-i]
+    a = max(a, t-A[i])
+    t += A[i]
+b, t = max(A[0]-A[1], A[1]-A[0]), A[0]
+for i in range(1, n-1):
+    b = max(b, t-A[i])
+    t += A[i]
 print(max(a+sum(A[1:]), b+sum(A[:-1]), max(A[1:-1])+sum(A[1:-1])))
