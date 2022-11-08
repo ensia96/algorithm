d00318860de54ae66abf8482b932ecd58b4730a2
춤추는망고 <ensia96@gmail.com>
2022-11-08 20:08:20 +0900

    [update] 꿀 따기(21758) - 코드 길이 줄이기

--- baekjoon/private-study/greedy/꿀 따기(21758).py
+++ baekjoon/private-study/greedy/꿀 따기(21758).py
@@ -1,11 +1,8 @@
 n = int(input())
 *A, = map(int, input().split())
-a, t = max(A[-1]-A[-2], A[-2]-A[-1]), A[-1]
+a, b, x, y = abs(A[-1]-A[-2]), abs(A[0]-A[1]), A[-1], A[0]
 for i in range(n-2, 0, -1):
-    a = max(a, t-A[i])
-    t += A[i]
-b, t = max(A[0]-A[1], A[1]-A[0]), A[0]
-for i in range(1, n-1):
-    b = max(b, t-A[i])
-    t += A[i]
+    a, b = max(a, x-A[i]), max(b, y-A[-i-1])
+    x += A[i]
+    y += A[-i-1]
 print(max(a+sum(A[1:]), b+sum(A[:-1]), max(A[1:-1])+sum(A[1:-1])))
