d2e486d327a892cc224d3f7655f5d8e7eab3e861
춤추는망고 <ensia96@gmail.com>
2022-12-03 14:56:21 +0900

    [update] 숫자의 신(1422) - 컴파일 에러 해결

--- baekjoon/private-study/greedy/숫자의 신(1422).py
+++ baekjoon/private-study/greedy/숫자의 신(1422).py
@@ -1,3 +1,3 @@
 k, n = map(int, input().split())
 A = [input()for _ in ' '*k]
-print(*sorted(A+[max(A, key=int)]*(n-k), key=lambda x: x*10)[::-1]), sep='')
+print(*sorted(A+[max(A, key=int)]*(n-k), key=lambda x: x*10)[::-1], sep='')
