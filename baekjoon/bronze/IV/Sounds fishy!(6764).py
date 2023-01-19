3f9a6dc276d0a31be608572d31796ef06a30de56
춤추는망고 <ensia96@gmail.com>
2023-01-19 22:03:23 +0900

    [update] Sounds fishy!(6764) - 틀렸습니다 해결

--- baekjoon/bronze/IV/Sounds fishy!(6764).py
+++ baekjoon/bronze/IV/Sounds fishy!(6764).py
@@ -1,3 +1,3 @@
 a, b, c, d = int(input()), int(input()), int(input()), int(input())
-print('Fish Rising'if a < b < c < d else 'Fish Diving'if a > b >
-      c > d else 'Constant Depth'if a == b == c == d else 'No Fish')
+print('Fish Rising'if a < b < c < d else 'Fish Diving'if a > b > c >
+      d else 'Fish At Constant Depth'if a == b == c == d else 'No Fish')
