e1af18666e9fe4feb2b23d67f387330cf3fed927
춤추는망고 <ensia96@gmail.com>
2023-02-03 21:31:00 +0900

    [update] 출석 이벤트(25704) - 틀렸습니다 해결

--- baekjoon/bronze/IV/출석 이벤트(25704).py
+++ baekjoon/bronze/IV/출석 이벤트(25704).py
@@ -1,7 +1,2 @@
 n, p = int(input())//5, int(input())
-A = [(500, 1), (p//10, 2), (2000, 3), (p//4, 4)]
-while n:
-    x, y = max(A[:n])
-    p = max(p-x, 0)
-    n = max(n-y, 0)
-print(p)
+print(p-max([500, p//10, 2000, p//4][:n]))
