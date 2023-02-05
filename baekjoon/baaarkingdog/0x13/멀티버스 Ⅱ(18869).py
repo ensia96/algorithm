df7f6de409fcb99dfa93bcc417ff681e670f17c0
춤추는망고 <ensia96@gmail.com>
2023-02-05 09:45:21 +0900

    [update] 멀티버스 Ⅱ(18869) - 런타임 에러 (NameError) 해결

--- baekjoon/baaarkingdog/0x13/멀티버스 Ⅱ(18869).py
+++ baekjoon/baaarkingdog/0x13/멀티버스 Ⅱ(18869).py
@@ -2,6 +2,6 @@ D = {}
 for l in [*open(0)][1:]:
     *l, = map(int, l.split())
     d = {j: i for i, j in enumerate(sorted([*set(l)]))}
-    k = *(d[e]for e in a),
+    k = *(d[i]for i in l),
     D[k] = D.get(k, 0)+1
 print(sum(v*~-v//2for v in D.values()))
