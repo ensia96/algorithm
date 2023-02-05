c4798758e668c6ec0f9954e76bc80e9a9448dd05
춤추는망고 <ensia96@gmail.com>
2023-02-05 09:10:21 +0900

    [update] 멀티버스 Ⅱ(18869) - 런타임 에러 (IndexError) 해결

--- baekjoon/baaarkingdog/0x13/멀티버스 Ⅱ(18869).py
+++ baekjoon/baaarkingdog/0x13/멀티버스 Ⅱ(18869).py
@@ -1,19 +1,4 @@
-L, T = lambda: map(int, input().split()), []
-M, N = L()
-S = [[*L()]for _ in range(M)]
-for i in range(M):
-    s = S[i]
-    c, t = [*sorted(set(s))], []
-    for j in range(N):
-        l, r, k = 0, N, s[j]
-        while l < r:
-            m = (l+r)//2
-            if c[m] < k:
-                l = m
-            elif c[m] > k:
-                r = m
-            else:
-                t += [m]
-                break
-    S[i] = (*t,)
-print(sum(S[i] == S[j]for i in range(M)for j in range(i+1, M)))
+m, n = map(int, input().split())
+T = [(*map(lambda x:x[1], sorted(map(lambda x:x[::-1],
+      enumerate(map(int, input().split()))))),)for _ in ' '*m]
+print(sum(T[i] == T[j]for i in range(m)for j in range(i+1, m)))
