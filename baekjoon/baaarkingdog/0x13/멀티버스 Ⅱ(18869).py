a3d5559fdf09009564eee88f04116242a49ac024
춤추는망고 <ensia96@gmail.com>
2023-02-05 09:17:35 +0900

    [update] 멀티버스 Ⅱ(18869) - 시간 초과 해결

--- baekjoon/baaarkingdog/0x13/멀티버스 Ⅱ(18869).py
+++ baekjoon/baaarkingdog/0x13/멀티버스 Ⅱ(18869).py
@@ -1,4 +1,5 @@
+import sys
 m, n = map(int, input().split())
 T = [(*map(lambda x:x[1], sorted(map(lambda x:x[::-1],
-      enumerate(map(int, input().split()))))),)for _ in ' '*m]
+      enumerate(map(int, sys.stdin.readline().split()))))),)for _ in ' '*m]
 print(sum(T[i] == T[j]for i in range(m)for j in range(i+1, m)))
