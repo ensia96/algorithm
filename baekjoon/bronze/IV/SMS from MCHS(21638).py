193634354f34ed10a2b67ecc4017732c92983074
춤추는망고 <ensia96@gmail.com>
2023-02-09 22:51:20 +0900

    [update] SMS from MCHS(21638) - 틀렸습니다 해결

--- baekjoon/bronze/IV/SMS from MCHS(21638).py
+++ baekjoon/bronze/IV/SMS from MCHS(21638).py
@@ -1,6 +1,5 @@
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 x, y, z = c < 0 and d > 9, c < a, b < d
-x+y + \
-    z and print(
-        'A storm warning for tomorrow! Be careful and stay home if possible!'if x else f'MCHS warns! {"Low temperature"if y else"Strong wind"} is expected tomorrow.')
+print(
+    'A storm warning for tomorrow! Be careful and stay home if possible!'if x else f'MCHS warns! {"Low temperature"if y else"Strong wind"} is expected tomorrow.'if x+y+z else 'No message')
