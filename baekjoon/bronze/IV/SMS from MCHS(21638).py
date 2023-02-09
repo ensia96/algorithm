05ca38f059f2df65231b08b1b0c15714d1e44687
춤추는망고 <ensia96@gmail.com>
2023-02-09 22:47:09 +0900

    SMS from MCHS(21638)

---
+++ baekjoon/bronze/IV/SMS from MCHS(21638).py
@@ -0,0 +1,4 @@
+x, y, z = c < 0 and d > 9, c < a, b < d
+x+y + \
+    z and print(
+        'A storm warning for tomorrow! Be careful and stay home if possible!'if x else f'MCHS warns! {"Low temperature"if y else"Strong wind"} is expected tomorrow.')
