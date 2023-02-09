b0ecdb0393797a20a924805a2f6f2a573287268d
춤추는망고 <ensia96@gmail.com>
2023-02-09 22:48:31 +0900

    [update] SMS from MCHS(21638) - 런타임 에러 (NameError) 해결

--- baekjoon/bronze/IV/SMS from MCHS(21638).py
+++ baekjoon/bronze/IV/SMS from MCHS(21638).py
@@ -1,4 +1,6 @@
-x, y, z = c < 0 and d > 9, c < a, b < d
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+x, y, z = c < -1 and d > 9, c < a, b < d
 x+y + \
     z and print(
         'A storm warning for tomorrow! Be careful and stay home if possible!'if x else f'MCHS warns! {"Low temperature"if y else"Strong wind"} is expected tomorrow.')
