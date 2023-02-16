857f840b166b09a4c557eaa4f1a6eca8e3e9222a
춤추는망고 <ensia96@gmail.com>
2023-02-16 22:05:45 +0900

    [update] Basketball One-on-One(18198) - 코드 길이 줄이기

--- baekjoon/bronze/IV/Basketball One-on-One(18198).py
+++ baekjoon/bronze/IV/Basketball One-on-One(18198).py
@@ -1,9 +1,1 @@
-D = [0]*2
-x, y = 'A', 'AB'
-for i in input():
-    if i in y:
-        x = ord(i)-65
-    else:
-        D[x] += int(i)
-    if (max(D) > 10)*(max(D)-min(D) > 1):
-        exit(print(y[D[0] < D[1]]))
+print(input()[-2])
