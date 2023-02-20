210831c5a8dbcc9a5586f6db1c7b98f9f2b99dae
춤추는망고 <ensia96@gmail.com>
2023-02-20 22:26:06 +0900

    [update] Majestic 10(25893) - 런타임 에러 (TypeError) 해결

--- baekjoon/bronze/IV/Majestic 10(25893).py
+++ baekjoon/bronze/IV/Majestic 10(25893).py
@@ -3,4 +3,4 @@ for _ in ' '*int(input()):
     a, b, c = map(int, input().split())
     print(a, b, c)
     y = (a > 9)+(b > 9)+(c > 9)
-    print(['zlich', y, y+'-'+y, 'triple-'+y][y])
+    print(['zlich', x, x+'-'+x, 'triple-'+x][y])
