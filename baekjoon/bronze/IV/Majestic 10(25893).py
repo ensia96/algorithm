2bc02188610b29923004d91a916d9cf8100d7ac6
춤추는망고 <ensia96@gmail.com>
2023-02-20 22:24:56 +0900

    Majestic 10(25893)

---
+++ baekjoon/bronze/IV/Majestic 10(25893).py
@@ -0,0 +1,6 @@
+x = 'double'
+for _ in ' '*int(input()):
+    a, b, c = map(int, input().split())
+    print(a, b, c)
+    y = (a > 9)+(b > 9)+(c > 9)
+    print(['zlich', y, y+'-'+y, 'triple-'+y][y])
