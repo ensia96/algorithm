94ef3e3ff2963d9190bcbd6dba00d276bc86a0a1
춤추는망고 <ensia96@gmail.com>
2023-02-20 22:31:43 +0900

    [update] Majestic 10(25893) - 틀렸습니다 해결

--- baekjoon/bronze/IV/Majestic 10(25893).py
+++ baekjoon/bronze/IV/Majestic 10(25893).py
@@ -1,7 +1,7 @@
 x, y = 'double', int(input())
 for i in range(y):
     a, b, c = map(int, input().split())
+    i and print()
     print(a, b, c)
     z = (a > 9)+(b > 9)+(c > 9)
-    print(['zlich', x, x+'-'+x, 'triple-'+x][z])
-    (i < y-1) and print()
+    print(['zilch', x, x+'-'+x, 'triple-'+x][z])
