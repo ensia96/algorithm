e5d28e308d2d54069c1eb34d7b1937ccfeced2b9
춤추는망고 <ensia96@gmail.com>
2023-02-21 21:26:13 +0900

    [update] Football Team(5358) - 출력 형식이 잘못되었습니다 해결

--- baekjoon/bronze/IV/Football Team(5358).py
+++ baekjoon/bronze/IV/Football Team(5358).py
@@ -1,3 +1,6 @@
-for l in open(0):
-    print(l.replace('i', '1').replace('e', '2').replace('I', '3').replace(
-        'E', '4').replace('1', 'e').replace('2', 'i').replace('3', 'E').replace('4', 'I'))
+while 1:
+    try:
+        print(input().replace('i', '1').replace('e', '2').replace('I', '3').replace(
+            'E', '4').replace('1', 'e').replace('2', 'i').replace('3', 'E').replace('4', 'I'))
+    except:
+        break
