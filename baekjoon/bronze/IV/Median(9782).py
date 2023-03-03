941a5ca1a08b22e38bad79fba035c238404e4c8e
춤추는망고 <ensia96@gmail.com>
2023-03-03 22:49:49 +0900

    [update] Median(9782) - 틀렸습니다 해결

--- baekjoon/bronze/IV/Median(9782).py
+++ baekjoon/bronze/IV/Median(9782).py
@@ -1,4 +1,4 @@
 for i, l in enumerate(open(0)):
     a, *A = map(int, l.split())
     x = not a % 2
-    a and print('Case', i, (A[a//2]+x*A[a//2-1])/(1+x))
+    a and print('Case', i+1, (A[a//2]+x*A[a//2-1])/(1+x))
