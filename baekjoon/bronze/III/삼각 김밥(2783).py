483ec372f6a6680d73cb1d6a6d18631c36a421ed
춤추는망고 <ensia96@gmail.com>
2023-03-28 21:00:31 +0900

    [update] 삼각 김밥(2783) - 컴파일 에러 해결

--- baekjoon/bronze/III/삼각 김밥(2783).py
+++ baekjoon/bronze/III/삼각 김밥(2783).py
@@ -1,1 +1,2 @@
-print(min([eval(input().replace(*' /'))*1000]+[1000*eval(input().replace(*' /'))for _ in ' '*int(input()))])
+print(min([eval(input().replace(*' /'))*1000] +
+      [1000*eval(input().replace(*' /'))for _ in ' '*int(input())]))
