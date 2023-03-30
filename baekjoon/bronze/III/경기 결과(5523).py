057abe0a97a8378253847f75833c7453325dcb2a
춤추는망고 <ensia96@gmail.com>
2023-03-30 21:58:39 +0900

    [update] 경기 결과(5523) - 런타임 에러 (NameError) 해결

--- baekjoon/bronze/III/경기 결과(5523).py
+++ baekjoon/bronze/III/경기 결과(5523).py
@@ -1,7 +1,7 @@
 import sys
 a = b = 0
 for _ in ' '*int(input()):
-    x = map(int, sys.stdin.readline().split())
+    x, y = map(int, sys.stdin.readline().split())
     a += x > y
     b += x < y
 print(a, b)
