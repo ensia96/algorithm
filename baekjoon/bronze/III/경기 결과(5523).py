16d440213aa1faeba8be37fb259a3eecc6698233
춤추는망고 <ensia96@gmail.com>
2023-03-30 21:57:13 +0900

    [update] 경기 결과(5523) - 60점(시간 초과) 해결

--- baekjoon/bronze/III/경기 결과(5523).py
+++ baekjoon/bronze/III/경기 결과(5523).py
@@ -1,7 +1,7 @@
 import sys
 a = b = 0
 for _ in ' '*int(input()):
-    x = sys.stdin.readline().replace
-    a += eval(x(*' >'))
-    b += eval(x(*' <'))
+    x = map(int, sys.stdin.readline().split())
+    a += x > y
+    b += x < y
 print(a, b)
