7e0db02234e2a5b4960e75c657d30d24308883e6
춤추는망고 <ensia96@gmail.com>
2023-03-31 22:55:35 +0900

    암호제작(1837)

---
+++ baekjoon/bronze/III/암호제작(1837).py
@@ -0,0 +1,10 @@
+p, k = map(int, input().split())
+k += 1
+A = [1]*k
+for i in range(2, int(k**.5)+1):
+    if A[i]:
+        for j in range(2*i, k, i):
+            A[j] = 0
+for i in range(2, k):
+    p % i or exit(print('BAD', i))
+print('GOOD')
