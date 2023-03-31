f7c3c8884adb4d0f2eea31f18d368760017756ec
춤추는망고 <ensia96@gmail.com>
2023-03-31 23:14:41 +0900

    [update] 암호제작(1837) - 틀렸습니다 해결

--- baekjoon/bronze/III/암호제작(1837).py
+++ baekjoon/bronze/III/암호제작(1837).py
@@ -1,10 +1,10 @@
 p, k = map(int, input().split())
-k += 1
-A = [1]*k
-for i in range(2, int(k**.5)+1):
+M = 1000001
+A = [1]*M
+for i in range(2, 1001):
     if A[i]:
-        for j in range(2*i, k, i):
+        for j in range(2*i, M, i):
             A[j] = 0
 for i in range(2, k):
     A[i] and (p % i or exit(print('BAD', i)))
-print('GOOD')
+print("GOOD")
