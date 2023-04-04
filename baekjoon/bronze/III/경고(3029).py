7ccdb91ce4c0609f575fe43e8ef3dfae0f07287c
춤추는망고 <ensia96@gmail.com>
2023-04-04 21:07:22 +0900

    [update] 경고(3029) - 틀렸습니다 해결

--- baekjoon/bronze/III/경고(3029).py
+++ baekjoon/bronze/III/경고(3029).py
@@ -1,4 +1,4 @@
 a, b, c = map(int, input().split(':'))
 x, y, z = map(int, input().split(':'))
 A = (60*(x-a)+y-b)*60+z-c
-print(f'{A//3600+24*(A<=3600):02d}:{A//60%60:02d}:{A%60:02d}')
+print(f'{A//3600+24*(A<=0):02d}:{A//60%60:02d}:{A%60:02d}')
