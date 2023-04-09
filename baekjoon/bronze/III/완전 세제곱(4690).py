5b695f426fa07be33f023ce43f78df6622ee1ec7
춤추는망고 <ensia96@gmail.com>
2023-04-09 12:12:31 +0900

    [update] 완전 세제곱(4690) - 틀렸습니다 해결

--- baekjoon/bronze/III/완전 세제곱(4690).py
+++ baekjoon/bronze/III/완전 세제곱(4690).py
@@ -4,4 +4,4 @@ for i in R(101):
         for k in R(j, i):
             for l in R(k, i):
                 i**3 == j**3+k**3 + \
-                    l**3 and print(f'Cube = {i}, Tripple = ({j},{k},{l})')
+                    l**3 and print(f'Cube = {i}, Triple = ({j},{k},{l})')
