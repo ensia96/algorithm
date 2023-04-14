d16e0fbbcc9f2ddd35c96de2c84b8b0fe5ef53ab
춤추는망고 <ensia96@gmail.com>
2023-04-14 20:45:16 +0900

    [update] 중간계 전쟁(4435) - 런타임 에러 (IndexError) 해결

--- baekjoon/bronze/III/중간계 전쟁(4435).py
+++ baekjoon/bronze/III/중간계 전쟁(4435).py
@@ -1,8 +1,8 @@
 A = [1, 2, 3, 3, 4, 10]
 B = [1, 2, 2, 2, 3, 5, 10]
-C = ['No victor on this battle field',
-     'Evil eradicates all trace of Good', 'Good triumphs over Evil']
+C = ['No victor on this battle field', 'Good triumphs over Evil',
+     'Evil eradicates all trace of Good']
 for i in range(int(input())):
-    x, y = sum(A[i]for i in map(int, input().split())), sum(B[i]
-                                                            for i in map(int, input().split()))
-    print(f'Battle {i}?:', C[(x > y)+2*(x < y)])
+    x, y = [*map(int, input().split())], [*map(int, input().split())]
+    X, Y = sum(A[i]*x[i]for i in range(6)), sum(B[i]*y[i]for i in range(7))
+    print(f'Battle {i+1}?:', C[(X > Y)+2*(X < Y)])
