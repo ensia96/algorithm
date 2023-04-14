af4f5349f25b3a2b1fd7e1a5d9c48f939ea88dbc
춤추는망고 <ensia96@gmail.com>
2023-04-14 20:45:50 +0900

    [update] 중간계 전쟁(4435) - 틀렸습니다 해결

--- baekjoon/bronze/III/중간계 전쟁(4435).py
+++ baekjoon/bronze/III/중간계 전쟁(4435).py
@@ -5,4 +5,4 @@ C = ['No victor on this battle field', 'Good triumphs over Evil',
 for i in range(int(input())):
     x, y = [*map(int, input().split())], [*map(int, input().split())]
     X, Y = sum(A[i]*x[i]for i in range(6)), sum(B[i]*y[i]for i in range(7))
-    print(f'Battle {i+1}?:', C[(X > Y)+2*(X < Y)])
+    print(f'Battle {i+1}:', C[(X > Y)+2*(X < Y)])
