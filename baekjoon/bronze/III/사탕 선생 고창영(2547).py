600bf3eb463b3b8aece3dc190799e29d44d85413
춤추는망고 <ensia96@gmail.com>
2023-04-15 19:21:25 +0900

    [update] 사탕 선생 고창영(2547) - 틀렸습니다 해결

--- baekjoon/bronze/III/사탕 선생 고창영(2547).py
+++ baekjoon/bronze/III/사탕 선생 고창영(2547).py
@@ -1,12 +1,5 @@
-n, *A = open(0)
-x = y = 0
-B = []
-while A:
-    try:
-        x += int(A.pop())
-        y += 1
-    except:
-        B += bool(x % y),
-        x = y = 0
-for b in B[::-1]:
-    print('YNEOS'[b::2])
+i, I = int, input
+for _ in ' '*i(I()):
+    I()
+    x = i(I())
+    print('YNEOS'[bool(sum(i(I())for _ in ' '*x) % x)::2])
