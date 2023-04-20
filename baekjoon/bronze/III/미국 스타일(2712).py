1d2aafa8430afb43306147a38fef8921aa5ef05c
춤추는망고 <ensia96@gmail.com>
2023-04-20 21:30:45 +0900

    미국 스타일(2712)

--- baekjoon/bronze/III/미국 스타일(2712).py
+++ baekjoon/bronze/III/미국 스타일(2712).py
@@ -1,5 +1,10 @@
-for l in [*open(0)][1:]:
-    a, b = l.split()
-    x, y = {'kg': (2.2046, 'lb'), 'lb': (0.4536, 'kg'),
-            'l': (0.2642, 'g'), 'g': (3.7854, 'l')}[b]
-    print(round(float(a)*x, 4), b)
+for _ in range(int(input())):
+    n, s = input().split()
+    if s == "kg":
+        print("%.4f %s" % (float(n)*2.2046, "lb"))
+    elif s == "lb":
+        print("%.4f %s" % (float(n)*0.4536, "kg"))
+    elif s == "l":
+        print("%.4f %s" % (float(n)*0.2642, "g"))
+    elif s == "g":
+        print("%.4f %s" % (float(n)*3.7854, "l"))
