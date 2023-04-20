de0e654e0e24993a9366f08e06fa63b090a13364
춤추는망고 <ensia96@gmail.com>
2023-04-20 21:29:58 +0900

    [update] 미국 스타일(2712) - 틀렸습니다 해결

--- baekjoon/bronze/III/미국 스타일(2712).py
+++ baekjoon/bronze/III/미국 스타일(2712).py
@@ -2,4 +2,4 @@ for l in [*open(0)][1:]:
     a, b = l.split()
     x, y = {'kg': (2.2046, 'lb'), 'lb': (0.4536, 'kg'),
             'l': (0.2642, 'g'), 'g': (3.7854, 'l')}[b]
-    print(f'{float(a)*x:.4f}', b)
+    print(round(float(a)*x, 4), b)
