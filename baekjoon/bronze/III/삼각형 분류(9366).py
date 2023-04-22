327c8dde60591a001b4d3f88576e30e55a02ac19
춤추는망고 <ensia96@gmail.com>
2023-04-22 19:10:41 +0900

    [update] 삼각형 분류(9366) - 런타임 에러 (IndexError) 해결

--- baekjoon/bronze/III/삼각형 분류(9366).py
+++ baekjoon/bronze/III/삼각형 분류(9366).py
@@ -3,4 +3,4 @@ for i, l in enumerate(open(0)):
         a, b, c = sorted(map(int, l.split()))
         x, y, z = a == b, b == c, c == a
         print(f'Case #{i}:', ('invalid!', (('scalene', 'isosceles')[
-              x+y+z], 'equilateral')[x*y*z])[a+b > c])
+              bool(x+y+z)], 'equilateral')[x*y*z])[a+b > c])
