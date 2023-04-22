37bab69a81097bd01e04528119fce84862b40c19
춤추는망고 <ensia96@gmail.com>
2023-04-22 19:13:36 +0900

    [update] 삼각형 분류(9366) - 컴파일 에러 해결

--- baekjoon/bronze/III/삼각형 분류(9366).py
+++ baekjoon/bronze/III/삼각형 분류(9366).py
@@ -1,6 +1,5 @@
-for i, l in enumerate(open(0)):
-    if i:
-        a, b, c = sorted(map(int, l.split()))
-        x, y, z = a == b, b == c, c == a
-        print(f'Case #{i}:', ('invalid!', (('scalene', 'isosceles')[
-              bool(x+y+z)], 'equilateral')[x*y*z])[a+b > c])
+for i, l in enumerate([*open(0)][1:]):
+    a, b, c = sorted(map(int, l.split()))
+    x, y, z = a == b, b == c, c == a
+    print(f'Case #{i+1}:', ('invalid!', (('scalene', 'isosceles')
+          [bool(x+y+z)], 'equilateral')[x*y*z])[a+b > c])
