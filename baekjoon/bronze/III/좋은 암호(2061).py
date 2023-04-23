a587365b9aa085e95a02544c927a81d658a1c53b
춤추는망고 <ensia96@gmail.com>
2023-04-23 18:19:20 +0900

    [update] 좋은 암호(2061) - 시간 초과 해결

--- baekjoon/bronze/III/좋은 암호(2061).py
+++ baekjoon/bronze/III/좋은 암호(2061).py
@@ -1,4 +1,4 @@
 k, l = map(int, input().split())
-for i in range(int(k**.5)+1, 1, -1):
-    not k % i and i < l and exit(print('BAD', i))
+for i in range(2, l):
+    k % i or exit(print('BAD', i))
 print('GOOD')
