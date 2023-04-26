ac219572b374ebed437a839615ce81232154381e
춤추는망고 <ensia96@gmail.com>
2023-04-26 20:34:35 +0900

    [update] Report Card Time(11367) - 틀렸습니다 해결

--- baekjoon/bronze/III/Report Card Time(11367).py
+++ baekjoon/bronze/III/Report Card Time(11367).py
@@ -1,4 +1,4 @@
 for l in [*open(0)][1:]:
     x, y = l.split()
     a, b = divmod(int(y), 10)
-    print('FFFFFFDCBAA'[a]+'+'*(a > 5)*(b > 6 or a > 9))
+    print(x, 'FFFFFFDCBAA'[a]+'+'*(a > 5)*(b > 6 or a > 9))
