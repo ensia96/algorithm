6edb337a28d5be37b0878ea2c3be195e64d64f13
춤추는망고 <ensia96@gmail.com>
2023-04-26 20:22:02 +0900

    [update] 받아올림(4388) - 틀렸습니다 해결

--- baekjoon/bronze/III/받아올림(4388).py
+++ baekjoon/bronze/III/받아올림(4388).py
@@ -1,9 +1,9 @@
 for l in [*open(0)][:-1]:
     x, y = map(int, l.split())
     i = j = 0
-    while x*y:
+    while (x > 0)+(y > 0):
         x, a = divmod(x, 10)
         y, b = divmod(y, 10)
         j = a+b+j > 9
         i += j
-    print(i+(max(x, y) % 10+j > 9))
+    print(i)
