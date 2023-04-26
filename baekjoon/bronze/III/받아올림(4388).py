64193adcf40cadbfdc0467c202d883f44b4a6b29
춤추는망고 <ensia96@gmail.com>
2023-04-26 20:17:51 +0900

    [update] 받아올림(4388) - 틀렸습니다 해결

--- baekjoon/bronze/III/받아올림(4388).py
+++ baekjoon/bronze/III/받아올림(4388).py
@@ -1,8 +1,9 @@
 for l in [*open(0)][:-1]:
     x, y = map(int, l.split())
-    z = 0
+    i = j = 0
     while x*y:
         x, a = divmod(x, 10)
         y, b = divmod(y, 10)
-        z += a+b > 9
-    print(z)
+        j = a+b+j > 9
+        i += j
+    print(i)
