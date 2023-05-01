eb8a59280e41c90a53f0cb74c08f155226007455
춤추는망고 <ensia96@gmail.com>
2023-05-01 22:48:40 +0900

    [update] 질투진서(15784) - 틀렸습니다 해결

--- baekjoon/bronze/III/질투진서(15784).py
+++ baekjoon/bronze/III/질투진서(15784).py
@@ -1,4 +1,4 @@
 a, *A = open(0)
 _, a, b = map(int, a.split())
 B = [l.split()[b-1]for l in A]
-print('HAANPGPRY'[int(B[a-1]) < max(map(int, A[a-1].split()+B))::2])
+print('HAANPGPRYY'[int(B[a-1]) < max(map(int, A[a-1].split()+B))::2])
