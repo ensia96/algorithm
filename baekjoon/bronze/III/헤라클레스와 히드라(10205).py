6bd63c0743366dc5053b608d57ea7ff24c7e2ea3
춤추는망고 <ensia96@gmail.com>
2023-05-05 17:03:10 +0900

    [update] 헤라클레스와 히드라(10205) - 틀렸습니다 해결

--- baekjoon/bronze/III/헤라클레스와 히드라(10205).py
+++ baekjoon/bronze/III/헤라클레스와 히드라(10205).py
@@ -1,5 +1,5 @@
 A = []
 for i in range(int(input())):
     n = int(input())
-    A += f'Data Set {i}:\n{n+sum(1+2*(ord(c)-99)for c in input())}',
+    A += f'Data Set {i+1}:\n{n+sum(1+2*(ord(c)-99)for c in input())}',
 print('\n\n'.join(A))
