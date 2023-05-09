64f29541c6310cbd553fa53b2ff36862ceeea171
춤추는망고 <ensia96@gmail.com>
2023-05-09 22:41:53 +0900

    [update] 괄호 없는 사칙연산(16503) - 런타임 에러 (ZeroDivisionError) 해결

--- baekjoon/bronze/III/괄호 없는 사칙연산(16503).py
+++ baekjoon/bronze/III/괄호 없는 사칙연산(16503).py
@@ -1,3 +1,8 @@
-a, x, b, y, c, f = *input().split(), lambda x, y, z: [eval(f"abs({x})//abs({z})")*(
-    1-2*(eval(f"{x}*{z}") < 0)), eval(f"{x}{y}{z}")][y in '+-*']
+a, x, b, y, c = input().split()
+
+
+def f(x, y, z): str(
+    eval([f"abs({x}){y}abs({z})*(1-2*({x}*{z}<0))", x+y+z][y in '+-*']))
+
+
 print(*sorted([f(f(a, x, b), y, c), f(a, x, f(b, y, c))]))
