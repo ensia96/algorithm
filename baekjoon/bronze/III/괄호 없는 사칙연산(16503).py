d7c67dc0ad5079f16d2627a291afaa7ad4e960ec
춤추는망고 <ensia96@gmail.com>
2023-05-09 22:51:04 +0900

    [update] 괄호 없는 사칙연산(16503) - 틀렸습니다 해결

--- baekjoon/bronze/III/괄호 없는 사칙연산(16503).py
+++ baekjoon/bronze/III/괄호 없는 사칙연산(16503).py
@@ -1,8 +1,9 @@
 a, x, b, y, c = input().split()
+a, b, c = map(int, [a, b, c])
 
 
-def f(x, y, z): return str(
-    eval([f"abs({x}){y}abs({z})*(1-2*({x}*{z}<0))", x+y+z][y in '+-*']))
+def f(a, b, op): return a+b if op == '+'else a-b if op == '-'else a * \
+    b if op == '*'else -1*(abs(a)//abs(b))if a*b < 0 else a//b
 
 
-print(*sorted([f(f(a, x, b), y, c), f(a, x, f(b, y, c))]))
+print(*sorted([f(f(a, b, x), c, y), f(a, f(b, c, y), x)]))
