f9bbe674477518f2aadf42057464d2fc2405c1f3
춤추는망고 <ensia96@gmail.com>
2023-05-10 21:17:14 +0900

    [update] 사격 내기(27960) - 틀렸습니다 해결

--- baekjoon/bronze/III/사격 내기(27960).py
+++ baekjoon/bronze/III/사격 내기(27960).py
@@ -1,4 +1,2 @@
 a, b = map(int, input().split())
-A = [*zip(map(int, bin(a)[2:].zfill(9)[::-1]),
-          map(int, bin(b)[2:].zfill(9)[::-1]))]
-print(sum(2**i*(sum(A[i]) == 1)for i in range(9)))
+print(int(bin(a ^ b)[2:], 2))
