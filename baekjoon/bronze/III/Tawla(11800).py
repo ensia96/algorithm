d11e25b35b7b82c77a1891550d2211640d9a5664
춤추는망고 <ensia96@gmail.com>
2023-05-13 21:11:27 +0900

    [update] Tawla(11800) - 틀렸습니다 해결

--- baekjoon/bronze/III/Tawla(11800).py
+++ baekjoon/bronze/III/Tawla(11800).py
@@ -1,5 +1,5 @@
 A = ["Yakk", "Doh", "Seh", "Ghar", "Bang", "Sheesh"]
-B = ["HabbYakk", "Dobara", "Dousa", "Dorgy", "Dabash", "Dosh"]
+B = ["Habb Yakk", "Dobara", "Dousa", "Dorgy", "Dabash", "Dosh"]
 for i in range(int(input())):
     a, b = sorted(map(int, input().split()))
     print(f'Case {i+1}:', [[f"{A[b-1]} {A[a-1]}", B[a-1]]
