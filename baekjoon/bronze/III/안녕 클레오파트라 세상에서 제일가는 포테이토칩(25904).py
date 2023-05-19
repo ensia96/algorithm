27d1e527913c6874d9efb07ea9cf6e583c92770e
춤추는망고 <ensia96@gmail.com>
2023-05-19 21:25:05 +0900

    [update] 안녕 클레오파트라 세상에서 제일가는 포테이토칩(25904) - 코드 길이 줄이기

--- baekjoon/bronze/III/안녕 클레오파트라 세상에서 제일가는 포테이토칩(25904).py
+++ baekjoon/bronze/III/안녕 클레오파트라 세상에서 제일가는 포테이토칩(25904).py
@@ -1,12 +1,6 @@
-N, X = map(int, input().split())
-T = list(map(int, input().split()))
-check = True
-while check:
-    for i in range(N):
-        if X <= T[i]:
-            X += 1
-        else:
-            result = i
-            check = False
-            break
-print(result+1)
+n, x = map(int, input().split())
+*A, = map(int, input().split())
+i = 0
+while A[i % n] >= x+i:
+    i += 1
+print(i % n+1)
