a1793081211fe33c391a726d6a13b57a82bdfb3f
춤추는망고 <ensia96@gmail.com>
2023-05-30 22:17:45 +0900

    [update] 길면 기차, 기차는 빨라, 빠른 것은 비행기(9493) - 틀렸습니다 해결

--- baekjoon/bronze/III/길면 기차, 기차는 빨라, 빠른 것은 비행기(9493).py
+++ baekjoon/bronze/III/길면 기차, 기차는 빨라, 빠른 것은 비행기(9493).py
@@ -1,5 +1,5 @@
 i, I = 60, 3600
 for l in [*open(0)][:-1]:
     m, a, b = map(int, l.split())
-    x = int(m*I/a-m*I/b)
-    print(f"{x//3600}:{x//i%i:02d}:{x%i:02d}")
+    x = m*I/a-m*I/b
+    print(f"{x//I}:{x%I//i:02d}:{x%i:02d}")
