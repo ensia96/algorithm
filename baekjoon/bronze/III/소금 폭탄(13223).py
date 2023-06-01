20da9e909e2bf5fb27f5bb57ca746df9d84165cc
춤추는망고 <ensia96@gmail.com>
2023-06-01 19:59:13 +0900

    [update] 소금 폭탄(13223) - 틀렸습니다 해결

--- baekjoon/bronze/III/소금 폭탄(13223).py
+++ baekjoon/bronze/III/소금 폭탄(13223).py
@@ -1,4 +1,4 @@
 a, b, c = map(int, input().split(':'))
 x, y, z = map(int, input().split(':'))
 t = 60*(60*(x-a)+y-b)+z-c
-print(f"{t//3600+24*(t<0):02d}:{t//60%60:02d}:{t%60:02d}")
+print(f"{t//3600+24*(t<=0):02d}:{t//60%60:02d}:{t%60:02d}")
