7a22ac25c93f8d9902a0bc4a8a42dc697b67146c
춤추는망고 <ensia96@gmail.com>
2023-06-06 13:57:42 +0900

    [update] 시간과 날짜(25024) - 틀렸습니다 해결

--- baekjoon/bronze/III/시간과 날짜(25024).py
+++ baekjoon/bronze/III/시간과 날짜(25024).py
@@ -1,5 +1,5 @@
 A = ['No', 'Yes']
 for l in [*open(0)][1:]:
     x, y = map(int, l.split())
-    print(A[x < 24 and y < 61], A[0 < x < 13 and 0 <
-          y < 32-(x in [2, 4, 6, 9, 11])-(x == 2)])
+    print(A[x < 24 and y < 60], A[0 < x < 13 and 0 <
+          y < 31-(x in [2, 4, 6, 9, 11])-(x == 2)])
