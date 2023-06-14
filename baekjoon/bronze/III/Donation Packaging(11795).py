c6eb8df28dbf053f07046040425c0c12f8500547
춤추는망고 <ensia96@gmail.com>
2023-06-14 20:42:09 +0900

    [update] Donation Packaging(11795) - 틀렸습니다 해결

--- baekjoon/bronze/III/Donation Packaging(11795).py
+++ baekjoon/bronze/III/Donation Packaging(11795).py
@@ -4,7 +4,7 @@ for _ in ' '*int(input()):
     A += a
     B += b
     C += c
-    if A > 30 and B > 30 and C > 30:
+    if A >= 30 and B >= 30 and C >= 30:
         x = min(A, B, C)
         A -= x
         B -= x
