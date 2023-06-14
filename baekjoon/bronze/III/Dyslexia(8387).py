229ebe56093a4ce806bf19c0370e9fcfc5dccd98
춤추는망고 <ensia96@gmail.com>
2023-06-14 20:54:49 +0900

    [update] Dyslexia(8387) - 틀렸습니다 해결

--- baekjoon/bronze/III/Dyslexia(8387).py
+++ baekjoon/bronze/III/Dyslexia(8387).py
@@ -1,7 +1,2 @@
-n = int(input())
-s1, s2 = input(), input()
-cnt = 0
-for i in range(n):
-    if s1[i] != s2[i]:
-        cnt += 1
-print(cnt)
+a, b, c = open(0).read().split()
+print(sum(x != y for x, y in zip(b.lower(), c.lower())))
