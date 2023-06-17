b3ef3e9da0e182c53eaaae8d1b5ba4a12a19c903
춤추는망고 <ensia96@gmail.com>
2023-06-17 20:51:24 +0900

    [update] Judging Olympia(4909) - 틀렸습니다 해결

--- baekjoon/bronze/III/Judging Olympia(4909).py
+++ baekjoon/bronze/III/Judging Olympia(4909).py
@@ -1,4 +1,3 @@
 for l in [*open(0)][:-1]:
-    *A, = map(int, l.split())
-    x = sum(A)-max(A)-min(A)
-    print(x/4 if x % 4 else int(x))
+    x = sum(sorted(map(int, l.split()))[1:-1])
+    print(x/4 if x % 4 else int(x/4))
