6caaac0120188f4d4043b25b41662df58b0f73c1
춤추는망고 <ensia96@gmail.com>
2023-06-18 19:03:25 +0900

    [update] Deficient, Perfect, and Abundant(6975) - 틀렸습니다 해결

--- baekjoon/bronze/III/Deficient, Perfect, and Abundant(6975).py
+++ baekjoon/bronze/III/Deficient, Perfect, and Abundant(6975).py
@@ -1,5 +1,5 @@
 for l in [*open(0)][1:]:
     n = int(l)
-    x = sum(i+n//i for i in range(1, n//2)if not n % i)-n
+    x = sum(i+n//i for i in range(1, n)if not n % i)-n
     print(n, 'is', ['a perfect', 'a deficient', 'an abundant']
           [(x < n)+2*(n < x)], 'number.')
