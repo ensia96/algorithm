e23b58eb07bb477a9c2c6dc5e1e974a932350eed
춤추는망고 <ensia96@gmail.com>
2023-06-21 22:09:34 +0900

    [update] Reducing Improper Fractions(9297) - 틀렸습니다 해결

--- baekjoon/bronze/III/Reducing Improper Fractions(9297).py
+++ baekjoon/bronze/III/Reducing Improper Fractions(9297).py
@@ -1,4 +1,5 @@
 for i in range(int(input())):
     a, b = map(int, input().split())
     x, y = divmod(a, b)
-    print(f"Case {i+1}:{' '+str(x)if x else''}{f' {y}/{b}'if y else''}")
+    print(
+        f"Case {i+1}:{' '+str(x)if x else''}{f' {y}/{b}'if y else''}{' 0' if a==0 else''}")
