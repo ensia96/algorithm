7247538df61640aac5d18f301140ae5536f53c3f
춤추는망고 <ensia96@gmail.com>
2023-06-26 21:24:32 +0900

    [update] Sumac Sequences(6780) - 틀렸습니다 해결

--- baekjoon/bronze/III/Sumac Sequences(6780).py
+++ baekjoon/bronze/III/Sumac Sequences(6780).py
@@ -1,4 +1,6 @@
-*A, = map(int, open(0).read().split())
-while A[-2] >= A[-1] > 0:
-    A += A[-2]-A[-1],
-print(len(A))
+a, b = map(int, open(0).read().split())
+c = 0
+while a >= b:
+    a, b = b, a-b
+    c += 1
+print(c)
