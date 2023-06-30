f30248ee8c3768034296fc356f1269f84a554d8e
춤추는망고 <ensia96@gmail.com>
2023-06-30 21:23:16 +0900

    [update] Arithmetic and Geometric Sums(9310) - 런타임 에러 (ZeroDivisionError) 해결

--- baekjoon/bronze/III/Arithmetic and Geometric Sums(9310).py
+++ baekjoon/bronze/III/Arithmetic and Geometric Sums(9310).py
@@ -1,5 +1,5 @@
 n = int(input())
 while n:
     a, b, c = map(int, input().split())
-    print([a*((b//a)**n-1)//(b//a-1), n*(2*a+(n-1)*(b-a))//2][c-b == b-a])
+    print(n*(2*a+(n-1)*(b-a))//2 if c-b == b-a else a*((b//a)**n-1)//(b//a-1))
     n = int(input())
