cea4561855b7405205be155defd9317511807b65
춤추는망고 <ensia96@gmail.com>
2023-06-30 21:55:52 +0900

    [update] Olympic Games(13773) - 틀렸습니다 해결

--- baekjoon/bronze/III/Olympic Games(13773).py
+++ baekjoon/bronze/III/Olympic Games(13773).py
@@ -2,7 +2,7 @@ for l in [*open(0)][:-1]:
     n = int(l)
     if n in [1916, 1940, 1944]:
         print(n, 'Games cancelled')
-    elif n % 4 < 1:
+    elif n % 4 < 1 and n > 1895:
         print(n, ['Summer Olympics', 'No city yet chosen'][n > 2020])
     else:
         print(n, 'No summer games')
