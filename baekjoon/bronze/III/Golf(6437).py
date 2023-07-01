4d55a97aa545fd4122b8dc34a46042e14c0f762e
춤추는망고 <ensia96@gmail.com>
2023-07-01 16:56:58 +0900

    [update] Golf(6437) - 틀렸습니다 해결

--- baekjoon/bronze/III/Golf(6437).py
+++ baekjoon/bronze/III/Golf(6437).py
@@ -2,6 +2,6 @@ x = 1
 T = []
 for l in [*open(0)][:-1]:
     p, s = map(int, l.split())
-    T += f"Hole #{x}\n{[['Par','Bordie',['Eagle','Hole-in-one'][s<2],'Double Eagle','Bogey'][p-s],'Double Bogey'][p-s<-1]}.",
+    T += f"Hole #{x}\n{[[['Par','Birdie','Eagle','Double eagle','Bogey'][p-s],'Hole-in-one'][s<2],'Double Bogey'][-2<p-s<4]}.",
     x += 1
 print('\n\n'.join(T))
