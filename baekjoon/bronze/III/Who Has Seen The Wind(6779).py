3d49201f0d67c6d44558ab06479db92f9cf2c714
춤추는망고 <ensia96@gmail.com>
2023-07-03 21:53:06 +0900

    Who Has Seen The Wind(6779)

---
+++ baekjoon/bronze/III/Who Has Seen The Wind(6779).py
@@ -0,0 +1,7 @@
+h, m = map(int, open(0))
+for t in range(1, m+1):
+    x = -6*t**4+h*t**3+2*t*t+t
+    if x <= 0:
+        break
+print('The balloon ', *[['first touches ground at hour:', t],
+      ['does not touch ground in the given time.']][t == m])
