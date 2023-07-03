69a6960b4e5caf73e60a2b99edade229fc48068e
춤추는망고 <ensia96@gmail.com>
2023-07-03 22:56:12 +0900

    [update] Das Blinkenlights(16625) - 런타임 에러 (ZeroDivisionError) 해결

--- baekjoon/bronze/III/Das Blinkenlights(16625).py
+++ baekjoon/bronze/III/Das Blinkenlights(16625).py
@@ -1,2 +1,2 @@
-x, y, z, f = *map(int, input().split()), lambda x, y: y == 1 or f(y, x % y)
+x, y, z, f = *map(int, input().split()), lambda a, b: b*(a == 0) or f(b % a, a)
 print('yneos'[x*y//f(x, y) >= z::2])
