41a7c2b2cbf9555493440f24c28e20462e092874
춤추는망고 <ensia96@gmail.com>
2023-07-03 21:53:59 +0900

    [update] Who Has Seen The Wind(6779) - 출력 형식이 잘못되었습니다 해결

--- baekjoon/bronze/III/Who Has Seen The Wind(6779).py
+++ baekjoon/bronze/III/Who Has Seen The Wind(6779).py
@@ -3,5 +3,5 @@ for t in range(1, m+1):
     x = -6*t**4+h*t**3+2*t*t+t
     if x <= 0:
         break
-print('The balloon ', *[['first touches ground at hour:', t],
+print('The balloon', *[['first touches ground at hour:', t],
       ['does not touch ground in the given time.']][t == m])
