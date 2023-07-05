40c277a782ab0759ecd06dbf7e7079922e9c0b5d
춤추는망고 <ensia96@gmail.com>
2023-07-05 22:43:29 +0900

    99 Problems(25270)

--- baekjoon/bronze/III/99 Problems(25270).py
+++ baekjoon/bronze/III/99 Problems(25270).py
@@ -1,5 +1,4 @@
-a = input()
-n, l = int(a), len(a)-1
-M = int(a[0]+'9'*l)
-m = M-10**l
-print(max(99, [M, m][abs(n-m) < abs(n-M)]))
+n = int(input())
+for i in range(100):
+    (n+i) % 100 == 99 and exit(print(n+i))
+    (n-i) % 100 == 99 and exit(print(max(n-i, 99)))
