fe139dc20875415a47e76ce4273a3a2b1703c502
춤추는망고 <ensia96@gmail.com>
2023-07-05 23:00:28 +0900

    [update] Liczba(8658) - 코드 길이 줄이기

--- baekjoon/bronze/III/Liczba(8658).py
+++ baekjoon/bronze/III/Liczba(8658).py
@@ -1,10 +1,3 @@
 n = int(input())
 for i in range(2, n):
-    if n % i:
-        m = i
-        break
-for i in range(n, 1, -1):
-    if n % i:
-        M = i
-        break
-print(m, M)
+    n % i and exit(print(i, n-1))
