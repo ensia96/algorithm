86efbe2389ed4914e8a1dbe8fc8ba23f776cc008
춤추는망고 <ensia96@gmail.com>
2023-07-07 21:29:21 +0900

    [update] Unit Conversion(14013) - 시간 초과 해결

--- baekjoon/bronze/III/Unit Conversion(14013).py
+++ baekjoon/bronze/III/Unit Conversion(14013).py
@@ -1,4 +1,4 @@
 x, y = map(float, input().split())
 for _ in ' '*int(input()):
     n, N = input().split()
-    print(eval(f'{n}/'+'y*x'[::1-2*(not ord(N)-65)]))
+    print(eval(f'{n}/'+'y*x'[::1-2*(N == 'A')]))
