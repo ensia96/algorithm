cedcba26eed150f4d0ca943c07bf87f72cd000ee
춤추는망고 <ensia96@gmail.com>
2023-07-08 19:17:19 +0900

    Crowing(2321)

---
+++ baekjoon/bronze/III/Crowing(2321).py
@@ -0,0 +1,15 @@
+# program t
+#  implicit none
+#  character(len=6)::a=",~"
+#  character(len=10)::b="( 9> //)"
+#  character(len=9)::c=" )(_///"
+#  character(len=10)::d="( \_>/"
+#  character(len=7)::e=" \__/"
+#  character(len=5)::f="  ^^"
+#  write(*,'(A)')a
+#  write(*,'(A)')b
+#  write(*,'(A)')c
+#  write(*,'(A)')d
+#  write(*,'(A)')e
+#  write(*,'(A)')f
+# end program t
