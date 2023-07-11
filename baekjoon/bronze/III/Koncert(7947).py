f046ad71ecaadfea6f17d6e5d7a9824c4d3d737b
춤추는망고 <ensia96@gmail.com>
2023-07-11 23:04:30 +0900

    [update] Koncert(7947) - 틀렸습니다 해결

--- baekjoon/bronze/III/Koncert(7947).py
+++ baekjoon/bronze/III/Koncert(7947).py
@@ -1,3 +1,7 @@
-for i in ' '*int(input()):
-    print(*[round(sum(i)/10)
-          for i in zip(*[[*map(int, input().split())]for _ in ' '*10])])
+n, *L = map(int, open(0).read().split())
+for i in range(n):
+    A = []
+    for j in range(3):
+        x, y = divmod(sum(L[30*i+j:30*i+30+j:3]), 10)
+        A += x+(y > 4),
+    print(*A)
