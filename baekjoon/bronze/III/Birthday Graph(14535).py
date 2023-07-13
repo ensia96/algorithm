a115162c777b6d588ac3fedfb47249da8cecdab3
춤추는망고 <ensia96@gmail.com>
2023-07-13 20:40:57 +0900

    Birthday Graph(14535)

--- baekjoon/bronze/III/Birthday Graph(14535).py
+++ baekjoon/bronze/III/Birthday Graph(14535).py
@@ -3,6 +3,7 @@ while x:
     x = int(input()) or exit()
     L = [[*map(int, input().split())][1]for _ in ' '*x]
     print(f'Case #{y}:')
+    y += 1
     for i in range(12):
         print('JanFebMarAprMayJunJulAugSepOctNovDec'[
               i*3:i*3+3]+':'+'*'*L.count(i+1))
