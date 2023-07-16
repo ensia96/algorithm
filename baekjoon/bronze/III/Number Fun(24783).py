cfb6cbad098e79280cbb8f2ee6b8a4b12f4e4072
춤추는망고 <ensia96@gmail.com>
2023-07-16 16:07:45 +0900

    [update] Number Fun(24783) - 틀렸습니다 해결

--- baekjoon/bronze/III/Number Fun(24783).py
+++ baekjoon/bronze/III/Number Fun(24783).py
@@ -1,3 +1,3 @@
 for l in [*open(0)][1:]:
     a, b, c = map(int, l.split())
-    print(['Im', 'P'][c in [a+b, a-b, a*b, a/b, b+a, b-a, b*a, b/a]]+'ossible')
+    print(['Imp', 'P'][c in [a+b, a-b, a*b, a/b, b+a, b-a, b*a, b/a]]+'ossible')
