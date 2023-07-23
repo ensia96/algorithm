0d36cdd56000f4cd81a90b40e814f01556a49ff7
춤추는망고 <ensia96@gmail.com>
2023-07-23 19:37:06 +0900

    [update] Triangles(5356) - 틀렸습니다 해결

--- baekjoon/bronze/III/Triangles(5356).py
+++ baekjoon/bronze/III/Triangles(5356).py
@@ -1,5 +1,5 @@
 t = []
 for l in [*open(0)][1:]:
     x, y = l.split()
-    t += '\n'.join([chr(65+-~i*(ord(y)+i-65) % 26)for i in range(int(x))]),
+    t += '\n'.join([-~i*chr(65+(ord(y)+i-65) % 26)for i in range(int(x))]),
 print('\n\n'.join(t))
