9b34f33556f3ccd2046c55b589e307fd98cb81eb
춤추는망고 <ensia96@gmail.com>
2023-07-24 20:56:00 +0900

    [update] Floppies(6436) - 틀렸습니다 해결

--- baekjoon/bronze/III/Floppies(6436).py
+++ baekjoon/bronze/III/Floppies(6436).py
@@ -2,6 +2,6 @@ x, A = 1860000, []
 for l in [*open(0)][:-1]:
     i = int(l)
     i = i//2+i % 2
-    i += i//2+i % 2
+    i += i//2
     A += f'File #{len(A)+1}\nJohn needs {(i+x-1)//x} floppies.',
 print('\n\n'.join(A))
