e0befe28479632ddea68a8850aafa3446cfb4c22
춤추는망고 <ensia96@gmail.com>
2023-07-24 20:50:21 +0900

    Floppies(6436)

---
+++ baekjoon/bronze/III/Floppies(6436).py
@@ -0,0 +1,7 @@
+x, A = 1860000, []
+for l in [*open(0)][:-1]:
+    i = int(l)
+    i = i//2+i % 2
+    i += i//2+i % 2
+    A += f'File #{len(A)+1}\nJohn needs {(i+x-1)//x} floppies.',
+print('\n\n'.join(A))
