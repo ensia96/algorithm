ff09d06a33bd2f5f14f1b86437ccd910b30ab48b
춤추는망고 <ensia96@gmail.com>
2023-07-31 12:47:38 +0900

    [update] 스케이트보드(28417) - 시간 초과 해결

--- baekjoon/bronze/III/스케이트보드(28417).py
+++ baekjoon/bronze/III/스케이트보드(28417).py
@@ -1,6 +1,6 @@
-_, *A = map(int, open(0).read().split())
+n, *A = map(int, open(0).read().split())
 t = 0
-while A:
-    _A, A = A[:7], A[7:]
+for i in range(n):
+    _A = A[i*7:-~i*7]
     t = max(t, max(_A[:2])+sum(sorted(_A[2:])[-2:]))
 print(t)
