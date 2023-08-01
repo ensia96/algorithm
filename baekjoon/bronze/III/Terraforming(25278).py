f1e55d569e71ef53a1c9278c47286e158c23c149
춤추는망고 <ensia96@gmail.com>
2023-08-01 20:45:11 +0900

    [update] Terraforming(25278) - 틀렸습니다 해결

--- baekjoon/bronze/III/Terraforming(25278).py
+++ baekjoon/bronze/III/Terraforming(25278).py
@@ -3,4 +3,4 @@ D = {a: 0, b: 0, c: 0}
 for l in [*open(0)][1:]:
     x, y = l.split()
     D[x] += int(y)
-print('not '*-~-((D[a] > 13)*(D[b] > 8)*(D[c] > 7))+'liveable')
+print('not '*-~-((D[a] > 13)*(D[b] > 8)*(D[c] > 37))+'liveable')
