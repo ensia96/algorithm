636cbf9a0fed0f9f768ef2ce32c43ed2d2c9c604
춤추는망고 <ensia96@gmail.com>
2023-08-01 20:44:07 +0900

    Terraforming(25278)

---
+++ baekjoon/bronze/III/Terraforming(25278).py
@@ -0,0 +1,6 @@
+a, b, c = 'oxygen', 'ocean', 'temperature'
+D = {a: 0, b: 0, c: 0}
+for l in [*open(0)][1:]:
+    x, y = l.split()
+    D[x] += int(y)
+print('not '*-~-((D[a] > 13)*(D[b] > 8)*(D[c] > 7))+'liveable')
