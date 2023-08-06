617570e7ad5edc9670029b61e5126d02b7207287
춤추는망고 <ensia96@gmail.com>
2023-08-06 16:20:55 +0900

    [update] Parity Bit(5343) - 틀렸습니다 해결

--- baekjoon/bronze/III/Parity Bit(5343).py
+++ baekjoon/bronze/III/Parity Bit(5343).py
@@ -1,4 +1,3 @@
-for _ in ' '*int(input()):
-    l = input()
-    print(sum(l[8*i:8*i+7].count('1') != int(l[8*i+7])
+for l in [*open(0)][1:]:
+    print(sum(l[8*i:8*i+7].count('1') % 2 != int(l[8*i+7])
           for i in range(len(l)//8)))
