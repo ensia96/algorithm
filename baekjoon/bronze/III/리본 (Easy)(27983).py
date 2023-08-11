7916d2919932dbc6dd80d66a86af89999c60a95b
춤추는망고 <ensia96@gmail.com>
2023-08-11 14:10:15 +0900

    [update] 리본 (Easy)(27983) - 틀렸습니다 해결

--- baekjoon/bronze/III/리본 (Easy)(27983).py
+++ baekjoon/bronze/III/리본 (Easy)(27983).py
@@ -3,7 +3,7 @@ n = int(n)
 *A, = map(int, C[:n+n])
 for i in range(n):
     for j in range(i, n):
-        if A[i]-A[j] <= A[n+i]+A[n+j] and C[n+n+i] != C[n+n+j]:
+        if abs(A[i]-A[j]) <= A[n+i]+A[n+j] and C[n+n+i] != C[n+n+j]:
             print('YES')
             exit(print(i+1, j+1))
 print('NO')
