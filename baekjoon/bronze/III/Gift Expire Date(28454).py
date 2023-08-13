192a0c24e3799104c73950f0c752babc04e8fc1b
춤추는망고 <ensia96@gmail.com>
2023-08-13 16:18:24 +0900

    [update] Gift Expire Date(28454) - 틀렸습니다 해결

--- baekjoon/bronze/III/Gift Expire Date(28454).py
+++ baekjoon/bronze/III/Gift Expire Date(28454).py
@@ -2,5 +2,5 @@ y, m, d = map(int, input().split('-'))
 t = 0
 for _ in ' '*int(input()):
     a, b, c = map(int, input().split('-'))
-    t += a > y or b > m or (b == m and c >= d)
+    t += a > y or (a == y and b > m) or (a == y and b == m and c >= d)
 print(t)
