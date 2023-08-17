71a065624f31b81ed5e392e8edc939b009fab0a2
춤추는망고 <ensia96@gmail.com>
2023-08-17 21:14:03 +0900

    [update] ИЗРАЗ(24348) - 틀렸습니다 해결

--- baekjoon/bronze/III/ИЗРАЗ(24348).py
+++ baekjoon/bronze/III/ИЗРАЗ(24348).py
@@ -1,2 +1,3 @@
-a, b, c = sorted(map(int, input().split()))
-print(a+b*c)
+a, b, c = map(int, input().split())
+print(max(max(x+y*z, x*y+z)for x, y,
+      z in [(a, b, c), (a, c, b), (b, a, c), (b, c, a), (c, a, b), (c, b, a)]))
