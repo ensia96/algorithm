cac3b22120857f78edf94e730e02bdb589114b46
춤추는망고 <ensia96@gmail.com>
2023-08-27 14:10:39 +0900

    [update] Thai Lottery Checking(11258) - 틀렸습니다 해결

--- baekjoon/bronze/III/Thai Lottery Checking(11258).py
+++ baekjoon/bronze/III/Thai Lottery Checking(11258).py
@@ -1,5 +1,5 @@
 *X, _ = open(0).read().split()
 a, A, b, B, c, C, d, D, e, E, f, F, *X = X
 for x in X:
-    print(sum((t in x)*int(T)
-          for t, T in [(a, A), (b, B), (c, C), (d, D), (e, E), (f, F)]))
+    print((a == x)*int(A)+(b == x[:3])*int(B)+(c == x[:3])*int(C) +
+          (d == x[3:])*int(D)+(e == x[3:])*int(E)+(f == x[4:])*int(F))
