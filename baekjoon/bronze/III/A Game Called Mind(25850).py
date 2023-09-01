28d90e3fe3482eafe746c5caeb16094cc9cd8781
춤추는망고 <ensia96@gmail.com>
2023-09-01 21:21:52 +0900

    [update] A Game Called Mind(25850) - 런타임 에러 (ValueError) 해결

--- baekjoon/bronze/III/A Game Called Mind(25850).py
+++ baekjoon/bronze/III/A Game Called Mind(25850).py
@@ -1,6 +1,6 @@
 D, d = [0]*100, 65
-for l in [*open(0)][1:]:
-    n, *A = map(int, l.split())
+for _ in ' '*int(input()):
+    n, *A = map(int, input().split())
     for a in A:
         D[a] = d
     d += 1
