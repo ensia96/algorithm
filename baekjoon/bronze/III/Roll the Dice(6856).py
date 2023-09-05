938becd543f806c4092c4dc6b31f94f1bf23847e
춤추는망고 <ensia96@gmail.com>
2023-09-05 21:30:27 +0900

    [update] Roll the Dice(6856) - 틀렸습니다 해결

--- baekjoon/bronze/III/Roll the Dice(6856).py
+++ baekjoon/bronze/III/Roll the Dice(6856).py
@@ -1,3 +1,3 @@
 x, y = int(input()), int(input())
-z = sum(0 < 10-i < 11 for i in range(min(10, y)))
+z = sum(0 < 10-i < 11 for i in range(y))
 print('There', 'airse'[z < 2::2], z, 'ways to get the sum 10.')
