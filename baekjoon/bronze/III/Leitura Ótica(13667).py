f36b06a26619a6368cb42896a85393d85eaa1968
춤추는망고 <ensia96@gmail.com>
2023-09-08 21:22:47 +0900

    [update] Leitura Ótica(13667) - 코드 길이 줄이기

--- baekjoon/bronze/III/Leitura Ótica(13667).py
+++ baekjoon/bronze/III/Leitura Ótica(13667).py
@@ -1,7 +1,7 @@
-while True:
+while 1:
     n = int(input())
     if n == 0:
         break
     for i in range(n):
         T = [a < 128 for a in map(int, input().split())]
-        print('*' if T.count(True) != 1 else 'ABCDE'[T.index(True)])
+        print('*'if 1 != T.count(True)else 'ABCDE'[T.index(True)])
