af7fe1e4ec46309072316e32d3fc074dfd2d370c
춤추는망고 <ensia96@gmail.com>
2023-09-08 20:16:27 +0900

    [update] Leitura Ótica(13667) - 런타임 에러 (ValueError) 해결

--- baekjoon/bronze/III/Leitura Ótica(13667).py
+++ baekjoon/bronze/III/Leitura Ótica(13667).py
@@ -1,6 +1,7 @@
 while 1:
     n = int(input())
-    n or exit()
+    if not n:
+        break
     for i in range(n):
         T = [a < 128 for a in map(int, input().split())]
         print('*'if 1 < T.count(1)else 'ABCDE'[T.index(1)])
