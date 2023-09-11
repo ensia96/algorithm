2f2412c59e5726c76fe0fea7d8587450bcfe1c2f
춤추는망고 <ensia96@gmail.com>
2023-09-11 23:32:31 +0900

    [update] 브실혜성(29722) - 틀렸습니다 해결

--- baekjoon/bronze/III/브실혜성(29722).py
+++ baekjoon/bronze/III/브실혜성(29722).py
@@ -1,3 +1,3 @@
 y, m, d = map(int, input().split('-'))
-t = (12*y+m)*30+d+int(input())
-print(f"{t//360}-{(t%360//30-(not t%30))or 12:02d}-{t%30 or 30:02d}")
+t = 360*y+30*m+d+int(input())
+print(f'{(t-31)//360}-{~-t%360//30 or 12:02d}-{t%30 or 30:02d}')
