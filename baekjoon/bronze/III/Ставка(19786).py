237c574e3944cfa03cfcfcbe1bb2e0b94159d226
춤추는망고 <ensia96@gmail.com>
2023-10-18 00:56:15 +0900

    [update] Ставка(19786) - 틀렸습니다 해결

--- baekjoon/bronze/III/Ставка(19786).py
+++ baekjoon/bronze/III/Ставка(19786).py
@@ -1,7 +1,7 @@
-f, F = lambda: map(int, input().split()), lambda r, g, b, y: (
-    r*r+g*g+b*b)+y*min([r, g, b])
+f, F = lambda: map(int, input().split()), lambda r, g, b, x, y: x * \
+    (r*r+g*g+b*b)+y*min([r, g, b])
 for _ in ' '*int(input()):
     x, y = f()
     r, g, b = f()
-    R, G, B = F(r+1, g, b, y), F(r, g+1, b, y), F(r, g, b+1, y)
+    R, G, B = F(r+1, g, b, x, y), F(r, g+1, b, x, y), F(r, g, b+1, x, y)
     print('RED'if B <= R >= G else 'GREEN'if R <= G >= B else 'BLUE')
