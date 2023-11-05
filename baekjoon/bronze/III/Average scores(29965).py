7a880e54099e98dc90ab6a9bd4886d137d5bdff9
춤추는망고 <ensia96@gmail.com>
2023-11-05 11:32:55 +0900

    [update] Average scores(29965) - 런타임 에러 (ZeroDivisionError) 해결

--- baekjoon/bronze/III/Average scores(29965).py
+++ baekjoon/bronze/III/Average scores(29965).py
@@ -3,5 +3,5 @@ A, B = A[::2], A[1::2]
 T = [0]*2
 for i in range(n):
     T[A[i]] += B[i]
-x, y = T[0]/A.count(0), T[1]/A.count(1)
+x, y = T[0]/(A.count(0) or 1), T[1]/(A.count(1) or 1)
 print('VMJ'[(x > y)+2*(x < y)])
