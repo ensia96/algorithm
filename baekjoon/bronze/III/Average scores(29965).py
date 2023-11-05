92b06f5e57ef385d5175ed9316db90407589490b
춤추는망고 <ensia96@gmail.com>
2023-11-05 11:31:24 +0900

    Average scores(29965)

---
+++ baekjoon/bronze/III/Average scores(29965).py
@@ -0,0 +1,7 @@
+n, *A, = map(int, open(0).read().replace(*'M0').replace(*'J1').split())
+A, B = A[::2], A[1::2]
+T = [0]*2
+for i in range(n):
+    T[A[i]] += B[i]
+x, y = T[0]/A.count(0), T[1]/A.count(1)
+print('VMJ'[(x > y)+2*(x < y)])
