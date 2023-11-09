50945b4175ee9c43e2d15ceda2b268dda4f2e996
춤추는망고 <ensia96@gmail.com>
2023-11-09 20:03:32 +0900

    [update] Increasing Sublist(30216) - 틀렸습니다 해결

--- baekjoon/bronze/III/Increasing Sublist(30216).py
+++ baekjoon/bronze/III/Increasing Sublist(30216).py
@@ -1,7 +1,8 @@
 n, *A = map(int, open(0).read().split())
 x = y = 1
 for i in range(n-1):
-    x *= (A[i-1] < A[i])
-    x += 1
-    y = max(x, y)
+    if A[i-1] < A[i]:
+        x += 1
+    else:
+        x, y = 1, max(x, y)
 print(y)
