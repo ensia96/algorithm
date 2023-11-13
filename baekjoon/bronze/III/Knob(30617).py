6b0f6d52bc2378f73d1ab1322bb0ff012f040742
춤추는망고 <ensia96@gmail.com>
2023-11-13 21:28:45 +0900

    [update] Knob(30617) - 틀렸습니다 해결

--- baekjoon/bronze/III/Knob(30617).py
+++ baekjoon/bronze/III/Knob(30617).py
@@ -3,9 +3,9 @@ T = [*zip(A[::2], A[1::2])]
 t = 0
 for i in range(n):
     a, b = T[i]
-    t += a*(a == b)
+    t += a and (a == b)
     j = i > 0
     c, d = T[i-1]
-    t += j*a*(a == c)
-    t += j*b*(b == d)
+    t += j and a and a == c
+    t += j and b and b == d
 print(t)
