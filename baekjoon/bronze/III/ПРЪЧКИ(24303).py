f29d3b214732f30499763224502ef929977a3c67
춤추는망고 <ensia96@gmail.com>
2023-11-17 18:43:54 +0900

    [update] ПРЪЧКИ(24303) - 틀렸습니다 해결

--- baekjoon/bronze/III/ПРЪЧКИ(24303).py
+++ baekjoon/bronze/III/ПРЪЧКИ(24303).py
@@ -1,9 +1,8 @@
 a, b, c, d, e, f, x = map(int, input().split())
 D = {a: d, b: e, c: f}
 a = 0
-x += 1
 for i in sorted(D)[::-1]:
-    y = min(x//i, D[i])
-    x -= i*y
-    a += y
-print(a*(x < 1))
+    i*D[i] > x and exit(print(-(x//-i)+a))
+    a += D[i]
+    x -= i*D[i]
+print(0)
