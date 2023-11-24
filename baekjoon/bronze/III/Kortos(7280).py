4cf7eec2d7750340776d273bb66a91a835f53d5a
춤추는망고 <ensia96@gmail.com>
2023-11-24 19:29:03 +0900

    [update] Kortos(7280) - 틀렸습니다 해결

--- baekjoon/bronze/III/Kortos(7280).py
+++ baekjoon/bronze/III/Kortos(7280).py
@@ -1,3 +1,4 @@
 *A, = open(0).read().split()
+*B, = map(int, A[1::2])
 print(*[i for i in 'SBVK'if A.count(i) < 13], *
-      [i for i in range(14)if A.count(str(i)) < 4])
+      [i for i in range(1, 14)if B.count(i) < 4])
