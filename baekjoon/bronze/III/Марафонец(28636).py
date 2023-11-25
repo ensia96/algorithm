1955ce1dfbbc72437be16fb44bca0c3980639f89
춤추는망고 <ensia96@gmail.com>
2023-11-25 18:39:01 +0900

    [update] Марафонец(28636) - 컴파일 에러 해결

--- baekjoon/bronze/III/Марафонец(28636).py
+++ baekjoon/bronze/III/Марафонец(28636).py
@@ -1,5 +1,6 @@
 _, *A = open(0).read().split()
 t = 0
 for a in A:
-    m, s = map(int, a.split(':'))t += 60*m+s
+    m, s = map(int, a.split(':'))
+    t += 60*m+s
 print(f"{t//3600:02d}:{t%3600//60:02d}:{t%60:02d}")
