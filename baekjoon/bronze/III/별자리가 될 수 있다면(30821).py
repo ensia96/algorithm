6e5c39298fe6baa8f2cb62f0f562a35171f2627b
춤추는망고 <ensia96@gmail.com>
2023-12-05 19:50:08 +0900

    [update] 별자리가 될 수 있다면(30821) - 코드 길이 줄이기

--- baekjoon/bronze/III/별자리가 될 수 있다면(30821).py
+++ baekjoon/bronze/III/별자리가 될 수 있다면(30821).py
@@ -1,15 +1,2 @@
-n = int(input())
-
-
-def factorial(k):
-    fact = 1
-    for i in range(1, k+1):
-        fact *= i
-    return fact
-
-
-def nCr(n, r):
-    return factorial(n) // (factorial(r) * factorial(n-r))
-
-
-print(nCr(n, 5))
+n, f = int(input()), lambda x: x < 1 or x*f(x-1)
+print(f(n)//(f(5)*f(n-5)))
