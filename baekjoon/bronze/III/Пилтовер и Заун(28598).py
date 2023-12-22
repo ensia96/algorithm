bd4115d76b38a346072013f71e962e177258eef9
춤추는망고 <ensia96@gmail.com>
2023-12-22 22:31:34 +0900

    [update] Пилтовер и Заун(28598) - 틀렸습니다 해결

--- baekjoon/bronze/III/Пилтовер и Заун(28598).py
+++ baekjoon/bronze/III/Пилтовер и Заун(28598).py
@@ -1,3 +1,8 @@
 a, b, c = map(int, input().split())
 x = a-b
-print('YNEOS'[c and x/c <= 2 or x % 2::2])
+if c == 0:
+    print('YNEOS'[a != b::2])
+elif x % 2 or x//2/c < 1:
+    print('NO')
+else:
+    print('YES')
