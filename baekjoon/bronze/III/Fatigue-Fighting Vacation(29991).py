fe31996b09f502247d946ef42cf615905445239b
춤추는망고 <ensia96@gmail.com>
2023-12-28 20:28:42 +0900

    [update] Fatigue-Fighting Vacation(29991) - 시간 초과 해결

--- baekjoon/bronze/III/Fatigue-Fighting Vacation(29991).py
+++ baekjoon/bronze/III/Fatigue-Fighting Vacation(29991).py
@@ -1,11 +1,6 @@
 d, c, r, *A = map(int, open(0).read().split())
-A, B = A[:c], A[c:]
-c = 0
+d += sum(A[c:])
 for a in A:
-    while d < a and B:
-        b, *B = B
-        c += 1
-        d += b
     d -= a
-    c += d >= 0
-print(c)
+    r += d >= 0
+print(r)
