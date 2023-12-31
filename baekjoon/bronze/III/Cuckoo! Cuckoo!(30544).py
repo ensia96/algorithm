6fd7ef6696bdbc18e8a56c7ac47434ecbb1685ae
춤추는망고 <ensia96@gmail.com>
2023-12-31 16:25:37 +0900

    [update] Cuckoo! Cuckoo!(30544) - 틀렸습니다 해결

--- baekjoon/bronze/III/Cuckoo! Cuckoo!(30544).py
+++ baekjoon/bronze/III/Cuckoo! Cuckoo!(30544).py
@@ -1,9 +1,9 @@
 M = 60
 a, b = open(0).read().split()
 h, m = map(int, a.split(':'))
-T = h*M+m-1
-b = int(b)
+b = int(b)-(m % 15 < 1)-(m < 1)*~-h
 while b > 0:
-    b -= 1+(T % M < 1)*(T//M % 12-1)
-    T += 15-T % 15
-print(f"{T//M%12 or 12:02d}:{T%M:02d}")
+    m = (m+15-m % 15) % M
+    h = (h+(m < 1)) % 13 or 1
+    b -= (m > 0)+(m < 1)*h
+print(f"{h:02}:{m:02d}")
