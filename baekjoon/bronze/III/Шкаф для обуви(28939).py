15b810103ca8a437f67684cfc2a635a84202092f
춤추는망고 <ensia96@gmail.com>
2024-01-04 20:32:51 +0900

    [update] Шкаф для обуви(28939) - 시간 초과 해결

--- baekjoon/bronze/III/Шкаф для обуви(28939).py
+++ baekjoon/bronze/III/Шкаф для обуви(28939).py
@@ -1,7 +1,8 @@
-n, k, p, q, *A = map(int, open(0).read().split())
-c = t = 0
+import sys
+C, F = 0, lambda: map(int, sys.stdin.readline().split())
+n, *_ = F()
+k, p, q = F()
 for _ in ' '*n:
-    x, y, *A = A
-    t += sum(k*x < q*a or p*a < x for a in A[c:c+y+1])
-    c += y
-print(t)
+    h, x, *A = F()
+    C += sum(k*h < q*a or p*a < h for a in A)
+print(C)
