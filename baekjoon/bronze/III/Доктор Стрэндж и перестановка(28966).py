a4fbfb330300197c8ff27202bfb7b8aff95b0dde
춤추는망고 <ensia96@gmail.com>
2024-01-05 18:41:57 +0900

    [update] Доктор Стрэндж и перестановка(28966) - 틀렸습니다 해결

--- baekjoon/bronze/III/Доктор Стрэндж и перестановка(28966).py
+++ baekjoon/bronze/III/Доктор Стрэндж и перестановка(28966).py
@@ -1,3 +1,4 @@
 n, *A = map(int, open(0).read().split())
 T = [-~i for i in range(n)if -~i % 2 != A[i] % 2]
-print(*[[[-1, -1], '13'][not T and n > 2], T][len(T) == 2])
+print(*[[[-1, -1], '13'][not T and n > 2], T]
+      [len(T) == 2 and T[0] % 2 != T[1] % 2])
