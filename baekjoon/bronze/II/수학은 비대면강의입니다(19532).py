40d7ffd98983b7dc5074404939e5289dddda1bf0
춤추는망고 <ensia96@gmail.com>
2024-01-20 14:18:34 +0900

    [update] 수학은 비대면강의입니다(19532) - 풀이 주석 추가

--- baekjoon/bronze/II/수학은 비대면강의입니다(19532).py
+++ baekjoon/bronze/II/수학은 비대면강의입니다(19532).py
@@ -1,3 +1,16 @@
 a, b, c, d, e, f = map(int, input().split())
 t = a*e-b*d
 print((c*e-b*f)//t, (a*f-c*d)//t)
+
+# ax + by = c
+# dx + ey = f
+#
+# ax + by - c = dx + ey - f
+#
+# adx + bdy - cd = adx + aey - af
+#
+# (bd - ae) * y = cd - af
+#
+# y = (cd - af) // (bd - ae)
+#
+# y = (af - cd) // (ae - bd)
