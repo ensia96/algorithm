a617ac67ef43c1414944500472420f19c541867d
춤추는망고 <ensia96@gmail.com>
2024-01-20 14:16:26 +0900

    [update] 수학은 비대면강의입니다(19532) - 코드 길이 줄이기

--- baekjoon/bronze/II/수학은 비대면강의입니다(19532).py
+++ baekjoon/bronze/II/수학은 비대면강의입니다(19532).py
@@ -1,5 +1,3 @@
 a, b, c, d, e, f = map(int, input().split())
-R = range(-999, 1000)
-for x in R:
-    for y in R:
-        a*x+b*y == c and d*x+e*y == f and exit(print(x, y))
+t = a*e-b*d
+print((c*e-b*f)//t, (a*f-c*d)//t)
