5bf9d61865a133d62b5e1f1ed945f3bd856675f2
춤추는망고 <ensia96@gmail.com>
2024-03-20 17:57:22 +0900

    싱기한 네자리 숫자(6679)

---
+++ baekjoon/bronze/II/싱기한 네자리 숫자(6679).py
@@ -0,0 +1,9 @@
+t = []
+for i in range(1000, 10000):
+    for j in [10, 12, 16]:
+        t += 0, x = i
+        while x:
+            t[-1] += x % j
+            x //= j
+    if len({*t}) < 2:
+        print(i)
