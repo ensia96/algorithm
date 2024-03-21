e1d856733ab10bdf54c2262b418f867a4e8a339f
춤추는망고 <ensia96@gmail.com>
2024-03-21 22:29:26 +0900

    [update] 주사위 네개(2484) - 런타임 에러 (TypeError) 해결

--- baekjoon/bronze/II/주사위 네개(2484).py
+++ baekjoon/bronze/II/주사위 네개(2484).py
@@ -1,2 +1,2 @@
 print(max([d, 10+[c, b][c < d], 5*[20+b*2, 4+a+c][b < c], 500+a*50]
-      [-len({a, b, c, d})]for i in [*open(0)][1:]for a, b, c, d in sorted(map(int, i.split())))*100)
+      [-len({a, b, c, d})]for i in [*open(0)][1:]for (a, b, c, d) in [sorted(map(int, i.split()))])*100)
