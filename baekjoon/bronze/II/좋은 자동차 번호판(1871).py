bdc269854428e4bc629efc454fa68e5d007d20e1
춤추는망고 <ensia96@gmail.com>
2024-03-29 17:50:51 +0900

    [update] 좋은 자동차 번호판(1871) - 틀렸습니다 해결

--- baekjoon/bronze/II/좋은 자동차 번호판(1871).py
+++ baekjoon/bronze/II/좋은 자동차 번호판(1871).py
@@ -3,4 +3,4 @@ for l in [*open(0)][1:]:
     s = 0
     for i in x:
         s = (s+ord(i)-65)*26
-    print(*[['not'], []][abs(s//26-int(y)) > 100], 'nice')
+    print(*[[], ['not']][abs(s//26-int(y)) > 100], 'nice')
