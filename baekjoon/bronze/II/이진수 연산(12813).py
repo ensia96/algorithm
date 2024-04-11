fd29a2452b66333af90bda3877dc461cd2b96fbd
춤추는망고 <ensia96@gmail.com>
2024-04-11 19:56:47 +0900

    [update] 이진수 연산(12813) - 틀렸습니다 해결

--- baekjoon/bronze/II/이진수 연산(12813).py
+++ baekjoon/bronze/II/이진수 연산(12813).py
@@ -1,5 +1,5 @@
 a, b = int(input(), 2), int(input(), 2)
-for i in [a & b, a | b, a ^ b]:
-    print(bin(i).split('b')[1].rjust(10, '0'))
-print(''.join('10'[int(i)]for i in bin(a)[2:]).rjust(10, '1'))
-print(''.join('10'[int(i)]for i in bin(b)[2:]).rjust(10, '1'))
+n = 100000
+m = 2**n-1
+for i in [a & b, a | b, a ^ b, a ^ m, b ^ m]:
+    print(bin(i).split('b')[1].rjust(n, '0'))
