413e3b0b638f1dce8417a4e2ae549aee807b9f69
춤추는망고 <ensia96@gmail.com>
2024-04-23 19:13:18 +0900

    [update] 방 배정(13304) - 틀렸습니다 해결

--- baekjoon/bronze/II/방 배정(13304).py
+++ baekjoon/bronze/II/방 배정(13304).py
@@ -2,5 +2,5 @@ n, k, *A = map(int, open(0).read().split())
 R = [k-1]*6
 for i in range(n):
     y = A[i*2+1]
-    R[2*(y-1)//2+A[i*2]*(y > 2)] += 1
+    R[(y-1)//2*2+A[i*2]*(y > 2)] += 1
 print(sum(r//k for r in R))
