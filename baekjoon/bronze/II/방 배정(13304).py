ccd807a81bbb39010cedae4374f9a90147c2f0a1
춤추는망고 <ensia96@gmail.com>
2024-04-23 19:05:24 +0900

    [update] 방 배정(13304) - 12점(틀렸습니다) 해결

--- baekjoon/bronze/II/방 배정(13304).py
+++ baekjoon/bronze/II/방 배정(13304).py
@@ -1,6 +1,6 @@
 n, k, *A = map(int, open(0).read().split())
-R = [0]*6
+R = [0]*5
 for i in range(n):
-    y = A[i*2+1]-1
-    R[[y-A[i*2], 0][y < 2]] += 1
+    y = A[i*2+1]
+    R[(y > 2)+2*(y > 4)+A[i*2]*(y > 2)] += 1
 print(sum(-(-r//k)for r in R))
