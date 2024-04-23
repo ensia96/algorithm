6f738b18e44abb583c93293fbff95bd123e6c610
춤추는망고 <ensia96@gmail.com>
2024-04-23 19:12:04 +0900

    [update] 방 배정(13304) - 코드 길이 줄이기

--- baekjoon/bronze/II/방 배정(13304).py
+++ baekjoon/bronze/II/방 배정(13304).py
@@ -1,6 +1,6 @@
 n, k, *A = map(int, open(0).read().split())
-R = [0]*5
+R = [k-1]*6
 for i in range(n):
     y = A[i*2+1]
-    R[(y > 2)+2*(y > 4)+A[i*2]*(y > 2)] += 1
-print(sum(-(-r//k)for r in R))
+    R[2*(y-1)//2+A[i*2]*(y > 2)] += 1
+print(sum(r//k for r in R))
