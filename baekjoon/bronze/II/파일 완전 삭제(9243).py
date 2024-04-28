ceef4c4456f92a9dd00d5c0947f4da70178b819a
춤추는망고 <ensia96@gmail.com>
2024-04-28 14:04:35 +0900

    [update] 파일 완전 삭제(9243) - 틀렸습니다 해결

--- baekjoon/bronze/II/파일 완전 삭제(9243).py
+++ baekjoon/bronze/II/파일 완전 삭제(9243).py
@@ -1,3 +1,9 @@
-n, a, b = open(0).read().split()
-print('Deletion', ['failed', 'succeeded']
-      [all([a[i] != b[i], a[i] == b[i]]for i in range(len(a)))])
+N = int(input())
+a, b = list(input()), list(input())
+for _ in range(N):
+    for i in range(len(a)):
+        if a[i] == '0':
+            a[i] = '1'
+        else:
+            a[i] = '0'
+print("Deletion succeeded" if a == b else "Deletion failed")
