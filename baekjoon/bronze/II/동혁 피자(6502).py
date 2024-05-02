001786fe9fb6c469a40df5a008a3605e93744760
춤추는망고 <ensia96@gmail.com>
2024-05-02 18:49:41 +0900

    [update] 동혁 피자(6502) - 런타임 에러 (NameError) 해결

--- baekjoon/bronze/II/동혁 피자(6502).py
+++ baekjoon/bronze/II/동혁 피자(6502).py
@@ -1,7 +1,7 @@
 for l in open(i := 0):
     i += 1
     a, *A = map(int, l.split())
-    len(t) < 2 and exit()
+    a or exit()
     b, c = A
     print('Pizza', i, ['fits', 'does not fit']
           [b*b+c*c > 4*a*a], 'on the table')
