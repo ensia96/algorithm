cd47b0841918c38194a2b8e1d3c531c04604a29d
춤추는망고 <ensia96@gmail.com>
2024-05-02 18:48:39 +0900

    [update] 동혁 피자(6502) - 틀렸습니다 해결

--- baekjoon/bronze/II/동혁 피자(6502).py
+++ baekjoon/bronze/II/동혁 피자(6502).py
@@ -1,7 +1,7 @@
 for l in open(i := 0):
     i += 1
-    t = l.split()
-    t[0] == '0' and exit()
-    a, b, c = map(int, t)
+    a, *A = map(int, l.split())
+    len(t) < 2 and exit()
+    b, c = A
     print('Pizza', i, ['fits', 'does not fit']
           [b*b+c*c > 4*a*a], 'on the table')
