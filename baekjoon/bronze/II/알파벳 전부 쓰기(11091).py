651827659906a0ce3d5e8a5ac6cf74db84ed3d20
춤추는망고 <ensia96@gmail.com>
2024-05-25 22:43:33 +0900

    [updtae] 알파벳 전부 쓰기(11091) - 틀렸습니다 해결

--- baekjoon/bronze/II/알파벳 전부 쓰기(11091).py
+++ baekjoon/bronze/II/알파벳 전부 쓰기(11091).py
@@ -2,4 +2,4 @@ import string
 A = string.ascii_lowercase
 for i in [*open(0)][1:]:
     t = {*A}-{a for a in A if a in i.lower()}
-    print(['paragraph', 'missing '+''.join(sorted(t))][len(t) > 0])
+    print(['pangram', 'missing '+''.join(sorted(t))][len(t) > 0])
