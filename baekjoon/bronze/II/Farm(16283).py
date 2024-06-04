4efc4d74a07cd17e0fbfe724067b6a8db56dcf0e
춤추는망고 <ensia96@gmail.com>
2024-06-04 20:20:30 +0900

    [update] Farm(16283) - 코드 길이 줄이기

--- baekjoon/bronze/II/Farm(16283).py
+++ baekjoon/bronze/II/Farm(16283).py
@@ -1,3 +1,3 @@
 a, b, n, w = map(int, input().split())
-A = [i for i in range(1, n)if (n-i)*b+a*i == w]
-print(*[A[0], n-A[0]]if len(A) == 1 else [-1])
+A = [(i, n-i)for i in range(1, n)if (n-i)*b+a*i == w]
+print(*A[0]if 1 == len(A)else [-1])
