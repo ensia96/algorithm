63f3e205ceafa70e6f6eec7b9ba75a4c8b0bd874
춤추는망고 <ensia96@gmail.com>
2024-06-07 20:55:44 +0900

    [update] 펫(1362) - 틀렸습니다 해결

--- baekjoon/bronze/II/펫(1362).py
+++ baekjoon/bronze/II/펫(1362).py
@@ -4,10 +4,10 @@ while 1:
     a, b = f()
     if a == '0':
         break
-    elif a == '#':
+    if a == '#':
         print(i, [':-'+'()'[o/2 < w < 2*o], 'RIP'][w < 0])
         i += 1
     elif a in 'EF':
-        w += int(b)*(1-2*(a == 'E'))
+        w += int(b)*(1-2*(a == 'E'))*(w > 0)
     else:
         o, w = map(int, [a, b])
