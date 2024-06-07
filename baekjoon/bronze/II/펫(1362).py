6953eddd8589bdcda5831b84ce1569cdb5e078c0
춤추는망고 <ensia96@gmail.com>
2024-06-07 20:49:58 +0900

    펫(1362)

---
+++ baekjoon/bronze/II/펫(1362).py
@@ -0,0 +1,13 @@
+f, i = lambda: input().split(), 1
+o = w = 0
+while 1:
+    a, b = f()
+    if a == '0':
+        break
+    elif a == '#':
+        print(i, [':-'+'()'[o/2 < w < 2*o], 'RIP'][w < 0])
+        i += 1
+    elif a in 'EF':
+        w += int(b)*(1-2*(a == 'E'))
+    else:
+        o, w = map(int, [a, b])
