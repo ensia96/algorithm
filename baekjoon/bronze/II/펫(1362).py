e44c3a387c2738c17e15a4514f63d5b2b84aa78e
춤추는망고 <ensia96@gmail.com>
2024-06-07 21:00:29 +0900

    [update] 펫(1362) - 틀렸습니다 해결

--- baekjoon/bronze/II/펫(1362).py
+++ baekjoon/bronze/II/펫(1362).py
@@ -1,13 +1,11 @@
 f, i = lambda: input().split(), 1
 o = w = 0
-while 1:
-    a, b = f()
-    if a == '0':
-        break
+while (A := f()) != [*'00']:
+    a, b = A
     if a == '#':
-        print(i, [':-'+'()'[o/2 < w < 2*o], 'RIP'][w < 0])
+        print(i, ['RIP', ':-'+'()'[o/2 < w < 2*o]][w > 0])
         i += 1
     elif a in 'EF':
         w += int(b)*(1-2*(a == 'E'))*(w > 0)
     else:
-        o, w = map(int, [a, b])
+        o, w = map(int, A)
