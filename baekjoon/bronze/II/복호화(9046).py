4f242bacd9c0aa01974ceffc60a20ae8d3d086b5
춤추는망고 <ensia96@gmail.com>
2024-06-08 11:56:01 +0900

    [update] 복호화(9046) - 틀렸습니다 해결

--- baekjoon/bronze/II/복호화(9046).py
+++ baekjoon/bronze/II/복호화(9046).py
@@ -1,4 +1,4 @@
 for l in [*open(0)][1:]:
-    D = {(l.count(i), i)for i in l}
+    D = {(l.count(i), i)for i in l.replace(' ', '')}
     *A, x, y = sorted(D)
     print([y[1], '?'][x[0] == y[0] and '\n' != x[1]])
