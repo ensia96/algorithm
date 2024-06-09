ffe29cf9d9baaa2e9e9f0b8a94d0500b70995af6
춤추는망고 <ensia96@gmail.com>
2024-06-09 11:16:31 +0900

    [update] 완전수(14563) - 틀렸습니다 해결

--- baekjoon/bronze/II/완전수(14563).py
+++ baekjoon/bronze/II/완전수(14563).py
@@ -1,4 +1,4 @@
 *A, = map(int, [*open(0)][1].split())
 for a in A:
     y = sum({i for i in range(2, a)if a % i < 1})+1
-    print([['Perfect', 'Abundant'][y > a], 'Deficient'][y < a])
+    print([['Perfect', 'Abundant'][y > a], 'Deficient'][y < a or a < 2])
