376ead5bec5283e7da7eea7fe055a0c5738879be
춤추는망고 <ensia96@gmail.com>
2024-06-12 19:27:56 +0900

    전구(21918)

---
+++ baekjoon/bronze/II/전구(21918).py
@@ -0,0 +1,10 @@
+_, S, *A = open(0)
+*S, = map(int, S.split())
+for a in A:
+    a, b, c = map(int, a.split())
+    b -= 1
+    if a > 1:
+        S[b:c] = [[+(not i), 0, 1][a-2]for i in S[b:c]]
+    else:
+        S[b] = c
+print(*S)
