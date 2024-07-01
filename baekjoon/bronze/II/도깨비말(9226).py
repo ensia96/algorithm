0c04ae391b35faa06ed778f286c387bff5ce51e2
춤추는망고 <ensia96@gmail.com>
2024-07-01 21:59:31 +0900

    [update] 도깨비말(9226) - 틀렸습니다 해결

--- baekjoon/bronze/II/도깨비말(9226).py
+++ baekjoon/bronze/II/도깨비말(9226).py
@@ -1,5 +1,6 @@
-for l in [*open(0)][:-1]:
+while (l := input()) != "#":
     for i in range(len(l)):
-        if l[i] in "aiueo\n":
-            print(l[i:-1] + l[:i] + "ay")
+        if l[i] in "aiueo":
+            l = l[i:] + l[:i]
             break
+    print(l + "ay")
