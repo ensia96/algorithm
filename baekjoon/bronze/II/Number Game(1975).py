110b873ba99c2e8a2c5ef2e3299d4b6dc8fec521
춤추는망고 <ensia96@gmail.com>
2024-07-07 00:38:38 +0900

    [update] Number Game(1975) - 런타임 에러 (RecursionError) 해결

--- baekjoon/bronze/II/Number Game(1975).py
+++ baekjoon/bronze/II/Number Game(1975).py
@@ -1,4 +1,4 @@
 f = lambda x, y: (not x % y) and 1 + f(x // y, y)
 for i in [*open(0)][1:]:
     n = int(i)
-    print(sum(f(n, i) for i in range(1, 1000)))
+    print(sum(f(n, -~i) for i in range(1, 1000)))
