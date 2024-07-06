e6f5f0eb8de28e2c4de639da6eebccca60a019a6
춤추는망고 <ensia96@gmail.com>
2024-07-06 23:33:47 +0900

    [update] Number Game(1975) - 틀렸습니다 해결

--- baekjoon/bronze/II/Number Game(1975).py
+++ baekjoon/bronze/II/Number Game(1975).py
@@ -1,4 +1,13 @@
-f = lambda x, y: (not x % y) and 1 + f(x // y, y)
-for i in [*open(0)][1:]:
-    n = int(i)
-    print(sum((not n % -~i) for i in range(1, 1000)))
+from sys import stdin
+
+
+def f(n, i):
+    return 1 + f(n // i, i) if n % i == 0 else 0
+
+
+for _ in range(int(stdin.readline())):
+    N = int(stdin.readline())
+    cnt = 0
+    for i in range(2, N + 1):
+        cnt += f(N, i)
+    print(cnt)
