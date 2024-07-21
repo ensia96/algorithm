9b70eeeaa568076e7e82ff9227109f18a0f1822c
춤추는망고 <ensia96@gmail.com>
2024-07-21 22:02:19 +0900

    [update] 풍선 놀이(6246) - 틀렸습니다 해결

--- baekjoon/bronze/II/풍선 놀이(6246).py
+++ baekjoon/bronze/II/풍선 놀이(6246).py
@@ -1,4 +1,2 @@
 n, q, *A = map(int, open(0).read().split())
-print(
-    n - sum(any(i % A[2 * j + 1] + 1 == A[2 * j] for j in range(q)) for i in range(n))
-)
+print(n - sum(any(-~i % A[2 * j + 1] == A[2 * j] for j in range(q)) for i in range(n)))
