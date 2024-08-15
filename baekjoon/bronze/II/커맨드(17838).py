bff189de097a608bbcf7523d70e95772552b427c
춤추는망고 <ensia96@gmail.com>
2024-08-15 22:58:47 +0900

    [update] 커맨드(17838) - 코드 길이 줄이기

--- baekjoon/bronze/II/커맨드(17838).py
+++ baekjoon/bronze/II/커맨드(17838).py
@@ -1,7 +1,2 @@
 for l in [*open(0)][1:]:
-    print(
-        +(
-            2 == len({*l.strip()})
-            and "!!??!??\n" == l.replace(l[0], "!").replace(l[2], "?")
-        )
-    )
+    print(+("!!??!??\n" == l.replace(l[0], "!").replace(l[2], "?")))
