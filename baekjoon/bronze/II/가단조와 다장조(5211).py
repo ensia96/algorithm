4a416bc64081f619deed6c58c662d78a5454b9b6
춤추는망고 <ensia96@gmail.com>
2024-09-04 21:25:17 +0900

    [update] 가단조와 다장조(5211) - 틀렸습니다 해결

--- baekjoon/bronze/II/가단조와 다장조(5211).py
+++ baekjoon/bronze/II/가단조와 다장조(5211).py
@@ -1,4 +1,4 @@
 A = input()
-l, A = A[-1], str(i[0] for i in A.split("|")).count
+l, A = A[-1], "".join(i[0] for i in A.split("|")).count
 x, y = sum(map(A, "CFG")), sum(map(A, "ADE"))
 print("CA--mmaijnoorr"[x < y + (x == y) * (l in "ADE") :: 2])
