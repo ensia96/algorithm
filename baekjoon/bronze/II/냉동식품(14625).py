3ffb3400e2e80adb0ebaba9c811d6a685671bc5a
춤추는망고 <ensia96@gmail.com>
2024-09-08 11:48:28 +0900

    [update] 냉동식품(14625) - 틀렸습니다 해결

--- baekjoon/bronze/II/냉동식품(14625).py
+++ baekjoon/bronze/II/냉동식품(14625).py
@@ -1,7 +1,2 @@
 a, b, c, d, e = map(int, open(0).read().split())
-print(
-    sum(
-        sum(str(e) in i for i in map(str, divmod(i, 60)))
-        for i in range(a * 60 + b, c * 60 + d)
-    )
-)
+print(sum(str(e) in f"{i//60:02}:{i%60:02}" for i in range(a * 60 + b, c * 60 + d + 1)))
