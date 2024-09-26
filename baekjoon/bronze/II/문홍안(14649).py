a0f749a3b53b1c7080b36c3e3ed3231790416cd9
춤추는망고 <ensia96@gmail.com>
2024-09-26 22:45:15 +0900

    문홍안(14649)

---
+++ baekjoon/bronze/II/문홍안(14649).py
@@ -0,0 +1,9 @@
+p, *A = open(0).read().split()
+for b in map(
+    [
+        sum([a < i, i < a][b == "L"] for a, b in zip(map(int, A[1::2]), A[2::2]))
+        for i in range(1, 101)
+    ].count,
+    range(3),
+):
+    print(f"{int(p)/100*b:.2f}")
