223174a5dcabf05e3638c0b001e4aad6028d0f96
춤추는망고 <ensia96@gmail.com>
2024-09-26 22:48:20 +0900

    [update] 문홍안(14649) - 틀렸습니다 해결

--- baekjoon/bronze/II/문홍안(14649).py
+++ baekjoon/bronze/II/문홍안(14649).py
@@ -1,7 +1,7 @@
 p, *A = open(0).read().split()
 for b in map(
     [
-        sum([a < i, i < a][b == "L"] for a, b in zip(map(int, A[1::2]), A[2::2]))
+        sum([a < i, i < a][b == "L"] for a, b in zip(map(int, A[1::2]), A[2::2])) % 3
         for i in range(1, 101)
     ].count,
     range(3),
