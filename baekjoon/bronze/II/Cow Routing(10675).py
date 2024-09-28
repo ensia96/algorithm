800b4450f8b458e7d86d3c6fee00edaac2da3ebb
춤추는망고 <ensia96@gmail.com>
2024-09-28 10:19:19 +0900

    Cow Routing(10675)

---
+++ baekjoon/bronze/II/Cow Routing(10675).py
@@ -0,0 +1,12 @@
+f = lambda: map(int, input().split())
+*A, n = f()
+y = 1000
+for _ in " " * n:
+    c, r = f()
+    x = 0
+    for i in f():
+        x += i == A[x]
+        if x > 1:
+            y = min(y, c)
+            break
+print(y)
