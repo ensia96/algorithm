2839380260cdd90ba7eba3804b86482bd96a2758
춤추는망고 <ensia96@gmail.com>
2024-10-01 18:40:27 +0900

    [update] 강력한 비밀번호(16944) - 코드 길이 줄이기

--- baekjoon/bronze/II/강력한 비밀번호(16944).py
+++ baekjoon/bronze/II/강력한 비밀번호(16944).py
@@ -1,18 +1,10 @@
-specialString = "!@#$%^&*()-+"
 n = int(input())
-s = input()
-tmp = [1, 1, 1, 1]
-for i in s:
-    if ord(i) >= 48 and ord(i) <= 57:
-        tmp[0] = 0
-    elif ord(i) >= 65 and ord(i) <= 91:
-        tmp[1] = 0
-    elif ord(i) >= 97 and ord(i) <= 123:
-        tmp[2] = 0
-    elif i in specialString:
-        tmp[3] = 0
-n += sum(tmp)
-if n > 6:
-    print(sum(tmp))
-else:
-    print(sum(tmp) + 6 - n)
+A = input()
+t = (
+    4
+    - any(map(str.isupper, A))
+    - any(map(str.islower, A))
+    - any(map(str.isdigit, A))
+    - any(map(lambda x: x in "!@#$%^&*()-+", A))
+)
+print([t + 6 - n, t][n + t > 6])
