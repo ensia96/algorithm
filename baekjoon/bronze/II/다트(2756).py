1702504de45e750b7d1a9899ec023840d7014beb
춤추는망고 <ensia96@gmail.com>
2024-10-03 21:28:45 +0900

    [update] 다트(2756) - 틀렸습니다 해결

--- baekjoon/bronze/II/다트(2756).py
+++ baekjoon/bronze/II/다트(2756).py
@@ -5,8 +5,4 @@ for l in [*open(0)][1:]:
         for i, j in zip(A[::2], A[1::2])
     ]
     x, y = sum(A[:3]), sum(A[3:])
-    print(
-        "SCORE:",
-        f"{max(x,y)} to {min(x,y)},",
-        [f"PLAYER {(x>y)+2*(x<y)} WINS.", "TIE."][x == y],
-    )
+    print("SCORE:", f"{x} to {y},", [f"PLAYER {(x>y)+2*(x<y)} WINS.", "TIE."][x == y])
