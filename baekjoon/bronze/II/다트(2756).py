5456f0a343ccd6f9780dd7807d8c8442baef54aa
춤추는망고 <ensia96@gmail.com>
2024-10-03 21:25:52 +0900

    [update] 다트(2756) - 틀렸습니다 해결

--- baekjoon/bronze/II/다트(2756).py
+++ baekjoon/bronze/II/다트(2756).py
@@ -1,7 +1,7 @@
 for l in [*open(0)][1:]:
     (*A,) = map(float, l.split())
     A = [
-        sum(20 * (-~k) ** 2 * 9 >= (i * i + j * j) for k in range(5))
+        20 * sum((-~k) ** 2 * 9 >= (i * i + j * j) for k in range(5))
         for i, j in zip(A[::2], A[1::2])
     ]
     x, y = sum(A[:3]), sum(A[3:])
