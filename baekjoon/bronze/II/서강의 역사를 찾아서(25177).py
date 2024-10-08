3b60f51ed471c81d6e10548c6c6ad156ad1237b7
춤추는망고 <ensia96@gmail.com>
2024-10-08 18:25:05 +0900

    [update] 서강의 역사를 찾아서(25177) - 코드 길이 줄이기

--- baekjoon/bronze/II/서강의 역사를 찾아서(25177).py
+++ baekjoon/bronze/II/서강의 역사를 찾아서(25177).py
@@ -1,2 +1,2 @@
-n, m, *A = map(int, open(0).read().split())
-print(max([A[i + n] - (i < n and A[i]) for i in range(m)] + [0]))
+n,m,*A=map(int,open(0).read().split())
+print(max(*A[i+n]-(i<n and A[i])for i in range(m),0))
