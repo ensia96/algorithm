3624f85a121cc512e251945468c90347e217a596
춤추는망고 <ensia96@gmail.com>
2024-10-23 20:05:40 +0900

    [update] ACM 대회 채점(11531) - 런타임 에러 (TypeError) 해결

--- baekjoon/bronze/II/ACM 대회 채점(11531).py
+++ baekjoon/bronze/II/ACM 대회 채점(11531).py
@@ -2,7 +2,7 @@ D = {}
 i = j = 0
 for l in [*open(0)][:-1]:
     t, p, r = l.split()
-    D[p] = (D.get(p, []) + int(t),)
+    D[p] = D.get(p, []) + [int(t)]
     if "t" in r:
         i += 1
         j += 20 * ~-len(D[p]) + D[p][-1]
