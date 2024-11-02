a31b14a345557eb56d6899cbd772d73a824e01a9
춤추는망고 <ensia96@gmail.com>
2024-11-02 18:04:56 +0900

    [update] 거울반사(25755) - 틀렸습니다 해결

--- baekjoon/bronze/II/거울반사(25755).py
+++ baekjoon/bronze/II/거울반사(25755).py
@@ -1,10 +1,6 @@
-d, n, *A = open(0).read().split()
-n = int(n)
+d, *A = open(0).read().split()
+n, *A = map(int, A)
 D = "?15??2??8?"
+A = [A[i * n : i * n + n] for i in range(n)]
 for i in range(n):
-    print(
-        *[
-            [D[int(A[(-i - 1) * n + j])], D[int(A[i * n - j - 1])]][d in "LR"]
-            for j in range(n)
-        ]
-    )
+    print(*[[D[int(A[-i - 1][j])], D[int(A[i][-j - 1])]][d in "LR"] for j in range(n)])
