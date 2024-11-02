373eb6265ba29e293df1baf99881a920caacfde9
춤추는망고 <ensia96@gmail.com>
2024-11-02 18:02:26 +0900

    거울반사(25755)

---
+++ baekjoon/bronze/II/거울반사(25755).py
@@ -0,0 +1,10 @@
+d, n, *A = open(0).read().split()
+n = int(n)
+D = "015??2??8?"
+for i in range(n):
+    print(
+        *[
+            [D[int(A[(-i - 1) * n + j])], D[int(A[i * n - j - 1])]][d in "LR"]
+            for j in range(n)
+        ]
+    )
