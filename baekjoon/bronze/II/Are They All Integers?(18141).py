a7a15668b06f327310abc5924920dbf42235e375
춤추는망고 <ensia96@gmail.com>
2024-11-08 19:52:43 +0900

    [update] Are They All Integers?(18141) - 코드 길이 줄이기

--- baekjoon/bronze/II/Are They All Integers?(18141).py
+++ baekjoon/bronze/II/Are They All Integers?(18141).py
@@ -3,7 +3,7 @@ N = range(n)
 print(
     "yneos"[
         any(
-            (A[i] - A[j]) / A[k] != (A[i] - A[j]) // A[k]
+            (A[i] - A[j]) % A[k]
             for i in N
             for j in N
             for k in N
