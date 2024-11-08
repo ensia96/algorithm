b5b7984855430cecdc4f6f2faf1870c988781294
춤추는망고 <ensia96@gmail.com>
2024-11-08 19:51:13 +0900

    Are They All Integers?(18141)

---
+++ baekjoon/bronze/II/Are They All Integers?(18141).py
@@ -0,0 +1,13 @@
+n, *A = map(int, open(0).read().split())
+N = range(n)
+print(
+    "yneos"[
+        any(
+            (A[i] - A[j]) / A[k] != (A[i] - A[j]) // A[k]
+            for i in N
+            for j in N
+            for k in N
+            if i != j and j != k and k != i
+        ) :: 2
+    ]
+)
