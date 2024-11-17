3a5e5fff8a7fbb3ad8da7098d579b8dbfbd1310d
춤추는망고 <ensia96@gmail.com>
2024-11-17 10:21:28 +0900

    [update] KLOPKA(10540) - 틀렸습니다 해결

--- baekjoon/bronze/II/KLOPKA(10540).py
+++ baekjoon/bronze/II/KLOPKA(10540).py
@@ -1,2 +1,2 @@
-(*A,) = map(int, open(0).read().split()[1:])
-print((max(A) - min(A)) ** 2)
+(*A,) = map(int, open(0).read().split())
+print(max(max(a := A[1::2]) - min(a), max(b := A[2::2]) - min(b)) ** 2)
