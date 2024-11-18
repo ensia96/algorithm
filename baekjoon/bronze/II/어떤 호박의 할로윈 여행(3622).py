17f9cbc3fee90385080f9563f179e8a2fe0792e2
춤추는망고 <ensia96@gmail.com>
2024-11-18 20:04:19 +0900

    [update] 어떤 호박의 할로윈 여행(3622) - 틀렸습니다 해결

--- baekjoon/bronze/II/어떤 호박의 할로윈 여행(3622).py
+++ baekjoon/bronze/II/어떤 호박의 할로윈 여행(3622).py
@@ -1,2 +1,2 @@
 A, a, B, b, P = map(int, input().split())
-print("YNeos"[1 - (A + B <= P or a >= B or b >= A) :: 2])
+print("YNeos"[1 - (A + B <= P or (P >= A and a >= B) or (P >= B and b >= A)) :: 2])
