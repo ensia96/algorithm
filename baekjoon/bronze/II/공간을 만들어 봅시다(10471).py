ae0ed4d744870c45bf0a162839eaa76437aa6b3f
춤추는망고 <ensia96@gmail.com>
2024-11-20 19:35:10 +0900

    [update] 공간을 만들어 봅시다(10471) - 코드 길이 줄이기

--- baekjoon/bronze/II/공간을 만들어 봅시다(10471).py
+++ baekjoon/bronze/II/공간을 만들어 봅시다(10471).py
@@ -1,3 +1,3 @@
 w, p, *A = map(int, open(0).read().split())
 A = [0, *A, w]
-print(*sorted({abs(A[i] - A[j]) for i in range(p + 2) for j in range(i + 1, p + 2)}))
+print(*sorted({abs(a - b) for a in A for b in A}))
