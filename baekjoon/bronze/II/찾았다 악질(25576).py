288fe5455a4a11ce0c8817063fb715ab61882f67
춤추는망고 <ensia96@gmail.com>
2024-11-21 20:21:19 +0900

    [update] 찾았다 악질(25576) - 틀렸습니다 해결

--- baekjoon/bronze/II/찾았다 악질(25576).py
+++ baekjoon/bronze/II/찾았다 악질(25576).py
@@ -2,6 +2,6 @@ n, m, *A = map(int, open(0).read().split())
 print(
     "YNEOS"[
         sum(2000 < sum(abs(A[j] - A[i * m + j]) for j in range(m)) for i in range(1, n))
-        <= n / 2 :: 2
+        <= ~-n / 2 :: 2
     ]
 )
