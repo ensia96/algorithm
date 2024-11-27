48651885322cc4e47a772495d94e814fe2f4cf5a
춤추는망고 <ensia96@gmail.com>
2024-11-27 18:20:20 +0900

    [update] 랜덤 숫자 만들기(6500) - 컴파일 에러 해결

--- baekjoon/bronze/II/랜덤 숫자 만들기(6500).py
+++ baekjoon/bronze/II/랜덤 숫자 만들기(6500).py
@@ -1,4 +1,6 @@
 for i in open(0).read().split()[:-1]:
- n,*t=int(i),
- while(n in t)^1:t+=n,;n=n**2%1e6//100
- print(len(t)
+    n, *t = (int(i),)
+    while (n in t) ^ 1:
+        t += (n,)
+        n = n**2 % 1e6 // 100
+    print(len(t))
