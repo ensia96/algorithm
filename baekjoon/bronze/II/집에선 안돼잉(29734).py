f151830af2dc03d9a4b9add5233069f519949b0d
춤추는망고 <ensia96@gmail.com>
2024-12-02 17:00:19 +0900

    [update] 집에선 안돼잉(29734) - 코드 길이 줄이기

--- baekjoon/bronze/II/집에선 안돼잉(29734).py
+++ baekjoon/bronze/II/집에선 안돼잉(29734).py
@@ -1,4 +1,4 @@
 a, b, c, d = map(int, open(0).read().split())
-T = [a + ~-a // 8 * d, c + b + ~-b // 8 * (2 * c + d)]
-print("ZDiopk"[T.index(min(T)) :: 2])
-print(min(T))
+a, b = a + ~-a // 8 * d, c + b + ~-b // 8 * (2 * c + d)
+print("ZDiopk"[a < b :: 2])
+print(min(a, b))
