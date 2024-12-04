bfc109f3b79f0b57d670d0e5d5e728c674a7dcf6
춤추는망고 <ensia96@gmail.com>
2024-12-04 21:10:19 +0900

    [update] K512에 바이러스 퍼뜨리기(31881) - 시간 초과 해결

--- baekjoon/bronze/II/K512에 바이러스 퍼뜨리기(31881).py
+++ baekjoon/bronze/II/K512에 바이러스 퍼뜨리기(31881).py
@@ -3,9 +3,11 @@ import sys
 f = lambda: [*map(int, sys.stdin.readline().split())]
 n, q = f()
 C = [0] * -~n
+s = n
 for _ in " " * q:
     a, *A = f()
     if len(A):
+        s += C[A[0]] - (a < 2)
         C[A[0]] = a < 2
     else:
-        print(n - sum(C))
+        print(s)
