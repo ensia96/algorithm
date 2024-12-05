cd523714a3692ca7e868e08508d3ba45ff0056f5
춤추는망고 <ensia96@gmail.com>
2024-12-05 19:47:48 +0900

    [update] 울타리 공사(32171) - 코드 길이 줄이기

--- baekjoon/bronze/II/울타리 공사(32171).py
+++ baekjoon/bronze/II/울타리 공사(32171).py
@@ -1,6 +1,9 @@
-n, *L = open(0)
-A, B, C, D = 10, 10, -10, -10
-for l in L:
+A = B = 9
+C = D = -9
+for l in [*open(0)][1:]:
     a, b, c, d = map(int, l.split())
-    A, B, C, D = min(A, a), min(B, b), max(C, c), max(D, d)
-    print(2 * (abs(C - A) + abs(D - B)))
+    C = max(C, c)
+    A = min(A, a)
+    D = max(D, d)
+    B = min(B, b)
+    print(C - A + D - B << 1)
