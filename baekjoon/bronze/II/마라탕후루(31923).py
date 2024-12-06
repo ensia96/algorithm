7c383d12964ff5d154c9dc81d4ff39cec77d66b5
춤추는망고 <ensia96@gmail.com>
2024-12-06 23:16:17 +0900

    [update] 마라탕후루(31923) - 틀렸습니다 해결

--- baekjoon/bronze/II/마라탕후루(31923).py
+++ baekjoon/bronze/II/마라탕후루(31923).py
@@ -3,7 +3,7 @@ A = [
     x
     for a, b in zip(A[:n], A[n:])
     if (p == q and a == b or (p > q) ^ (a > b) and ((a - b) % (q - p) < 1))
-    and (x := (a - b) // (q - p)) <= 10000
+    and (x := (a - b) // (q - p)) < 10000
 ]
 print("YNEOS"[(y := len(A) != n) :: 2])
 y or print(*A)
