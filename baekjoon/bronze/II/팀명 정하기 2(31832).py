f7548f10495fcdd137d96385d54ac18b7dcbbb33
춤추는망고 <ensia96@gmail.com>
2024-12-08 11:19:24 +0900

    [update] 팀명 정하기 2(31832) - 틀렸습니다 해결

--- baekjoon/bronze/II/팀명 정하기 2(31832).py
+++ baekjoon/bronze/II/팀명 정하기 2(31832).py
@@ -1,9 +1,9 @@
 print(
     *[
         l
-        for l in [*open(0)][1:]
-        if sum(map(str.islower, l)) > len(l) / 2 - 1
-        and len(l) < 12
-        and sum(map(str.isnumeric, l)) < len(l) - 1
+        for l in [*map(str.strip, open(0))][1:]
+        if sum(map(str.islower, l)) > sum(map(str.isupper, l))
+        and 11 > len(l)
+        and sum(map(str.isnumeric, l)) < len(l)
     ]
 )
