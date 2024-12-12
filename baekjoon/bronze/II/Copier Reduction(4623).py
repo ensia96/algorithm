30f0bf0f14bd99b49de3cca19f144e2039a88a39
춤추는망고 <ensia96@gmail.com>
2024-12-12 20:33:32 +0900

    [update] Copier Reduction(4623) - 코드 길이 줄이기

--- baekjoon/bronze/II/Copier Reduction(4623).py
+++ baekjoon/bronze/II/Copier Reduction(4623).py
@@ -1,3 +1,3 @@
 for l in [*open(0)][:-1]:
     a, b, c, d = map(int, l.split())
-    print(f"{int(min(100,max(min(c/a,d/b),min(c/b,d/a))*100))}%")
+    print(f"{int(min(1,max(min(c/a,d/b),min(c/b,d/a)))*100)}%")
