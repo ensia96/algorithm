0445f1b8323baa0e815acd5b0b42752ce2e8b875
춤추는망고 <ensia96@gmail.com>
2024-12-13 23:04:30 +0900

    Centauri Prime (Small1)(12517)

---
+++ baekjoon/bronze/II/Centauri Prime (Small1)(12517).py
@@ -0,0 +1,8 @@
+for l in map(str.strip, [*open((t := 0))][1:]):
+    t += 1
+    print(
+        f"Case #{t}:",
+        l,
+        "is ruled by",
+        [["a king", "a queen"][l[-1] in "aeiou"], "nobody"][l[-1] == "y"],
+    )
