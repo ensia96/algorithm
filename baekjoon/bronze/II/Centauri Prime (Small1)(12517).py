be6bb04b5cc2d4ccf5869b5ec99eec0ed83bff7a
춤추는망고 <ensia96@gmail.com>
2024-12-14 18:59:24 +0900

    [update] Centauri Prime (Small1)(12517) - 코드 길이 줄이기

--- baekjoon/bronze/II/Centauri Prime (Small1)(12517).py
+++ baekjoon/bronze/II/Centauri Prime (Small1)(12517).py
@@ -1,8 +1,8 @@
-for l in map(str.strip, [*open((t := 0))][1:]):
+for l in [*open((t := 0))][1:]:
     t += 1
     print(
         f"Case #{t}:",
-        l,
+        l[:-1],
         "is ruled by",
-        [["a king", "a queen"][l[-1] in "aeiou"], "nobody"][l[-1] == "y"] + ".",
+        [["a king", "a queen"][l[-2] in "aeiou"], "nobody"][l[-2] == "y"] + ".",
     )
