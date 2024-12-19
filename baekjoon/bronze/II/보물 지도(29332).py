7d77c969711c091d92bbb2b415bcb523873c3364
춤추는망고 <ensia96@gmail.com>
2024-12-19 22:29:24 +0900

    보물 지도(29332)

---
+++ baekjoon/bronze/II/보물 지도(29332).py
@@ -0,0 +1,14 @@
+i = 1e5
+n = p = i
+m = q = -i
+for l in [*open(0)][1:]:
+    x, y, d = l.split()
+    if d == "L":
+        n = min(n, int(x))
+    if d == "R":
+        m = max(m, int(x))
+    if d == "U":
+        q = max(q, int(y))
+    if d == "D":
+        p = min(p, int(y))
+print([(n - m - 1) * (p - q - 1), "Infinity"][n == p == i or m == q == -i])
