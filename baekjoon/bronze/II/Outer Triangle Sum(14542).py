899b2b118128047fb7c4ef72da6702d282083e1d
춤추는망고 <ensia96@gmail.com>
2024-12-20 20:01:16 +0900

    [update] Outer Triangle Sum(14542) - 컴파일 에러 해결

--- baekjoon/bronze/II/Outer Triangle Sum(14542).py
+++ baekjoon/bronze/II/Outer Triangle Sum(14542).py
@@ -1,5 +1,5 @@
 t = 0
 while (n := int(input())) != 0:
     print(
-        f"Case #{(t:=t+1)}:{sum([sum(T:=[*map(int,input().split())]),T[0]+(1<len(T)and T[-1])][len(T)<n]for _ in" "*n)}"
+        f"Case #{(t:=t+1)}:{sum([sum(T:=[*map(int,input().split())]),T[0]+(1<len(T)and T[-1])][len(T)<n]for _ in' '*n)}"
     )
