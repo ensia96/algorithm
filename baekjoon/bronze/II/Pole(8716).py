aef8f1c948b6ddb2713335e963329d0813272799
춤추는망고 <ensia96@gmail.com>
2024-12-28 17:47:55 +0900

    [update] Pole(8716) - 틀렸습니다 해결

--- baekjoon/bronze/II/Pole(8716).py
+++ baekjoon/bronze/II/Pole(8716).py
@@ -1,2 +1,2 @@
 a, b, c, d, e, f, g, h = map(int, open(0).read().split())
-print(abs(min(c, g) - max(a, e)) * abs(min(b, f) - max(d, h)))
+print(max(min(c, g) - max(a, e), 0) * max(min(b, f) - max(d, h), 0))
