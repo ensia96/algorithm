c90a49364d8607042c66026702472c3899e09141
춤추는망고 <ensia96@gmail.com>
2025-01-12 13:40:47 +0900

    [update] 다트판(32400) - 틀렸습니다 해결

--- baekjoon/bronze/II/다트판(32400).py
+++ baekjoon/bronze/II/다트판(32400).py
@@ -1,9 +1,3 @@
-print(
-    ["Tie", "Bob", "Alice"][
-        (
-            (x := sum((A := [*map(int, input().split())])) / 20)
-            > (y := (A[1] + A[0] + A[-1]) / 3)
-        )
-        + 2 * (x < y)
-    ]
-)
+(*A,) = map(int, input().split())
+x, y = (A[x := A.index(20)] + A[x - 1] + A[x + 1]) / 3, sum(A) / 20
+print(["TBioeb"[x < y :: 2], "Alice"][x > y])
