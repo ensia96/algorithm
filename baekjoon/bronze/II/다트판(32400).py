e83502da104ef0a93a3b5d7a4262fb4ac9f4dd73
춤추는망고 <ensia96@gmail.com>
2025-01-12 13:42:00 +0900

    [update] 다트판(32400) - 런타임 에러 (IndexError) 해결

--- baekjoon/bronze/II/다트판(32400).py
+++ baekjoon/bronze/II/다트판(32400).py
@@ -1,3 +1,3 @@
 (*A,) = map(int, input().split())
-x, y = (A[x := A.index(20)] + A[x - 1] + A[x + 1]) / 3, sum(A) / 20
+x, y = (A[x := A.index(20)] + A[x - 1] + A[-~x % 20]) / 3, sum(A) / 20
 print(["TBioeb"[x < y :: 2], "Alice"][x > y])
