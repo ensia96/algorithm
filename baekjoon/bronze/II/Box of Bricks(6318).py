c84bb597f0538b8a3bf0d02f84aa406bd0fffe6f
춤추는망고 <ensia96@gmail.com>
2025-01-13 20:58:35 +0900

    [update] Box of Bricks(6318) - 출력 형식이 잘못되었습니다 해결

--- baekjoon/bronze/II/Box of Bricks(6318).py
+++ baekjoon/bronze/II/Box of Bricks(6318).py
@@ -4,4 +4,4 @@ for l in L[1::2]:
     (*A,) = map(int, l.split())
     x = sum(A) // len(A)
     print(f"Set #{y}")
-    print(f"The minimum number of moves is {sum(abs(a-x)for a in A)//2}.")
+    print(f"The minimum number of moves is {sum(abs(a-x)for a in A)//2}.\n")
