6190256a4afef1706903ccde4ec20155f13510e0
춤추는망고 <ensia96@gmail.com>
2025-01-19 11:39:37 +0900

    [update] 가격(11296) - 틀렸습니다 해결

--- baekjoon/bronze/II/가격(11296).py
+++ baekjoon/bronze/II/가격(11296).py
@@ -2,5 +2,5 @@ H = 100
 for l in [*open(0)][1:]:
     a, b, c, d = l.split()
     print(
-        f"${round(float(a)/H*(H-5*(' WOYB G  R'.index(b)))/H*(H-5*(c=='C'))+.5,2-(d=='C')):.2f}"
+        f"${round(float(a)/H*(H-5*(' WOYB G  R'.index(b)))/H*(H-5*(c=='C')),2-(d=='C')):.2f}"
     )
