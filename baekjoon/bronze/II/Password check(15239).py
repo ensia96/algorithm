30251d5ac0073f2d813145359adbbdd27302d9b4
춤추는망고 <ensia96@gmail.com>
2025-01-30 18:10:21 +0900

    Password check(15239)

---
+++ baekjoon/bronze/II/Password check(15239).py
@@ -0,0 +1,13 @@
+for l in [*open(0)][2::2]:
+    print(
+        (
+            1
+            ^ (len(l) > 13)
+            * any(i.islower() for i in l)
+            * any(i.isupper() for i in l)
+            * any(i.isdigit() for i in l)
+            * any(i in "+_)(*&^%$#@!./,;{}" for i in l)
+        )
+        * "in"
+        + "valid"
+    )
