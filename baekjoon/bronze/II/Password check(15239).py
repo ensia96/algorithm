84fb2b71f299c1a99ee1d7438325e03a84925833
춤추는망고 <ensia96@gmail.com>
2025-01-30 18:10:55 +0900

    [update] Password check(15239) - 틀렸습니다 해결

--- baekjoon/bronze/II/Password check(15239).py
+++ baekjoon/bronze/II/Password check(15239).py
@@ -2,7 +2,7 @@ for l in [*open(0)][2::2]:
     print(
         (
             1
-            ^ (len(l) > 13)
+            ^ (len(l) > 12)
             * any(i.islower() for i in l)
             * any(i.isupper() for i in l)
             * any(i.isdigit() for i in l)
