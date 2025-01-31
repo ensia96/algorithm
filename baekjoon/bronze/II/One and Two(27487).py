27e86b0cb40cb76255c38a555a8117234fafff66
춤추는망고 <ensia96@gmail.com>
2025-01-31 18:46:16 +0900

    [update] One and Two(27487) - 틀렸습니다 해결

--- baekjoon/bronze/II/One and Two(27487).py
+++ baekjoon/bronze/II/One and Two(27487).py
@@ -1,5 +1,10 @@
 for l in [*open(0)][2::2]:
-    A = [0]
-    for i in map(int, l.split()):
-        A += (A[-1] + i,)
-    print((A[-1] // 2 in A and A.index(A[-1] // 2)) - (len(A) > 3))
+    (*A,) = map(int, l.split())
+    x = y = A.count(2)
+    if x < 2:
+        print([1, -1][x])
+    else:
+        for i in range(len(A) - 1):
+            y -= A[i] == 2
+            if x // 2 == y:
+                print(i)
