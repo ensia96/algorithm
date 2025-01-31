443b2c691bfd1994086120349aa05d6bb5d4268e
춤추는망고 <ensia96@gmail.com>
2025-01-31 18:52:05 +0900

    [update] One and Two(27487) - 틀렸습니다 해결

--- baekjoon/bronze/II/One and Two(27487).py
+++ baekjoon/bronze/II/One and Two(27487).py
@@ -1,6 +1,7 @@
-for l in [*open(i := 0)][2::2]:
+for l in [*open(0)][2::2]:
     (*A,) = map(int, l.split())
     n = A.count(2)
+    i = 0
     while A[:i].count(2) < n // 2:
         i += 1
     print([[i, -1][n % 2], 1][n < 1])
