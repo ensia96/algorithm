16864fe1455c0b7e0ea484685850bad762f46e22
춤추는망고 <ensia96@gmail.com>
2025-02-11 22:20:28 +0900

    [update] AcidBase(10182) - 틀렸습니다 해결

--- baekjoon/bronze/II/AcidBase(10182).py
+++ baekjoon/bronze/II/AcidBase(10182).py
@@ -3,4 +3,4 @@ import math
 for _ in " " * int(input()):
     a, _, b = input().split()
     x = a != "H"
-    print(14 * x + (2 * x - 1) * math.log10(float(b)))
+    print(f"{14*x+(2*x-1)*math.log10(float(b)):0.2f}")
