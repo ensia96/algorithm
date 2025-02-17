e934b1d2ed37e0577703428f5611e5c8d46df9b5
춤추는망고 <ensia96@gmail.com>
2025-02-17 21:38:00 +0900

    [update] Bookings(7598) - 틀렸습니다 해결

--- baekjoon/bronze/II/Bookings(7598).py
+++ baekjoon/bronze/II/Bookings(7598).py
@@ -1,11 +1,9 @@
-a = A = ""
-B = 0
+a = ""
 while "#" != a:
     a, b = input().split()
     b = int(b)
     if a in "X#":
         print(A, B)
-        a == "#" and exit()
     elif "B" == a:
         B += b * (B + b < 68)
     elif "C" == a:
