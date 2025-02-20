1fa4f14456ed1ae89cd8b41fa0f7fddbaf28e670
춤추는망고 <ensia96@gmail.com>
2025-02-20 19:44:42 +0900

    [update] SPC에 가는 길(32626) - 틀렸습니다 해결

--- baekjoon/bronze/II/SPC에 가는 길(32626).py
+++ baekjoon/bronze/II/SPC에 가는 길(32626).py
@@ -1,2 +1,7 @@
 a, b, c, d, e, f = map(int, open(0).read().split())
-print([[1, 2 * (b == f) * (a < e < c)][b == d], 2 * (a == e) * (b < f < d)][a == c])
+print(
+    [
+        [1, 2 * (b == f) * ((a < e < c) + (c < e < a))][b == d],
+        2 * (a == e) * ((b < f < d) + (d < f < b)),
+    ][a == c]
+)
