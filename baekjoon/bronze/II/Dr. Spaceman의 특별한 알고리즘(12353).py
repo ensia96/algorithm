b98e4cec586552564eae3961dc0abf218d8a2805
춤추는망고 <ensia96@gmail.com>
2025-02-23 20:16:33 +0900

    Dr. Spaceman의 특별한 알고리즘(12353)

---
+++ baekjoon/bronze/II/Dr. Spaceman의 특별한 알고리즘(12353).py
@@ -0,0 +1,8 @@
+for l in [*open(i := 0)][1:]:
+    i += 1
+    g, f, m = l.split()
+    a, b, c, d = map(int, [*f[:-1].split("'"), *m[:-1].split("'")])
+    x = (a * 12 + b + c * 12 + d + 5 * (1 - 2 * (g == "G"))) / 2
+    print(
+        f"Case #{i}: {int((x-4)//12//1)}'{int(-(-((x-4)%12)//1))}\" to {int((x+4)//12//1)}'{int((x+4)%12//1)}\""
+    )
