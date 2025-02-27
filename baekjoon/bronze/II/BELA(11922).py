efb78678d70970ceb8e33c159c5789fe851ec8f2
춤추는망고 <ensia96@gmail.com>
2025-02-27 21:16:23 +0900

    [update] BELA(11922) - 코드 길이 줄이기

--- baekjoon/bronze/II/BELA(11922).py
+++ baekjoon/bronze/II/BELA(11922).py
@@ -1,9 +1,9 @@
-_, b, *A = open(0).read().split()
+i, *A = open(0)
 print(
     sum(
         [11, 11, 4, 4, 3, 3, 2, 20, 10, 10, 0, 14, 0, 0, 0, 0][
-            2 * "AKQJT987".index(x) + (y == b)
+            2 * "AKQJT987".index(x) + (y == i[-2])
         ]
-        for x, y in A
+        for x, y, _ in A
     )
 )
