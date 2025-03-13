5fa36f3f77e4bff091fdd22c6d8e165c5010169a
춤추는망고 <ensia96@gmail.com>
2025-03-13 20:11:39 +0900

    [update] Ducky Debugging(20216) - 시간 초과 해결

--- baekjoon/bronze/II/Ducky Debugging(20216).py
+++ baekjoon/bronze/II/Ducky Debugging(20216).py
@@ -1,5 +1,2 @@
-for l in open(0):
-    print()
-    if "I quacked the code!\n" == l:
-        exit()
-    print("Q*uNaocdk*!"[l[-2] < "?" :: 2])
+while (l := input()) != "I quacked the code!":
+    print(["Quack!", "*Nod*"][l[-1] < "?"])
