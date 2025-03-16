98b70596674193de9d4845cf8a80ca4c9ee35d03
춤추는망고 <ensia96@gmail.com>
2025-03-16 19:33:10 +0900

    [update] Ancient Keyboard(7286) - 코드 길이 줄이기

--- baekjoon/bronze/II/Ancient Keyboard(7286).py
+++ baekjoon/bronze/II/Ancient Keyboard(7286).py
@@ -3,8 +3,6 @@ for _ in " " * int(input()):
     n = int(input())
     A = [0] * x
     for _ in " " * n:
-        t, a, b = input().split()
-        a, b = int(a), int(b)
-        for i in range(a, b):
+        for i in range(*map(int, input().split()[1:])):
             A[i] += 1
     print(*[chr(64 + A[i]) for i in range(x) if A[i]], sep="")
