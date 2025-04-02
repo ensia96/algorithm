d281f14e79e61005ead32282f4825a1864de912c
춤추는망고 <ensia96@gmail.com>
2025-04-02 19:27:42 +0900

    The Traveling Orienteerer(11113)

---
+++ baekjoon/bronze/II/The Traveling Orienteerer(11113).py
@@ -0,0 +1,14 @@
+n, *A = map(float, open(0).read().split())
+n = 2 * int(n)
+A, B = A[:n], [*map(int, A[n + 1 :])]
+while B:
+    m, *B = B
+    x, *T = map(int, B[:m])
+    S = 0
+    for t in T:
+        a, b = A[2 * x : 2 * x + 2]
+        c, d = A[2 * t : 2 * t + 2]
+        S += int((abs(c - a) ** 2 + abs(d - b) ** 2) ** 0.5)
+        x = t
+    print(S)
+    B = B[m:]
