0b8beb6704aac98aae8d3320ba3c1b52fa343da0
춤추는망고 <ensia96@gmail.com>
2025-04-08 21:21:03 +0900

    [update] 래환이의 여자친구 사귀기 대작전(32529) - 틀렸습니다 해결

--- baekjoon/bronze/II/래환이의 여자친구 사귀기 대작전(32529).py
+++ baekjoon/bronze/II/래환이의 여자친구 사귀기 대작전(32529).py
@@ -1,7 +1,7 @@
 n, m, *A = map(int, open(t := 0).read().split())
 for i in range(n - 1):
     A[-i - 2] += A[-i - 1]
-    if A[-i - 2] >= m:
-        t = n - i
+    if A[-i - 1] >= m:
+        t = n - i + 1
         break
 print(t - 1)
