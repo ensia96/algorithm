a4cce6377190926b19cb1d983ef50223981dbd5a
춤추는망고 <ensia96@gmail.com>
2025-04-09 19:46:08 +0900

    [update] 동아리비 횡령(32621) - 틀렸습니다 해결

--- baekjoon/bronze/II/동아리비 횡령(32621).py
+++ baekjoon/bronze/II/동아리비 횡령(32621).py
@@ -1,2 +1,6 @@
 A = input().split("+")
-print(["No Money", "CUTE"][2 == len(A) and A[0] == A[1] and A[0].isdigit()])
+print(
+    ["No Money", "CUTE"][
+        2 == len(A) and A[0] == A[1] and A[0].isdigit() and A[0][0] != "0"
+    ]
+)
