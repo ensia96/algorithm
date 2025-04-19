dede01246ff1a53b58324293538c29367ad2ede1
춤추는망고 <ensia96@gmail.com>
2025-04-19 20:23:16 +0900

    [update] Arrival Time(14041) - 틀렸습니다 해결

--- baekjoon/bronze/II/Arrival Time(14041).py
+++ baekjoon/bronze/II/Arrival Time(14041).py
@@ -1,6 +1,6 @@
 h, m = map(int, input().split(':'))
 t = h*60+m
 for i in range(120):
-    t += 1+(420 < t < 600)+(900 < t < 1140)
+    t += 1+(420 <= t < 600)+(900 <= t < 1140)
 h, m = divmod(t, 60)
 print(f'{h%24:02}:{m:02}')
