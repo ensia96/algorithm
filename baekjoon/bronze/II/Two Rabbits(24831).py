a8ad9e59ad5e75591b0b75a9788bdf94f4030c90
춤추는망고 <ensia96@gmail.com>
2025-04-28 19:44:29 +0900

    [update] Two Rabbits(24831) - 시간 초과 해결

--- baekjoon/bronze/II/Two Rabbits(24831).py
+++ baekjoon/bronze/II/Two Rabbits(24831).py
@@ -1,8 +1,4 @@
 for l in [*open(0)][1:]:
     x, y, a, b = map(int, l.split())
-    t = 0
-    while x < y:
-        x += a
-        y -= b
-        t += 1
-    print(-(x != y) or t)
+    t, r = divmod(y-x, a+b)
+    print(-bool(r) or t)
