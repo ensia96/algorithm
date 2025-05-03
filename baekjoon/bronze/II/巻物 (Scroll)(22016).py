de1d704ec98684a3ead44bed076c3eb446226219
춤추는망고 <ensia96@gmail.com>
2025-05-03 14:20:33 +0900

    [update] 巻物 (Scroll)(22016) - 코드 길이 줄이기

--- baekjoon/bronze/II/巻物 (Scroll)(22016).py
+++ baekjoon/bronze/II/巻物 (Scroll)(22016).py
@@ -1,4 +1,3 @@
 n, k = map(int, input().split())
 A = input()
-print(''.join([[A[i].upper(), A[i].lower()]
-      [ord(A[i]) < 97], A[i]][i < k-1]for i in range(n)))
+print(''.join([A[i].swapcase(), A[i]][i < k-1]for i in range(n)))
