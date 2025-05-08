311740fe5ff1b68a06b6c10ba36e51acfb9f2d54
춤추는망고 <ensia96@gmail.com>
2025-05-08 22:05:48 +0900

    [update] Sascha(2391) - 코드 길이 줄이기

--- baekjoon/bronze/II/Sascha(2391).py
+++ baekjoon/bronze/II/Sascha(2391).py
@@ -1,6 +1,7 @@
-for _ in ' '*int(input()):
-    t, T = input(), []
-    for j in range(int(input())):
-        w = input()
+i = input
+for _ in ' '*int(i()):
+    t, T = i(), []
+    for j in range(int(i())):
+        w = i()
         T += (sum(a != b for a, b in zip(t, w)), j, w),
     print(sorted(T)[0][2])
