23ce12944e92b664ee0f55a1a49b96131dfa52e6
춤추는망고 <ensia96@gmail.com>
2025-05-09 20:32:59 +0900

    [update] Undercut(7360) - 틀렸습니다 해결

--- baekjoon/bronze/II/Undercut(7360).py
+++ baekjoon/bronze/II/Undercut(7360).py
@@ -1,14 +1,13 @@
 *A, _ = open(0)
+t = []
 for A, B in zip(A[1::3], A[2::3]):
     x = y = 0
     for a, b in zip(A.split(), B.split()):
         a, b = int(a), int(b)
-        if a-b == 1:
-            y += (a+b)*(1+(a == 2))
-        elif b-a == 1:
-            x += (a+b)*(1+(b == 2))
-        elif a > b:
-            x += a
-        elif b < a:
-            y += b
-    print("A has", x, "points. B has", y, "points.")
+        s, u = a+b, abs(a-b) == 1
+        a, b = [[max(a, b)*(a != b), s*(1+(s == 3))]
+                [u], 0][::1-(2*(u or a < b))]
+        x += a
+        y += b
+    t += f"A has {x} points. B has {y} points.",
+print('\n\n'.join(t))
