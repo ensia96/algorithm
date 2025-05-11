6062e79d213b3557e7e4676a061fd076c003f724
춤추는망고 <ensia96@gmail.com>
2025-05-11 15:21:19 +0900

    Egg Drop(11606)

---
+++ baekjoon/bronze/II/Egg Drop(11606).py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+x = 0
+for _ in ' '*n:
+    f, s = input().split()
+    f = int(f)
+    if 'A' in s:
+        x = max(x, f)
+    else:
+        k = min(k, f)
+print(x+1, k-1)
