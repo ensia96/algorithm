394fbf93ddedf3140a7a78e9a4bd1f1c6ca8bf37
춤추는망고 <ensia96@gmail.com>
2025-05-19 13:59:59 +0900

    [update] Imena(13939) - 틀렸습니다 해결

--- baekjoon/bronze/II/Imena(13939).py
+++ baekjoon/bronze/II/Imena(13939).py
@@ -1,4 +1,6 @@
-input()
-for s in input().split('.')[:-1]:
-    print(sum(i[0].isupper() and all(map(str.islower, i[1:]))
-          for i in s.split()))
+for i in [*open(x := 0)][1].split():
+    t = i[-1] in '.?!'
+    x += i[0].isupper()*all(map(str.islower, [i[1:], i[1:-1]][t]))
+    if t:
+        print(x)
+        x = 0
