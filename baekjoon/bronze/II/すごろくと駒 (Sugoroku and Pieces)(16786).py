24bb89b14ed6c566a8d253bd49ff9a3ac2a9c494
춤추는망고 <ensia96@gmail.com>
2025-05-28 09:48:53 +0900

    [update] すごろくと駒 (Sugoroku and Pieces)(16786) - 코드 길이 줄이기

--- baekjoon/bronze/II/すごろくと駒 (Sugoroku and Pieces)(16786).py
+++ baekjoon/bronze/II/すごろくと駒 (Sugoroku and Pieces)(16786).py
@@ -1,4 +1,4 @@
 n, *A = map(int, open(0).read().split())
 for i in A[n+1:]:
-    A[i-1] += 2020 > A[i-1]+1 < ((i < n)*A[i] or 2020)
+    A[i-1] += 2020 > A[i-1]+1 < [A[i], 2020][i < n]
 print(*A[:n])
