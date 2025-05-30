b818329d95c2b51ed39772bcd1ebad52f5fc6c0e
춤추는망고 <ensia96@gmail.com>
2025-05-30 10:05:14 +0900

    [update] Znak działania(8713) - 틀렸습니다 해결

--- baekjoon/bronze/II/Znak działania(8713).py
+++ baekjoon/bronze/II/Znak działania(8713).py
@@ -1,3 +1,3 @@
 a, b, f = *map(int, input().split()), lambda x: [f"{x}", f'({x})'][x < 0]
 T = {eval(t := f(a)+i+f(b)): t for i in '+-*'}
-print(T[m := max(T)]+'='+f(m))
+print(['NIE', T[m := max(T)]+'='+f(m)][len(T) > 2])
