5d3da216bddc84aad9760715ba7830d28e41f0cd
춤추는망고 <ensia96@gmail.com>
2025-06-08 14:16:15 +0900

    [update] Bob부 멍충이(34009) - 코드 길이 줄이기

--- baekjoon/bronze/II/Bob부 멍충이(34009).py
+++ baekjoon/bronze/II/Bob부 멍충이(34009).py
@@ -1,13 +1,1 @@
-input()
-A = sorted(map(int, input().split()))[::-1]
-t = len(A)
-x = y = z = 0
-for a in A:
-    if t % 2:
-        y += a
-    else:
-        x += a
-    t -= 1
-    if x < y:
-        z = 1
-print(['Alice', 'Bob'][z or y == x])
+print(int(input()) % 2*'Bob' or 'Alice')
