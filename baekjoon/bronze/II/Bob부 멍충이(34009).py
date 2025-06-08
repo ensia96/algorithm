8fcd862d0a7a9ff513b0b26e37cb075535420210
춤추는망고 <ensia96@gmail.com>
2025-06-08 14:12:39 +0900

    Bob부 멍충이(34009)

---
+++ baekjoon/bronze/II/Bob부 멍충이(34009).py
@@ -0,0 +1,13 @@
+input()
+A = sorted(map(int, input().split()))
+t = len(A)
+x = y = z = 0
+for a in A:
+    if t % 2:
+        y += a
+    else:
+        x += a
+    t -= 1
+    if y < x:
+        z = 1
+print(['Alice', 'Bob'][z or y == x])
