7f77a34f825bb3d53572b689e130a1fd44f1b542
춤추는망고 <ensia96@gmail.com>
2025-06-12 19:47:01 +0900

    [update] Battle Simulation(13462) - 시간 초과 해결

--- baekjoon/bronze/II/Battle Simulation(13462).py
+++ baekjoon/bronze/II/Battle Simulation(13462).py
@@ -1,8 +1,6 @@
 A = input()
 i = len(A)
-t = ''
 while i > 0:
-    x = i > 2 and A[-i] != A[-i+1] != A[-i+2]
-    t += ['SKH'['RBL'.index(A[-i])], 'C'][x]
+    x = i > 2 and (A[-i] != A[-i+1] != A[-i+2])
+    print(['SKH'['RBL'.index(A[-i])], 'C'][x], end='')
     i -= 1+2*x
-print(t)
