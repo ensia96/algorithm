6ca0898b73583c940d4a93e91ab15786d19d9875
춤추는망고 <ensia96@gmail.com>
2025-06-12 10:20:27 +0900

    [update] Battle Simulation(13462) - 시간 초과 해결

--- baekjoon/bronze/II/Battle Simulation(13462).py
+++ baekjoon/bronze/II/Battle Simulation(13462).py
@@ -1,6 +1,8 @@
+A = input().translate(str.maketrans('RBL', 'SKH'))
+i = 0
 t = ''
-for a in input():
-    t += 'SKH'['RBL'.index(a)]
-    if t[-3:] in ["SKH", "SHK", "KSH", "KHS", "HSK", "HKS"]:
-        t = t[:-3]+'C'
+while i < len(A):
+    x = A[i:i+3] in ["SKH", "SHK", "KSH", "KHS", "HSK", "HKS"]
+    t += [A[i], 'C'][x]
+    i += 1+2*x
 print(t)
