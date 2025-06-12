3b30c5d67ff30f4b8957cd6ae47a469e46a474fe
춤추는망고 <ensia96@gmail.com>
2025-06-12 20:30:05 +0900

    [update] Battle Simulation(13462) - 코드 길이 줄이기

--- baekjoon/bronze/II/Battle Simulation(13462).py
+++ baekjoon/bronze/II/Battle Simulation(13462).py
@@ -2,6 +2,6 @@ A = input()
 i = 0
 n = len(A)
 while i < n:
-    x = i+2 < n and A[i] != A[i+1] != A[i+2]
+    x = len(set(A[i:i+3])) == 3
     print(['SKH'['RBL'.find(A[i])], 'C'][x], end='')
     i += 1+2*x
