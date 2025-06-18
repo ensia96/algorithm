4360144ef2ab7e1bd7fb180878369b36f1a6e80b
춤추는망고 <ensia96@gmail.com>
2025-06-18 09:48:14 +0900

    [update] Starship Hakodate-maru(9151) - 코드 길이 줄이기

--- baekjoon/bronze/II/Starship Hakodate-maru(9151).py
+++ baekjoon/bronze/II/Starship Hakodate-maru(9151).py
@@ -1,3 +1,3 @@
+R = range(97)
 for i in map(int, [*open(0)][:-1]):
-    print(max(x for j in range(54)
-          for k in range(97)if (x := j**3+(k*-~k*-~-~k)//6) <= i))
+    print(max((x := j**3+(k*-~k*-~-~k)//6)*(x <= i)for j in R for k in R))
