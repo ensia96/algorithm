1156eb28097232e0e921eaed618acce8fcbfe3a7
춤추는망고 <ensia96@gmail.com>
2025-07-04 10:01:48 +0900

    [update] Check Digits(9228) - 틀렸습니다 해결

--- baekjoon/bronze/II/Check Digits(9228).py
+++ baekjoon/bronze/II/Check Digits(9228).py
@@ -1,4 +1,4 @@
 while (A := input()) != '#':
     t = 1
-    x = 11-sum(i*(t := t+1)for i in map(int, A[::-1])) % 11
+    x = (11-sum(i*(t := t+1)for i in map(int, A[::-1])) % 11) % 11
     print(A, '->', [x, 'Rejected'][x == 10])
