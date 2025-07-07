0d35e5433512f13edd076bbfc2112093d5079619
춤추는망고 <ensia96@gmail.com>
2025-07-07 10:09:28 +0900

    [update] K-Goodness String(22999) - 틀렸습니다 해결

--- baekjoon/bronze/II/K-Goodness String(22999).py
+++ baekjoon/bronze/II/K-Goodness String(22999).py
@@ -3,4 +3,4 @@ for _ in ' '*int(input()):
     n, k = map(int, input().split())
     A = input()
     t += 1
-    print(f'Case #{t}: {max(0,k-sum(A[i]!=A[n-i-1]for i in range(n//2)))}')
+    print(f"Case #{t}:", abs(k-sum(A[i] != A[n-i-1]for i in range(n//2))))
