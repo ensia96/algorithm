6cb826ccc6f4819b7629fbadcebf5bac4ee580a4
춤추는망고 <ensia96@gmail.com>
2025-07-08 09:41:02 +0900

    [update] The Navi-Computer is Down!(4471) - 틀렸습니다 해결

--- baekjoon/bronze/II/The Navi-Computer is Down!(4471).py
+++ baekjoon/bronze/II/The Navi-Computer is Down!(4471).py
@@ -2,4 +2,4 @@ n, *A = open(0)
 for i in range(int(n)):
     a, b, c = map(float, A[4*i+1].split())
     d, e, f = map(float, A[4*i+3].split())
-    print(A[4*i][:-1], 'to', A[4*i+2][:-1], ((a-d)**2+(b-e)**2+(c-f)**2)**.5)
+    print(f"{A[4*i][:-1]} to {A[4*i+2][:-1]}: {((a-d)**2+(b-e)**2+(c-f)**2)**.5:.2f}")
