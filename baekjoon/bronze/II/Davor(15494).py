e29926aaa89001792ae8c9a44232de4c31930ccd
춤추는망고 <ensia96@gmail.com>
2025-07-11 10:43:38 +0900

    [update] Davor(15494) - 코드 길이 줄이기

--- baekjoon/bronze/II/Davor(15494).py
+++ baekjoon/bronze/II/Davor(15494).py
@@ -1,3 +1,3 @@
-n = int(input())
-print(*[[i, x//1092]for i in range(101)[::-1]
-      if (x := n-364*i) > 0 and not x % 1092][0], sep='\n')
+n = int(input())//364
+x = max(3, n-98)//3
+print(n-3*x, x)
