e9fc746ab9e9163ac3009ffca05a02dc6b073f72
춤추는망고 <ensia96@gmail.com>
2025-07-13 14:31:53 +0900

    [update] Old Fishin’ Hole(6811) - 코드 길이 줄이기

--- baekjoon/bronze/II/Old Fishin’ Hole(6811).py
+++ baekjoon/bronze/II/Old Fishin’ Hole(6811).py
@@ -1,9 +1,4 @@
-a, b, c, d = map(int, open(t := 0))
+a, b, c, d = map(int, open(0))
 R = range(101)
-for i in R:
-    for j in R:
-        for k in R:
-            if 0 < (a * i + b * j + c * k) <= d:
-                print(i, "Brown Trout,", j, "Northern Pike,", k, "Yellow Pickerel")
-                t += 1
-print("Number of ways to catch fish:", t)
+print("Number of ways to catch fish:", len([print(i, "Brown Trout,", j, "Northern Pike,",
+      k, "Yellow Pickerel")for i in R for j in R for k in R if 0 < (a*i+b*j+c*k) <= d]))
