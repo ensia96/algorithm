ca60cff9c3aa4485adeeb9f2c2e55c01fe15147c
춤추는망고 <ensia96@gmail.com>
2025-07-14 10:39:11 +0900

    [update] 보물 찾기(32068) - 48점(시간 초과) 해결

--- baekjoon/bronze/II/보물 찾기(32068).py
+++ baekjoon/bronze/II/보물 찾기(32068).py
@@ -1,9 +1,4 @@
 for _ in ' '*int(input()):
     l, r, s = map(int, input().split())
-    x = y = 1
-    while 1:
-        t, x = [(s-x, x+1), (s+x, x)][y % 2]
-        if t in [l, r]:
-            break
-        y += 1
-    print(y+1)
+    x, y = s-l, r-s
+    print([y*2, x*2+1][x < y])
