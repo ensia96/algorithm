e9aa49d3ef13b063e4612026b39a00daf7fe4456
춤추는망고 <ensia96@gmail.com>
2025-07-15 10:00:30 +0900

    [update] Cameras(13064) - 틀렸습니다 해결

--- baekjoon/bronze/II/Cameras(13064).py
+++ baekjoon/bronze/II/Cameras(13064).py
@@ -1,3 +1,3 @@
 for l in [*open(0)][1:]:
-    l[0] == l[1] and all([l[i].isdigit, l[i].isupper][i == 4]()
+    l[0] == l[1] and all([l[i] in '0123456789', l[i].isupper][i == 4]
                          for i in range(8)) and print(l[:-1])
