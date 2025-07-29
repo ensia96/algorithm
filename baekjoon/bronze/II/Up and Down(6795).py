48cb1bae846ef81ceffc3b31d6bda9a83d5f52da
춤추는망고 <ensia96@gmail.com>
2025-07-29 10:13:09 +0900

    [update] Up and Down(6795) - 코드 길이 줄이기

--- baekjoon/bronze/II/Up and Down(6795).py
+++ baekjoon/bronze/II/Up and Down(6795).py
@@ -1,26 +1,6 @@
 a, b, c, d, e = map(int, open(0))
-N = B = 0
-x, y = a, c
-while e:
-    if x > 0:
-        N += 1
-        x -= 1
-        if x == 0:
-            x = -b
-    else:
-        N -= 1
-        x += 1
-        if x == 0:
-            x = a
-    if y > 0:
-        B += 1
-        y -= 1
-        if y == 0:
-            y = -d
-    else:
-        B -= 1
-        y += 1
-        if y == 0:
-            y = c
-    e -= 1
-print(['NBiykrkoyn'[N < B::2], 'Tied'][N == B])
+x, y = divmod(e, a+b)
+n = x*(a-b)+[y, a+a-y][y > a]
+x, y = divmod(e, c+d)
+b = x*(c-d)+[y, c+c-y][y > c]
+print(["NBiykrkoyn"[b > n::2], "Tied"][n == b])
