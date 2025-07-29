2b45c7ff302dfe7e788f602a194dc02bd31a0ec6
춤추는망고 <ensia96@gmail.com>
2025-07-29 10:08:20 +0900

    [update] Up and Down(6795) - 틀렸습니다 해결

--- baekjoon/bronze/II/Up and Down(6795).py
+++ baekjoon/bronze/II/Up and Down(6795).py
@@ -4,15 +4,23 @@ x, y = a, c
 while e:
     if x > 0:
         N += 1
-        x = [x-1, -b][x == 1]
+        x -= 1
+        if x == 0:
+            x = -b
     else:
         N -= 1
-        x = [x+1, a][x == -1]
+        x += 1
+        if x == 0:
+            x = a
     if y > 0:
         B += 1
-        y = [y-1, -d][y == 1]
+        y -= 1
+        if y == 0:
+            y = -d
     else:
         B -= 1
-        y = [y+1, c][y == -1]
+        y += 1
+        if y == 0:
+            y = c
     e -= 1
 print(['NBiykrkoyn'[N < B::2], 'Tie'][N == B])
