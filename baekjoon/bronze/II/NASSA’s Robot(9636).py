b5e35aea00af593d043748d6cee647ff658b0cb2
춤추는망고 <ensia96@gmail.com>
2025-07-30 10:08:32 +0900

    [update] NASSA’s Robot(9636) - 코드 길이 줄이기

--- baekjoon/bronze/II/NASSA’s Robot(9636).py
+++ baekjoon/bronze/II/NASSA’s Robot(9636).py
@@ -1,8 +1,3 @@
 for l in [*open(0)][1:]:
-    x = y = 0
-    z = l.count('?')
-    for i in l:
-        i, j = [(-1, 0), (1, 0), (0, 1), (0, -1), (0, 0)]['LRUD'.find(i)]
-        x += i
-        y += j
-    print(x-z, y-z, x+z, y+z)
+    l, r, u, d, t = map(l.count, 'LRUD?')
+    print(r-l-t, u-d-t, r-l+t, u-d+t)
