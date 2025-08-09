866cfd93aa759b577d577ce13247b1e5e9ac5a4c
춤추는망고 <ensia96@gmail.com>
2025-08-09 12:54:21 +0900

    2c66f0f5 [update] Arithmetic Sequences(15130) - 코드 길이 줄이기

--- baekjoon/bronze/II/Arithmetic Sequences(15130).py
+++ baekjoon/bronze/II/Arithmetic Sequences(15130).py
@@ -1,4 +1,5 @@
+R = range(10)
 *A, = map(int, input().split())
-a, b = [i for i in range(10)if A[i]]
+a, b = [i for i in R if A[i]]
 x, y = divmod(A[b]-A[a], b-a)
-print(*[[A[a]-x*(a-i)for i in range(10)], [-1]][y > 0])
+print(*[[A[a]-x*(a-i)for i in R], [-1]][y > 0])
