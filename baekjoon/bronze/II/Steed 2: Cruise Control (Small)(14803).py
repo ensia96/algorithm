330524ac73def879d60c980f591e06f40ca1dbc9
춤추는망고 <ensia96@gmail.com>
2025-08-17 07:21:20 +0900

    [update] Steed 2: Cruise Control (Small)(14803) - 코드 길이 줄이기

--- baekjoon/bronze/II/Steed 2: Cruise Control (Small)(14803).py
+++ baekjoon/bronze/II/Steed 2: Cruise Control (Small)(14803).py
@@ -1,7 +1,5 @@
-for i in range(int(input())):
-    d, n = map(int, input().split())
-    A = 1e18
-    for _ in ' '*n:
-        k, s = map(int, input().split())
-        A = min(A, d/(d-k)*s)
-    print(f'Case #{i+1}:', A)
+I = input
+for i in range(int(I())):
+    d, n = map(int, I().split())
+    print(f'Case #{i+1}:', min(s*d/(d-k)
+          for k, s in [map(int, I().split())for _ in ' '*n]))
