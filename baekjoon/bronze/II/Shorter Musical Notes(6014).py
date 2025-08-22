3d7ebe1c94e78bafbfad9a6a14a820b1d618f99a
춤추는망고 <ensia96@gmail.com>
2025-08-22 09:52:41 +0900

    [update] Shorter Musical Notes(6014) - 코드 길이 줄이기

--- baekjoon/bronze/II/Shorter Musical Notes(6014).py
+++ baekjoon/bronze/II/Shorter Musical Notes(6014).py
@@ -1,7 +1,6 @@
-from bisect import bisect_right
 n, q, *L = map(int, open(0).read().split())
-A = [0]
-for a in L[:n]:
-    A += a+A[-1],
+A = []
+for i in range(n):
+    A += [i]*L[i]
 for l in L[n:]:
-    print(bisect_right(A, l))
+    print(A[l]+1)
