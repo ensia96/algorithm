8cc2fe35847f2c4bec6871c95c7d8b6eaba86621
춤추는망고 <ensia96@gmail.com>
2025-08-23 11:06:07 +0900

    [update] Jean and Joe’s Clothes(5100) - 틀렸습니다 해결

--- baekjoon/bronze/II/Jean and Joe’s Clothes(5100).py
+++ baekjoon/bronze/II/Jean and Joe’s Clothes(5100).py
@@ -1,5 +1,5 @@
 n, *L = map(str.strip, open(0))
 while n := int(n):
-    print(*map(sum, zip(*[[l in 'ML', '11' < l < 'A',
-          l < '12', l == 'S', l == 'X']for l in L[:n]])))
+    print(*map(sum, zip(*[[l in 'ML', (x := l.isdigit()) and int(l)
+          > 11, x and int(l) < 12, l == 'S', l == 'X']for l in L[:n]])))
     n, *L = L[n:]
