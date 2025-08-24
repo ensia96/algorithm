e2003debb281a7634d44077d03e916d5b7290e55
춤추는망고 <ensia96@gmail.com>
2025-08-24 17:09:28 +0900

    [update] Longest Subsequence(10202) - 코드 길이 줄이기

--- baekjoon/bronze/II/Longest Subsequence(10202).py
+++ baekjoon/bronze/II/Longest Subsequence(10202).py
@@ -1,7 +1,4 @@
 for l in [*open(0)][1:]:
-    _, *A = l.split()
-    x = y = 0
-    for a in A:
-        x = [0, x+1][a == 'X']
-        y = max(y, x)
-    print("The longest contiguous subsequence of X's is of length", y)
+    c = 0
+    print("The longest contiguous subsequence of X's is of length",
+          max(c := -~c*(i == 'X')for i in l.split()))
