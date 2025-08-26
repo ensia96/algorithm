c27654b4d74215dea23a40312faa293b240c96f5
춤추는망고 <ensia96@gmail.com>
2025-08-26 10:05:53 +0900

    Boiled Eggs(9784)

---
+++ baekjoon/bronze/II/Boiled Eggs(9784).py
@@ -0,0 +1,11 @@
+_, *A = map(int, open(i := 0).read().split())
+while A:
+    n, p, q, *A = A
+    c = w = 0
+    for a in A[:n]:
+        if c < p and w+a < q:
+            c += 1
+            w += a
+    i += 1
+    A = A[n:]
+    print(f"Case {i}:", c)
