393d10756f7aa7efa8e50eee9faf3315be6122fa
춤추는망고 <ensia96@gmail.com>
2025-08-26 10:40:31 +0900

    [update] Boiled Eggs(9784) - 코드 길이 줄이기

--- baekjoon/bronze/II/Boiled Eggs(9784).py
+++ baekjoon/bronze/II/Boiled Eggs(9784).py
@@ -1,6 +1,6 @@
-f, i = lambda: map(int, input().split()), 0
-for _ in ' '*int(*f()):
+f = lambda: map(int, input().split())
+for i in range(*f()):
     n, p, q = f()
     w = 0
-    i += 1
-    print(f"Case {i}:", min(p, sum([q >= (w := w+a)for a in sorted(f())])))
+    print(f"Case {i + 1}:",
+          min(p, sum([q >= (w := w + a)for a in sorted(f())])))
