b69cb30d02ef83a1db6424f7f1865cd0d1d7b08d
춤추는망고 <ensia96@gmail.com>
2025-08-27 09:52:11 +0900

    [update] Дробь(21412) - 틀렸습니다 해결

--- baekjoon/bronze/II/Дробь(21412).py
+++ baekjoon/bronze/II/Дробь(21412).py
@@ -1,2 +1,2 @@
 g, n = lambda a, b: b * (a == 0) or g(b % a, a), int(input())
-print(*[[i, n - i]for i in range(n // 2)if g(i, n - i) == 1][-1])
+print(*[[i, n - i]for i in range(n // 2 + 1)if g(i, n - i) == 1][-1])
