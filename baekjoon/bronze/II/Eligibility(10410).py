7b6c7b682e194aafe41c2457c585878a7f85f3e3
춤추는망고 <ensia96@gmail.com>
2025-08-30 12:20:06 +0900

    [update] Eligibility(10410) - 틀렸습니다 해결

--- baekjoon/bronze/II/Eligibility(10410).py
+++ baekjoon/bronze/II/Eligibility(10410).py
@@ -1,4 +1,4 @@
 for l in [*open(0)][1:]:
     n, s, b, c = l.split()
     print(n, [['coach petitions', "ineligible"][int(c) > 40], "eligible"]
-          [2009 < int(s.split('/')[0]) or int(b.split('/')[0]) > 1991])
+          [2009 < int(s.split('/')[0]) or int(b.split('/')[0]) > 1990])
