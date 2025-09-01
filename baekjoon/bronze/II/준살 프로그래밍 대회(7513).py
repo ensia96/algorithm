1f9d1538c3b8602bf77572a59fdaa76a9958fc21
춤추는망고 <ensia96@gmail.com>
2025-09-01 10:01:08 +0900

    [update] 준살 프로그래밍 대회(7513) - 코드 길이 줄이기

--- baekjoon/bronze/II/준살 프로그래밍 대회(7513).py
+++ baekjoon/bronze/II/준살 프로그래밍 대회(7513).py
@@ -1,9 +1,8 @@
 I = input
+P = print
 T = []
 for i in range(int(I())):
     A = [I()for _ in ' ' * int(I())]
-    t = f"Scenario #{i + 1}:"
+    P(f"Scenario #{i + 1}:")
     for _ in ' ' * int(I()):
-        t += '\n' + ''.join(A[i]for i in [*map(int, I().split())][1:])
-    T += t,
-print('\n\n'.join(T))
+        P(''.join(A[i]for i in [*map(int, I().split())][1:]))
