2983250371f2a567dfce83d24fac86da67d2bc0b
춤추는망고 <ensia96@gmail.com>
2025-09-08 10:08:53 +0900

    [update] Anagram Checker(6843) - 코드 길이 줄이기

--- baekjoon/bronze/II/Anagram Checker(6843).py
+++ baekjoon/bronze/II/Anagram Checker(6843).py
@@ -1,3 +1,2 @@
-A, B = open(0)
-print('Is', ['not ', ''][all(a == ' ' or B.count(a) == A.count(a)
-      for a in A)] + 'an anagram.')
+f = lambda: sorted(input().replace(' ', ''))
+print('Is', 'not ' * (f() != f()) + 'an anagram.')
