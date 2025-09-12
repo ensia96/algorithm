9791785a6b88d1ccc767611ee7685bcb48cec5af
춤추는망고 <ensia96@gmail.com>
2025-09-12 09:49:26 +0900

    [update] Encoding Binary Numbers(9791) - 코드 길이 줄이기

--- baekjoon/bronze/II/Encoding Binary Numbers(9791).py
+++ baekjoon/bronze/II/Encoding Binary Numbers(9791).py
@@ -2,8 +2,8 @@ for l in [*open(0)][:-1]:
     x, *l = l
     y, r = 1, ''
     for i in l:
-        if i == x:
-            y += 1
-        else:
+        t = i == x
+        if 1 ^ t:
             r, x, y = r + str(y) + x, i, 1
+        y += t
     print(r)
