c8ab7af5672c14af5aa53047acd2173d2cc9344a
춤추는망고 <ensia96@gmail.com>
2025-09-14 07:13:58 +0900

    [update] GBus count (Small)(12184) - 코드 길이 줄이기

--- baekjoon/bronze/II/GBus count (Small)(12184).py
+++ baekjoon/bronze/II/GBus count (Small)(12184).py
@@ -2,7 +2,6 @@ r, f = range, lambda: [*map(int, input().split())]
 for i in r(*f()):
     f()
     A = f()
-    T = [*zip(A[::2], A[1::2])]
-    print(f"Case #{i + 1}:", *[sum(a <= c <= b for a, b in T)
-          for c in [f()[0]for _ in r(*f())]])
+    print(f"Case #{i + 1}:", *[sum(a <= c <= b for a,
+          b in zip(A[::2], A[1::2]))for c in [f()[0]for _ in r(*f())]])
     f()
