d10a260c5cad8a95c24aea73495495f6cb9bd477
춤추는망고 <ensia96@gmail.com>
2025-09-13 11:21:44 +0900

    [update] Minions’ Master(11262) - 틀렸습니다 해결

--- baekjoon/bronze/II/Minions’ Master(11262).py
+++ baekjoon/bronze/II/Minions’ Master(11262).py
@@ -1,4 +1,4 @@
 for l in [*open(0)][1:]:
     n, *P = map(int, l.split())
     A = sum(P) / n
-    print(f"{A:.3f} {sum(p > A for p in P) * 100 / n:.3f}%")
+    print(f"{A + 1e-12:.3f} {sum(p > A for p in P) * 100 / n + 1e-12:.3f}%")
