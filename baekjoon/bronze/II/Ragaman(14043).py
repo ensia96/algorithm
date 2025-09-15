7fd0ab575f4516db51b40488c0e87f8037764d9e
춤추는망고 <ensia96@gmail.com>
2025-09-15 09:52:21 +0900

    [update] Ragaman(14043) - 틀렸습니다 해결

--- baekjoon/bronze/II/Ragaman(14043).py
+++ baekjoon/bronze/II/Ragaman(14043).py
@@ -1,2 +1,3 @@
 A, B = open(0)
-print("AN"[sum(A.count(a) > B.count(a)for a in {*A}) > B.count('*')])
+print("AN"[0 < len({*B} - {*A}) or sum(A.count(a) != B.count(a)
+      for a in {*A}) > B.count('*')])
