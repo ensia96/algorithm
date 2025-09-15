63460c5ad02eeae25186cc989742cbebfb84941e
춤추는망고 <ensia96@gmail.com>
2025-09-15 10:10:09 +0900

    [update] Ragaman(14043) - 코드 길이 줄이기

--- baekjoon/bronze/II/Ragaman(14043).py
+++ baekjoon/bronze/II/Ragaman(14043).py
@@ -1,11 +1,3 @@
 A, B = open(0)
-T = [0] * 26
-for a, b in zip(A[:-1], B):
-    T[ord(a) - 97] += 1
-    if '*' == b:
-        continue
-    T[ord(b) - 97] -= 1
-A = B.count('*')
-for i in range(26):
-    A -= (T[i] > 0) * T[i]
-print('NA'[A == 0])
+print('NA'[sum((t := A.count(a) - B.count(a)) * (t > 0)
+      for a in {*A}) == B.count('*')])
