2ea853e14f74d86a48d00ce5455697fa31e54cce
춤추는망고 <ensia96@gmail.com>
2025-09-15 10:07:12 +0900

    [update] Ragaman(14043) - 틀렸습니다 해결

--- baekjoon/bronze/II/Ragaman(14043).py
+++ baekjoon/bronze/II/Ragaman(14043).py
@@ -1,3 +1,11 @@
 A, B = open(0)
-print("AN"[0 < len({*B} - {*A}) or sum(A.count(a) != B.count(a)
-      for a in {*A}) > B.count('*')])
+T = [0] * 26
+for a, b in zip(A[:-1], B):
+    T[ord(a) - 97] += 1
+    if '*' == b:
+        continue
+    T[ord(b) - 97] -= 1
+A = B.count('*')
+for i in range(26):
+    A -= (T[i] > 0) * T[i]
+print('NA'[A == 0])
