b00828425ace6354a31e71d6954c3822938c8291
춤추는망고 <ensia96@gmail.com>
2025-09-17 09:55:50 +0900

    [update] Disastrous Doubling(14991) - 코드 길이 줄이기

--- baekjoon/bronze/II/Disastrous Doubling(14991).py
+++ baekjoon/bronze/II/Disastrous Doubling(14991).py
@@ -1,7 +1,3 @@
-_, *A = map(int, open(0).read().split())
-t = 1
-for a in A:
-    t = t * 2 - a
-    if t < 0:
-        break
-print([t % 1000000007, 'error'][t < 0])
+for i in [*open(0)][b := 1].split():
+    b = 2 * b - int(i)
+print([b % (10**9 + 7), 'error'][b < 0])
