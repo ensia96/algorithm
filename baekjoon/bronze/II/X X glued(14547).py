1874a8005e89f33cbc86116982c751db6570f5b7
춤추는망고 <ensia96@gmail.com>
2025-09-19 10:03:48 +0900

    [update] X X glued(14547) - 런타임 에러 (TypeError) 해결

--- baekjoon/bronze/II/X X glued(14547).py
+++ baekjoon/bronze/II/X X glued(14547).py
@@ -1,5 +1,5 @@
 while '#' < (l := input()):
     t = 1
     for i in {i for i, j in zip(l, l[1:])if i == j}:
-        print(*['and', i, i, 'glued'][i:])
+        print(*['and', i, i, 'glued'][t:])
         t = 0
