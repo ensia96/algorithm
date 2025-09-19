9867b19eaf52b5c0a9fc5c486fd50b116740bd6a
춤추는망고 <ensia96@gmail.com>
2025-09-19 09:59:36 +0900

    [update] X X glued(14547) - 틀렸습니다 해결

--- baekjoon/bronze/II/X X glued(14547).py
+++ baekjoon/bronze/II/X X glued(14547).py
@@ -1,4 +1,4 @@
 for l in [*open(0)][:-1]:
     _, N, _ = l.split()
     len(T := {i for i, j in zip(N, N[1:])if i == j}) and print(
-        ' and '.join(f'{t} {t} gluded'for t in sorted(T)))
+        ' and '.join(f'{t} {t} glued'for t in sorted(T)))
