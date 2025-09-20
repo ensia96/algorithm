891d2a9952cb698b35d4990916d5c3577ec839e2
춤추는망고 <ensia96@gmail.com>
2025-09-20 18:33:51 +0900

    [update] Martian Volleyball(16255) - 코드 길이 줄이기

--- baekjoon/bronze/II/Martian Volleyball(16255).py
+++ baekjoon/bronze/II/Martian Volleyball(16255).py
@@ -1,5 +1,3 @@
 for l in [*open(0)][1:]:
-    k, x, y = map(int, l.split())
-    if x > y:
-        x, y = y, x
-    print(max(2 - y + x, k - y))
+    k, *I = map(int, l.split())
+    print(max(min(I) + 2, k) - max(I))
