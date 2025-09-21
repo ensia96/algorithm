0057a8d5ddb67ce3fee64e558fa03fe367b88831
춤추는망고 <ensia96@gmail.com>
2025-09-21 12:27:57 +0900

    [update] Fifty Shades of Pink(20735) - 코드 길이 줄이기

--- baekjoon/bronze/II/Fifty Shades of Pink(20735).py
+++ baekjoon/bronze/II/Fifty Shades of Pink(20735).py
@@ -1,2 +1,2 @@
-print(sum('pink' in (t := i.lower()) or 'rose' in t for i in [
-      *open(0)][1:]) or 'I must watch Star Wars with my daughter')
+print(sum('pink' in (t := i.lower()) or 'rose' in t for i in open(0))
+      or 'I must watch Star Wars with my daughter')
