05f765c7f63ab37f4b4a1a9c3adc616ae2a7aea3
춤추는망고 <ensia96@gmail.com>
2025-09-27 08:32:01 +0900

    [update] KABISA(18403) - 코드 길이 줄이기

--- baekjoon/bronze/II/KABISA(18403).py
+++ baekjoon/bronze/II/KABISA(18403).py
@@ -1,3 +1,2 @@
 for _ in ' ' * int(input()):
-    print(*[y for y in map(int, input().split(','))if y %
-          4 < 1 or y % 400 < 1 and y % 100])
+    print(*[y for y in map(int, input().split(','))if y % 4 < 1])
