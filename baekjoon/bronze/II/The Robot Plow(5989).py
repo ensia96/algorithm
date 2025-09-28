2595a59c14bb6f41c9712d47e05b09fdb805fa58
춤추는망고 <ensia96@gmail.com>
2025-09-28 15:06:42 +0900

    [update] The Robot Plow(5989) - 코드 길이 줄이기

--- baekjoon/bronze/II/The Robot Plow(5989).py
+++ baekjoon/bronze/II/The Robot Plow(5989).py
@@ -1,10 +1,5 @@
 R = range
-l, *L = open(0)
-x, y, _ = map(int, l.split())
-D = [x * [0]for _ in ' ' * y]
-for l in L:
-    a, b, c, d = map(int, l.split())
-    for i in R(b - 1, d):
-        for j in R(a - 1, c):
-            D[i][j] = 1
-print(sum(map(sum, D)))
+s = set()
+for a, b, c, d in [map(int, l.split())for l in open(0)][1:]:
+    s |= {(i, j)for i in R(a - 1, c)for j in R(b - 1, d)}
+print(len(s))
