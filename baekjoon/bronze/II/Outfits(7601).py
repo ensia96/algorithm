3e1c330300eb56d24ec32e966071e4e062821f8e
춤추는망고 <ensia96@gmail.com>
2025-10-03 12:08:43 +0900

    [update] Outfits(7601) - 틀렸습니다 해결

--- baekjoon/bronze/II/Outfits(7601).py
+++ baekjoon/bronze/II/Outfits(7601).py
@@ -2,7 +2,8 @@ I, P, R, t = input, print, range, 1
 while '00' < (l := I()):
     n, d = map(int, l.split())
     a, b = int(I()), int(I())
-    a, b = [i for i in R(1, n + 1)if i != a], [i for i in R(n, 0, -1)if i != b]
+    a, b = [i for i in R(1, n + 1)if i !=
+            a], [i for i in R(n, 0, -1)if n - i + 1 != b]
     P("Scenario", t)
     for i in R(d):
         x, y = map(int, I().split())
