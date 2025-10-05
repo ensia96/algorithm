be1738aef26deb0f4328d3f4d8e501e95ef057c1
춤추는망고 <ensia96@gmail.com>
2025-10-05 10:06:36 +0900

    [update] Kiwis vs Kangaroos(15177) - 틀렸습니다 해결

--- baekjoon/bronze/II/Kiwis vs Kangaroos(15177).py
+++ baekjoon/bronze/II/Kiwis vs Kangaroos(15177).py
@@ -1,2 +1,2 @@
-t = sum("kangaroos".count(i) - "kiwibird".count(i)for i in input().lower())
+t = sum("kangaroo".count(i) - "kiwibird".count(i)for i in input().lower())
 print(["Feud continues", "Kiwis", "Kangaroos"][(t < 0) + 2 * (t > 0)])
