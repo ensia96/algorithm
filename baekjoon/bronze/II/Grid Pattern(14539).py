2a4b32c22d23b913a4c6e7b5b11a15ca4a6bdcb6
춤추는망고 <ensia96@gmail.com>
2025-10-07 09:50:56 +0900

    [update] Grid Pattern(14539) - 코드 길이 줄이기

--- baekjoon/bronze/II/Grid Pattern(14539).py
+++ baekjoon/bronze/II/Grid Pattern(14539).py
@@ -1,5 +1,6 @@
-i, j, k, l, m = '-|+*\n'
-for t in range(int(input())):
-    r, c, w, h = map(int, input().split())
-    print(f"Case #{t + 1}:" + ((m + (l * w).join([j] * -~c)) * h).join(
-        [m + ((i * w).join([k] * -~c))] * -~r))
+i, j, k, l, m = '-|+* '
+for L in [*open(T := 0)][1:]:
+    T += 1
+    r, c, w, h = map(int, L.split())
+    print(f'Case #{T}:', a := k + (i * w + k) * c,
+          *[((j + w * l) * c + j + m) * h + a] * r)
