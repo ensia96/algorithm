868479e4b0ce6c6cfb0f261e6bc310a371eb5f34
춤추는망고 <ensia96@gmail.com>
2025-10-07 09:42:11 +0900

    [update] Grid Pattern(14539) - 틀렸습니다 해결

--- baekjoon/bronze/II/Grid Pattern(14539).py
+++ baekjoon/bronze/II/Grid Pattern(14539).py
@@ -1,5 +1,5 @@
 i, j, k, l, m = '-|+*\n'
 for t in range(int(input())):
     r, c, w, h = map(int, input().split())
-    print(f"Case #{t + 1}:" + (m + (l *
-          w).join([j] * -~c)).join([m + ((i * w).join([k] * -~c))] * -~h))
+    print(f"Case #{t + 1}:" + ((m + (l * w).join([j] * -~c)) * r).join(
+        [m + ((i * w).join([k] * -~c))] * -~h))
