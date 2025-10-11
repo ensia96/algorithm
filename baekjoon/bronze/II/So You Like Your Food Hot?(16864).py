7c6681c32a4ce059d02da07f444cc829310b1e33
춤추는망고 <ensia96@gmail.com>
2025-10-11 16:29:49 +0900

    [update] So You Like Your Food Hot?(16864) - 틀렸습니다 해결

--- baekjoon/bronze/II/So You Like Your Food Hot?(16864).py
+++ baekjoon/bronze/II/So You Like Your Food Hot?(16864).py
@@ -1,6 +1,10 @@
 a, b, c = map(float, input().split())
 i = j = 0
-while (x := b * i) <= a:
-    (y := int((a - x) // c)) * c == (a - x) and (j := 1) and print(i, y)
+while b * i <= a:
+    x = (a - (b * i))
+    t = int(x // c)
+    if x == t * c:
+        j = 1
+        print(i, t)
     i += 1
 j or print('none')
