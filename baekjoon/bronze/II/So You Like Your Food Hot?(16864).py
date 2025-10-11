22b5604586d2a154fe3194bde533265566656e1f
춤추는망고 <ensia96@gmail.com>
2025-10-11 16:35:20 +0900

    [update] So You Like Your Food Hot?(16864) - 틀렸습니다 해결

--- baekjoon/bronze/II/So You Like Your Food Hot?(16864).py
+++ baekjoon/bronze/II/So You Like Your Food Hot?(16864).py
@@ -2,5 +2,5 @@ a, b, c, *T = map(lambda x: int((float(x) + 0.005) * 100), input().split())
 for i in range(0, a + 1, b):
     if not (a - i) % c:
         T += (i // b, (a - i) // c),
-for t in T or [('none')]:
+for t in T or [('none',)]:
     print(*t)
