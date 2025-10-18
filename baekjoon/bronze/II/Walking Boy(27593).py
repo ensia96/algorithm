b1b361a66c8048cdab9463d90c2785108ac439d7
춤추는망고 <ensia96@gmail.com>
2025-10-18 09:26:00 +0900

    [update] Walking Boy(27593) - 코드 길이 줄이기

--- baekjoon/bronze/II/Walking Boy(27593).py
+++ baekjoon/bronze/II/Walking Boy(27593).py
@@ -1,1 +1,1 @@
-for l in[*open(0)][2::2]:l=0,*map(int,l.split()),1440;print('YNEOS'[sum((j-i)//120for i,j in zip(l,l[1:]))<2::2])
+for l in[*open(0)][2::2]:l=*map(int,l.split()),1440;print('YNEOS'[sum((j-i)//120for i,j in zip([0,*l],l))<2::2])
