6cb879e15ebd735e4a6d9560fdc8629c08663e74
춤추는망고 <ensia96@gmail.com>
2025-10-19 15:42:20 +0900

    Fifty Coats of Gray(4697)

---
+++ baekjoon/bronze/II/Fifty Coats of Gray(4697).py
@@ -0,0 +1,5 @@
+I = input
+while '1' < (A := I()):
+    n, w, l, h, a, m = map(int, A.split())
+    print(-(n * (2 * (w + l) * h + w * l - sum(eval(I().replace(*' *'))
+          for _ in ' ' * m)) // -a))
