969bf537b3dab0f1bbb3b5f29d4c97c70072b1b6
춤추는망고 <ensia96@gmail.com>
2025-10-27 09:58:39 +0900

    [update] Equal Total Scores(4968) - 틀렸습니다 해결

--- baekjoon/bronze/II/Equal Total Scores(4968).py
+++ baekjoon/bronze/II/Equal Total Scores(4968).py
@@ -4,4 +4,4 @@ while S(A := [*map(int, input().split())]):
     A, B = [int(I())for _ in " " * n], [int(I())for _ in " " * m]
     x, y = S(A), S(B)
     T = [(a, b)for a in A for b in B if x - a + b == y - b + a]
-    print(-(len(T) < 1) or min(T, key=S))
+    print(*[(-1), min(T, key=S)][len(T) > 0])
