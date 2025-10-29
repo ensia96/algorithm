43fb170d2c0d50fea3ac89aaf5f5141218256038
춤추는망고 <ensia96@gmail.com>
2025-10-29 11:05:56 +0900

    [update] 후문으로(34544) - 코드 길이 줄이기

--- baekjoon/bronze/II/후문으로(34544).py
+++ baekjoon/bronze/II/후문으로(34544).py
@@ -1,5 +1,4 @@
-s = 1
-for l in [*open(0)][1:]:
+for l in [*open(s := 0)][1:]:
     a, b = map(int, l.split())
-    s += b - a - (a < 0 < b) + (a > 0 > b)
-print(s - (s < 1))
+    s -= b - a - (0 < b) + (0 < a)
+print((s < 1) - s)
