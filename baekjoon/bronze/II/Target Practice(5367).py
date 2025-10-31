06b398c16e8e73a29df9fbee90ce14562402e22f
춤추는망고 <ensia96@gmail.com>
2025-10-31 10:25:14 +0900

    [update] Target Practice(5367) - 코드 길이 줄이기

--- baekjoon/bronze/II/Target Practice(5367).py
+++ baekjoon/bronze/II/Target Practice(5367).py
@@ -1,11 +1,10 @@
+P = print
 a, b, c, d = '|-* '
 n = int(input())
-print(a + b * (n - 2) + a)
+P(x := a + b * (n - 2) + a)
 for i in range(1, n - 1):
     l = [d] * n
-    l[0] = a
-    l[-1] = a
-    l[i] = c
-    l[-i - 1] = c
-    print(''.join(l))
-print(a + b * (n - 2) + a)
+    l[0] = l[-1] = a
+    l[i] = l[~i] = c
+    P(''.join(l))
+P(x)
