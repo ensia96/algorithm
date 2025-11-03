f4e5bc15ca5a7aa181a0cab735de4ae1a6e6c382
춤추는망고 <ensia96@gmail.com>
2025-11-03 10:14:19 +0900

    [update] Close Enough Computations(4727) - 틀렸습니다 해결

--- baekjoon/bronze/II/Close Enough Computations(4727).py
+++ baekjoon/bronze/II/Close Enough Computations(4727).py
@@ -1,3 +1,3 @@
 while 0 < sum(A := [*map(int, input().split())]):
     C, f, c, p = A
-    print('yneos'[round((f * 9 + (c + p) * 4 + 1) / 10) * 10 != C::2])
+    print('yneos'[int((f * 9 + (c + p) * 4 + 5) / 10) * 10 != C::2])
