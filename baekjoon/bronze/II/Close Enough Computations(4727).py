b364288f637be4e26a63ef9b46a1dcb70dbd7802
춤추는망고 <ensia96@gmail.com>
2025-11-03 10:41:07 +0900

    [update] Close Enough Computations(4727) - 틀렸습니다 해결

--- baekjoon/bronze/II/Close Enough Computations(4727).py
+++ baekjoon/bronze/II/Close Enough Computations(4727).py
@@ -1,4 +1,4 @@
 while 0 < sum(A := [*map(int, input().split())]):
     C, f, c, p = A
-    print('yneos'[(9 * max(f - .5, 0) + 4 * max(c - .5, 0) + 4 *
-          max(p - .5, 0)) > C < 9 * (f + .5) + 4 * (c + p + 1)::2])
+    print('yneos'[not ((9 * max(f - .5, 0) + 4 * (max(c - .5, 0) +
+          max(p - .5, 0))) <= C <= 9 * (f + .5) + 4 * (c + p + 1))::2])
