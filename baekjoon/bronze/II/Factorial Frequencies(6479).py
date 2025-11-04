ea1ea7b184544f8e2cd64fea73dc3ec86063e957
춤추는망고 <ensia96@gmail.com>
2025-11-04 10:13:00 +0900

    Factorial Frequencies(6479)

---
+++ baekjoon/bronze/II/Factorial Frequencies(6479).py
@@ -0,0 +1,9 @@
+I = input
+P = print
+while (n := int(I())) > 0:
+    I()
+    t = 1
+    for i in range(n):
+        t *= i + 1
+    T = [f"({i})" + str(str(t).count(i)).rjust(5)for i in '0123456789']
+    P(f"{n}! --\n   {'    '.join(T[:5])} \n   {'    '.join(T[5:])} ")
