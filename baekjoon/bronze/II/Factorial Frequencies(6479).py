1b71164664135180eb213ce94b114ebed92db4b0
춤추는망고 <ensia96@gmail.com>
2025-11-04 10:20:48 +0900

    [update] Factorial Frequencies(6479) - 코드 길이 줄이기

--- baekjoon/bronze/II/Factorial Frequencies(6479).py
+++ baekjoon/bronze/II/Factorial Frequencies(6479).py
@@ -4,5 +4,5 @@ while (n := int(I())) > 0:
     t = 1
     for i in range(n):
         t *= i + 1
-    T = [f"({i})" + str(str(t).count(i)).rjust(5)for i in '0123456789']
+    T = [f"({i}){str(t).count(i):5}"for i in '0123456789']
     print(f"{n}! --\n   {'    '.join(T[:5])} \n   {'    '.join(T[5:])} ")
