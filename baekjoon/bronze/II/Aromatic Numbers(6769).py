d46357e48910b40196d6fdcc952806030a440f67
춤추는망고 <ensia96@gmail.com>
2025-11-05 10:20:29 +0900

    [update] Aromatic Numbers(6769) - 코드 길이 줄이기

--- baekjoon/bronze/II/Aromatic Numbers(6769).py
+++ baekjoon/bronze/II/Aromatic Numbers(6769).py
@@ -1,4 +1,4 @@
 l = input()
 R = [[1, 5, 10, 50, 100, 500, 1000]['IVXLCDM'.index(i)]for i in l[1::2]]
-print(sum(int(l[2 * i]) * R[i] * (1 - 2 * (2 * -~i < len(l)
-      and R[i] < R[i + 1]))for i in range(len(l) // 2)))
+print(sum(R[i] * int(l[i * 2]) * (1 - 2 * (i * 2 < len(l) -
+      2 != R[i] < R[i + 1]))for i in range(len(l) // 2)))
