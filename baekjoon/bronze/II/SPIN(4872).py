e6a794e039ee04394780b8adbe2c9a70876315cd
춤추는망고 <ensia96@gmail.com>
2025-11-10 10:19:57 +0900

    [update] SPIN(4872) - 코드 길이 줄이기

--- baekjoon/bronze/II/SPIN(4872).py
+++ baekjoon/bronze/II/SPIN(4872).py
@@ -1,5 +1,2 @@
-l, *L = open(0)
-*D, = map(int, l[:-1])
-for l in L:
-    *D, = map(sum, zip(D, map(int, l)))
-print(*[d % 10 for d in D], sep='')
+for i in [*zip(*open(0))][:-1]:
+    print(sum(map(int, i)) % 10, end='')
