46d26c9a922692bed3ccc4d854ad9ed231f0b557
춤추는망고 <ensia96@gmail.com>
2025-11-16 13:24:37 +0900

    [update] Ljeto(23401) - 부분 정답(25점) 해결

--- baekjoon/bronze/II/Ljeto(23401).py
+++ baekjoon/bronze/II/Ljeto(23401).py
@@ -1,6 +1,7 @@
+I = input
 n = m = x = y = 0
-for l in [*open(0)][1:]:
-    t, a, b = map(int, l.split())
+for _ in ' ' * int(I()):
+    t, a, b = map(int, I().split())
     z = 100 + 50 * (x == a and y + 10 >= t)
     n, m, x, y = n + z * (a < 5), m + z * (a > 4), a, t
 print(n, m)
