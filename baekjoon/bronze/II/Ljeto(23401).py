8134ec9065a8aa72535169aca938904e01a1ff52
춤추는망고 <ensia96@gmail.com>
2025-11-16 13:43:22 +0900

    [update] Ljeto(23401) - 코드 길이 줄이기

--- baekjoon/bronze/II/Ljeto(23401).py
+++ baekjoon/bronze/II/Ljeto(23401).py
@@ -1,6 +1,7 @@
-G = [[]for _ in range(8)]
+S = [0] * 2
+T = [-9] * 9
 for l in [*open(0)][1:]:
-    t, a, b = map(int, l.split())
-    G[a - 1] += t,
-G = [100 * len(g) + 50 * sum(j - i <= 10 for i, j in zip(g, g[1:]))for g in G]
-print(sum(G[:4]), sum(G[4:]))
+    t, a, _ = map(int, l.split())
+    S[a > 4] += 100 + (t < T[a] + 11) * 50
+    T[a] = t
+print(*S)
