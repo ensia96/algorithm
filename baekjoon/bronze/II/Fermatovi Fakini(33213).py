3ead99e233c57cc7e3eabd36eb46dd7b1f513aea
춤추는망고 <ensia96@gmail.com>
2025-11-19 10:07:02 +0900

    [update] Fermatovi Fakini(33213) - 틀렸습니다 해결

--- baekjoon/bronze/II/Fermatovi Fakini(33213).py
+++ baekjoon/bronze/II/Fermatovi Fakini(33213).py
@@ -1,7 +1,9 @@
+I = input
+I()
 G = [set()] * 2
-for a in map(int, [*open(0)][1].split()):
+for a in map(int, I().split()):
     G[a % 2].add(a)
 G = max(G, key=len)
-for i in range([2, 1][max(G) % 2], max(G) + 3, 2):
-    if i not in G:
-        exit(print(i))
+t = max(G) % 2
+for i in range(1, 10**6):
+    i not in G and i % 2 == t and exit(print(i))
