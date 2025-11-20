5f12166bbf8632377efb3b121ce4816ee378e518
춤추는망고 <ensia96@gmail.com>
2025-11-20 10:12:24 +0900

    [update] Mia(24765) - 코드 길이 줄이기

--- baekjoon/bronze/II/Mia(24765).py
+++ baekjoon/bronze/II/Mia(24765).py
@@ -1,6 +1,6 @@
-f = lambda x, y: (((x * y == 2) * 10 + (x == y) * x)
-                  * 10 + max(x, y)) * 10 + min(x, y)
+f = lambda x, y: (((x * y == 2) * 2 + (x == y)) *
+                  10 + max(x, y)) * 10 + min(x, y)
 while '1' < (l := input()):
-    a, b, c, d = map(int, l.split())
-    x, y = f(a, b), f(c, d)
+    a, b, *c = map(int, l.split())
+    x, y = f(a, b), f(*c)
     print([f'Player {1 + (y > x)} wins.', 'Tie.'][x == y])
