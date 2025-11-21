fdf26c3f6dd24272d99e7cd7cb82d05edba6099f
춤추는망고 <ensia96@gmail.com>
2025-11-21 09:46:31 +0900

    [update] Chess Puzzle(5239) - 코드 길이 줄이기

--- baekjoon/bronze/II/Chess Puzzle(5239).py
+++ baekjoon/bronze/II/Chess Puzzle(5239).py
@@ -1,3 +1,3 @@
 for l in [*open(0)][1:]:
-    n, *R = map(int, l.split())
-    print(*["NOT", "SAFE"][len({*R[::2]}) == n == len({*R[1::2]}):])
+    n, *R = l.split()
+    print('NOT ' * any(int(n) - len({*R[i::2]})for i in [0, 1]) + 'SAFE')
