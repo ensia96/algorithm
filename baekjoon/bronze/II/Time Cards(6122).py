190c569c7411fb10f72391aeda5d0e04b0fff3a0
춤추는망고 <ensia96@gmail.com>
2025-11-27 09:53:12 +0900

    [update] Time Cards(6122) - 런타임 에러 (IndexError) 해결

--- baekjoon/bronze/II/Time Cards(6122).py
+++ baekjoon/bronze/II/Time Cards(6122).py
@@ -3,7 +3,7 @@ c, n = map(int, I().split())
 C = [[]for _ in ' ' * c]
 for _ in ' ' * n:
     c, a, h, m = I().split()
-    C[int(c)] += int(h) * 60 + int(m),
+    C[int(c) - 1] += int(h) * 60 + int(m),
 for c in C:
     w = sum(e - s for s, e in zip(c[::2], c[1::2] + [1440]))
     print(w // 60, w % 60)
