157cabd48150035b670c1cab3c853c9c44cbb7fc
춤추는망고 <ensia96@gmail.com>
2025-11-30 13:54:28 +0900

    [update] Spelling Bee(24820) - 틀렸습니다 해결

--- baekjoon/bronze/II/Spelling Bee(24820).py
+++ baekjoon/bronze/II/Spelling Bee(24820).py
@@ -1,3 +1,2 @@
 n, *A = open(0)
-print(*[a[:-1]for a in A if 1 > len({*a} - {*n})
-      and 3 < len(a) and n[0] in a], sep='\n')
+print(*[a[:-1]for a in A if 1 > len({*a} - {*n}) and 4 < len(a) and n[0] in a])
