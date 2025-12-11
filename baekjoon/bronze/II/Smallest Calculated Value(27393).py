e64f63da71e976cf3aeb476af9268ec043c315ba
춤추는망고 <ensia96@gmail.com>
2025-12-11 09:54:06 +0900

    [update] Smallest Calculated Value(27393) - 틀렸습니다 해결

--- baekjoon/bronze/II/Smallest Calculated Value(27393).py
+++ baekjoon/bronze/II/Smallest Calculated Value(27393).py
@@ -1,3 +1,3 @@
 a, b, c = map(int, input().split())
-print(min(j for x in [a + b, a - b, a * b, a / b]if x % 1 ==
+print(min(int(j) for x in [a + b, a - b, a * b, a / b]if x % 1 ==
       0 for j in [x + c, x - c, x * c, x / c]if j % 1 == 0 and j >= 0))
