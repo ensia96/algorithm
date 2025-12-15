00c9be77777511f82e8c95a152e86eb0457d6bf9
춤추는망고 <ensia96@gmail.com>
2025-12-15 10:10:37 +0900

    [update] Letterle(24747) - 틀렸습니다 해결

--- baekjoon/bronze/II/Letterle(24747).py
+++ baekjoon/bronze/II/Letterle(24747).py
@@ -1,5 +1,5 @@
 A, *L = open(0)
-for l in L:
-    C = ''.join('XYG'[2 * (A[i] == l[i]) or l[i] in A]for i in range(5))
-    print([C, 'WINNER'][f := C == 'GGGGG'])
-    f and exit()
+L = [''.join('XYG'[2 * (A[i] == l[i]) or l[i] in A]
+             for i in range(5))for l in L]
+print(*L[:L.index('GGGGG')if (A := 'GGGGG' in L)else -1],
+      'WLIONSNEERR'[1 - A::2])
