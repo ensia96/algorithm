f76b2879027e23eb88fa31956102a2f00df6bd29
춤추는망고 <ensia96@gmail.com>
2025-12-16 09:56:21 +0900

    [update] Picture Perfect(6904) - 틀렸습니다 해결

--- baekjoon/bronze/II/Picture Perfect(6904).py
+++ baekjoon/bronze/II/Picture Perfect(6904).py
@@ -1,4 +1,4 @@
-while (n := int(input())):
+while n := int(input()):
     i = max(i * (1 > n % i)for i in range(1, int(n**.5) + 1))
-    print('Minimum perimeter is', 2 * (i // n + n),
-          'with dimensions', n, 'x', i // n)
+    print('Minimum perimeter is', 2 * (n // i + i),
+          'with dimensions', i, 'x', n // i)
