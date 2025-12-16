59515885b033bdd4856182fce8fa7795a2095aae
춤추는망고 <ensia96@gmail.com>
2025-12-16 09:40:50 +0900

    Picture Perfect(6904)

---
+++ baekjoon/bronze/II/Picture Perfect(6904).py
@@ -0,0 +1,4 @@
+while (n := int(input())):
+    p, x, y = min((2 * (i + n // i), i, n // i)
+                  for i in range(1, n)if 1 > n % i)
+    print("Minimum perimeter is", p, "with dimensions", x, "x", y)
