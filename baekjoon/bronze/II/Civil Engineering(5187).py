73c8bee0337116faaa2f82624227e7f5d57c5046
춤추는망고 <ensia96@gmail.com>
2025-12-21 13:43:10 +0900

    [update] Civil Engineering(5187) - 코드 길이 줄이기

--- baekjoon/bronze/II/Civil Engineering(5187).py
+++ baekjoon/bronze/II/Civil Engineering(5187).py
@@ -1,11 +1,12 @@
 I = input
 P = print
-for t in range(int(I())):
-    m, n = map(int, I().split())
+f = lambda: map(int, I().split())
+for t in range(*f()):
+    m, n = f()
     M = [int(I())for _ in ' ' * m]
     A = 0
     for _ in ' ' * n:
-        h, w, d, i = map(int, I().split())
+        h, w, d, i = f()
         A += h * w * d * M[i - 1]
     P(f'Data Set {t + 1}:')
     P(A)
