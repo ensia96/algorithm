a90f063a0b364bb02ce838696e7ec784829b60a6
춤추는망고 <ensia96@gmail.com>
2025-12-22 10:37:11 +0900

    [update] Googol String (Small)(12074) - 틀렸습니다 해결

--- baekjoon/bronze/II/Googol String (Small)(12074).py
+++ baekjoon/bronze/II/Googol String (Small)(12074).py
@@ -1,6 +1,5 @@
-a = '0'
-while len(a) < 1e5:
-    a += '0' + ''.join('01'[x == '0']for x in a[-2::-1])
+a = ''
+while len(a) < 10:
+    a += '0' + ''.join('01'[x == '0']for x in a[-1::-1])
 for l in [*open(t := 0)][1:]:
-    t += 1
-    print(f"Case #{t}:", a[int(l)])
+    print(f"Case #{t + 1}:", a[int(l)])
