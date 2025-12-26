0bc2f81c6fc537d185f610f5195f671a5ba2ad0b
춤추는망고 <ensia96@gmail.com>
2025-12-27 08:43:33 +0900

    [update] ПОДАРЪК(31262) - 코드 길이 줄이기

--- baekjoon/bronze/II/ПОДАРЪК(31262).py
+++ baekjoon/bronze/II/ПОДАРЪК(31262).py
@@ -1,3 +1,2 @@
-A = input()
-print(*[a + b for a, b in zip(sorted(filter(str.isupper, A)),
-      sorted(filter(str.isdigit, A))[::-1])], sep='')
+a, b, c, d, e, f = sorted(input())
+print(d + c + e + b + f + a)
