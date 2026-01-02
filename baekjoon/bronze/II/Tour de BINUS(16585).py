4dfbd29b5984de2457cdfbe493b960f8943607c6
춤추는망고 <ensia96@gmail.com>
2026-01-02 12:12:30 +0900

    [update] Tour de BINUS(16585) - 컴파일 에러 해결

--- baekjoon/bronze/II/Tour de BINUS(16585).py
+++ baekjoon/bronze/II/Tour de BINUS(16585).py
@@ -4,4 +4,4 @@ a, x = A.split()
 a = int(a)
 b, y = B.split()
 b = int(b)
-print(sum([R[a - 1:], R[:a]]['l' in x]), [R[b - 1:], R[:b]]['l' in y].count(0))f
+print(sum([R[a - 1:], R[:a]]['l' in x]), [R[b - 1:], R[:b]]['l' in y].count(0))
