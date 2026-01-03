0d975192a650310287b04beb53b56d0b64f3e8f3
춤추는망고 <ensia96@gmail.com>
2026-01-03 14:40:24 +0900

    [update] Alarmist(5121) - 런타임 에러 (ValueError) 해결

--- baekjoon/bronze/II/Alarmist(5121).py
+++ baekjoon/bronze/II/Alarmist(5121).py
@@ -3,6 +3,6 @@ A = []
 for t in range(*f()):
     n, w = f()
     *S, = f()
-    m, *S, M = sorted(sum(S[i:i + w]) // w for i in range(n - w + 1))
-    A += f"Data Set {t + 1}:\n{M - m}",
+    S = sorted(sum(S[i:i + w]) // w for i in range(n - w + 1))
+    A += f"Data Set {t + 1}:\n{S[-1] - S[0]}",
 print(*A, sep="\n\n")
