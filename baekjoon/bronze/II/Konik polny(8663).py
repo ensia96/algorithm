cfad5f4cf368c02f57af31c47d2f3b6bc1f608c9
춤추는망고 <ensia96@gmail.com>
2026-01-06 09:41:41 +0900

    [update] Konik polny(8663) - 코드 길이 줄이기

--- baekjoon/bronze/II/Konik polny(8663).py
+++ baekjoon/bronze/II/Konik polny(8663).py
@@ -1,7 +1,7 @@
 x, s = map(int, input().split())
-a = b = 0
-while b < x and s > 1:
-    b += s
-    a += 1
-    s >>= 1
-print(a + max(0, x - b))
+a = 0
+while s:
+    a += x > 0
+    x -= s
+    s //= 2
+print(a + x * (x > 0))
