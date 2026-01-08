d8fb16c733f8a30a79fc1abb084d7efc0bc3a1d1
춤추는망고 <ensia96@gmail.com>
2026-01-08 09:52:40 +0900

    [update] Word Swap(10193) - 틀렸습니다 해결

--- baekjoon/bronze/II/Word Swap(10193).py
+++ baekjoon/bronze/II/Word Swap(10193).py
@@ -1,5 +1,5 @@
 for l in [*open(0)][1:]:
     A, B = l.split()
     x = sum(ord(a) - ord(b)for a, b in zip(A, B))
-    print("Swapping letters to make", A, "look like",
-          B, ["was FREE.", f"earned {x} coins."][x > 0])
+    print("Swapping letters to make", A, "look like", B, [
+          f"{['cost', 'earned'][x > 0]} {abs(x)} coins.", "was FREE."][x == 0])
