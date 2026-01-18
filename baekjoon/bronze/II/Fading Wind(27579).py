7985f7ecbcfb08d095c531e7f7268ca55e14f5c6
춤추는망고 <ensia96@gmail.com>
2026-01-18 12:33:50 +0900

    [update] Fading Wind(27579) - 코드 길이 줄이기

--- baekjoon/bronze/II/Fading Wind(27579).py
+++ baekjoon/bronze/II/Fading Wind(27579).py
@@ -1,4 +1,4 @@
 h,k,v,s=map(int,input().split())
 x=0
-while h:v+=s;v-=v//10or 1;h+=v>=k;h-=0<v<k;v*=(h>=1or v>=k)*(v>0);h*=v>0;x+=v;s-=s>0
+while h:v+=s;v-=v//10or 1;h=(v>0)*(h+(v>=k)-(v<k));x+=v*(h>0);s-=s>0
 print(x)
