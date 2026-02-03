28f062feca195b687cbc25f92327950eb0745b0c
춤추는망고 <ensia96@gmail.com>
2026-02-03 09:49:48 +0900

    [update] Ascending hike(31448) - 틀렸습니다 해결

--- baekjoon/bronze/II/Ascending hike(31448).py
+++ baekjoon/bronze/II/Ascending hike(31448).py
@@ -1,2 +1,2 @@
-*A, = map(int, open(x := 0).read().split())
-print(max(x := (x - i + j) * (i < j)for i, j in zip(A, A[1:])))
+n, *A = map(int, open(x := 0).read().split())
+print(max([x := (x + j - i) * (j > i)for i, j in zip(A, A[1:])]))
