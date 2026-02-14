850d58151ef2e074c9abec7a035e55da5cbeb7d4
춤추는망고 <ensia96@gmail.com>
2026-02-14 18:09:30 +0900

    [update] New Password(26416) - 코드 길이 줄이기

--- baekjoon/bronze/II/New Password(26416).py
+++ baekjoon/bronze/II/New Password(26416).py
@@ -1,6 +1,6 @@
 for l in [*open(t := 0)][2::2]:
     l = l[:-1]
     t += 1
-    l += "A"[l != l.lower():] + "a"[l != l.upper():] + \
+    l += "A"[l < l.lower():] + "a"[l > l.upper():] + \
         "0"[any(map(str.isdigit, l)):] + "#"[len({*l} & {*"#@*&"}):]
-    print(f"Case #{t}:", l + "a" * (7 - len(l)))
+    print(f"Case #{t}: {l:a<7}")
