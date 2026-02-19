0acb8a1725732e3a458574bea6efe8abbc83fd8a
춤추는망고 <ensia96@gmail.com>
2026-02-19 10:32:03 +0900

    [update] Gholam’s Simple Game(9387) - 런타임 에러 (IndexError) 해결

--- baekjoon/bronze/II/Gholam’s Simple Game(9387).py
+++ baekjoon/bronze/II/Gholam’s Simple Game(9387).py
@@ -3,4 +3,5 @@ for I, J in zip(*[iter(L)] * 2):
     m, n = map(int, I.split())
     *A, = map(int, J.split())
     i, j = A.index(a := max(A)), 5 - 2 * a
-    print(sum(0 == A[(i := i + (j := [j, -j][i % ~-m < 1]))]for _ in '_' * n))
+    print(sum(0 == A[(i := i + (j := [-j, j][i + j in range(m)]))]
+          for _ in '_' * n))
