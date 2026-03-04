1c89e8ba096e8981e14cf6748461c741903478af
춤추는망고 <ensia96@gmail.com>
2026-03-04 10:25:15 +0900

    [update] Map Interface(5149) - 컴파일 에러 해결

--- baekjoon/bronze/II/Map Interface(5149).py
+++ baekjoon/bronze/II/Map Interface(5149).py
@@ -2,4 +2,5 @@ f = lambda: [*map(int, input().split())]
 for i in range(*f()):
     A = [f()for _ in ' ' * f()[0]]
     X, Y = zip(*[A[b - 1]for b in f()])
-    print(f"Data Set {i + 1}:\n{sum(min(X) <= a <= max(X) and min(Y) <= b <= max(Y)for a, b in A)}\n")Z
+    print(
+        f"Data Set {i + 1}:\n{sum(min(X) <= a <= max(X) and min(Y) <= b <= max(Y)for a, b in A)}\n")
