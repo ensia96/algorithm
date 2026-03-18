c9dc4169b903667e96ecb8767ad88aa01f72f4bf
춤추는망고 <ensia96@gmail.com>
2026-03-18 10:40:22 +0900

    [update] Fix(6892) - 틀렸습니다 해결

--- baekjoon/bronze/II/Fix(6892).py
+++ baekjoon/bronze/II/Fix(6892).py
@@ -1,2 +1,2 @@
 exec(
-    "W=eval('input(),'*3);print('YNeos'[any(x in(y[:len(x)],y[-len(x):])for x in W for y in W if x!=y)::2]);" * int(input()))
+    "W=eval('input(),'*3);print('YNeos'[any((x:=W[i])in(W[j][:len(x)],W[j][-len(x):])for i in(0,1,2)for j in(0,1,2)if i-j)::2]);" * int(input()))
