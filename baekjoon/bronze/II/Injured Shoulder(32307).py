fc4334a536baaba8f2814a2eb0b3eaddf6ca95c4
춤추는망고 <ensia96@gmail.com>
2026-03-28 19:06:06 +0900

    [update] Injured Shoulder(32307) - 코드 길이 줄이기

--- baekjoon/bronze/II/Injured Shoulder(32307).py
+++ baekjoon/bronze/II/Injured Shoulder(32307).py
@@ -1,4 +1,5 @@
-I = input
-d = {*eval('I(),' * int(I()))}
-D = {i + j for i in d for j in d}
-exec("print(+((w:=I())in d or 2*(w in D)));" * int(I()))
+n, *a = open(0)
+n = int(n)
+for i in a[n + 1:]:
+    print(+(i in a[:n])
+          or (i in {x[:-1] + y for x in a[:n]for y in a[:n]}) * 2)
