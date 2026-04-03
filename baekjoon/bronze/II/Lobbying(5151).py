d14bd8e774682ae67a92931d05ab0fac8fc2fdea
춤추는망고 <ensia96@gmail.com>
2026-04-03 09:31:00 +0900

    [update] Lobbying(5151) - 메모리 초과 해결

--- baekjoon/bronze/II/Lobbying(5151).py
+++ baekjoon/bronze/II/Lobbying(5151).py
@@ -1,8 +1,11 @@
 I = input
 for D in range(int(I())):
     n, m, T = map(int, I().split())
-    N = [0.] * n
-    exec("i,t,d=I().split();N[int(i)-1]+=eval(d)*(-1<T-int(t)<1e3);" * m)
+    N = [0] * n
+    while m:
+        m -= 1
+        i, t, d = I().split()
+        N[int(i) - 1] += eval(d) * (-1 < T - int(t) < 1e3)
     N = [I() > 'N' or -1 / (1 + d / 1e4)for d in N]
     y = N.count(1)
     print('Data Set %d:\n%.2f %.2f\n' % (-~D, y, y - sum(N)))
