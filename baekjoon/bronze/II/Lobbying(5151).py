8d7b64a00f91b8fd9e45786dead592f075a60e7e
춤추는망고 <ensia96@gmail.com>
2026-04-03 16:59:00 +0900

    [update] Lobbying(5151) - 코드 길이 줄이기

--- baekjoon/bronze/II/Lobbying(5151).py
+++ baekjoon/bronze/II/Lobbying(5151).py
@@ -1,11 +1,15 @@
+f = lambda: map(eval, I().split())
 I = input
-for D in range(int(I())):
-    n, m, T = map(int, I().split())
-    N = [0] * n
+Y, N = 0, 1
+for T in range(*f()):
+    n, m, t = f()
+    a = [0] * n
+    v = [0] * 2
     while m:
         m -= 1
-        i, t, d = I().split()
-        N[int(i) - 1] += eval(d) * (-1 < T - int(t) < 1e3)
-    N = [I() > 'N' or -1 / (1 + d / 1e4)for d in N]
-    y = N.count(1)
-    print('Data Set %d:\n%.2f %.2f\n' % (-~D, y, y - sum(N)))
+        i, x, d = f()
+        a[i - 1] += (-1 < t - x < 1e3) * d
+    for i in a:
+        b, = f()
+        v[b] += 1 / (1 + b * i / 1e4)
+    print('Data Set %d: %.2f %.2f\n' % (T + 1, *v))
