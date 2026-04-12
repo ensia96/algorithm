ae85e8331b10c713ff707b50d0555677a5ff2da9
춤추는망고 <ensia96@gmail.com>
2026-04-12 17:55:21 +0900

    [update] Welcome Sign(32841) - 코드 길이 줄이기

--- baekjoon/bronze/II/Welcome Sign(32841).py
+++ baekjoon/bronze/II/Welcome Sign(32841).py
@@ -1,6 +1,6 @@
 _, r, *A = open(t := 0).read().split()
-r = int(r)
 for a in A:
-    d, m = divmod(r - len(a), 2)
-    print("." * (d + m * t) + a + "." * (d + m - m * t))
-    t ^= m
+    x = int(r) - len(a)
+    l = x + t >> 1
+    print('.' * l + a + '.' * (x - l))
+    t ^= x % 2
