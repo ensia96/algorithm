368b7eac12c9d7fc34e698714112d49d2978d3c5
춤추는망고 <ensia96@gmail.com>
2026-04-16 09:24:28 +0900

    [update] Черно-белая графика(21675) - 코드 길이 줄이기

--- baekjoon/bronze/II/Черно-белая графика(21675).py
+++ baekjoon/bronze/II/Черно-белая графика(21675).py
@@ -1,4 +1,3 @@
 _, *L, O = open(0)
-n = len(L) // 2
-for a, b in zip(L[:n], L[n:]):
-    print("".join(O[int(i + j, 2)]for i, j in zip(a, b)if i > "/"))
+for i, j in zip(L, L[len(L) // 2:]):
+    print(*[O[int(x + y, 2)]for x, y in zip(i, j[:-1])], sep='')
