fcbf0f32971cef418fbd38b2c6819c6d049db327
춤추는망고 <ensia96@gmail.com>
2026-09-04 09:25:10 +0900

    607. Sales Person

---
+++ leetcode/607. Sales Person.py
@@ -0,0 +1,13 @@
+'''
+# Write your MySQL query statement below
+SELECT sp.name
+FROM SalesPerson AS sp
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Orders AS o
+    JOIN Company AS c
+      ON c.com_id = o.com_id
+    WHERE o.sales_id = sp.sales_id
+      AND c.name = 'RED'
+);
+'''
