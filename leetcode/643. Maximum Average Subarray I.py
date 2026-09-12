aa03875438a6911cfa1cc2eacd5bcb20d4b34e15
춤추는망고 <ensia96@gmail.com>
2026-09-12 13:43:41 +0900

    [update] 643. Maximum Average Subarray I - Wrong Answer 해결

--- leetcode/643. Maximum Average Subarray I.py
+++ leetcode/643. Maximum Average Subarray I.py
@@ -1,4 +1,4 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        x = sum(nums[:k])
-        return max((x := x + nums[i + k] - nums[i] for i in range(len(nums) - k)), default=x) / k
+        y = x = sum(nums[:k])
+        return max([y] + [x := x + nums[i + k] - nums[i] for i in range(len(nums) - k)]) / k
